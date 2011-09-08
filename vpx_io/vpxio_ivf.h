ba36a4cdc52cce51422eb7799f7e8871eac0147f
James Berry <jamesberry@google.com>
2011-09-08 11:19:03 -0400

    vpxio addition
    vpxio library added to streamline  file input and
    output for vpxenc, vpxdec and all examples.

---
+++ vpx_io/vpxio_ivf.h
@@ -0,0 +1,9 @@
+int write_ivf_file_header(struct vpxio_ctx *output_ctx);
+int write_ivf_frame_header(FILE *outfile, const vpx_codec_cx_pkt_t *pkt);
+int write_ivf_frame_header_dec(FILE *outfile, uint64_t timeStamp, uint32_t frameSize);
+int vpxio_read_open_raw_file_ivf(struct vpxio_ctx *input_ctx);
+int vpxio_read_img_ivf(struct vpxio_ctx *input_ctx, vpx_image_t *img_raw);
+int vpxio_write_pkt_ivf(struct vpxio_ctx *output_ctx, const vpx_codec_cx_pkt_t *pkt);
+int vpxio_write_close_enc_file_ivf(struct vpxio_ctx *output_ctx);
+int vpxio_read_pkt_ivf(struct vpxio_ctx *input_ctx, uint8_t **buf, size_t *buf_sz, size_t *buf_alloc_sz);
+int vpxio_write_img_ivf(struct vpxio_ctx *output_ctx, vpx_image_t *img, uint8_t *buf);
