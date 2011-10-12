9dc49d1cb798d052a4605c56f5089c49bc2606fa
Scott LaVarnway <slavarnway@google.com>
2011-10-12 11:24:20 -0400

    Remove usage of predict buffer for decode
    Instead of using the predict buffer, the decoder now writes
    the predictor into the recon buffer.  For blocks with eob=0,
    unnecessary idcts can be eliminated.  This gave a performance
    boost of ~1.8% for the HD clips used.
    
    Tero: Added needed changes to ARM side and scheduled some
          assembly code to prevent interlocks.
    
    Patch Set 6:  Merged (I1bcdca7a95aacc3a181b9faa6b10e3a71ee24df3)
    into this commit because of similarities in the idct
    functions.
    
    Change-Id: Ie31d90b5d3522e1108163f2ac491e455e3f955e6

--- vp8/common/arm/neon/dc_only_idct_add_neon.asm
+++ vp8/common/arm/neon/dc_only_idct_add_neon.asm
@@ -14,34 +14,41 @@
     PRESERVE8
 
     AREA ||.text||, CODE, READONLY, ALIGN=2
-;void vp8_dc_only_idct_add_neon(short input_dc, unsigned char *pred_ptr,
-;                               unsigned char *dst_ptr, int pitch, int stride)
+
+
+;void vp8_dc_only_idct_add_neon(short input_dc,
+;                               unsigned char *dst_ptr,
+;                               int stride)
 ; r0  input_dc
-; r1  pred_ptr
-; r2  dst_ptr
-; r3  pitch
-; sp  stride
+; r1  dst_ptr
+; r2  stride
 |vp8_dc_only_idct_add_neon| PROC
     add             r0, r0, #4
     asr             r0, r0, #3
-    ldr             r12, [sp]
+
+    add             r3, r1, r2
+    lsl             r2, #1
+
     vdup.16         q0, r0
 
-    vld1.32         {d2[0]}, [r1], r3
-    vld1.32         {d2[1]}, [r1], r3
-    vld1.32         {d4[0]}, [r1], r3
-    vld1.32         {d4[1]}, [r1]
+    vld1.32         {d2[0]}, [r1], r2
+    vld1.32         {d2[1]}, [r3], r2
+    vld1.32         {d4[0]}, [r1]
+    vld1.32         {d4[1]}, [r3]
 
     vaddw.u8        q1, q0, d2
     vaddw.u8        q2, q0, d4
 
+    sub             r1, r1, r2
+    sub             r3, r3, r2
+
     vqmovun.s16     d2, q1
     vqmovun.s16     d4, q2
 
-    vst1.32         {d2[0]}, [r2], r12
-    vst1.32         {d2[1]}, [r2], r12
-    vst1.32         {d4[0]}, [r2], r12
-    vst1.32         {d4[1]}, [r2]
+    vst1.32         {d2[0]}, [r1], r2
+    vst1.32         {d2[1]}, [r3], r2
+    vst1.32         {d4[0]}, [r1]
+    vst1.32         {d4[1]}, [r3]
 
     bx             lr
 
