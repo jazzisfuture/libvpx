688589d3fa03dbfe8f4681d1edd5ef84d2252f9c
Yunqing Wang <yunqingwang@google.com>
2011-10-25 15:14:16 -0400

    Multiple-resolution encoder
    The example encoder down-samples the input video frames a number of
    times with a down-sampling factor, and then encodes and outputs
    bitstreams with different resolutions.
    
    Support arbitrary down-sampling factor, and down-sampling factor
    can be different for each encoding level.
    
    For example, the encoder can be tested as follows.
    1. Configure with multi-resolution encoding enabled:
    ../libvpx/configure --target=x86-linux-gcc --disable-codecs
    --enable-vp8 --enable-runtime_cpu_detect --enable-debug
    --disable-install-docs --enable-error-concealment
    --enable-multi-res-encoding
    2. Run make
    3. Encode:
    If input video is 1280x720, run:
    ./vp8_multi_resolution_encoder 1280 720 input.yuv 1.ivf 2.ivf 3.ivf 1
    (output: 1.ivf(1280x720); 2.ivf(640x360); 3.ivf(320x180).
    The last parameter is set to 1/0 to show/not show PSNR.)
    4. Decode:
    ./simple_decoder 1.ivf 1.yuv
    ./simple_decoder 2.ivf 2.yuv
    ./simple_decoder 3.ivf 3.yuv
    5. View video:
    mplayer 1.yuv -demuxer rawvideo -rawvideo w=1280:h=720 -loop 0 -fps 30
    mplayer 2.yuv -demuxer rawvideo -rawvideo w=640:h=360 -loop 0 -fps 30
    mplayer 3.yuv -demuxer rawvideo -rawvideo w=320:h=180 -loop 0 -fps 30
    
    The encoding parameters can be modified in vp8_multi_resolution_encoder.c,
    for example, target bitrate, frame rate...
    
    Start to modify API.
    
    Change-Id: I03be9a51167eddf94399f92d269599fb3f3d54f5

--- vpx/vpx_encoder.h
+++ vpx/vpx_encoder.h
@@ -635,6 +635,36 @@ extern "C" {
          */
         unsigned int           ts_layer_id[MAX_PERIODICITY];
 
+        /*
+         * Multi-resolution encoding support
+         */
+
+        /*!\brief Number of total resolutions encoded
+         *
+         * This value specifies the number of total resolutions need to be
+         * encoded.
+         */
+        unsigned int           mr_total_resoutions;
+
+        /*!\brief Current encoder ID
+         *
+         * This value specifies the current encoder ID.
+         */
+        unsigned int           mr_encoder_id;
+
+        /*!\brief Down-sampling factor
+         *
+         * This value specifies the down-sampling factor from current higher
+         * level to the lower level. For example, 8/3 means 3*higher_level_
+         * image_width equals 8*lower_level_image_width.
+         */
+        struct vpx_rational    mr_down_sampling_factor;
+
+        /*!\brief memory location to store low-resolution encoder's mode info.
+         *
+         * This value specifies the mode info storing location.
+         */
+        void*                  mr_low_res_mode_info;
     } vpx_codec_enc_cfg_t; /**< alias for struct vpx_codec_enc_cfg */
 
 
@@ -673,6 +703,44 @@ extern "C" {
      */
 #define vpx_codec_enc_init(ctx, iface, cfg, flags) \
     vpx_codec_enc_init_ver(ctx, iface, cfg, flags, VPX_ENCODER_ABI_VERSION)
+
+
+    /*!\brief Initialize multi-encoder instance
+     *
+     * Initializes multi-encoder context using the given interface. Applications
+     * should call the vpx_codec_enc_init_multi convenience macro instead of this
+     * function directly, to ensure that the ABI version number parameter
+     * is properly initialized.
+     *
+     * In XMA mode (activated by setting VPX_CODEC_USE_XMA in the flags
+     * parameter), the storage pointed to by the cfg parameter must be
+     * kept readable and stable until all memory maps have been set.
+     *
+     * \param[in]    ctx     Pointer to this instance's context.
+     * \param[in]    iface   Pointer to the algorithm interface to use.
+     * \param[in]    cfg     Configuration to use, if known. May be NULL.
+     * \param[in]    flags   Bitfield of VPX_CODEC_USE_* flags
+     * \param[in]    ver     ABI version number. Must be set to
+     *                       VPX_ENCODER_ABI_VERSION
+     * \retval #VPX_CODEC_OK
+     *     The decoder algorithm initialized.
+     * \retval #VPX_CODEC_MEM_ERROR
+     *     Memory allocation failed.
+     */
+    vpx_codec_err_t vpx_codec_enc_init_multi_ver(vpx_codec_ctx_t      *ctx,
+                                                 vpx_codec_iface_t    *iface,
+                                                 vpx_codec_enc_cfg_t  *cfg,
+                                                 int                   num_enc,
+                                                 vpx_codec_flags_t     flags,
+                                                 int                   ver);
+
+
+    /*!\brief Convenience macro for vpx_codec_enc_init_multi_ver()
+     *
+     * Ensures the ABI version parameter is properly set.
+     */
+#define vpx_codec_enc_init_multi(ctx, iface, cfg, num_enc, flags) \
+    vpx_codec_enc_init_multi_ver(ctx, iface, cfg, num_enc, flags, VPX_ENCODER_ABI_VERSION)
 
 
     /*!\brief Get a default configuration
@@ -780,6 +848,34 @@ extern "C" {
                                       vpx_enc_frame_flags_t       flags,
                                       unsigned long               deadline);
 
+    /*!\brief Multi-encoder encodes a frame
+     *
+     * The difference between this function and vpx_codec_encode() is that this
+     * function runs multiple encoders, which encode a frame in some order, and
+     * may share data between encoders.
+     *
+     * \param[in]    ctx       Pointer to this instance's context
+     * \param[in]    img       Image data to encode, NULL to flush.
+     * \param[in]    ppts      Pointer to presentation time stamp.
+     * \param[in]    duration  Duration to show frame, in timebase units.
+     * \param[in]    flags     Flags to use for encoding this frame.
+     * \param[in]    deadline  Time to spend encoding, in microseconds. (0=infinite)
+     * \param[in]    num_enc   Total number of encoders.
+     *
+     * \retval #VPX_CODEC_OK
+     *     The configuration was populated.
+     * \retval #VPX_CODEC_INCAPABLE
+     *     Interface is not an encoder interface.
+     * \retval #VPX_CODEC_INVALID_PARAM
+     *     A parameter was NULL, the image format is unsupported, etc.
+     */
+    vpx_codec_err_t  vpx_codec_encode_multi(vpx_codec_ctx_t            *ctx,
+                                            const vpx_image_t          *img,
+                                            vpx_codec_pts_t            *ppts,
+                                            unsigned long               duration,
+                                            vpx_enc_frame_flags_t       flags,
+                                            unsigned long               deadline,
+                                            int                         num_enc);
 
     /*!\brief Set compressed data output buffer
      *
