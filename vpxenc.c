53ff53ce52f69b54f7413ebede8020bc334e36b2
James Berry <jamesberry@google.com>
2011-11-28 14:09:35 -0500

    bugfix: all vpxenc arguments were not parsed under all conditions
    dynamicly assign ARG_CTRL_CNT_MAX and
    add check to make sure argument instance
    doesnt already exist before creating a duplicate
    
    Change-Id: I4f78a9c5346cda8e812cd89c077afe8996493508

--- vpxenc.c
+++ vpxenc.c
@@ -1442,7 +1442,8 @@ static void show_rate_histogram(struct rate_hist          *hist,
     show_histogram(hist->bucket, buckets, hist->total, scale);
 }
 
-#define ARG_CTRL_CNT_MAX 10
+#define NELEMENTS(x) (sizeof(x)/sizeof(x[0]))
+#define ARG_CTRL_CNT_MAX NELEMENTS(vp8_arg_ctrl_map)
 
 int main(int argc, const char **argv_)
 {
@@ -1725,9 +1726,24 @@ int main(int argc, const char **argv_)
 
                 if (arg_ctrl_cnt < ARG_CTRL_CNT_MAX)
                 {
-                    arg_ctrls[arg_ctrl_cnt][0] = ctrl_args_map[i];
-                    arg_ctrls[arg_ctrl_cnt][1] = arg_parse_enum_or_int(&arg);
-                    arg_ctrl_cnt++;
+                    int j;
+                    int arg_exist = 0;
+
+                    for (j = 0; j < arg_ctrl_cnt; j=j+1)
+                    {
+                        if(arg_ctrls[j][0] == ctrl_args_map[i])
+                        {
+                            arg_ctrls[j][1] = arg_parse_enum_or_int(&arg);
+                            arg_exist = 1;
+                        }
+                    }
+
+                    if(!arg_exist)
+                    {
+                        arg_ctrls[arg_ctrl_cnt][0] = ctrl_args_map[i];
+                        arg_ctrls[arg_ctrl_cnt][1] = arg_parse_enum_or_int(&arg);
+                        arg_ctrl_cnt++;
+                    }
                 }
             }
         }
