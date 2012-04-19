32a7db6391a16dc5bfb0864703e21c9dbfccbaf0
Attila Nagy <attilanagy@google.com>
2012-04-19 15:55:03 +0300

    Ports vpx_xcaler to new RTCD method
    We can get rid of all remaining global initializers now:
    vp8_scale_machine_specific_config()
    vp8_initialize()
    vp8dx_initialize()
    
    Change-Id: I2825cea5d1c01ad9f6c45df49a0f86d803bfeb69

--- vpx_scale/vpx_scale.mk
+++ vpx_scale/vpx_scale.mk
@@ -7,12 +7,7 @@ SCALE_SRCS-yes += generic/vpxscale.c
 SCALE_SRCS-yes += generic/yv12config.c
 SCALE_SRCS-yes += generic/yv12extend.c
 SCALE_SRCS-yes += generic/yv12extend_generic.h
-SCALE_SRCS-yes += generic/scalesystemdependent.c
 SCALE_SRCS-$(CONFIG_SPATIAL_RESAMPLING) += generic/gen_scalers.c
-
-#arm
-SCALE_SRCS-$(ARCH_ARM)    += arm/yv12extend_arm.h
-SCALE_SRCS-$(ARCH_ARM)    += arm/scalesystemdependent.c
 
 #neon
 SCALE_SRCS-$(HAVE_NEON)  += arm/neon/vp8_vpxyv12_copyframe_func_neon$(ASM)
