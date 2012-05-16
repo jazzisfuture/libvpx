5976d3b9dec178a4f87aa3e52ae9b8930060a35d
John Koleszar <jkoleszar@google.com>
2012-05-16 16:25:51 -0700

    Build unit tests monolithically
    Rework unit tests  to have a single executable rather than many, which
    should avoid pollution of the visual studio project namespace, improve
    build times, and make it easier to use the gtest test sharding system
    when we get these going on the continuous build cluster.
    
    Change-Id: If4c3e5d4b3515522869de6c89455c2a64697cca6

--- test/test_libvpx.cc
+++ test/test_libvpx.cc
@@ -7,24 +7,7 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
-
-
- extern "C" {
-    void vp8_short_idct4x4llm_mmx(short *input, unsigned char *pred_ptr,
-                            int pred_stride, unsigned char *dst_ptr,
-                            int dst_stride);
-}
-
-#include "vp8/common/idctllm_test.h"
-
-namespace
-{
-
-INSTANTIATE_TEST_CASE_P(MMX, IDCTTest,
-                        ::testing::Values(vp8_short_idct4x4llm_mmx));
-
-} // namespace
-
+#include "third_party/googletest/src/include/gtest/gtest.h"
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
