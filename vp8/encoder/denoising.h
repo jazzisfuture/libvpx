32132658a6bbbeba397d88152a58f85311254879
Christian Duvivier <cduvivier@google.com>
2012-05-19 14:33:47 -0700

    vp8d - Optimized Denoiser
    Faster version of denoiser, cut cost by 1.7x for C path, by 3.3x for
    SSE2 path.
    
    Change-Id: I154786308550763bc0e3497e5fa5bfd1ce651beb

--- vp8/encoder/denoising.h
+++ vp8/encoder/denoising.h
@@ -12,6 +12,11 @@
 #define VP8_ENCODER_DENOISING_H_
 
 #include "block.h"
+static const unsigned int NOISE_MOTION_THRESHOLD = 20 * 20;
+static const unsigned int NOISE_DIFF2_THRESHOLD = 75;
+// SSE_DIFF_THRESHOLD is selected as ~95% confidence assuming var(noise) ~= 100.
+static const unsigned int SSE_DIFF_THRESHOLD = 16 * 16 * 20;
+static const unsigned int SSE_THRESHOLD = 16 * 16 * 40;
 
 typedef struct vp8_denoiser
 {
@@ -29,5 +34,13 @@ void vp8_denoiser_denoise_mb(VP8_DENOISER *denoiser,
                              unsigned int zero_mv_sse,
                              int recon_yoffset,
                              int recon_uvoffset);
+
+union coeff_pair
+{
+    uint32_t as_int;
+    uint16_t as_short[2];
+};
+
+union coeff_pair *vp8_get_filter_coeff_LUT(unsigned int motion_magnitude);
 
 #endif  // VP8_ENCODER_DENOISING_H_
