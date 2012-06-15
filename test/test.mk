4f9213ea3ac9eac11a127d3a7664e8fd6845f320
Paul Wilkins <paulwilkins@google.com>
2012-06-15 11:55:37 +0100

    Added unit test of vp8_set_roimap()
    Change-Id: I99937cbdd6bfe52b7c8ae42f05526dfc06a602f4

--- test/test.mk
+++ test/test.mk
@@ -7,4 +7,5 @@ LIBVPX_TEST_SRCS-yes += idctllm_test.cc
 LIBVPX_TEST_SRCS-yes += keyframe_test.cc
 LIBVPX_TEST_SRCS-yes += resize_test.cc
 LIBVPX_TEST_SRCS-yes += test_libvpx.cc
+LIBVPX_TEST_SRCS-yes += set_roi.cc
 LIBVPX_TEST_SRCS-yes += video_source.h
