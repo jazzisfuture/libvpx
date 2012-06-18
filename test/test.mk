72d4af589b7e587681e3ed9396b9f6d1b464d41a
Yaowu Xu <yaowu@google.com>
2012-06-18 13:41:17 -0700

    Added unit test for 4x4 forward dct
    Change-Id: I1607676879c29adc0173a3c0355a0e5d8a84fc3b

--- test/test.mk
+++ test/test.mk
@@ -3,6 +3,7 @@ LIBVPX_TEST_SRCS-yes += boolcoder_test.cc
 LIBVPX_TEST_SRCS-yes += encode_test_driver.cc
 LIBVPX_TEST_SRCS-yes += encode_test_driver.h
 LIBVPX_TEST_SRCS-yes += idctllm_test.cc
+LIBVPX_TEST_SRCS-yes += fdct4x4_test.cc
 LIBVPX_TEST_SRCS-yes += keyframe_test.cc
 LIBVPX_TEST_SRCS-yes += resize_test.cc
 LIBVPX_TEST_SRCS-yes += test_libvpx.cc
