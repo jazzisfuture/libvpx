addf8cd097789be34dc24ba7e65ed060ae02754c
John Koleszar <jkoleszar@google.com>
2012-06-19 21:05:36 -0700

    Fix compilation with -werror (WIP)
    Temporary hack in configure to force werror, do not submit until this
    is removed.
    
    Change-Id: Ic7c67616c370d0ff87562a232fb1e5df0702dc86

--- vpxenc.c
+++ vpxenc.c
@@ -68,6 +68,20 @@ typedef long off_t;
 #define ftello ftell
 #endif
 
+/* Swallow warnings about unused results of fread/fwrite */
+size_t wrap_fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+    return fread(ptr, size, nmemb, stream);
+}
+#define fread wrap_fread
+
+size_t wrap_fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+    return fwrite(ptr, size, nmemb, stream);
+}
+#define fwrite wrap_fwrite
+
+
 static const char *exec_name;
 
 static const struct codec_item
@@ -599,7 +613,7 @@ Ebml_StartSubElement(EbmlGlobal *glob, EbmlLoc *ebmlLoc,
 {
     /* todo this is always taking 8 bytes, this may need later optimization */
     /* this is a key that says length unknown */
-    uint64_t unknownLen = 0x01FFFFFFFFFFFFFF;
+    uint64_t unknownLen = ~(((uint64_t)(0xFE))<<56);
 
     Ebml_WriteID(glob, class_id);
     *ebmlLoc = ftello(glob->stream);
@@ -617,7 +631,7 @@ Ebml_EndSubElement(EbmlGlobal *glob, EbmlLoc *ebmlLoc)
 
     /* Calculate the size of this element */
     size = pos - *ebmlLoc - 8;
-    size |= 0x0100000000000000;
+    size |= ((uint64_t)0x01)<<56;
 
     /* Seek back to the beginning of the element and write the new size */
     fseeko(glob->stream, *ebmlLoc, SEEK_SET);
