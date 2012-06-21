5db483657a62a0e76a6683e6396526258193e69d
Jim Bankoski <jimbankoski@google.com>
2012-06-21 07:49:27 -0700

    add auto keyframe unit test
    To do so we add a framework for encoding a yv12 file..
    
    Change-Id: I94a061eb916beaf6cde920cf1aaadb6eed10a717

--- test/test.mk
+++ test/test.mk
@@ -8,3 +8,7 @@ LIBVPX_TEST_SRCS-yes += keyframe_test.cc
 LIBVPX_TEST_SRCS-yes += resize_test.cc
 LIBVPX_TEST_SRCS-yes += test_libvpx.cc
 LIBVPX_TEST_SRCS-yes += video_source.h
+
+LIBVPX_TEST_SRCS-yes += yv12_video_source.h
+
+LIBVPX_TEST_DATA-yes += hantro_collage_w352h288.yuv
