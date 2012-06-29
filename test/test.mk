dc261a1aa5a93b5e4c5ab96a0f0d7a62e379b1d0
John Koleszar <jkoleszar@google.com>
2012-06-29 12:15:00 -0700

    Build unit test driver from the default target
    We need an easy way to build the unit test driver without running the
    tests. This enables passing options like --gtest_filter to the
    executable, which can't be done very cleanly when running under
    `make test`.
    
    Fixed a number of compiler errors/warnings when building the tests
    in various configurations by Jenkins.
    
    Change-Id: I9198122600bcf02520688e5f052ab379f963b77b

--- test/test.mk
+++ test/test.mk
@@ -1,16 +1,25 @@
 LIBVPX_TEST_SRCS-yes += test.mk
+ifeq ($(CONFIG_VP8_ENCODER)$(CONFIG_VP8_DECODER),yesyes)
+# These tests require both the encoder and decoder to be built.
 LIBVPX_TEST_SRCS-yes += boolcoder_test.cc
+endif
+
+# These tests require only the encoder.
+ifeq ($(CONFIG_VP8_ENCODER), yes)
 LIBVPX_TEST_SRCS-yes += config_test.cc
 LIBVPX_TEST_SRCS-yes += encode_test_driver.cc
 LIBVPX_TEST_SRCS-yes += encode_test_driver.h
-LIBVPX_TEST_SRCS-yes += i420_video_source.h
-LIBVPX_TEST_SRCS-yes += idctllm_test.cc
-LIBVPX_TEST_SRCS-yes += intrapred_test.cc
 LIBVPX_TEST_SRCS-yes += fdct4x4_test.cc
+LIBVPX_TEST_SRCS-yes += i420_video_source.h
 LIBVPX_TEST_SRCS-yes += keyframe_test.cc
-LIBVPX_TEST_SRCS-yes += pp_filter_test.cc
 LIBVPX_TEST_SRCS-yes += resize_test.cc
-LIBVPX_TEST_SRCS-yes += test_libvpx.cc
 LIBVPX_TEST_SRCS-yes += video_source.h
 
 LIBVPX_TEST_DATA-yes += hantro_collage_w352h288.yuv
+endif
+
+# These tests are valid for both the encoder and decoder.
+LIBVPX_TEST_SRCS-yes += idctllm_test.cc
+LIBVPX_TEST_SRCS-yes += intrapred_test.cc
+LIBVPX_TEST_SRCS-$(CONFIG_POSTPROC) += pp_filter_test.cc
+LIBVPX_TEST_SRCS-yes += test_libvpx.cc
