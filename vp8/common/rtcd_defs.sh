2f64220c14bb4246d32fd87603e394fcde946cef
Christian Duvivier <cduvivier@google.com>
2012-08-14 18:19:09 -0700

    First partial snapshot of vectorized 8-tap filtering.
    About 3.5x faster, 15% overall encoder speedup. Rest of optimizations
    will come soon (see TODO section in filter_sse4.c).
    
    Change-Id: If18108048bfd5345fc942e8574e4c7f58e0e86e0

--- vp8/common/rtcd_defs.sh
+++ vp8/common/rtcd_defs.sh
@@ -4,3 +4,12 @@ struct blockd;
 EOF
 }
 forward_decls common_forward_decls
+
+prototype void vp8_filter_block2d_4x4_8 "const unsigned char *src_ptr, const unsigned int src_stride, const short *HFilter_aligned16, const short *VFilter_aligned16, unsigned char *dst_ptr, unsigned int dst_stride"
+specialize vp8_filter_block2d_4x4_8 sse4_1
+prototype void vp8_filter_block2d_8x4_8 "const unsigned char *src_ptr, const unsigned int src_stride, const short *HFilter_aligned16, const short *VFilter_aligned16, unsigned char *dst_ptr, unsigned int dst_stride"
+specialize vp8_filter_block2d_8x4_8 sse4_1
+prototype void vp8_filter_block2d_8x8_8 "const unsigned char *src_ptr, const unsigned int src_stride, const short *HFilter_aligned16, const short *VFilter_aligned16, unsigned char *dst_ptr, unsigned int dst_stride"
+specialize vp8_filter_block2d_8x8_8 sse4_1
+prototype void vp8_filter_block2d_16x16_8 "const unsigned char *src_ptr, const unsigned int src_stride, const short *HFilter_aligned16, const short *VFilter_aligned16, unsigned char *dst_ptr, unsigned int dst_stride"
+specialize vp8_filter_block2d_16x16_8 sse4_1
