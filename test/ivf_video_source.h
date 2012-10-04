da2774b02d35c7197d1b41f758868c018e0d5675
Yunqing Wang <yunqingwang@google.com>
2012-10-04 12:59:36 -0700

    Add unit test for decoder test_vector_test
    Got 61 test vectors from vp8-test-vectors.git
    (http://git.chromium.org/gitweb/?p=webm/vp8-test-vectors.git)
    
    Added decoder test vectors downloading in unit tests. Uploaded
    the test vectors and their md5 files to WebM website.
      $ gsutil cp *.* gs://downloads.webmproject.org/test_data/libvpx
    
    Added their sha1sum to the test/test-data.sha1 file.
    
    In unit tests, download the test vectors to LIBVPX_TEST_DATA_PATH.
    
    Test_vector_test goes through the test vectors, decodes them, and
    compute the md5 checksums. The checksums are compared with the
    expected md5 checksums to tell if the decoder decodes correctly.
    
    Change-Id: Ia1e84f9347ddf1d4a02e056c0fee7d28dccfae15

---
+++ test/ivf_video_source.h
@@ -0,0 +1,96 @@
+/*
+ *  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef TEST_IVF_VIDEO_SOURCE_H_
+#define TEST_IVF_VIDEO_SOURCE_H_
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include "test/video_source.h"
+
+namespace libvpx_test {
+const unsigned int kCodeBufferSize = 256 * 1024;
+const unsigned int kIvfFileHdrSize = 32;
+const unsigned int kIvfFrameHdrSize = 12;
+// This class extends VideoSource to allow parsing of ivf files,
+// so that we can do actual file decodes.
+class IVFVideoSource : public CompressedVideoSource {
+ public:
+  IVFVideoSource(const std::string &file_name, uint8_t *frame_buf)
+      : file_name_(file_name),
+        input_file_(NULL),
+        compressed_frame_buf_(frame_buf),
+        frame_sz_(0),
+        frame_(0),
+        end_of_file_(false) {
+  }
+
+  virtual ~IVFVideoSource() {
+    if (input_file_)
+      fclose(input_file_);
+  }
+
+  static unsigned int mem_get_le32(const uint8_t *mem) {
+    return (mem[3] << 24) | (mem[2] << 16) | (mem[1] << 8) | (mem[0]);
+  }
+
+  virtual void Begin() {
+    uint8_t file_hdr[kIvfFileHdrSize];
+    input_file_ = OpenTestDataFile(file_name_);
+    ASSERT_TRUE(input_file_) << "Input file open failed.";
+
+    // Read file header
+    ASSERT_TRUE(fread(file_hdr, 1, kIvfFileHdrSize, input_file_)
+        == kIvfFileHdrSize) << "File header read failed.";
+    // Check file header
+    ASSERT_TRUE(file_hdr[0] == 'D' && file_hdr[1] == 'K' && file_hdr[2] == 'I'
+        && file_hdr[3] == 'F') << "Input is not an IVF file.";
+
+    FillFrame();
+  }
+
+  virtual void Next() {
+    ++frame_;
+    FillFrame();
+  }
+
+  void FillFrame() {
+    uint8_t frame_hdr[kIvfFrameHdrSize];
+    // Check frame header and read a frame from input_file.
+    if (fread(frame_hdr, 1, kIvfFrameHdrSize, input_file_)
+        != kIvfFrameHdrSize) {
+      end_of_file_ = true;
+    } else {
+      end_of_file_ = false;
+
+      frame_sz_ = mem_get_le32(frame_hdr);
+      ASSERT_LE(frame_sz_, kCodeBufferSize)
+          << "Frame is too big for allocated code buffer";
+      ASSERT_EQ(fread(compressed_frame_buf_, 1, frame_sz_, input_file_),
+          frame_sz_) << "Failed to read complete frame";
+    }
+  }
+
+  virtual const uint8_t *cxdata() const {
+    return end_of_file_ ? NULL : compressed_frame_buf_;
+  }
+  virtual const unsigned int frame_size() const { return frame_sz_; }
+
+ protected:
+  std::string file_name_;
+  FILE *input_file_;
+  uint8_t *compressed_frame_buf_;
+  unsigned int frame_sz_;
+  unsigned int frame_;
+  bool end_of_file_;
+};
+
+}  // namespace libvpx_test
+
+#endif  // TEST_IVF_VIDEO_SOURCE_H_
