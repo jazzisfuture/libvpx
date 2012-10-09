aeefb1fb17c1dc46266af73d0d062550e25269f0
Deb Mukherjee <debargha@google.com>
2012-10-09 13:19:15 -0700

    New b-intra mode where direction is contextual
    Preliminary patch on a new 4x4 intra mode B_CONTEXT_PRED where the
    dominant direction from the context is used to encode. Various decoder
    changes are needed to support decoding of B_CONTEXT_PRED in conjunction
    with hybrid transforms since the scan order and tokenization depends on
    the actual direction of prediction obtained from the context. Currently
    the traditional directional modes are used in conjunction with the
    B_CONTEXT_PRED, which also seems to provide the best results.
    
    The gains are small - in the 0.1% range.
    
    Change-Id: I5a7ea80b5218f42a9c0dfb42d3f79a68c7f0cdc2

---
+++ vp8/common/reconintra.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2010 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+
+#ifndef __INC_RECONINTRA_H
+#define __INC_RECONINTRA_H
+
+#include "blockd.h"
+void d45_predictor(unsigned char *ypred_ptr, int y_stride, int n,
+                   unsigned char *yabove_row, unsigned char *yleft_col);
+void d135_predictor(unsigned char *ypred_ptr, int y_stride, int n,
+                    unsigned char *yabove_row, unsigned char *yleft_col);
+void d116_predictor(unsigned char *ypred_ptr, int y_stride, int n,
+                    unsigned char *yabove_row, unsigned char *yleft_col);
+void d153_predictor(unsigned char *ypred_ptr, int y_stride, int n,
+                    unsigned char *yabove_row, unsigned char *yleft_col);
+void d27_predictor(unsigned char *ypred_ptr, int y_stride, int n,
+                   unsigned char *yabove_row, unsigned char *yleft_col);
+void d64_predictor(unsigned char *ypred_ptr, int y_stride, int n,
+                   unsigned char *yabove_row, unsigned char *yleft_col);
+void corner_predictor(unsigned char *ypred_ptr, int y_stride, int n,
+                      unsigned char *yabove_row, unsigned char *yleft_col);
+B_PREDICTION_MODE vp8_find_dominant_direction(unsigned char *ptr,
+                                              int stride, int n);
+B_PREDICTION_MODE vp8_find_bpred_context(BLOCKD *x);
+
+extern void init_intra_left_above_pixels(MACROBLOCKD *xd);
+
+#endif
---
+++ vp8/encoder/modecosts.c
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2010 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+
+#include "vp8/common/blockd.h"
+#include "onyx_int.h"
+#include "treewriter.h"
+#include "vp8/common/entropymode.h"
+
+
+void vp8_init_mode_costs(VP8_COMP *c) {
+  VP8_COMMON *x = &c->common;
+  {
+    const vp8_tree_p KT = vp8_kf_bmode_tree;
+    const vp8_tree_p T = vp8_bmode_tree;
+
+    int i = 0;
+
+    do {
+      int j = 0;
+
+      do {
+        vp8_cost_tokens((int *)c->mb.bmode_costs[i][j], x->kf_bmode_prob[i][j], KT);
+      } while (++j < VP8_KF_BINTRAMODES);
+    } while (++i < VP8_KF_BINTRAMODES);
+
+    vp8_cost_tokens((int *)c->mb.inter_bmode_costs, x->fc.bmode_prob, T);
+  }
+  vp8_cost_tokens((int *)c->mb.inter_bmode_costs,
+                  x->fc.sub_mv_ref_prob[0], vp8_sub_mv_ref_tree);
+
+  vp8_cost_tokens(c->mb.mbmode_cost[1], x->fc.ymode_prob, vp8_ymode_tree);
+  vp8_cost_tokens(c->mb.mbmode_cost[0],
+                  x->kf_ymode_prob[c->common.kf_ymode_probs_index],
+                  vp8_kf_ymode_tree);
+  vp8_cost_tokens(c->mb.intra_uv_mode_cost[1],
+                  x->fc.uv_mode_prob[VP8_YMODES - 1], vp8_uv_mode_tree);
+  vp8_cost_tokens(c->mb.intra_uv_mode_cost[0],
+                  x->kf_uv_mode_prob[VP8_YMODES - 1], vp8_uv_mode_tree);
+  vp8_cost_tokens(c->mb.i8x8_mode_costs,
+                  x->fc.i8x8_mode_prob, vp8_i8x8_mode_tree);
+
+  {
+    int i;
+    for (i = 0; i <= VP8_SWITCHABLE_FILTERS; ++i)
+      vp8_cost_tokens((int *)c->mb.switchable_interp_costs[i],
+                      x->fc.switchable_interp_prob[i],
+                      vp8_switchable_interp_tree);
+  }
+}
