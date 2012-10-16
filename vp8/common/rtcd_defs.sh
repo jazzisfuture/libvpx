f998f6652f8bc46355d02b73d164b355275f4712
Jim Bankoski <jimbankoski@google.com>
2012-10-16 11:10:11 -0700

    Reinstate revert "removed the recon. rtcd invoke macro code"
    This reinstates reverted commit 2113a831575d81faeadd9966e256d58b6b2b1633
    
    Change-Id: I9a9af13497d1e58d4f467e3e083fddf06b1b786c

--- vp8/common/rtcd_defs.sh
+++ vp8/common/rtcd_defs.sh
@@ -20,12 +20,12 @@ prototype void vp8_filter_block2d_16x16_8 "const unsigned char *src_ptr, const u
 # At the very least, MSVC 2008 has compiler bug exhibited by this code; code
 # compiles warning free but a dissassembly of generated code show bugs. To be
 # on the safe side, only enabled when compiled with 'gcc'.
-if [ "$CONFIG_GCC" = "yes" ]; then
-    specialize vp8_filter_block2d_4x4_8 sse4_1 sse2
-    specialize vp8_filter_block2d_8x4_8 sse4_1 sse2
-    specialize vp8_filter_block2d_8x8_8 sse4_1 sse2
-    specialize vp8_filter_block2d_16x16_8 sse4_1 sse2
-fi
+#if [ "$CONFIG_GCC" = "yes" ]; then
+#    specialize vp8_filter_block2d_4x4_8 sse4_1 sse2
+#    specialize vp8_filter_block2d_8x4_8 sse4_1 sse2
+#    specialize vp8_filter_block2d_8x8_8 sse4_1 sse2
+#    specialize vp8_filter_block2d_16x16_8 sse4_1 sse2
+#fi
 
 
 #
@@ -42,7 +42,10 @@ specialize vp8_copy_mem8x8 mmx media neon dspr2
 vp8_copy_mem8x8_media=vp8_copy_mem8x8_v6
 vp8_copy_mem8x8_dspr2=vp8_copy_mem8x8_dspr2
 prototype void vp8_copy_mem8x4 "unsigned char *src, int src_pitch, unsigned char *dst, int dst_pitch"
+specialize vp8_copy_mem8x4 mmx
+specialize vp8_copy_mem8x4
 prototype void vp8_intra4x4_predict "unsigned char *Above, unsigned char *yleft, int left_stride, B_PREDICTION_MODE b_mode, unsigned char *dst, int dst_stride, unsigned char top_left"
+specialize vp8_intra4x4_predict
 prototype void vp8_avg_mem16x16 "unsigned char *src, int src_pitch, unsigned char *dst, int dst_pitch"
 specialize vp8_avg_mem16x16
 prototype void vp8_avg_mem8x8 "unsigned char *src, int src_pitch, unsigned char *dst, int dst_pitch"
@@ -56,14 +59,15 @@ specialize vp8_recon_b
 prototype void vp8_recon_uv_b "unsigned char *pred_ptr, short *diff_ptr, unsigned char *dst_ptr, int stride"
 specialize vp8_recon_uv_b
 prototype void vp8_recon2b "unsigned char *pred_ptr, short *diff_ptr, unsigned char *dst_ptr, int stride"
-specialize vp8_recon2b
+specialize vp8_recon2b sse2
 prototype void vp8_recon4b "unsigned char *pred_ptr, short *diff_ptr, unsigned char *dst_ptr, int stride"
-specialize vp8_recon4b
+specialize vp8_recon4b sse2
 prototype void vp8_recon_mb "MACROBLOCKD *x"
 specialize vp8_recon_mb
 prototype void vp8_recon_mby "MACROBLOCKD *x"
 specialize vp8_recon_mby
 prototype void vp8_build_intra_predictors_mby_s "MACROBLOCKD *x"
+specialize vp8_build_intra_predictors_mby_s
 prototype void vp8_build_intra_predictors_sby_s "MACROBLOCKD *x"
 specialize vp8_build_intra_predictors_sby_s;
 prototype void vp8_build_intra_predictors_sbuv_s "MACROBLOCKD *x"
