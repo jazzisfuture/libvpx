07d559e535ef56795392f60a68d28787fb703e28
John Koleszar <jkoleszar@google.com>
2012-10-16 13:52:39 -0700

    Interleave modes/residual per macroblock
    Packs the bitstream with each mb's residual following its mode/mv
    information.
    
    TODO: There are still a few fields that should be packed into partition
    0 but are included in partition 1, due to them being serialized from
    write_kfmodes/pack_inter_mode_mvs, which execute after the first
    partition is finalized. These need to be separated out into a separate
    function, similar to mb_mode_mv_init() in decodemv.c.
    
    Change-Id: I43a46c363601ab36954d07ebe498760e1e2e3af4

--- vp8/encoder/encodeframe.c
+++ vp8/encoder/encodeframe.c
@@ -1050,9 +1050,6 @@ static void encode_sb(VP8_COMP *cpi,
         cpi->inter_zz_count++;
     }
 
-    // TODO Partitioning is broken!
-    cpi->tplist[mb_row].stop = *tp;
-
 #if CONFIG_SUPERBLOCKS
     if (xd->mode_info_context->mbmi.encoded_as_sb) {
       x->src.y_buffer += 32;
@@ -1085,6 +1082,9 @@ static void encode_sb(VP8_COMP *cpi,
     assert((xd->prev_mode_info_context - cpi->common.prev_mip) ==
            (xd->mode_info_context - cpi->common.mip));
 #endif
+    (*tp)->Token = EOSB_TOKEN;
+    (*tp)++;
+    cpi->tplist[mb_row].stop = *tp;
   }
 
   // debug output
