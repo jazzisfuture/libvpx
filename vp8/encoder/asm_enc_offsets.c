791a0f1383def29f9c7f51e871b4022b02cbe0c7
Ronald S. Bultje <rbultje@google.com>
2012-10-30 17:12:12 -0700

    Change non-function symbol vp8_ prefixes to vp9_.
    For local symbols, make them static instead.
    
    Change-Id: I13d60947a46f711bc8991e16100cea2a13e3a22e

--- vp8/encoder/asm_enc_offsets.c
+++ vp8/encoder/asm_enc_offsets.c
@@ -20,30 +20,30 @@
 BEGIN
 
 /* regular quantize */
-DEFINE(vp8_block_coeff,                         offsetof(BLOCK, coeff));
-DEFINE(vp8_block_zbin,                          offsetof(BLOCK, zbin));
-DEFINE(vp8_block_round,                         offsetof(BLOCK, round));
-DEFINE(vp8_block_quant,                         offsetof(BLOCK, quant));
-DEFINE(vp8_block_quant_fast,                    offsetof(BLOCK, quant_fast));
-DEFINE(vp8_block_zbin_extra,                    offsetof(BLOCK, zbin_extra));
-DEFINE(vp8_block_zrun_zbin_boost,               offsetof(BLOCK, zrun_zbin_boost));
-DEFINE(vp8_block_quant_shift,                   offsetof(BLOCK, quant_shift));
+DEFINE(vp9_block_coeff,                         offsetof(BLOCK, coeff));
+DEFINE(vp9_block_zbin,                          offsetof(BLOCK, zbin));
+DEFINE(vp9_block_round,                         offsetof(BLOCK, round));
+DEFINE(vp9_block_quant,                         offsetof(BLOCK, quant));
+DEFINE(vp9_block_quant_fast,                    offsetof(BLOCK, quant_fast));
+DEFINE(vp9_block_zbin_extra,                    offsetof(BLOCK, zbin_extra));
+DEFINE(vp9_block_zrun_zbin_boost,               offsetof(BLOCK, zrun_zbin_boost));
+DEFINE(vp9_block_quant_shift,                   offsetof(BLOCK, quant_shift));
 
-DEFINE(vp8_blockd_qcoeff,                       offsetof(BLOCKD, qcoeff));
-DEFINE(vp8_blockd_dequant,                      offsetof(BLOCKD, dequant));
-DEFINE(vp8_blockd_dqcoeff,                      offsetof(BLOCKD, dqcoeff));
-DEFINE(vp8_blockd_eob,                          offsetof(BLOCKD, eob));
+DEFINE(vp9_blockd_qcoeff,                       offsetof(BLOCKD, qcoeff));
+DEFINE(vp9_blockd_dequant,                      offsetof(BLOCKD, dequant));
+DEFINE(vp9_blockd_dqcoeff,                      offsetof(BLOCKD, dqcoeff));
+DEFINE(vp9_blockd_eob,                          offsetof(BLOCKD, eob));
 
 /* subtract */
 DEFINE(vp8_block_base_src,                      offsetof(BLOCK, base_src));
 DEFINE(vp8_block_src,                           offsetof(BLOCK, src));
-DEFINE(vp8_block_src_diff,                      offsetof(BLOCK, src_diff));
+DEFINE(vp9_block_src_diff,                      offsetof(BLOCK, src_diff));
 DEFINE(vp8_block_src_stride,                    offsetof(BLOCK, src_stride));
 
 DEFINE(vp8_blockd_predictor,                    offsetof(BLOCKD, predictor));
 
 /* pack tokens */
-DEFINE(vp8_writer_lowvalue,                     offsetof(vp8_writer, lowvalue));
+DEFINE(vp9_writer_lowvalue,                     offsetof(vp8_writer, lowvalue));
 DEFINE(vp8_writer_range,                        offsetof(vp8_writer, range));
 DEFINE(vp8_writer_value,                        offsetof(vp8_writer, value));
 DEFINE(vp8_writer_count,                        offsetof(vp8_writer, count));
@@ -58,10 +58,10 @@ DEFINE(TOKENEXTRA_SZ,                           sizeof(TOKENEXTRA));
 
 DEFINE(vp8_extra_bit_struct_sz,                 sizeof(vp8_extra_bit_struct));
 
-DEFINE(vp8_token_value,                         offsetof(vp8_token, value));
+DEFINE(vp9_token_value,                         offsetof(vp8_token, value));
 DEFINE(vp8_token_len,                           offsetof(vp8_token, Len));
 
-DEFINE(vp8_extra_bit_struct_tree,               offsetof(vp8_extra_bit_struct, tree));
+DEFINE(vp9_extra_bit_struct_tree,               offsetof(vp8_extra_bit_struct, tree));
 DEFINE(vp8_extra_bit_struct_prob,               offsetof(vp8_extra_bit_struct, prob));
 DEFINE(vp8_extra_bit_struct_len,                offsetof(vp8_extra_bit_struct, Len));
 DEFINE(vp8_extra_bit_struct_base_val,           offsetof(vp8_extra_bit_struct, base_val));
