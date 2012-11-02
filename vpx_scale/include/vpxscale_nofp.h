3db9a96e1c26bd4bc269078beaada41dbc77c36a
John Koleszar <jkoleszar@google.com>
2012-11-01 19:14:42 -0700

    vpx_scale: sync from experimental
    Import changes made on the experimental branch in preparation for
    merging the two branches.
    
    Change-Id: I7b5b8fb4fca155cb1d72e7ba13eef18e6a94a298

--- vpx_scale/include/vpxscale_nofp.h
+++ vpx_scale/include/vpxscale_nofp.h
@@ -9,20 +9,8 @@
  */
 
 
-/****************************************************************************
-*
-*****************************************************************************
-*/
-
-#ifndef SCALE_MODE_H
-#define SCALE_MODE_H
-
-typedef enum {
-  MAINTAIN_ASPECT_RATIO   = 0x0,
-  SCALE_TO_FIT            = 0x1,
-  CENTER                  = 0x2,
-  OTHER                   = 0x3
-} SCALE_MODE;
-
-
+#if defined(__S60_V20__) || defined(__SYMBIAN32__) && !defined(__WINS__)
+#include "symbian\vpxscale_nofp.h"
+#else
+#include "generic\vpxscale_nofp.h"
 #endif
