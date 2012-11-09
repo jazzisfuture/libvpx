a0972b5d03f1899436181cb3dcfcadfa3353e2cd
John Koleszar <jkoleszar@google.com>
2012-11-08 17:09:30 -0800

    support building vp8 and vp9 into a single lib
    WIP: Not tested on Windows yet.
    
    Change-Id: Ib8f8a66c9fd31e508cdc9caa662192f38433aa3d

--- vp9/vp9_common.mk
+++ vp9/vp9_common.mk
@@ -115,15 +115,15 @@ endif
 
 VP9_COMMON_SRCS-$(HAVE_SSE4_1) += common/x86/filter_sse4.c
 ifeq ($(HAVE_SSE4_1),yes)
-$(call xform_obj_path,vp9/common/x86/filter_sse4.c.o): CFLAGS += -msse4
+$(call xform_obj_path_o_d,vp9/common/x86/filter_sse4.c): CFLAGS += -msse4
 endif
 
 VP9_COMMON_SRCS-$(HAVE_SSE2) += common/x86/filter_sse2.c
 VP9_COMMON_SRCS-$(HAVE_SSE2) += common/x86/sadmxn_x86.c
 ifeq ($(HAVE_SSE2),yes)
-$(call xform_obj_path,vp9/common/x86/filter_sse2.c.o): CFLAGS += -msse2
-$(call xform_obj_path,vp9/common/x86/loopfilter_x86.c.o): CFLAGS += -msse2
-$(call xform_obj_path,vp9/common/x86/sadmxn_x86.c.o): CFLAGS += -msse2
+$(call xform_obj_path_o_d,vp9/common/x86/filter_sse2.c): CFLAGS += -msse2
+$(call xform_obj_path_o_d,vp9/common/x86/loopfilter_x86.c): CFLAGS += -msse2
+$(call xform_obj_path_o_d,vp9/common/x86/sadmxn_x86.c): CFLAGS += -msse2
 endif
 
 VP9_COMMON_SRCS-$(ARCH_ARM)  += common/arm/arm_systemdependent.c
@@ -182,3 +182,5 @@ VP9_COMMON_SRCS-$(HAVE_ARMV7)  += common/arm/neon/recon_neon.c
 
 $(eval $(call asm_offsets_template,\
          vp9_asm_com_offsets.asm, $(VP9_PREFIX)common/asm_com_offsets.c))
+
+$(eval $(call rtcd_h_template,vp9_rtcd,vp9/common/rtcd_defs.sh))
