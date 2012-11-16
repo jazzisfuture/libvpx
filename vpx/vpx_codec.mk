4c1647314abfd71994650df1111dc0e86992733f
Johann <johannkoenig@google.com>
2012-11-15 17:19:44 -0800

    Move documentation file list to relevant make file
    Change-Id: I2fe3095c4eb60233f00830d4124583ce7a64caa4

--- vpx/vpx_codec.mk
+++ vpx/vpx_codec.mk
@@ -13,9 +13,13 @@ API_EXPORTS += exports
 
 API_SRCS-$(CONFIG_VP8_ENCODER) += vp8.h
 API_SRCS-$(CONFIG_VP8_ENCODER) += vp8cx.h
+API_DOC_SRCS-$(CONFIG_VP8_ENCODER) += vp8.h
+API_DOC_SRCS-$(CONFIG_VP8_ENCODER) += vp8cx.h
 
 API_SRCS-$(CONFIG_VP8_DECODER) += vp8.h
 API_SRCS-$(CONFIG_VP8_DECODER) += vp8dx.h
+API_DOC_SRCS-$(CONFIG_VP8_DECODER) += vp8.h
+API_DOC_SRCS-$(CONFIG_VP8_DECODER) += vp8dx.h
 
 API_SRCS-yes                += src/vpx_decoder.c
 API_SRCS-yes                += vpx_decoder.h
