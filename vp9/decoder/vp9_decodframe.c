b956c4e888fd1f4bc01c8f65783598d5310ed56d
Ronald S. Bultje <rbultje@google.com>
2013-03-08 09:15:39 -0800

    New DC token prediction scheme.
    Instead of using left/above n_coeffs > 0 as a context, use the actual
    DC (absolute) value from left/above as a predictor for the scope of
    our DC value (i.e. it's token class), similar to how previous tokens
    predict the scope of the next (AC) token within a block.
    
    FIXME:
    - if top or left edge, use only 1 context, not both (like DC_PRED)
    - tables need to be re-trained
    
    Change-Id: I2d24b7acfa3eb13fb57131d43cf66d97d688e1bb

--- vp9/decoder/vp9_decodframe.c
+++ vp9/decoder/vp9_decodframe.c
@@ -1191,8 +1191,6 @@ static void read_coef_probs_common(BOOL_DECODER* const bc,
       for (j = 0; j < REF_TYPES; j++) {
         for (k = 0; k < COEF_BANDS; k++) {
           for (l = 0; l < PREV_COEF_CONTEXTS; l++) {
-            if (l >= 3 && k == 0)
-              continue;
             for (m = CONFIG_CODE_NONZEROCOUNT; m < ENTROPY_NODES; m++) {
               vp9_prob *const p = coef_probs[i][j][k][l] + m;
 
