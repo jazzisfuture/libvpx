8fa0fbe3322bec6a5434aa79728afbd1427dedd9
Deb Mukherjee <debargha@google.com>
2013-03-12 14:21:08 -0700

    Implicit weighted prediction experiment
    Adds an experiment to use a weighted prediction of two INTER predictors,
    where the weight is one of (1/4, 34), (1/2, 1/2), or (3/4, 1/4), and
    is chosen implicitly based on consistency of the predictors to the already
    reconstructed pixels to the top and left of the current macroblock
    or superblock.
    
    Currently the weighting is not applied to color or SPLITMV modes,
    which default to the usual (1/2, 1/2) weighting.
    
    Change-Id: I5438539ff4485c5752874cd1eb078ff14bf5235a

--- vp9/common/vp9_reconinter.h
+++ vp9/common/vp9_reconinter.h
@@ -38,6 +38,19 @@ void vp9_build_inter16x16_predictors_mb(MACROBLOCKD *xd,
                                         int mb_row,
                                         int mb_col);
 
+void vp9_build_inter32x32_predictors_sby(MACROBLOCKD *x,
+                                         uint8_t *dst_y,
+                                         int dst_ystride,
+                                         int mb_row,
+                                         int mb_col);
+
+void vp9_build_inter32x32_predictors_sbuv(MACROBLOCKD *x,
+                                          uint8_t *dst_u,
+                                          uint8_t *dst_v,
+                                          int dst_uvstride,
+                                          int mb_row,
+                                          int mb_col);
+
 void vp9_build_inter32x32_predictors_sb(MACROBLOCKD *x,
                                         uint8_t *dst_y,
                                         uint8_t *dst_u,
@@ -46,6 +59,19 @@ void vp9_build_inter32x32_predictors_sb(MACROBLOCKD *x,
                                         int dst_uvstride,
                                         int mb_row,
                                         int mb_col);
+
+void vp9_build_inter64x64_predictors_sby(MACROBLOCKD *x,
+                                         uint8_t *dst_y,
+                                         int dst_ystride,
+                                         int mb_row,
+                                         int mb_col);
+
+void vp9_build_inter64x64_predictors_sbuv(MACROBLOCKD *x,
+                                          uint8_t *dst_u,
+                                          uint8_t *dst_v,
+                                          int dst_uvstride,
+                                          int mb_row,
+                                          int mb_col);
 
 void vp9_build_inter64x64_predictors_sb(MACROBLOCKD *x,
                                         uint8_t *dst_y,
