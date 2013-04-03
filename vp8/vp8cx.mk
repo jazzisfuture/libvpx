9f6d62fcecf7f67f18eff5007055f09d729a648f
Johann <johann.koenig@duck.com>
2013-04-03 14:49:30 -0700

    Remove asm_offsets dependency in quantize_b_ssse3
    Replace it with some intrinsic code and inline assembly.
    
    Change-Id: I81b4df146db3d01039059be7dae31083e2943b97

--- vp8/vp8cx.mk
+++ vp8/vp8cx.mk
@@ -90,11 +90,17 @@ VP8_CX_SRCS-$(HAVE_MMX) += encoder/x86/vp8_enc_stubs_mmx.c
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/dct_sse2.asm
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/fwalsh_sse2.asm
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/quantize_sse2.c
+VP8_CX_SRCS-$(HAVE_SSSE3) += encoder/x86/quantize_ssse3.c
 
 # TODO(johann) make this generic
 ifeq ($(HAVE_SSE2),yes)
 vp8/encoder/x86/quantize_sse2.c.o: CFLAGS += -msse2
 vp8/encoder/x86/quantize_sse2.c.d: CFLAGS += -msse2
+endif
+
+ifeq ($(HAVE_SSSE3),yes)
+vp8/encoder/x86/quantize_ssse3.c.o: CFLAGS += -mssse3
+vp8/encoder/x86/quantize_ssse3.c.d: CFLAGS += -mssse3
 endif
 
 ifeq ($(CONFIG_TEMPORAL_DENOISING),yes)
@@ -108,7 +114,6 @@ endif
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/subtract_sse2.asm
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/temporal_filter_apply_sse2.asm
 VP8_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp8_enc_stubs_sse2.c
-VP8_CX_SRCS-$(HAVE_SSSE3) += encoder/x86/quantize_ssse3.asm
 VP8_CX_SRCS-$(HAVE_SSE4_1) += encoder/x86/quantize_sse4.asm
 VP8_CX_SRCS-$(ARCH_X86)$(ARCH_X86_64) += encoder/x86/quantize_mmx.asm
 VP8_CX_SRCS-$(ARCH_X86)$(ARCH_X86_64) += encoder/x86/encodeopt.asm
