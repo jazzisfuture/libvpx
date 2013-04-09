54d03e19277958715df50e6d9a41579eaf69f92e
John Koleszar <jkoleszar@google.com>
2013-04-09 10:54:19 -0700

    Remove the mb_no_coeff_skip flag
    This flag was added to VP8 to allow a mode where MB-level skipping
    was not allowed, saving a bit per mb. It was never used in practice,
    and hasn't been tested in VP9, so remove it.
    
    This does change the bitstream slightly, since the top-level bit
    is removed, so it's probably best to wait for another change of
    this type to submit it. It'll need a minor rebase after mb and
    sb functions are combined, but posting for discussion for now.
    
    Change-Id: Id450ec6904c6d06c1919508e7efc52d05cde5631

--- vp9/encoder/vp9_bitstream.c
+++ vp9/encoder/vp9_bitstream.c
@@ -800,9 +800,7 @@ static void pack_inter_mode_mvs(VP9_COMP *cpi, MODE_INFO *m,
     }
   }
 
-  if (!pc->mb_no_coeff_skip) {
-    skip_coeff = 0;
-  } else if (vp9_segfeature_active(xd, segment_id, SEG_LVL_SKIP)) {
+  if (vp9_segfeature_active(xd, segment_id, SEG_LVL_SKIP)) {
     skip_coeff = 1;
   } else {
     skip_coeff = m->mbmi.mb_skip_coeff;
@@ -995,8 +993,8 @@ static void pack_inter_mode_mvs(VP9_COMP *cpi, MODE_INFO *m,
        (rf != INTRA_FRAME && !(mode == SPLITMV &&
                                mi->partitioning == PARTITIONING_4X4))) &&
       pc->txfm_mode == TX_MODE_SELECT &&
-      !((pc->mb_no_coeff_skip && skip_coeff) ||
-        (vp9_segfeature_active(xd, segment_id, SEG_LVL_SKIP)))) {
+          !(skip_coeff || vp9_segfeature_active(xd, segment_id,
+                                                SEG_LVL_SKIP))) {
     TX_SIZE sz = mi->txfm_size;
     // FIXME(rbultje) code ternary symbol once all experiments are merged
     vp9_write(bc, sz != TX_4X4, pc->prob_tx[0]);
@@ -1023,9 +1021,7 @@ static void write_mb_modes_kf(const VP9_COMP *cpi,
     write_mb_segid(bc, &m->mbmi, xd);
   }
 
-  if (!c->mb_no_coeff_skip) {
-    skip_coeff = 0;
-  } else if (vp9_segfeature_active(xd, segment_id, SEG_LVL_SKIP)) {
+  if (vp9_segfeature_active(xd, segment_id, SEG_LVL_SKIP)) {
     skip_coeff = 1;
   } else {
     skip_coeff = m->mbmi.mb_skip_coeff;
@@ -1073,8 +1069,7 @@ static void write_mb_modes_kf(const VP9_COMP *cpi,
     write_uv_mode(bc, m->mbmi.uv_mode, c->kf_uv_mode_prob[ym]);
 
   if (ym <= I8X8_PRED && c->txfm_mode == TX_MODE_SELECT &&
-      !((c->mb_no_coeff_skip && skip_coeff) ||
-        (vp9_segfeature_active(xd, segment_id, SEG_LVL_SKIP)))) {
+      !(skip_coeff || vp9_segfeature_active(xd, segment_id, SEG_LVL_SKIP))) {
     TX_SIZE sz = m->mbmi.txfm_size;
     // FIXME(rbultje) code ternary symbol once all experiments are merged
     vp9_write(bc, sz != TX_4X4, c->prob_tx[0]);
@@ -2823,15 +2818,9 @@ void vp9_pack_bitstream(VP9_COMP *cpi, unsigned char *dest,
   active_section = 2;
 #endif
 
-  // Write out the mb_no_coeff_skip flag
-  vp9_write_bit(&header_bc, pc->mb_no_coeff_skip);
-  if (pc->mb_no_coeff_skip) {
-    int k;
-
-    vp9_update_skip_probs(cpi);
-    for (k = 0; k < MBSKIP_CONTEXTS; ++k) {
-      vp9_write_literal(&header_bc, pc->mbskip_pred_probs[k], 8);
-    }
+  vp9_update_skip_probs(cpi);
+  for (i = 0; i < MBSKIP_CONTEXTS; ++i) {
+    vp9_write_literal(&header_bc, pc->mbskip_pred_probs[i], 8);
   }
 
   if (pc->frame_type == KEY_FRAME) {
