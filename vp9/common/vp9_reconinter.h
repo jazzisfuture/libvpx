c8a8ab147c192ee3030a04e48d197147bd294f49
John Koleszar <jkoleszar@google.com>
2013-04-17 13:41:18 -0700

    make build_inter_predictors block size agnostic (split)
    All build_inter_predictors can now be serviced by the same inner
    function.
    
    Change-Id: I40b08bee8f047286db4b1aad9dcae37b879c3f2a

--- vp9/common/vp9_reconinter.h
+++ vp9/common/vp9_reconinter.h
@@ -16,19 +16,6 @@
 
 struct subpix_fn_table;
 
-void vp9_build_inter16x16_predictors_mby(MACROBLOCKD *xd,
-                                         uint8_t *dst_y,
-                                         int dst_ystride,
-                                         int mb_row,
-                                         int mb_col);
-
-void vp9_build_inter16x16_predictors_mbuv(MACROBLOCKD *xd,
-                                          uint8_t *dst_u,
-                                          uint8_t *dst_v,
-                                          int dst_uvstride,
-                                          int mb_row,
-                                          int mb_col);
-
 void vp9_build_inter16x16_predictors_mb(MACROBLOCKD *xd,
                                         uint8_t *dst_y,
                                         uint8_t *dst_u,
@@ -38,6 +25,20 @@ void vp9_build_inter16x16_predictors_mb(MACROBLOCKD *xd,
                                         int mb_row,
                                         int mb_col);
 
+void vp9_build_inter_predictors_sby(MACROBLOCKD *xd,
+                                    uint8_t *dst_y,
+                                    int dst_ystride,
+                                    int mb_row,
+                                    int mb_col,
+                                    BLOCK_SIZE_TYPE bsize);
+
+void vp9_build_inter_predictors_sbuv(MACROBLOCKD *xd,
+                                     uint8_t *dst_u,
+                                     uint8_t *dst_v,
+                                     int dst_uvstride,
+                                     int mb_row,
+                                     int mb_col,
+                                     BLOCK_SIZE_TYPE bsize);
 void vp9_build_inter_predictors_sb(MACROBLOCKD *mb,
                                    int mb_row, int mb_col,
                                    BLOCK_SIZE_TYPE bsize);
