2a8dbfed2234e14afbc509c55b44cfe5b203e3ad
John Koleszar <jkoleszar@google.com>
2013-04-25 22:07:29 -0700

    Remove all asm offset files from VP9
    The previous commit 15255ee "Move dequant from BLOCKD to per-plane MACROBLOCKD"
    removed the vp9_asm_enc_offsets.c file, but didn't update the build system
    completely, resulting in a broken windows build. This commit completes the
    process, removing the unused decoder and common offsets files.
    
    Change-Id: Ieb4242d14273efdf24149bda33f9591540bba06a

--- vp9/vp9dx.mk
+++ vp9/vp9dx.mk
@@ -17,7 +17,6 @@ VP9_DX_SRCS_REMOVE-no  += $(VP9_COMMON_SRCS_REMOVE-no)
 
 VP9_DX_SRCS-yes += vp9_dx_iface.c
 
-VP9_DX_SRCS-yes += decoder/vp9_asm_dec_offsets.c
 VP9_DX_SRCS-yes += decoder/vp9_dboolhuff.c
 VP9_DX_SRCS-yes += decoder/vp9_decodemv.c
 VP9_DX_SRCS-yes += decoder/vp9_decodframe.c
@@ -42,6 +41,3 @@ ifeq ($(HAVE_SSE2),yes)
 vp9/decoder/x86/vp9_dequantize_x86.c.o: CFLAGS += -msse2
 vp9/decoder/x86/vp9_dequantize_x86.c.d: CFLAGS += -msse2
 endif
-
-$(eval $(call asm_offsets_template,\
-         vp9_asm_dec_offsets.asm, $(VP9_PREFIX)decoder/vp9_asm_dec_offsets.c))
