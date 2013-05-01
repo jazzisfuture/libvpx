f76f1b8d74e624624b05e8f5f658c7d591b26d3b
Jingning Han <jingning@google.com>
2013-05-01 14:35:07 -0700

    Fix bugs in sb8x8 experiment/context prob update
    Fix bugs occur in contextual partition probability update, when
    sb8x8 is enabled
    
    Change-Id: I19e2cec8a54c2dafd2be2803bbfde7337a2ae45f

--- vp9/common/vp9_blockd.h
+++ vp9/common/vp9_blockd.h
@@ -453,7 +453,7 @@ typedef struct macroblockd {
 static INLINE void update_partition_context(MACROBLOCKD *xd,
                                             BLOCK_SIZE_TYPE sb_type,
                                             BLOCK_SIZE_TYPE sb_size) {
-  int bsl = mi_width_log2(sb_size), bs = 1 << bsl;
+  int bsl = mi_width_log2(sb_size), bs;
   int bwl = mi_width_log2(sb_type);
   int bhl = mi_height_log2(sb_type);
   int boffset = mi_width_log2(BLOCK_SIZE_SB64X64) - bsl;
@@ -461,6 +461,8 @@ static INLINE void update_partition_context(MACROBLOCKD *xd,
   // skip macroblock partition
   if (bsl == 0)
     return;
+
+  bs = 1 << (bsl - 1);
 
   // update the partition context at the end notes. set partition bits
   // of block sizes larger than the current one to be one, and partition
