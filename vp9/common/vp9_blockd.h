e73a564c173272d1e96bd07df1cc5d121bffa434
Paul Wilkins <paulwilkins@google.com>
2013-05-07 13:58:36 +0100

    Deprecate the newbintramode experiment.
    Clean out code relating to newbintramode.
    
    Change-Id: Ie91f4f156cdf60ce0da8ca407c1c9cb00c7d0705

--- vp9/common/vp9_blockd.h
+++ vp9/common/vp9_blockd.h
@@ -148,9 +148,6 @@ typedef enum {
   B_D27_PRED,
   B_D63_PRED,
   B_TM_PRED,
-#if CONFIG_NEWBINTRAMODES
-  B_CONTEXT_PRED,
-#endif
 
   LEFT4X4,
   ABOVE4X4,
@@ -163,15 +160,8 @@ typedef enum {
 #define VP9_BINTRAMODES (LEFT4X4)
 #define VP9_SUBMVREFS (1 + NEW4X4 - LEFT4X4)
 
-#if CONFIG_NEWBINTRAMODES
-/* The number of I4X4_PRED intra modes that are replaced by B_CONTEXT_PRED */
-#define CONTEXT_PRED_REPLACEMENTS  0
-#define VP9_KF_BINTRAMODES (VP9_BINTRAMODES - 1)
-#define VP9_NKF_BINTRAMODES  (VP9_BINTRAMODES - CONTEXT_PRED_REPLACEMENTS)
-#else
 #define VP9_KF_BINTRAMODES (VP9_BINTRAMODES)   /* 10 */
 #define VP9_NKF_BINTRAMODES (VP9_BINTRAMODES)  /* 10 */
-#endif
 
 #if !CONFIG_SB8X8
 typedef enum {
@@ -190,9 +180,6 @@ typedef enum {
 union b_mode_info {
   struct {
     B_PREDICTION_MODE first;
-#if CONFIG_NEWBINTRAMODES
-    B_PREDICTION_MODE context;
-#endif
   } as_mode;
   int_mv as_mv[2];  // first, second inter predictor motion vectors
 };
@@ -619,12 +606,6 @@ static TX_TYPE txfm_map(B_PREDICTION_MODE bmode) {
     case B_D27_PRED :
       return DCT_ADST;
 
-#if CONFIG_NEWBINTRAMODES
-    case B_CONTEXT_PRED:
-      assert(0);
-      break;
-#endif
-
     default:
       return DCT_DCT;
   }
@@ -654,10 +635,6 @@ static TX_TYPE get_tx_type_4x4(const MACROBLOCKD *xd, int ib) {
   if (xd->mode_info_context->mbmi.mode == I4X4_PRED &&
       xd->q_index < ACTIVE_HT) {
     tx_type = txfm_map(
-#if CONFIG_NEWBINTRAMODES
-        xd->mode_info_context->bmi[ib].as_mode.first == B_CONTEXT_PRED ?
-          xd->mode_info_context->bmi[ib].as_mode.context :
-#endif
         xd->mode_info_context->bmi[ib].as_mode.first);
 #if !CONFIG_SB8X8
   } else if (xd->mode_info_context->mbmi.mode == I8X8_PRED &&
