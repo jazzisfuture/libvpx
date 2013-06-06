07ba7539800a24ce58df2eb58b30b5d6b0b62cc2
Jingning Han <jingning@google.com>
2013-06-06 15:33:57 -0700

    Handle partition type coding of boundary blocks
    The partition types of blocks sitting on the frame boundary are
    constrained by the block size and the position of each sub-block
    relative to the frame. Hence we use truncated probability models
    to handle the coding of such information.
    
    WIP: experimenting
    
    Change-Id: I85d9b45665c15280069c0234ea6f778af586d87d

--- vp9/encoder/vp9_encodeframe.c
+++ vp9/encoder/vp9_encodeframe.c
@@ -852,6 +852,11 @@ static void encode_sb(VP9_COMP *cpi, TOKENEXTRA **tp,
 
   bwl = b_width_log2(c1), bhl = b_height_log2(c1);
 
+  // check consistency of partition type parse for block sitting on the
+  // frame boundary
+  if (!check_bsize_coverage(cm, xd, mi_row, mi_col, bsize))
+    assert(bwl < bsl && bhl < bsl);
+
   if (bsl == bwl && bsl == bhl) {
     if (output_enabled && bsize >= BLOCK_SIZE_SB8X8)
         cpi->partition_count[pl][PARTITION_NONE]++;
@@ -1422,7 +1427,7 @@ static void encode_sb_row(VP9_COMP *cpi, int mi_row,
 
   // Code each SB in the row
   for (mi_col = cm->cur_tile_mi_col_start;
-       mi_col < cm->cur_tile_mi_col_end; mi_col += 8) {
+       mi_col < cm->cur_tile_mi_col_end; mi_col += 64 / MI_SIZE) {
     int dummy_rate, dummy_dist;
     if (cpi->speed < 5) {
       rd_pick_partition(cpi, tp, mi_row, mi_col, BLOCK_SIZE_SB64X64,
