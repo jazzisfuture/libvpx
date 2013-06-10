4a09624f662ace3ea8e181e31167e44201ae1fe4
Paul Wilkins <paulwilkins@google.com>
2013-06-10 13:22:18 +0100

    Bug fix in find_seg_id.
    Returned illegal segment id at border.
    
    Change-Id: Ifcdd3288d7ce55d7921da75b9cdc1311d594842b

--- vp9/encoder/vp9_encodeframe.c
+++ vp9/encoder/vp9_encodeframe.c
@@ -474,19 +474,20 @@ static unsigned find_seg_id(uint8_t *buf, BLOCK_SIZE_TYPE bsize,
   int x, y;
   unsigned seg_id = -1;
 
- if (bsize > BLOCK_SIZE_MB16X16)
-   bsize = bsize;
+  if (bsize > BLOCK_SIZE_MB16X16)
+    bsize = bsize;
 
-  buf += width * start_y;
-  for (y = start_y; y < end_y; y++, buf += width) {
-    for (x = start_x; x < end_x; x++) {
-      seg_id = MIN(seg_id, buf[x]);
+  if ((start_x >= width) || (start_y >= height)) {
+    return 0;  // Off edge return seg 0
+  } else {
+    buf += width * start_y;
+    for (y = start_y; y < end_y; y++, buf += width) {
+      for (x = start_x; x < end_x; x++) {
+        seg_id = MIN(seg_id, buf[x]);
+      }
     }
   }
 
-  // TODO(paulwilkins) temporary trap of illegal segment id return.
-  if (seg_id > 7)
-    seg_id = 0;
   return seg_id;
 }
 
