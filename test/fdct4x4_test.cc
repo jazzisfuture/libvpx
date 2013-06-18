33d5f398192af215ae252149ef78af9a385d2947
Jingning Han <jingning@google.com>
2013-06-18 10:46:33 -0700

    Add unit tests for 4x4 ADST
    Enable sign bias check and round-trip error unit tests for 4x4 ADST.
    
    Change-Id: Icd3d839f098d4b92b00ff76eac146765b039d0d3

--- test/fdct4x4_test.cc
+++ test/fdct4x4_test.cc
@@ -25,7 +25,7 @@ using libvpx_test::ACMRandom;
 
 namespace {
 
-TEST(Vp9Fdct4x4Test, SignBiasCheck) {
+TEST(VP9Fdct4x4Test, SignBiasCheck) {
   ACMRandom rnd(ACMRandom::DeterministicSeed());
   int16_t test_input_block[16];
   int16_t test_output_block[16];
@@ -88,7 +88,7 @@ TEST(Vp9Fdct4x4Test, SignBiasCheck) {
   }
 };
 
-TEST(Vp9Fdct4x4Test, RoundTripErrorCheck) {
+TEST(VP9Fdct4x4Test, RoundTripErrorCheck) {
   ACMRandom rnd(ACMRandom::DeterministicSeed());
   int max_error = 0;
   double total_error = 0;
@@ -141,4 +141,115 @@ TEST(Vp9Fdct4x4Test, RoundTripErrorCheck) {
       << "Error: FDCT/IDCT has average roundtrip error > 1 per block";
 };
 
+TEST(VP9Fadst4x4Test, SignBiasCheck) {
+  ACMRandom rnd(ACMRandom::DeterministicSeed());
+  int16_t test_input_block[16];
+  int16_t test_output_block[16];
+  const int pitch = 4;
+  int count_sign_block[16][2];
+  const int count_test_block = 1000000;
+
+  memset(count_sign_block, 0, sizeof(count_sign_block));
+
+  for (int i = 0; i < count_test_block; ++i) {
+    // Initialize a test block with input range [-255, 255].
+    for (int j = 0; j < 16; ++j)
+      test_input_block[j] = rnd.Rand8() - rnd.Rand8();
+
+    // test 4x4 2D ADST transform
+    vp9_short_fht4x4_c(test_input_block, test_output_block, pitch, 3);
+
+    for (int j = 0; j < 16; ++j) {
+      if (test_output_block[j] < 0)
+        ++count_sign_block[j][0];
+      else if (test_output_block[j] > 0)
+        ++count_sign_block[j][1];
+    }
+  }
+
+  for (int j = 0; j < 16; ++j) {
+    const bool bias_acceptable = (abs(count_sign_block[j][0] -
+                                      count_sign_block[j][1]) < 10000);
+    EXPECT_TRUE(bias_acceptable)
+        << "Error: 4x4 FADST has a sign bias > 1%"
+        << " for input range [-255, 255] at index " << j;
+  }
+
+  memset(count_sign_block, 0, sizeof(count_sign_block));
+
+  for (int i = 0; i < count_test_block; ++i) {
+    // Initialize a test block with input range [-15, 15].
+    for (int j = 0; j < 16; ++j)
+      test_input_block[j] = (rnd.Rand8() >> 4) - (rnd.Rand8() >> 4);
+
+    vp9_short_fht4x4_c(test_input_block, test_output_block, pitch, 3);
+
+    for (int j = 0; j < 16; ++j) {
+      if (test_output_block[j] < 0)
+        ++count_sign_block[j][0];
+      else if (test_output_block[j] > 0)
+        ++count_sign_block[j][1];
+    }
+  }
+
+  for (int j = 0; j < 16; ++j) {
+    const bool bias_acceptable = (abs(count_sign_block[j][0] -
+                                      count_sign_block[j][1]) < 100000);
+    EXPECT_TRUE(bias_acceptable)
+        << "Error: 4x4 FADST has a sign bias > 10%"
+        << " for input range [-15, 15] at index " << j;
+  }
+};
+
+TEST(VP9Fadst4x4Test, RoundTripErrorCheck) {
+  ACMRandom rnd(ACMRandom::DeterministicSeed());
+  int max_error = 0;
+  double total_error = 0;
+  const int count_test_block = 1000000;
+  for (int i = 0; i < count_test_block; ++i) {
+    int16_t test_input_block[16];
+    int16_t test_temp_block[16];
+    uint8_t dst[16], src[16];
+
+    for (int j = 0; j < 16; ++j) {
+      src[j] = rnd.Rand8();
+      dst[j] = rnd.Rand8();
+    }
+    // Initialize a test block with input range [-255, 255].
+    for (int j = 0; j < 16; ++j)
+      test_input_block[j] = src[j] - dst[j];
+
+    // test 4x4 2D ADST
+    const int pitch = 4;
+    vp9_short_fht4x4_c(test_input_block, test_temp_block, pitch, 3);
+
+    for (int j = 0; j < 16; ++j) {
+        if(test_temp_block[j] > 0) {
+          test_temp_block[j] += 2;
+          test_temp_block[j] /= 4;
+          test_temp_block[j] *= 4;
+        } else {
+          test_temp_block[j] -= 2;
+          test_temp_block[j] /= 4;
+          test_temp_block[j] *= 4;
+        }
+    }
+
+    // Because the bitstream is not frozen yet, use the idct in the codebase.
+    vp9_short_iht4x4_add_c(test_temp_block, dst, 4, 3);
+
+    for (int j = 0; j < 16; ++j) {
+      const int diff = dst[j] - src[j];
+      const int error = diff * diff;
+      if (max_error < error)
+        max_error = error;
+      total_error += error;
+    }
+  }
+  EXPECT_GE(1, max_error)
+      << "Error: FADST/IADST has an individual roundtrip error > 1";
+
+  EXPECT_GE(count_test_block, total_error)
+      << "Error: FADST/IADST has average roundtrip error > 1 per block";
+};
 }  // namespace
