6a867d31eb4e41d7ae91a29eacadea1b0eedace3
Jim Bankoski <jimbankoski@google.com>
2013-06-20 09:24:04 -0700

    adds force partitioning greater than or less than block size
    adds a new speed feature to force partitioning to be greater than
    or less than a certain size
    
    Change-Id: I8c048eeeef93700ae822eccf98f8751a45b2e7d0

--- vp9/common/vp9_debugmodes.c
+++ vp9/common/vp9_debugmodes.c
@@ -14,7 +14,7 @@
 #include "vp9/common/vp9_onyxc_int.h"
 
 static void log_frame_info(VP9_COMMON *cm, const char *str, FILE *f) {
-  fprintf(f, str);
+  fprintf(f, "%s", str);
   fprintf(f, "(Frame %d, Show:%d, Q:%d): \n", cm->current_video_frame,
           cm->show_frame, cm->base_qindex);
 }
