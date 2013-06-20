02760282937f4c6f5bc86368e1c06d6182971a2e
Dmitry Kovalev <dkovalev@google.com>
2013-06-20 15:19:05 -0700

    Transforming scale_mv_component_q4 into scale_mv_q4 function.
    Using MV instead of int_mv for function arguments.
    
    Change-Id: Ic25e13dccbc98fac1fa1b3255127e00cca2a57f6

--- vp9/common/vp9_blockd.h
+++ vp9/common/vp9_blockd.h
@@ -249,9 +249,8 @@ struct scale_factors {
   int (*scale_value_x)(int val, const struct scale_factors *scale);
   int (*scale_value_y)(int val, const struct scale_factors *scale);
   void (*set_scaled_offsets)(struct scale_factors *scale, int row, int col);
-  int_mv32 (*scale_mv_q3_to_q4)(const int_mv *src_mv,
-                                const struct scale_factors *scale);
-  int32_t (*scale_mv_component_q4)(int mv_q4, int scale_fp, int offset_q4);
+  MV (*scale_mv_q3_to_q4)(const MV *mv, const struct scale_factors *scale);
+  MV (*scale_mv_q4)(const MV *mv, const struct scale_factors *scale);
 
   convolve_fn_t predict[2][2][2];  // horiz, vert, avg
 };
