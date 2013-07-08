6160810a19ef864df123be3258401a5c2dceb4db
Deb Mukherjee <debargha@google.com>
2013-07-08 16:01:01 -0700

    Prunes out full-rd computation based on modeled rd
    Adds a speed feature to eliminate full-rd computation if the modeled
    rd or rd on based on a different parameter but the same mode is already
    a lot larger than the best rd yet.
    
    Specifically, only search the sharp and smooth filters if the modeled
    rd cost based on the  regular filter is within a certain factor of the
    best rd cost so far. Also, skip full-rd computation of non splitmv
    inter modes if the modeled rd cost based on pred error is within the
    same factor of the best rd cost so far.
    
    Also adds some enhancements in the rd search for splitmv mode to
    speed things up by early breakouts. Negligible impact on performance.
    
    Resuts on derfraw300:
    psnr:    -0.27% with the residual breakout feature on.
    speedup: 6% with splitmv enhancements, 20% with also residual breakout
             (tested on football sequence at 600 Kbps)
    
    Change-Id: I37abc308ea9f110c1679ce649b6a7e73ab1ad5fc

--- vp9/encoder/vp9_onyx_int.h
+++ vp9/encoder/vp9_onyx_int.h
@@ -274,6 +274,7 @@ typedef struct {
   // The heuristics selected are based on  flags
   // defined in the MODE_SEARCH_SKIP_HEURISTICS enum
   unsigned int mode_search_skip_flags;
+  int use_rd_breakout;
 } SPEED_FEATURES;
 
 enum BlockSize {
