f98a5274c4005d0f6331cb37d7e2dea96e430734
Deb Mukherjee <debargha@google.com>
2013-07-10 16:51:07 -0700

    Some minor cleanups for efficiency
    Implements some of the helper functions more efficiently with
    lookups rathers than branches. Modeling function is consolidated
    to reduce some computations.
    
    Also merged the two enums BLOCK_SIZE_TYPES and BlockSize into
    one because there is no need to keep them separate (even though
    the semantics are a little different).
    
    No bitstream or output change.
    
    About 0.5% speedup
    
    Change-Id: I7d71a66e8031ddb340744dc493f22976052b8f9f

--- vp9/common/vp9_common_data.h
+++ vp9/common/vp9_common_data.h
@@ -17,5 +17,12 @@ extern const int b_width_log2_lookup[BLOCK_SIZE_TYPES];
 extern const int b_height_log2_lookup[BLOCK_SIZE_TYPES];
 extern const int mi_width_log2_lookup[BLOCK_SIZE_TYPES];
 extern const int mi_height_log2_lookup[BLOCK_SIZE_TYPES];
+extern const BLOCK_SIZE_TYPE subsize_lookup[PARTITION_TYPES][BLOCK_SIZE_TYPES];
+extern const TX_SIZE max_txsize_lookup[BLOCK_SIZE_TYPES];
+extern const TX_SIZE max_uv_txsize_lookup[BLOCK_SIZE_TYPES];
+extern const BLOCK_SIZE_TYPE bsize_from_dim_lookup[5][5];
+
+extern int sb_index_offset_lookup[BLOCK_SIZE_TYPES];
+void vp9_fill_lookups();
 
 #endif    // VP9_COMMON_VP9_COMMON_DATA_H
