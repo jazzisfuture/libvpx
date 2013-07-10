330976791cadcabc7ee4b4384b32a78de8dd9600
Ronald S. Bultje <rbultje@google.com>
2013-07-10 10:23:41 -0700

    Remove unused 16x3/3x16 sad SSE2 functions.
    Change-Id: I30a597c0cc366e34c9a3e2afe32d70e044f95ca4

--- vp9/vp9_common.mk
+++ vp9/vp9_common.mk
@@ -96,7 +96,6 @@ VP9_COMMON_SRCS-$(HAVE_SSE3) += common/x86/vp9_mask_sse3.asm
 endif
 
 VP9_COMMON_SRCS-$(HAVE_SSE2) += common/x86/vp9_idct_intrin_sse2.c
-VP9_COMMON_SRCS-$(HAVE_SSE2) += common/x86/vp9_sadmxn_sse2.c
 
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_loopfilter_neon$(ASM)
 
