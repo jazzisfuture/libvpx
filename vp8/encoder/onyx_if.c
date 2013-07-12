048207d114910c40c1623d62bfd23f54ea659ca8
James Zern <jzern@google.com>
2013-07-12 14:11:53 -0700

    yv12config: remove YUV_TYPE
    this was never fleshed out in the context of VP8, for which it was
    added. for VP9 it has no meaning.
    
    Change-Id: Iba2ecc026d9e947067b96690245d337e51e26eff

--- vp8/encoder/onyx_if.c
+++ vp8/encoder/onyx_if.c
@@ -4822,7 +4822,6 @@ int vp8_receive_raw_frame(VP8_COMP *cpi, unsigned int frame_flags, YV12_BUFFER_C
 #if HAVE_NEON
     int64_t store_reg[8];
 #endif
-    VP8_COMMON            *cm = &cpi->common;
     struct vpx_usec_timer  timer;
     int                    res = 0;
 
@@ -4848,7 +4847,6 @@ int vp8_receive_raw_frame(VP8_COMP *cpi, unsigned int frame_flags, YV12_BUFFER_C
     if(vp8_lookahead_push(cpi->lookahead, sd, time_stamp, end_time,
                           frame_flags, cpi->active_map_enabled ? cpi->active_map : NULL))
         res = -1;
-    cm->clr_type = sd->clrtype;
     vpx_usec_timer_mark(&timer);
     cpi->time_receive_data += vpx_usec_timer_elapsed(&timer);
 
