98836d5a0f9c9a61eed70c4388f41fe0ceeb66fb
Jim Bankoski <jimbankoski@google.com>
2013-07-21 22:42:33 -0700

    clean up bw, bh
    many structures use bw and bh and they have different meanings.   This cl attempts
    to start this clean up and remove unneccessary 2 step look up log and then
    shift operations...
    
    also removed partition type multiple operation code in bitstream.c can remove in
    decode frame and encodeframe.c next.
    
    Change-Id: I7e03e552bdfc0939738e430862e3073d30fdd5db

--- vp9/encoder/vp9_encodemv.c
+++ vp9/encoder/vp9_encodemv.c
@@ -513,14 +513,14 @@ void vp9_update_nmv_count(VP9_COMP *cpi, MACROBLOCK *x,
   MODE_INFO *mi = x->e_mbd.mode_info_context;
   MB_MODE_INFO *const mbmi = &mi->mbmi;
   MV diff;
-  const int bw = 1 << b_width_log2(mbmi->sb_type);
-  const int bh = 1 << b_height_log2(mbmi->sb_type);
+  const int block_width = block_type_to_width[mbmi->sb_type];
+  const int block_height = block_type_to_height[mbmi->sb_type];
   int idx, idy;
 
   if (mbmi->sb_type < BLOCK_SIZE_SB8X8) {
     PARTITION_INFO *pi = x->partition_info;
-    for (idy = 0; idy < 2; idy += bh) {
-      for (idx = 0; idx < 2; idx += bw) {
+    for (idy = 0; idy < 2; idy += block_height) {
+      for (idx = 0; idx < 2; idx += block_width) {
         const int i = idy * 2 + idx;
         if (pi->bmi[i].mode == NEWMV) {
           diff.row = mi->bmi[i].as_mv[0].as_mv.row - best_ref_mv->as_mv.row;
