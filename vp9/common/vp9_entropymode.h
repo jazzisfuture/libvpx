87a9828a6f8a4ebe487b1d4527ef7ef648eaddb4
Dmitry Kovalev <dkovalev@google.com>
2013-07-24 10:31:07 -0700

    Removing vp9_adapt_mode_context function.
    Moving code from vp9_adapt_mode_context to vp9_adapt_mode_probs.
    
    Change-Id: I60829c30b28968cd813551ef3a206dfb98d323c9

--- vp9/common/vp9_entropymode.h
+++ vp9/common/vp9_entropymode.h
@@ -69,8 +69,6 @@ void vp9_init_mbmode_probs(struct VP9Common *x);
 
 void vp9_init_mode_contexts(struct VP9Common *pc);
 
-void vp9_adapt_mode_context(struct VP9Common *pc);
-
 void vp9_adapt_mode_probs(struct VP9Common *);
 
 void vp9_accum_mv_refs(struct VP9Common *pc, MB_PREDICTION_MODE m, int context);
