2cece481fe8283e62756a380041d76bc2b40d9b9
Jingning Han <jingning@google.com>
2013-08-07 14:45:37 -0700

    SSE2 high precision 32x32 forward DCT
    Enable SSE2 implementation of high precision 32x32 forward DCT. The
    run-time goes down from 32126 cycles to 13442 cycles.
    
    Change-Id: Ib5ccafe3176c65bd6f2dbdef790bd47bbc880e56

--- vp9/encoder/x86/vp9_dct_sse2.c
+++ vp9/encoder/x86/vp9_dct_sse2.c
@@ -12,6 +12,8 @@
 #include "vp9/common/vp9_idct.h"  // for cospi constants
 #include "vpx_ports/mem.h"
 
+#include "stdio.h"
+
 void vp9_short_fdct4x4_sse2(int16_t *input, int16_t *output, int pitch) {
   // The 2D transform is done with two passes which are actually pretty
   // similar. In the first one, we transform the columns and transpose
@@ -2573,13 +2575,13 @@ void vp9_short_fht16x16_sse2(int16_t *input, int16_t *output,
 }
 
 #define FDCT32x32_2D vp9_short_fdct32x32_rd_sse2
-#define FDCT32x32_LOW_PRECISION 1
+#define FDCT32x32_HIGH_PRECISION 0
 #include "vp9/encoder/x86/vp9_dct32x32_sse2.c"
 #undef  FDCT32x32_2D
-#undef  FDCT32x32_LOW_PRECISION
+#undef  FDCT32x32_HIGH_PRECISION
 
 #define FDCT32x32_2D vp9_short_fdct32x32_sse2
-#define FDCT32x32_LOW_PRECISION 0
+#define FDCT32x32_HIGH_PRECISION 1
 #include "vp9/encoder/x86/vp9_dct32x32_sse2.c" // NOLINT
 #undef  FDCT32x32_2D
-#undef  FDCT32x32_LOW_PRECISION
+#undef  FDCT32x32_HIGH_PRECISION
