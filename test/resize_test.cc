7b745af936260f875e6430d8b572ad5b050e7067
Adrian Grange <agrange@google.com>
2013-09-13 10:14:16 -0700

    Added encoder control to set absolute frame size
    Added an encoder control to enable the frame size to be
    set to a defined (width, height). Previously, the frame
    size could only be set either to a defined fraction of
    the current frame size, or the width and height could be
    set independently (requiring two reconfigurations rather
    than one for the underlying buffers).
    
    In addition, I extended the resize unit test to test
    this new resizing mode.
    
    Change-Id: I496730e205a6266c9beaaccc23ecf47fb3ce5a2a

--- test/resize_test.cc
+++ test/resize_test.cc
@@ -13,8 +13,8 @@
 #include "test/codec_factory.h"
 #include "test/encode_test_driver.h"
 #include "test/i420_video_source.h"
-#include "test/video_source.h"
 #include "test/util.h"
+#include "test/video_source.h"
 
 // Enable(1) or Disable(0) writing of the compressed bitstream.
 #define WRITE_COMPRESSED_STREAM 0
@@ -23,15 +23,15 @@ namespace {
 
 #if WRITE_COMPRESSED_STREAM
 static void mem_put_le16(char *const mem, const unsigned int val) {
-    mem[0] = val;
-    mem[1] = val >> 8;
+  mem[0] = val;
+  mem[1] = val >> 8;
 }
 
 static void mem_put_le32(char *const mem, const unsigned int val) {
-    mem[0] = val;
-    mem[1] = val >> 8;
-    mem[2] = val >> 16;
-    mem[3] = val >> 24;
+  mem[0] = val;
+  mem[1] = val >> 8;
+  mem[2] = val >> 16;
+  mem[3] = val >> 24;
 }
 
 static void write_ivf_file_header(const vpx_codec_enc_cfg_t *const cfg,
@@ -57,7 +57,7 @@ static void write_ivf_file_header(const vpx_codec_enc_cfg_t *const cfg,
 
 static void write_ivf_frame_size(FILE *const outfile, const size_t size) {
   char header[4];
-  mem_put_le32(header, (int)size);
+  mem_put_le32(header, static_cast<unsigned int>(size));
   (void)fwrite(header, 1, 4, outfile);
 }
 
@@ -70,13 +70,13 @@ static void write_ivf_frame_header(const vpx_codec_cx_pkt_t *const pkt,
     return;
 
   pts = pkt->data.frame.pts;
-  mem_put_le32(header, (int)pkt->data.frame.sz);
+  mem_put_le32(header, static_cast<unsigned int>(pkt->data.frame.sz));
   mem_put_le32(header + 4, pts & 0xFFFFFFFF);
   mem_put_le32(header + 8, pts >> 32);
 
   (void)fwrite(header, 1, 12, outfile);
 }
-#endif
+#endif  // WRITE_COMPRESSED_STREAM
 
 const unsigned int kInitialWidth = 320;
 const unsigned int kInitialHeight = 240;
@@ -102,6 +102,8 @@ class ResizingVideoSource : public ::libvpx_test::DummyVideoSource {
     limit_ = 60;
   }
 
+  virtual ~ResizingVideoSource() {}
+
  protected:
   virtual void Next() {
     ++frame_;
@@ -114,7 +116,16 @@ class ResizingVideoSource : public ::libvpx_test::DummyVideoSource {
 class ResizeTest : public ::libvpx_test::EncoderTest,
   public ::libvpx_test::CodecTestWithParam<libvpx_test::TestMode> {
  protected:
+#if WRITE_COMPRESSED_STREAM
+  ResizeTest()
+      : EncoderTest(GET_PARAM(0)),
+        outfile_(NULL),
+        out_frames_(0) {}
+#else
   ResizeTest() : EncoderTest(GET_PARAM(0)) {}
+#endif
+
+  virtual ~ResizeTest() {}
 
   struct FrameInfo {
     FrameInfo(vpx_codec_pts_t _pts, unsigned int _w, unsigned int _h)
@@ -135,7 +146,42 @@ class ResizeTest : public ::libvpx_test::EncoderTest,
     frame_info_list_.push_back(FrameInfo(pts, img.d_w, img.d_h));
   }
 
+  virtual void BeginPassHook(unsigned int /*pass*/) {
+#if WRITE_COMPRESSED_STREAM
+    outfile_ = fopen("resize_test.ivf", "wb");
+#endif
+  }
+
+  virtual void EndPassHook() {
+#if WRITE_COMPRESSED_STREAM
+    if (outfile_) {
+      if (!fseek(outfile_, 0, SEEK_SET))
+        write_ivf_file_header(&cfg_, out_frames_, outfile_);
+      fclose(outfile_);
+      outfile_ = NULL;
+    }
+#endif
+  }
+
+  virtual void FramePktHook(const vpx_codec_cx_pkt_t *pkt) {
+#if WRITE_COMPRESSED_STREAM
+    ++out_frames_;
+
+    // Write initial file header if first frame.
+    if (pkt->data.frame.pts == 0)
+      write_ivf_file_header(&cfg_, 0, outfile_);
+
+    // Write frame header and data.
+    write_ivf_frame_header(pkt, outfile_);
+    (void)fwrite(pkt->data.frame.buf, 1, pkt->data.frame.sz, outfile_);
+#endif
+  }
+
   std::vector< FrameInfo > frame_info_list_;
+#if WRITE_COMPRESSED_STREAM
+  FILE *outfile_;
+  unsigned int out_frames_;
+#endif
 };
 
 TEST_P(ResizeTest, TestExternalResizeWorks) {
@@ -155,42 +201,22 @@ TEST_P(ResizeTest, TestExternalResizeWorks) {
   }
 }
 
-#define STEP_DOWN_FRAME 3
-#define STEP_UP_FRAME   6
+const unsigned int kStepDownFrame = 3;
+const unsigned int kStepUpFrame = 6;
 
-class ResizeInternalTest : public ResizeTest {
+class ResizeByRatioInternalTest : public ResizeTest {
  protected:
-  ResizeInternalTest() :
-#if WRITE_COMPRESSED_STREAM
-    ResizeTest(), frame0_psnr_(0.0), outfile_(NULL), out_frames_(0) {}
-#else
-    ResizeTest(), frame0_psnr_(0.0) {}
-#endif
+  ResizeByRatioInternalTest() : ResizeTest(), frame0_psnr_(0.0) {}
 
-  virtual void BeginPassHook(unsigned int pass) {
-#if WRITE_COMPRESSED_STREAM
-    outfile_ = fopen("vp90-2-05-resize.ivf", "wb");
-#endif
-  }
-
-  virtual void EndPassHook() {
-#if WRITE_COMPRESSED_STREAM
-    if (outfile_) {
-      if (!fseek(outfile_, 0, SEEK_SET))
-        write_ivf_file_header(&cfg_, out_frames_, outfile_);
-      fclose(outfile_);
-      outfile_ = NULL;
-    }
-#endif
-  }
+  virtual ~ResizeByRatioInternalTest() {}
 
   virtual void PreEncodeFrameHook(libvpx_test::VideoSource *video,
                                   libvpx_test::Encoder *encoder) {
-    if (video->frame() == STEP_DOWN_FRAME) {
+    if (video->frame() == kStepDownFrame) {
       struct vpx_scaling_mode mode = {VP8E_FOURFIVE, VP8E_THREEFIVE};
       encoder->Control(VP8E_SET_SCALEMODE, &mode);
     }
-    if (video->frame() == STEP_UP_FRAME) {
+    if (video->frame() == kStepUpFrame) {
       struct vpx_scaling_mode mode = {VP8E_NORMAL, VP8E_NORMAL};
       encoder->Control(VP8E_SET_SCALEMODE, &mode);
     }
@@ -202,37 +228,13 @@ class ResizeInternalTest : public ResizeTest {
     EXPECT_NEAR(pkt->data.psnr.psnr[0], frame0_psnr_, 1.0);
   }
 
-  virtual void FramePktHook(const vpx_codec_cx_pkt_t *pkt) {
-#if WRITE_COMPRESSED_STREAM
-    ++out_frames_;
-
-    // Write initial file header if first frame.
-    if (pkt->data.frame.pts == 0)
-      write_ivf_file_header(&cfg_, 0, outfile_);
-
-    // Write frame header and data.
-    write_ivf_frame_header(pkt, outfile_);
-    (void)fwrite(pkt->data.frame.buf, 1, pkt->data.frame.sz, outfile_);
-#endif
-  }
-
   double frame0_psnr_;
-#if WRITE_COMPRESSED_STREAM
-  FILE *outfile_;
-  unsigned int out_frames_;
-#endif
 };
 
-TEST_P(ResizeInternalTest, TestInternalResizeWorks) {
+TEST_P(ResizeByRatioInternalTest, TestInternalResizeByRatioWorks) {
   ::libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
                                        30, 1, 0, 10);
   init_flags_ = VPX_CODEC_USE_PSNR;
-
-  // If the number of frames being encoded is smaller than g_lag_in_frames
-  // the encoded frame is unavailable using the current API. Comparing
-  // frames to detect mismatch would then not be possible. Set
-  // g_lag_in_frames = 0 to get around this.
-  cfg_.g_lag_in_frames = 0;
 
   // q picked such that initial keyframe on this clip is ~30dB PSNR
   cfg_.rc_min_quantizer = cfg_.rc_max_quantizer = 48;
@@ -242,12 +244,13 @@ TEST_P(ResizeInternalTest, TestInternalResizeWorks) {
   // frames to detect mismatch would then not be possible. Set
   // g_lag_in_frames = 0 to get around this.
   cfg_.g_lag_in_frames = 0;
+
   ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
 
   for (std::vector<FrameInfo>::iterator info = frame_info_list_.begin();
        info != frame_info_list_.end(); ++info) {
     const vpx_codec_pts_t pts = info->pts;
-    if (pts >= STEP_DOWN_FRAME && pts < STEP_UP_FRAME) {
+    if (pts >= kStepDownFrame && pts < kStepUpFrame) {
       ASSERT_EQ(282U, info->w) << "Frame " << pts << " had unexpected width";
       ASSERT_EQ(173U, info->h) << "Frame " << pts << " had unexpected height";
     } else {
@@ -257,7 +260,67 @@ TEST_P(ResizeInternalTest, TestInternalResizeWorks) {
   }
 }
 
+class ResizeAbsoluteInternalTest : public ResizeTest {
+ protected:
+  ResizeAbsoluteInternalTest() : ResizeTest(), frame0_psnr_(0.0) {}
+
+  virtual void PreEncodeFrameHook(libvpx_test::VideoSource *video,
+                                  libvpx_test::Encoder *encoder) {
+    // Note: Starts at original resolution.
+    if (video->frame() == kStepDownFrame) {
+      struct vpx_absolute_scaling_mode mode = {197, 233};
+      encoder->Control(VP9E_SET_FRAMESIZE, &mode);
+    }
+    if (video->frame() == kStepUpFrame) {
+      struct vpx_absolute_scaling_mode mode = {260, 284};
+      encoder->Control(VP9E_SET_FRAMESIZE, &mode);
+    }
+  }
+
+  virtual void PSNRPktHook(const vpx_codec_cx_pkt_t *pkt) {
+    if (!frame0_psnr_)
+      frame0_psnr_ = pkt->data.psnr.psnr[0];
+    EXPECT_NEAR(pkt->data.psnr.psnr[0], frame0_psnr_, 1.0);
+  }
+
+  double frame0_psnr_;
+};
+
+TEST_P(ResizeAbsoluteInternalTest, TestInternalResizeAbsoluteWorks) {
+  ::libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
+                                       30, 1, 0, 10);
+  init_flags_ = VPX_CODEC_USE_PSNR;
+
+  // q picked such that initial keyframe on this clip is ~30dB PSNR
+  cfg_.rc_min_quantizer = cfg_.rc_max_quantizer = 48;
+
+  // If the number of frames being encoded is smaller than g_lag_in_frames
+  // the encoded frame is unavailable using the current API. Comparing
+  // frames to detect mismatch would then not be possible. Set
+  // g_lag_in_frames = 0 to get around this.
+  cfg_.g_lag_in_frames = 0;
+
+  ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
+
+  for (std::vector<FrameInfo>::iterator info = frame_info_list_.begin();
+       info != frame_info_list_.end(); ++info) {
+    const vpx_codec_pts_t pts = info->pts;
+    if (pts < kStepDownFrame) {
+      ASSERT_EQ(352U, info->w) << "Frame " << pts << " had unexpected width";
+      ASSERT_EQ(288U, info->h) << "Frame " << pts << " had unexpected height";
+    } else if (pts >= kStepDownFrame && pts < kStepUpFrame) {
+      ASSERT_EQ(197U, info->w) << "Frame " << pts << " had unexpected width";
+      ASSERT_EQ(233U, info->h) << "Frame " << pts << " had unexpected height";
+    } else {
+      EXPECT_EQ(260U, info->w) << "Frame " << pts << " had unexpected width";
+      EXPECT_EQ(284U, info->h) << "Frame " << pts << " had unexpected height";
+    }
+  }
+}
+
 VP8_INSTANTIATE_TEST_CASE(ResizeTest, ONE_PASS_TEST_MODES);
-VP9_INSTANTIATE_TEST_CASE(ResizeInternalTest,
+VP9_INSTANTIATE_TEST_CASE(ResizeByRatioInternalTest,
+                          ::testing::Values(::libvpx_test::kOnePassBest));
+VP9_INSTANTIATE_TEST_CASE(ResizeAbsoluteInternalTest,
                           ::testing::Values(::libvpx_test::kOnePassBest));
 }  // namespace
