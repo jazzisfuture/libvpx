f4fc1e80b3b73a5b792ba1c3188e71039af9c308
Yunqing Wang <yunqingwang@google.com>
2013-09-23 13:45:43 -0700

    Hide global symbols for macho32/64
    Added hiding global symbols for macho32 and macho64 in x86inc.asm.
    This was done to fix exported symbol issue in Chrome build.
    
    Change-Id: I08d5c559b985b82f655b537469fee125615e78c0

--- third_party/x86inc/x86inc.asm
+++ third_party/x86inc/x86inc.asm
@@ -592,12 +592,18 @@ DECLARE_ARG 7, 8, 9, 10, 11, 12, 13, 14
         CAT_XDEFINE cglobaled_, %1, 1
     %endif
     %xdefine current_function %1
+
+    ; Added hiding global symbols for MAC
     %ifidn __OUTPUT_FORMAT__,elf
         global %1:function hidden
     %elifidn __OUTPUT_FORMAT__,elf32
         global %1:function hidden
     %elifidn __OUTPUT_FORMAT__,elf64
         global %1:function hidden
+    %elifidn __OUTPUT_FORMAT__,macho32
+        global %1:private_extern
+    %elifidn __OUTPUT_FORMAT__,macho64
+        global %1:private_extern
     %else
         global %1
     %endif
