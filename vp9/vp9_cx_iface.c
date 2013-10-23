08ae3bfb9052b2b13b5bde51e1478848b78c60e8
Yaowu Xu <yaowu@google.com>
2013-10-23 16:57:25 -0700

    Disable a couple of API calls for now
    The APIs need to be implemented and tested to insure consistent with
    waht VP9 codec can suppoert internally. Disable them for now.
    
    Change-Id: I198124ee318f0883b58d1d36cea3c7ccd742a57e

--- vp9/vp9_cx_iface.c
+++ vp9/vp9_cx_iface.c
@@ -975,8 +975,10 @@ static vpx_codec_err_t vp9e_use_reference(vpx_codec_alg_priv_t *ctx,
 static vpx_codec_err_t vp9e_set_roi_map(vpx_codec_alg_priv_t *ctx,
                                         int ctr_id,
                                         va_list args) {
+  // TODO(yaowu): Need to re-implement and test for VP9.
+  return VPX_CODEC_INVALID_PARAM;
+  /*
   vpx_roi_map_t *data = va_arg(args, vpx_roi_map_t *);
-
   if (data) {
     vpx_roi_map_t *roi = (vpx_roi_map_t *)data;
 
@@ -988,12 +990,16 @@ static vpx_codec_err_t vp9e_set_roi_map(vpx_codec_alg_priv_t *ctx,
   } else {
     return VPX_CODEC_INVALID_PARAM;
   }
+  */
 }
 
 
 static vpx_codec_err_t vp9e_set_activemap(vpx_codec_alg_priv_t *ctx,
                                           int ctr_id,
                                           va_list args) {
+  // TODO(yaowu): Need to re-implement and test for VP9.
+  return VPX_CODEC_INVALID_PARAM;
+  /*
   vpx_active_map_t *data = va_arg(args, vpx_active_map_t *);
 
   if (data) {
@@ -1006,6 +1012,7 @@ static vpx_codec_err_t vp9e_set_activemap(vpx_codec_alg_priv_t *ctx,
   } else {
     return VPX_CODEC_INVALID_PARAM;
   }
+  */
 }
 
 static vpx_codec_err_t vp9e_set_scalemode(vpx_codec_alg_priv_t *ctx,
