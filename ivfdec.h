3fb707823af6b68ecf7d5e0c4ca72d17ebdc7c19
Tom Finegan <tomfinegan@google.com>
2013-11-12 11:07:54 -0800

    vpx[dec|enc]: Extract IVF support from the apps.
    - Move IVF reading support into ivfdec.c and ivfdec.h
    - Move IVF writing support into ivfenc.c and ivfenc.h
    
    Note to drive by reviewers: I am abusing jenkins; this not ready for review!
    
    Change-Id: I70adf6240d0320fdd232d8546ed573f0f68dd793

---
+++ ivfdec.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2013 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef IVFDEC_H_
+#define IVFDEC_H_
+
+#include "./tools_common.h"
+
+unsigned int file_is_ivf(struct VpxInputContext *input);
+
+int ivf_read_frame(struct VpxInputContext *input,
+                   uint8_t **buffer,
+                   size_t *bytes_read,
+                   size_t *buffer_size);
+
+#endif  // IVFDEC_H_
