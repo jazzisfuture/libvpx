b43f130b28f07bbbc7968f936b8816e4de5dacc9
Tom Finegan <tomfinegan@google.com>
2013-11-21 15:45:25 -0800

    vpxenc: Move config check code into its own source files.
    - Add command line args that allow display of warnings without prompting
      for user input.
    - Extend warning code to make it somewhat scalable.
    
    Change-Id: I2bad8f9315f6eed120c2e1bbe0a2a5ede15fbf35

---
+++ warnings.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2013 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef WARNINGS_H_
+#define WARNINGS_H_
+
+struct vpx_codec_enc_cfg;
+
+/*
+ * Checks config for improperly used settings. Warns user upon encountering
+ * settings that will lead to poor output quality. Prompts user to continue
+ * when warnings are issued.
+ */
+void check_encoder_config(int disable_prompt, struct vpx_codec_enc_cfg* config);
+
+#endif  // WARNINGS_H_
