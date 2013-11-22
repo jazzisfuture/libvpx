f2bb70ae15814c6afe610142015be357565cfa4c
Yaowu Xu <yaowu@google.com>
2013-11-21 17:39:33 -0800

    Fix the cpuid macro for x86_64 non-gcc build
    Change-Id: I0c44800db10db8d74c1ddfe89abecfd1c53d0f8d

--- vpx_ports/x86.h
+++ vpx_ports/x86.h
@@ -72,10 +72,10 @@ typedef enum {
 #else /* end __SUNPRO__ */
 #if ARCH_X86_64
 void __cpuid(int CPUInfo[4], int info_type);
-#pragma intrinsic(__cpuid)
+#pragma intrinsic(__cpuidex)
 #define cpuid(func, func2, a, b, c, d) do {\
     int regs[4];\
-    __cpuid(regs, func, func2);
+    __cpuidex(regs, func, func2); \
     a = regs[0];  b = regs[1];  c = regs[2];  d = regs[3];\
   } while(0)
 #else
