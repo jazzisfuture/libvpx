d137b012fd4883d715930798a85bdd58c2189cab
Dmitry Kovalev <dkovalev@google.com>
2013-12-02 16:51:34 -0800

    WIP. decode_coeffs simplification.
    Change-Id: Id9944563be34ef793a895f0dcc9c4be38b313472

--- vp9/decoder/vp9_detokenize.c
+++ vp9/decoder/vp9_detokenize.c
@@ -37,26 +37,35 @@
 #define CAT4_MIN_VAL   19
 #define CAT5_MIN_VAL   35
 #define CAT6_MIN_VAL   67
+
 #define CAT1_PROB0    159
+static const vp9_prob cat1_probs[] = { CAT1_PROB0 };
+
 #define CAT2_PROB0    145
 #define CAT2_PROB1    165
+static const vp9_prob cat2_probs[] = { CAT2_PROB1, CAT2_PROB0 };
 
 #define CAT3_PROB0 140
 #define CAT3_PROB1 148
 #define CAT3_PROB2 173
+static const vp9_prob cat3_probs[] = { CAT3_PROB2, CAT3_PROB1, CAT3_PROB0 };
 
 #define CAT4_PROB0 135
 #define CAT4_PROB1 140
 #define CAT4_PROB2 155
 #define CAT4_PROB3 176
+static const vp9_prob cat4_probs[] = { CAT4_PROB3, CAT4_PROB2, CAT4_PROB1,
+                                       CAT4_PROB0 };
 
 #define CAT5_PROB0 130
 #define CAT5_PROB1 134
 #define CAT5_PROB2 141
 #define CAT5_PROB3 157
 #define CAT5_PROB4 180
+static const vp9_prob cat5_probs[] = { CAT5_PROB4, CAT5_PROB3, CAT5_PROB2,
+                                       CAT5_PROB1, CAT5_PROB0 };
 
-static const vp9_prob cat6_prob[15] = {
+static const vp9_prob cat6_probs[15] = {
   254, 254, 254, 252, 249, 243, 230, 196, 177, 153, 140, 133, 130, 129, 0
 };
 
@@ -73,23 +82,34 @@ static const int token_to_counttoken[MAX_ENTROPY_TOKENS] = {
   } while (0)
 
 
-#define WRITE_COEF_CONTINUE(val, token)                  \
-  {                                                      \
-    v = (val * dqv) >> dq_shift; \
-    dqcoeff_ptr[scan[c]] = (vp9_read_bit(r) ? -v : v); \
+#define WRITE_COEF(val, token)                           \
+  do {                                                   \
+    v = (val * dqv) >> dq_shift;                         \
+    dqcoeff_ptr[scan[c]] = (vp9_read_bit(r) ? -v : v);   \
     INCREMENT_COUNT(token);                              \
     token_cache[scan[c]] = vp9_pt_energy_class[token];   \
     ++c;                                                 \
     pt = get_coef_context(nb, token_cache, c);           \
-    dqv = dq[1];                                          \
-    continue;                                            \
-  }
-
-
-#define ADJUST_COEF(prob, bits_count)                   \
-  do {                                                  \
-    val += (vp9_read(r, prob) << bits_count);           \
+    dqv = dq[1];                                         \
   } while (0)
+
+static INLINE int read_coeff(const vp9_prob *probs, int n, vp9_reader *r) {
+  int i, val = 0;
+  for (i = 0; i < n; ++i)
+    val = (val << 1) | vp9_read(r, probs[i]);
+  return val;
+}
+
+const vp9_tree_index tree[TREE_SIZE(MAX_ENTROPY_TOKENS)] = {
+  2, 6,                                     /* 0 = LOW_VAL */
+  -TWO_TOKEN, 4,                            /* 1 = TWO */
+  -THREE_TOKEN, -FOUR_TOKEN,                /* 2 = THREE */
+  8, 10,                                    /* 3 = HIGH_LOW */
+  -DCT_VAL_CATEGORY1, -DCT_VAL_CATEGORY2,   /* 4 = CAT_ONE */
+  12, 14,                                   /* 5 = CAT_THREEFOUR */
+  -DCT_VAL_CATEGORY3, -DCT_VAL_CATEGORY4,   /* 6 = CAT_THREE */
+  -DCT_VAL_CATEGORY5, -DCT_VAL_CATEGORY6    /* 7 = CAT_FIVE */
+};
 
 static int decode_coefs(VP9_COMMON *cm, const MACROBLOCKD *xd,
                         vp9_reader *r, int block_idx,
@@ -107,13 +127,12 @@ static int decode_coefs(VP9_COMMON *cm, const MACROBLOCKD *xd,
       counts->coef[tx_size][type][ref];
   unsigned int (*eob_branch_count)[PREV_COEF_CONTEXTS] =
       counts->eob_branch[tx_size][type][ref];
-  const uint8_t *cat6;
   const uint8_t *band_translate = get_band_translate(tx_size);
   const int dq_shift = (tx_size == TX_32X32);
   const scan_order *so = get_scan(xd, tx_size, type, block_idx);
   const int16_t *scan = so->scan;
   const int16_t *nb = so->neighbors;
-  int v;
+  int v, token;
   int16_t dqv = dq[0];
 
   while (c < max_eob) {
@@ -139,68 +158,43 @@ static int decode_coefs(VP9_COMMON *cm, const MACROBLOCKD *xd,
       prob = coef_probs[band][pt];
     }
 
-    // ONE_CONTEXT_NODE_0_
     if (!vp9_read(r, prob[ONE_CONTEXT_NODE])) {
-      WRITE_COEF_CONTINUE(1, ONE_TOKEN);
+      WRITE_COEF(1, ONE_TOKEN);
+      continue;
     }
 
-    prob = vp9_pareto8_full[prob[PIVOT_NODE] - 1];
-
-    // LOW_VAL_CONTEXT_NODE_0_
-    if (!vp9_read(r, prob[LOW_VAL_CONTEXT_NODE])) {
-      if (!vp9_read(r, prob[TWO_CONTEXT_NODE])) {
-        WRITE_COEF_CONTINUE(2, TWO_TOKEN);
-      }
-      if (!vp9_read(r, prob[THREE_CONTEXT_NODE])) {
-        WRITE_COEF_CONTINUE(3, THREE_TOKEN);
-      }
-      WRITE_COEF_CONTINUE(4, FOUR_TOKEN);
+    token = vp9_read_tree(r, tree, vp9_pareto8_full[prob[PIVOT_NODE] - 1]);
+    switch (token) {
+      case TWO_TOKEN:
+      case THREE_TOKEN:
+      case FOUR_TOKEN:
+        WRITE_COEF(token, token);
+        break;
+      case DCT_VAL_CATEGORY1:
+        val = CAT1_MIN_VAL + read_coeff(cat1_probs, 1, r);
+        WRITE_COEF(val, DCT_VAL_CATEGORY1);
+        break;
+      case DCT_VAL_CATEGORY2:
+        val = CAT2_MIN_VAL + read_coeff(cat2_probs, 2, r);
+        WRITE_COEF(val, DCT_VAL_CATEGORY2);
+        break;
+      case DCT_VAL_CATEGORY3:
+        val = CAT3_MIN_VAL + read_coeff(cat3_probs, 3, r);
+        WRITE_COEF(val, DCT_VAL_CATEGORY3);
+        break;
+      case DCT_VAL_CATEGORY4:
+        val = CAT4_MIN_VAL + read_coeff(cat4_probs, 4, r);
+        WRITE_COEF(val, DCT_VAL_CATEGORY4);
+        break;
+      case DCT_VAL_CATEGORY5:
+        val = CAT5_MIN_VAL + read_coeff(cat5_probs, 5, r);
+        WRITE_COEF(val, DCT_VAL_CATEGORY5);
+        break;
+      case DCT_VAL_CATEGORY6:
+        val = CAT6_MIN_VAL + read_coeff(cat6_probs, 14, r);
+        WRITE_COEF(val, DCT_VAL_CATEGORY6);
+        break;
     }
-    // HIGH_LOW_CONTEXT_NODE_0_
-    if (!vp9_read(r, prob[HIGH_LOW_CONTEXT_NODE])) {
-      if (!vp9_read(r, prob[CAT_ONE_CONTEXT_NODE])) {
-        val = CAT1_MIN_VAL;
-        ADJUST_COEF(CAT1_PROB0, 0);
-        WRITE_COEF_CONTINUE(val, DCT_VAL_CATEGORY1);
-      }
-      val = CAT2_MIN_VAL;
-      ADJUST_COEF(CAT2_PROB1, 1);
-      ADJUST_COEF(CAT2_PROB0, 0);
-      WRITE_COEF_CONTINUE(val, DCT_VAL_CATEGORY2);
-    }
-    // CAT_THREEFOUR_CONTEXT_NODE_0_
-    if (!vp9_read(r, prob[CAT_THREEFOUR_CONTEXT_NODE])) {
-      if (!vp9_read(r, prob[CAT_THREE_CONTEXT_NODE])) {
-        val = CAT3_MIN_VAL;
-        ADJUST_COEF(CAT3_PROB2, 2);
-        ADJUST_COEF(CAT3_PROB1, 1);
-        ADJUST_COEF(CAT3_PROB0, 0);
-        WRITE_COEF_CONTINUE(val, DCT_VAL_CATEGORY3);
-      }
-      val = CAT4_MIN_VAL;
-      ADJUST_COEF(CAT4_PROB3, 3);
-      ADJUST_COEF(CAT4_PROB2, 2);
-      ADJUST_COEF(CAT4_PROB1, 1);
-      ADJUST_COEF(CAT4_PROB0, 0);
-      WRITE_COEF_CONTINUE(val, DCT_VAL_CATEGORY4);
-    }
-    // CAT_FIVE_CONTEXT_NODE_0_:
-    if (!vp9_read(r, prob[CAT_FIVE_CONTEXT_NODE])) {
-      val = CAT5_MIN_VAL;
-      ADJUST_COEF(CAT5_PROB4, 4);
-      ADJUST_COEF(CAT5_PROB3, 3);
-      ADJUST_COEF(CAT5_PROB2, 2);
-      ADJUST_COEF(CAT5_PROB1, 1);
-      ADJUST_COEF(CAT5_PROB0, 0);
-      WRITE_COEF_CONTINUE(val, DCT_VAL_CATEGORY5);
-    }
-    val = 0;
-    cat6 = cat6_prob;
-    while (*cat6)
-      val = (val << 1) | vp9_read(r, *cat6++);
-    val += CAT6_MIN_VAL;
-
-    WRITE_COEF_CONTINUE(val, DCT_VAL_CATEGORY6);
   }
 
   return c;
