f2b25925ba8f7a11efc7679b07a69b1b35655c20
Jim Bankoski <jimbankoski@google.com>
2013-12-17 16:42:17 -0800

    rename loop filter functions
    This renames all the loop filter functions so that they no
    longer refer to mb
    
    Change-Id: I8a58a8c7fd253d835cb619bde13913e896ece90b

--- vp9/common/x86/vp9_loopfilter_intrin_sse2.c
+++ vp9/common/x86/vp9_loopfilter_intrin_sse2.c
@@ -846,7 +846,7 @@ static void mb_lpf_horizontal_edge_w_sse2_16(unsigned char *s,
 }
 
 // TODO(yunqingwang): remove count and call these 2 functions(8 or 16) directly.
-void vp9_mb_lpf_horizontal_edge_w_sse2(unsigned char *s,
+void vp9_lpf_horizontal_16_sse2(unsigned char *s,
                                        int p,
                                        const unsigned char *_blimit,
                                        const unsigned char *_limit,
@@ -858,7 +858,7 @@ void vp9_mb_lpf_horizontal_edge_w_sse2(unsigned char *s,
     mb_lpf_horizontal_edge_w_sse2_16(s, p, _blimit, _limit, _thresh);
 }
 
-void vp9_mbloop_filter_horizontal_edge_sse2(unsigned char *s,
+void vp9_lpf_horizontal_8_sse2(unsigned char *s,
                                             int p,
                                             const unsigned char *_blimit,
                                             const unsigned char *_limit,
@@ -1083,7 +1083,7 @@ void vp9_mbloop_filter_horizontal_edge_sse2(unsigned char *s,
   }
 }
 
-void vp9_mbloop_filter_horizontal_edge_16_sse2(uint8_t *s, int p,
+void vp9_lpf_horizontal_8_dual_sse2(uint8_t *s, int p,
                                                const uint8_t *_blimit0,
                                                const uint8_t *_limit0,
                                                const uint8_t *_thresh0,
@@ -1330,7 +1330,7 @@ void vp9_mbloop_filter_horizontal_edge_16_sse2(uint8_t *s, int p,
   }
 }
 
-void vp9_loop_filter_horizontal_edge_16_sse2(unsigned char *s,
+void vp9_lpf_horizontal_4_dual_sse2(unsigned char *s,
                                             int p,
                                             const unsigned char *_blimit0,
                                             const unsigned char *_limit0,
@@ -1598,7 +1598,7 @@ static INLINE void transpose(unsigned char *src[], int in_p,
   } while (++idx8x8 < num_8x8_to_transpose);
 }
 
-void vp9_loop_filter_vertical_edge_16_sse2(uint8_t *s, int p,
+void vp9_lpf_vertical_4_dual_sse2(uint8_t *s, int p,
                                            const uint8_t *blimit0,
                                            const uint8_t *limit0,
                                            const uint8_t *thresh0,
@@ -1613,7 +1613,7 @@ void vp9_loop_filter_vertical_edge_16_sse2(uint8_t *s, int p,
   transpose8x16(s - 4, s - 4 + p * 8, p, t_dst, 16);
 
   // Loop filtering
-  vp9_loop_filter_horizontal_edge_16_sse2(t_dst + 4 * 16, 16, blimit0, limit0,
+  vp9_lpf_horizontal_4_dual_sse2(t_dst + 4 * 16, 16, blimit0, limit0,
                                           thresh0, blimit1, limit1, thresh1);
   src[0] = t_dst;
   src[1] = t_dst + 8;
@@ -1624,7 +1624,7 @@ void vp9_loop_filter_vertical_edge_16_sse2(uint8_t *s, int p,
   transpose(src, 16, dst, p, 2);
 }
 
-void vp9_mbloop_filter_vertical_edge_sse2(unsigned char *s, int p,
+void vp9_lpf_vertical_8_sse2(unsigned char *s, int p,
                                           const unsigned char *blimit,
                                           const unsigned char *limit,
                                           const unsigned char *thresh,
@@ -1641,7 +1641,7 @@ void vp9_mbloop_filter_vertical_edge_sse2(unsigned char *s, int p,
   transpose(src, p, dst, 8, 1);
 
   // Loop filtering
-  vp9_mbloop_filter_horizontal_edge_sse2(t_dst + 4 * 8, 8, blimit, limit,
+  vp9_lpf_horizontal_8_sse2(t_dst + 4 * 8, 8, blimit, limit,
                                          thresh, 1);
 
   src[0] = t_dst;
@@ -1651,7 +1651,7 @@ void vp9_mbloop_filter_vertical_edge_sse2(unsigned char *s, int p,
   transpose(src, 8, dst, p, 1);
 }
 
-void vp9_mbloop_filter_vertical_edge_16_sse2(uint8_t *s, int p,
+void vp9_lpf_vertical_8_dual_sse2(uint8_t *s, int p,
                                              const uint8_t *blimit0,
                                              const uint8_t *limit0,
                                              const uint8_t *thresh0,
@@ -1666,7 +1666,7 @@ void vp9_mbloop_filter_vertical_edge_16_sse2(uint8_t *s, int p,
   transpose8x16(s - 4, s - 4 + p * 8, p, t_dst, 16);
 
   // Loop filtering
-  vp9_mbloop_filter_horizontal_edge_16_sse2(t_dst + 4 * 16, 16, blimit0, limit0,
+  vp9_lpf_horizontal_8_dual_sse2(t_dst + 4 * 16, 16, blimit0, limit0,
                                             thresh0, blimit1, limit1, thresh1);
   src[0] = t_dst;
   src[1] = t_dst + 8;
@@ -1678,7 +1678,7 @@ void vp9_mbloop_filter_vertical_edge_16_sse2(uint8_t *s, int p,
   transpose(src, 16, dst, p, 2);
 }
 
-void vp9_mb_lpf_vertical_edge_w_sse2(unsigned char *s, int p,
+void vp9_lpf_vertical_16_sse2(unsigned char *s, int p,
                                      const unsigned char *blimit,
                                      const unsigned char *limit,
                                      const unsigned char *thresh) {
@@ -1706,7 +1706,7 @@ void vp9_mb_lpf_vertical_edge_w_sse2(unsigned char *s, int p,
   transpose(src, 8, dst, p, 2);
 }
 
-void vp9_mb_lpf_vertical_edge_w_16_sse2(unsigned char *s, int p,
+void vp9_lpf_vertical_16_dual_sse2(unsigned char *s, int p,
                                         const uint8_t *blimit,
                                         const uint8_t *limit,
                                         const uint8_t *thresh) {
