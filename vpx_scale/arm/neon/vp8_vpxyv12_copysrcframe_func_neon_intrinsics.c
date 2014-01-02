f83a926932c6645237181a4e5e12a8de79766dfa
James Yu <james.yu@linaro.org>
2014-01-02 11:29:32 +0800

    VPX scale for ARMv8 by using NEON intrinsics. Remove file
    Remove unused (no effect) asm file for test_libvpx and test video.
    I left it in empty C function. I will rewrite it if needed.
    
    Change-Id: Ia2c8d09177fbda6d2fc40b86b635a3422bfffb6d
    Signed-off-by: James Yu <james.yu@linaro.org>

---
+++ vpx_scale/arm/neon/vp8_vpxyv12_copysrcframe_func_neon_intrinsics.c
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+/*
+ * ARMv8 support by rewrite NEON assembly to NEON intrinsics.
+ * Enable with -mfpu=neon compile option.
+ *
+ * This file includes below assembly code.
+ * vp8_yv12_copy_src_frame_func_neon.asm
+ *
+ * <james.yu at linaro.org>
+ * SEEMS UNUSED. I didn't rewrite it to useing NEON intrinsics.
+ * I will rewrite it if needed.
+ */
+
+#include <arm_neon.h>
+#include <stdio.h>
+#include "./vpx_scale_rtcd.h"
+
+void vp8_yv12_copy_src_frame_func_neon(
+        const struct yv12_buffer_config *src_ybc,
+        struct yv12_buffer_config *dst_ybc) {
+    // printf("[%s:%d]\n", __func__, __LINE__);
+    return;
+}
