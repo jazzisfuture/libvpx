795813a6d14c23ec8e0f78e8e47ebe15c3320f8b
James Yu <james.yu@linaro.org>
2014-01-05 19:18:03 +0800

    VP8 for ARMv8 by using NEON intrinsics 18
    Add buildintrapredictorsmby_neon.c
    - vp8_build_intra_predictors_mby_neon_func, Unused, Can't test
    - vp8_build_intra_predictors_mby_s_neon_func, Unused, Can't test
    
    Left Can't test functions in empty C function. Will rewrite if needed.
    
    Change-Id: Iee109608fc5c90cf49703ffca29f158e2ca64db4
    Signed-off-by: James Yu <james.yu@linaro.org>

---
+++ vp8/common/arm/neon/buildintrapredictorsmby_neon.c
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <arm_neon.h>
+#include <stdio.h>
+
+// Can't test functions
+void vp8_build_intra_predictors_mby_neon_func(
+        unsigned char *y_buffer,
+        unsigned char *ypred_ptr,
+        int y_stride,
+        int mode,
+        int Up,
+        int Left) {
+    // printf("%s:%d\n", __func__, __LINE__);
+    return;
+}
+
+void vp8_build_intra_predictors_mby_s_neon_func(
+        unsigned char *y_buffer,
+        unsigned char *ypred_ptr,
+        int y_stride,
+        int mode,
+        int Up,
+        int Left) {
+    // printf("%s:%d\n", __func__, __LINE__);
+    return;
+}
