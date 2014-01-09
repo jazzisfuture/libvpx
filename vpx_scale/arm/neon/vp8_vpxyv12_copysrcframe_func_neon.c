557a8c8fc65fd6a1ce65809a2e1dd4d9fdab05af
James Yu <james.yu@linaro.org>
2014-01-09 21:58:52 +0800

    VPX scale for ARMv8 by using NEON intrinsics 4
    Add vp8_vpxyv12_copysrcframe_func_neon.c
    - vp8_yv12_copy_src_frame_func_neon, Can't test
    
    I left Can't test functions to empty C function. I will rewrite it if needed.
    
    Change-Id: Iec42972c136b64d3f353318e3aeb6cf670ad2fc1
    Signed-off-by: James Yu <james.yu@linaro.org>

---
+++ vpx_scale/arm/neon/vp8_vpxyv12_copysrcframe_func_neon.c
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <stdio.h>
+#include <arm_neon.h>
+#include "./vpx_scale_rtcd.h"
+
+void vp8_yv12_copy_src_frame_func_neon(
+        const struct yv12_buffer_config *src_ybc,
+        struct yv12_buffer_config *dst_ybc) {
+    // printf("[%s:%d]\n", __func__, __LINE__);
+    return;
+}
