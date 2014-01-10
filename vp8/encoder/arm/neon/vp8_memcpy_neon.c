82e5b1ecf45b7356589ea173f2fd9cd4812a1575
James Yu <james.yu@linaro.org>
2014-01-10 14:42:53 +0800

    VP8 encoder for ARMv8 by using NEON intrinsics 4
    Add vp8_memcpy_neon.c
    - vp8_memcpy_partial_neon, Can't test
    
    I left Can't test functions to empty C function. I will rewrite it if needed.
    
    Change-Id: I0aa66648c5db4565fff325b396a745c8a8c52a48
    Signed-off-by: James Yu <james.yu@linaro.org>

---
+++ vp8/encoder/arm/neon/vp8_memcpy_neon.c
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <arm_neon.h>
+#include <stdio.h>
+
+void vp8_memcpy_partial_neon(
+        unsigned char *dst_ptr,
+        unsigned char *src_ptr) {
+    // printf("%s\n", __func__);
+    return;
+}
