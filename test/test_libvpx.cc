28f7e524f1ef06f97a878c1482b2b9e8c9a508bb
James Zern <jzern@google.com>
2014-01-10 19:08:44 -0800

    test_libvpx: fix negative CPU filter
    negative patterns begin with _one_ '-', not one before each pattern.
    
    https://code.google.com/p/googletest/wiki/V1_7_AdvancedGuide#Running_a_Subset_of_the_Tests
    
    Change-Id: I6fd527b31bc2571dfdaa453cbbb57c77b3409563

--- test/test_libvpx.cc
+++ test/test_libvpx.cc
@@ -36,17 +36,17 @@ int main(int argc, char **argv) {
 #if ARCH_X86 || ARCH_X86_64
   const int simd_caps = x86_simd_caps();
   if (!(simd_caps & HAS_MMX))
-    append_negative_gtest_filter(":-MMX/*");
+    append_negative_gtest_filter(":MMX/*");
   if (!(simd_caps & HAS_SSE))
-    append_negative_gtest_filter(":-SSE/*");
+    append_negative_gtest_filter(":SSE/*");
   if (!(simd_caps & HAS_SSE2))
-    append_negative_gtest_filter(":-SSE2/*");
+    append_negative_gtest_filter(":SSE2/*");
   if (!(simd_caps & HAS_SSE3))
-    append_negative_gtest_filter(":-SSE3/*");
+    append_negative_gtest_filter(":SSE3/*");
   if (!(simd_caps & HAS_SSSE3))
-    append_negative_gtest_filter(":-SSSE3/*");
+    append_negative_gtest_filter(":SSSE3/*");
   if (!(simd_caps & HAS_SSE4_1))
-    append_negative_gtest_filter(":-SSE4_1/*");
+    append_negative_gtest_filter(":SSE4_1/*");
   if (!(simd_caps & HAS_AVX))
     append_negative_gtest_filter(":AVX/*");
   if (!(simd_caps & HAS_AVX2))
