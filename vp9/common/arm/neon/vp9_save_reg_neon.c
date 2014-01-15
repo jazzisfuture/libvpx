5a9dd1e4856b05725aa7798a3c84bc8536d35af9
James Yu <james.yu@linaro.org>
2014-01-15 16:11:21 +0800

    VP9 common for ARMv8 by using NEON intrinsics 20
    Add vp9_save_reg_neon.c
    - vp9_push_neon, unused
    - vp9_pop_neon, unused
    
    Useless for NEON intrinsics version.
    
    Change-Id: I64b187eb50106607dc06a6c1b45bdaf44960e5ae
    Signed-off-by: James Yu <james.yu@linaro.org>

---
+++ vp9/common/arm/neon/vp9_save_reg_neon.c
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <arm_neon.h>
+#include <stdio.h>
+
+// Unused functions
+void vp9_push_neon(int64_t *store) {
+    // printf("[%s:%d]\n", __func__, __LINE__);
+    return;
+}
+
+void vp9_pop_neon(int64_t *store) {
+    // printf("[%s:%d]\n", __func__, __LINE__);
+    return;
+}
