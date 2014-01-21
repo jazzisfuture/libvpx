2ce6486ad24db00abac538ccc031ab03575c0ca0
James Yu <james.yu@linaro.org>
2014-01-21 09:43:29 +0800

    VP9 common for ARMv8 by using NEON intrinsics 01
    Add vp9_loopfilter_neon.c
    - vp9_lpf_horizontal_4_neon
    - vp9_lpf_vertical_4_neon
    - vp9_lpf_horizontal_8_neon
    - vp9_lpf_vertical_8_neon
    
    With all VP9 intrinsics patches have arround 7% performance loss.
    
    Change-Id: I97a0d7b399a431c21ee77396be3d5f5a1f7ebccb
    Signed-off-by: James Yu <james.yu@linaro.org>

--- vp9/vp9_common.mk
+++ vp9/vp9_common.mk
@@ -127,7 +127,6 @@ VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_idct16x16_neon.c
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_loopfilter_16_neon.c
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_convolve8_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_convolve8_avg_neon$(ASM)
-VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_loopfilter_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_loopfilter_16_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_dc_only_idct_add_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_idct4x4_1_add_neon$(ASM)
@@ -145,5 +144,7 @@ VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_copy_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_avg_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_save_reg_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_reconintra_neon$(ASM)
+
+VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_loopfilter_neon.c
 
 $(eval $(call rtcd_h_template,vp9_rtcd,vp9/common/vp9_rtcd_defs.sh))
