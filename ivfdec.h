62b24563d23c0dc328158abb1d84673044c39e3f
Dmitry Kovalev <dkovalev@google.com>
2014-01-22 16:31:20 -0800

    Implementing simple API to read video files.
    New API is supposed to be used from example code. Current implementation
    only supports IVF containers (will be extended to Y4M).
    
    Change-Id: Ib7da87237690b1a28297bdf03bc41c6836a84b7e

--- ivfdec.h
+++ ivfdec.h
@@ -21,6 +21,20 @@ int file_is_ivf(struct VpxInputContext *input);
 int ivf_read_frame(FILE *infile, uint8_t **buffer,
                    size_t *bytes_read, size_t *buffer_size);
 
+struct vpx_video;
+typedef struct vpx_video vpx_video_t;
+
+vpx_video_t *vpx_video_open_file(FILE *file);
+void vpx_video_close(vpx_video_t *video);
+
+int vpx_video_get_width(vpx_video_t *video);
+int vpx_video_get_height(vpx_video_t *video);
+unsigned int vpx_video_get_fourcc(vpx_video_t *video);
+
+int vpx_video_read_frame(vpx_video_t *video);
+const unsigned char *vpx_video_get_frame(vpx_video_t *video, size_t *size);
+
+
 #ifdef __cplusplus
 }  /* extern "C" */
 #endif
