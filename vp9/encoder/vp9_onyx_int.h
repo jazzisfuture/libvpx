381ff73e7a20df3df1e328081db7d34c7012c8c6
Yunqing Wang <yunqingwang@google.com>
2014-02-04 11:09:34 -0800

    Enable encode_breakout in real time encoding
    In real time encoding, we enable encode_breakout to make encoding
    fast. A speed feature "use_encode_breakout" is defined to set
    encode_breakout thresholds for different speeds.
    
    However, currently, static_thresh is an encoder option. The encode_
    breakout can be turned off if user sets static_thresh=0 specifically.
    
    The rtc set borg test result: (need to set --static_thresh=1)
    speed -5, psnr loss -3.543%;
    speed -4, psnr loss -2.358%;
    speed -3, psnr loss -0.771%.
    
    Encoding speed test:
    speed -5, 11% - 60% speedup;
    speed -4, 5.5% - 28% speedup;
    speed -3, 0.8% - 7% speedup.
    
    Change-Id: Icde592ffbe77eac7446f872a2e9eb2051733677b

--- vp9/encoder/vp9_onyx_int.h
+++ vp9/encoder/vp9_onyx_int.h
@@ -419,6 +419,10 @@ typedef struct {
 
   // This flag control the use of the new super fast rtc mode
   int super_fast_rtc;
+
+  // This variable sets the encode_breakout threshold. Currently, it is only
+  // enabled in real time mode.
+  int use_encode_breakout;
 } SPEED_FEATURES;
 
 typedef struct VP9_COMP {
@@ -567,6 +571,12 @@ typedef struct VP9_COMP {
   unsigned int max_mv_magnitude;
   int mv_step_param;
 
+  int allow_encode_breakout;   // Default value is 1. From first pass stats,
+                               // encode_breakout may be disabled.
+  int encode_breakout;         // Get threshold from external input. In real
+                               // time mode, it can be overwritten according to
+                               // encoding speed.
+
   unsigned char *segmentation_map;
 
   // segment threashold for encode breakout
@@ -685,8 +695,6 @@ typedef struct VP9_COMP {
   int initial_height;
 
   int number_spatial_layers;
-  int enable_encode_breakout;   // Default value is 1. From first pass stats,
-                                // encode_breakout may be disabled.
 
 #if CONFIG_MULTIPLE_ARF
   // ARF tracking variables.
