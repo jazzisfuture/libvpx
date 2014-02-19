2f4ebc10c67a0b937cddb4e4be21f37e46530be7
Tom Finegan <tomfinegan@google.com>
2014-02-19 12:28:07 -0800

    video_source.h: Minor clean up.
    - Use size_t for frame_size() (fixes warnings elsewhere)
    - Remove unnecessary use of const.
    
    Change-Id: Id2cba182ac3fd2f67d25fd1db3a0bc3e1eda040b

--- test/ivf_video_source.h
+++ test/ivf_video_source.h
@@ -94,8 +94,8 @@ class IVFVideoSource : public CompressedVideoSource {
   virtual const uint8_t *cxdata() const {
     return end_of_file_ ? NULL : compressed_frame_buf_;
   }
-  virtual const unsigned int frame_size() const { return frame_sz_; }
-  virtual const unsigned int frame_number() const { return frame_; }
+  virtual size_t frame_size() const { return frame_sz_; }
+  virtual unsigned int frame_number() const { return frame_; }
 
  protected:
   std::string file_name_;
