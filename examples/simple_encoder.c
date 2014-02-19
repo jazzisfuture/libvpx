f914c3916d2029eecd5b299d5400127b31bb61a3
Tom Finegan <tomfinegan@google.com>
2014-02-18 23:14:34 -0800

    simple_encoder: Use encode_vpx_frame().
    Remove one (of many) copy of the libvpx frame encoding boilerplate code.
    
    Change-Id: Ib7a3ec47cab027fd93f8cef296917dcfac80ce8b

--- examples/simple_encoder.c
+++ examples/simple_encoder.c
@@ -100,30 +100,20 @@ void usage_exit() {
 }
 
 static void encode_frame(vpx_codec_ctx_t *codec,
-                         vpx_image_t *img,
+                         const vpx_image_t *img,
                          int frame_index,
                          VpxVideoWriter *writer) {
-  vpx_codec_iter_t iter = NULL;
-  const vpx_codec_cx_pkt_t *pkt = NULL;
-  const vpx_codec_err_t res = vpx_codec_encode(codec, img, frame_index, 1, 0,
-                                               VPX_DL_GOOD_QUALITY);
-  if (res != VPX_CODEC_OK)
-    die_codec(codec, "Failed to encode frame");
-
-  while ((pkt = vpx_codec_get_cx_data(codec, &iter)) != NULL) {
-    if (pkt->kind == VPX_CODEC_CX_FRAME_PKT) {
-      const int keyframe = (pkt->data.frame.flags & VPX_FRAME_IS_KEY) != 0;
-      if (!vpx_video_writer_write_frame(writer,
-                                        pkt->data.frame.buf,
-                                        pkt->data.frame.sz,
-                                        pkt->data.frame.pts)) {
-        die_codec(codec, "Failed to write compressed frame");
-      }
-
-      printf(keyframe ? "K" : ".");
-      fflush(stdout);
-    }
+  vpx_codec_cx_pkt_t packet = {0};
+  encode_vpx_frame(codec, img, frame_index, &packet);
+  const int keyframe = (packet.data.frame.flags & VPX_FRAME_IS_KEY) != 0;
+  if (!vpx_video_writer_write_frame(writer,
+                                    packet.data.frame.buf,
+                                    packet.data.frame.sz,
+                                    packet.data.frame.pts)) {
+    die_codec(codec, "Failed to write compressed frame");
   }
+  printf(keyframe ? "K" : ".");
+  fflush(stdout);
 }
 
 int main(int argc, char **argv) {
