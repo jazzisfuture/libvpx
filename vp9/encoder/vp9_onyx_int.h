d0f79df3e5c23296bc1fc13eb45c8cb80250d67a
Marco Paniconi <marpan@google.com>
2014-03-04 16:49:37 -0800

    In-frame q adjustment for cyclic background refresh.
    Change-Id: Ied628b9e7bd0e88b0c75790276bca75b19eb5c07

--- vp9/encoder/vp9_onyx_int.h
+++ vp9/encoder/vp9_onyx_int.h
@@ -463,6 +463,26 @@ typedef enum {
   USAGE_CONSTANT_QUALITY      = 0x3,
 } END_USAGE;
 
+typedef struct {
+  // Percentage of blocks (in 8x8 unit) per frame that are cyclicly refreshed.
+  int max_mbs_perframe;
+  // Maximum q-delta as percentage of base q.
+  int max_qdelta_perc;
+  // Block size below which we don't apply cyclic refresh.
+  BLOCK_SIZE min_block_size;
+  // Macroblock starting index for cycling through the frame.
+  int mb_index;
+  // Control for how long a block will need to wait to be refreshed again.
+  int time_for_refresh;
+  // Actual number of blocks that have been applied delta-q (segment 1).
+  int num_seg_blocks;
+  // Actual encoding bits for segment 1.
+  int actual_seg_bits;
+  // RD mult. parameters for segment 1.
+  int rdmult;
+  // Cyclic refresh map.
+  signed char *map;
+} CYCLIC_REFRESH;
 
 typedef enum {
   MODE_GOODQUALITY    = 0x1,
@@ -483,6 +503,7 @@ typedef enum {
   NO_AQ = 0,
   VARIANCE_AQ = 1,
   COMPLEXITY_AQ = 2,
+  CYCLIC_REFRESH_AQ = 3,
   AQ_MODES_COUNT  // This should always be the last member of the enum
 } AQ_MODES;
 
@@ -753,6 +774,8 @@ typedef struct VP9_COMP {
 
   unsigned char *active_map;
   unsigned int active_map_enabled;
+
+  CYCLIC_REFRESH cyclic_refresh;
 
   fractional_mv_step_fp *find_fractional_mv_step;
   fractional_mv_step_comp_fp *find_fractional_mv_step_comp;
