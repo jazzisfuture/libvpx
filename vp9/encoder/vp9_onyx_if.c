abdd71c5288d0fa18a6ce970a2ec7f59fadf59a0
Yaowu Xu <yaowu@google.com>
2014-03-07 08:47:09 -0800

    Increase the redo frequency to 4
    Change-Id: Ic7b04d45b6dc15c461e0edbbb4e78aec20348291

--- vp9/encoder/vp9_onyx_if.c
+++ vp9/encoder/vp9_onyx_if.c
@@ -841,9 +841,8 @@ static void set_rt_speed_feature(VP9_COMMON *cm,
   }
   if (speed >= 5) {
     int i;
-    sf->mode_search_skip_flags |= FLAG_SKIP_COMP_REFMISMATCH |
-                                  FLAG_EARLY_TERMINATE;
     sf->use_fast_coef_costing = 0;
+    sf->last_partitioning_redo_frequency = 4;
     sf->adaptive_rd_thresh = 5;
     sf->auto_min_max_partition_size = frame_is_intra_only(cm) ?
         RELAXED_NEIGHBORING_MIN_MAX : STRICT_NEIGHBORING_MIN_MAX;
