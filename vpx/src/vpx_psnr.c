85b2d2ebcfed4f1a4c426b93fcd3dc1d566d5a29
Dmitry Kovalev <dkovalev@google.com>
2014-03-07 16:33:01 -0800

    Changing vpx_sse_to_psnr() signature.
    Change-Id: I8b7de2e9caf808db95e4d494f1c08e5801123331

--- vpx/src/vpx_psnr.c
+++ vpx/src/vpx_psnr.c
@@ -14,9 +14,9 @@
 
 #define MAX_PSNR 100.0
 
-double vpx_sse_to_psnr(double samples, double peak, double sse) {
-  if (sse > 0.0) {
-    const double psnr = 10.0 * log10(samples * peak * peak / sse);
+double vpx_sse_to_psnr(int64_t sse, int64_t samples, int peak) {
+  if (sse > 0) {
+    const double psnr = 10.0 * log10((double)samples * peak * peak / sse);
     return psnr > MAX_PSNR ? MAX_PSNR : psnr;
   } else {
     return MAX_PSNR;
