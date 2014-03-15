b86d73dfcf1721c92c2ab74b4175d84a793c6dba
Dmitry Kovalev <dkovalev@google.com>
2014-03-14 18:26:00 -0700

    Fixing warnings/errors from c++ compiler.
    Change-Id: Ia561dda53f2dd10e3a10a2df2adb8027ab19397a

--- vp9/common/vp9_debugmodes.c
+++ vp9/common/vp9_debugmodes.c
@@ -22,7 +22,7 @@ static void log_frame_info(VP9_COMMON *cm, const char *str, FILE *f) {
  * and uses the passed in member offset to print out the value of an integer
  * for each mbmi member value in the mi structure.
  */
-static void print_mi_data(VP9_COMMON *cm, FILE *file, char *descriptor,
+static void print_mi_data(VP9_COMMON *cm, FILE *file, const char *descriptor,
                           size_t member_offset) {
   int mi_row;
   int mi_col;
