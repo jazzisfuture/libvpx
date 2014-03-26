9f53d3d4dd79b669e60948129a0ada924a6fc6dd
Marco Paniconi <marpan@google.com>
2014-03-25 17:07:11 -0700

    Cyclic refresh: factor segment delta-q into rate control.
    Change-Id: Iad5e72a4f491e45699a5e3dda98113c5bc32e1f8

--- vp9/encoder/vp9_aq_cyclicrefresh.c
+++ vp9/encoder/vp9_aq_cyclicrefresh.c
@@ -19,7 +19,6 @@
 #include "vp9/encoder/vp9_rdopt.h"
 #include "vp9/encoder/vp9_segmentation.h"
 
-
 // Check if we should turn off cyclic refresh based on bitrate condition.
 static int apply_cyclic_refresh_bitrate(const VP9_COMMON *cm,
                                         const RATE_CONTROL *rc) {
@@ -68,6 +67,62 @@ static int candidate_refresh_aq(const CYCLIC_REFRESH *cr,
     else
       return 1;
   }
+}
+
+int vp9_estimate_bits_at_q_cyclicrefresh(const VP9_COMP *cpi, int q,
+                                         double correction_factor) {
+  const VP9_COMMON *const cm = &cpi->common;
+  const CYCLIC_REFRESH *const cr = &cpi->cyclic_refresh;
+  int mbs = cm->MBs;
+  int estimated_bits;
+
+  // Weight for segment 1 in previous encoded frame.
+  float weight_segment =
+      (float)cr->num_seg_blocks / mbs;
+
+  // Compute delta-q that was used in previous encoded frame.
+  int deltaq = vp9_compute_qdelta_by_rate(cpi, q, cr->rate_ratio_qdelta);
+  if ((-deltaq) > cr->max_qdelta_perc * q / 100) {
+    deltaq = -cr->max_qdelta_perc * q / 100;
+  }
+
+  estimated_bits = (int)((1.0 - weight_segment) *
+      vp9_estimate_bits_at_q(cm->frame_type, q, mbs, correction_factor) +
+      weight_segment *
+      vp9_estimate_bits_at_q(cm->frame_type, q + deltaq, mbs,
+                             correction_factor));
+  return estimated_bits;
+}
+
+int vp9_rc_bits_per_mb_cyclicrefresh(const VP9_COMP *cpi, int i,
+                                     double correction_factor) {
+  const VP9_COMMON *const cm = &cpi->common;
+  const CYCLIC_REFRESH *const cr = &cpi->cyclic_refresh;
+  int bits_per_mb_seg0, bits_per_mb_seg1, deltaq, bits_per_mb_at_this_q;
+
+  // Prior to encode, estimate weight for segment 1 in next frame: take the
+  // average of actual number/percent in previous frame and target for next
+  // frame.
+  float weight_segment =
+      0.5 * ((float)cr->num_seg_blocks / (cm->mi_rows * cm->mi_cols)) +
+      0.5 * ((float)cr->max_mbs_perframe / 100.0);
+
+  bits_per_mb_seg0 =
+      vp9_rc_bits_per_mb(cm->frame_type, i, correction_factor);
+
+  // Compute delta-q corresponding to qindex i.
+  deltaq = vp9_compute_qdelta_by_rate(cpi, i, cr->rate_ratio_qdelta);
+  if (-deltaq > cr->max_qdelta_perc * i / 100) {
+    deltaq = -cr->max_qdelta_perc * i / 100;
+  }
+
+  bits_per_mb_seg1 =
+      vp9_rc_bits_per_mb(cm->frame_type, i + deltaq, correction_factor);
+
+  bits_per_mb_at_this_q = (int)((1.0 - weight_segment) * bits_per_mb_seg0 +
+      weight_segment * bits_per_mb_seg1);
+
+  return bits_per_mb_at_this_q;
 }
 
 // Prior to coding a given prediction block, of size bsize at (mi_row, mi_col),
@@ -149,11 +204,10 @@ void vp9_setup_cyclic_refresh_aq(VP9_COMP *const cpi) {
     int i, x, y, block_count;
     int mi_row, mi_col, qindex2;
 
-    // Rate target ratio to set q delta.
-    const float rate_ratio_qdelta = 2.0;
     vp9_clear_system_state();
     // Some of these parameters may be set via codec-control function later.
     cr->max_mbs_perframe = 10;
+    cr->rate_ratio_qdelta = 2.0;
     cr->max_qdelta_perc = 50;
     cr->min_block_size = BLOCK_16X16;
     cr->time_for_refresh = 1;
@@ -194,11 +248,10 @@ void vp9_setup_cyclic_refresh_aq(VP9_COMP *const cpi) {
     // Set the q delta for segment 1.
     qindex_delta = vp9_compute_qdelta_by_rate(cpi,
                                               cm->base_qindex,
-                                              rate_ratio_qdelta);
-    // TODO(marpan): Incorporate the actual-vs-target rate over/undershoot from
-    // previous encoded frame.
-    if (-qindex_delta > cr->max_qdelta_perc * cm->base_qindex / 100)
+                                              cr->rate_ratio_qdelta);
+    if ((-qindex_delta) > cr->max_qdelta_perc * cm->base_qindex / 100) {
       qindex_delta = -cr->max_qdelta_perc * cm->base_qindex / 100;
+    }
 
     // Compute rd-mult for segment 1.
     qindex2 = clamp(cm->base_qindex + cm->y_dc_delta_q + qindex_delta, 0, MAXQ);
