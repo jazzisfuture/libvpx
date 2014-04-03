4e513c2044c1f178446d3839710e3a8921c1df79
Vignesh Venkatasubramanian <vigneshv@google.com>
2014-04-03 00:41:14 -0700

    Changing webmdec to use libwebm
    Changing webmdec to use libwebm for WebM file parsing.
    
    Change-Id: I2a57a7b44dbed05eaa04409e1e75e6fc03b30fbc

--- webmdec.h
+++ webmdec.h
@@ -16,18 +16,26 @@
 extern "C" {
 #endif
 
-struct nestegg;
-struct nestegg_packet;
 struct VpxInputContext;
 
 struct WebmInputContext {
-  uint32_t chunk;
-  uint32_t chunks;
-  uint32_t video_track;
-  struct nestegg *nestegg_ctx;
-  struct nestegg_packet *pkt;
+  void *reader;
+  void *segment;
+  uint8_t *buffer;
+  const void *cluster;
+  const void *block_entry;
+  const void *block;
+  int block_frame_index;
+  int video_track_index;
+  int is_first_block;
+  uint64_t timestamp_ns;
 };
 
+// Checks if the input is a WebM file. If so, initializes WebMInputContext so
+// that webm_read_frame can be called to retrieve a video frame.
+// Returns 1 on success and 0 on failure or input is not WebM file.
+// TODO(vigneshv): Refactor this function into two smaller functions specific
+// to their task.
 int file_is_webm(struct WebmInputContext *webm_ctx,
                  struct VpxInputContext *vpx_ctx);
 
@@ -36,9 +44,11 @@ int webm_read_frame(struct WebmInputContext *webm_ctx,
                     size_t *bytes_in_buffer,
                     size_t *buffer_size);
 
+// Guesses the frame rate of the input file based on the container timestamps.
 int webm_guess_framerate(struct WebmInputContext *webm_ctx,
                          struct VpxInputContext *vpx_ctx);
 
+// Resets the WebMInputContext.
 void webm_free(struct WebmInputContext *webm_ctx);
 
 #ifdef __cplusplus
