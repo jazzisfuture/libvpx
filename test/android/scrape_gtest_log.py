bdb97b9131f7c8b5c5425d8c16631e12bc6c25b9
Joshua Litt <joshualitt@google.com>
2014-04-04 16:59:04 -0700

    Small fixes to script based off of cry-infra
    Change-Id: I5a65d6fdb27516aefda0473ac45b05186cf0913c

--- test/android/scrape_gtest_log.py
+++ test/android/scrape_gtest_log.py
@@ -13,16 +13,44 @@ waterfall to gather json results mixed in with gtest logs.  This is
 dubious software engineering.
 """
 
+import getopt
 import json
+import os
 import re
 import sys
 
 
 def main():
+  try:
+    opts, args = \
+        getopt.getopt(sys.argv[1:], \
+                      'o:', ['output-json='])
+  except:
+    print 'scrape_gtest_log.py -o <output_json>'
+    sys.exit(2)
+
+  output_json = ''
+  for opt, arg in opts:
+    if opt in ('-o', '--output-json'):
+      output_json = os.path.join(arg)
+
+  if len(sys.argv) != 3:
+    print "Expects a file to write json to!"
+    exit(1)
+
+  dir = os.path.dirname(output_json)
+  if not os.path.exists(dir):
+    os.makedirs(dir)
+
   blob = sys.stdin.read()
   json_string = '[' + ','.join('{' + x + '}' for x in
                                re.findall(r'{([^}]*.?)}', blob)) + ']'
-  print json.dumps(json.loads(json_string), indent=4, sort_keys=True)
+  print blob
+  
+  output = json.dumps(json.loads(json_string), indent=4, sort_keys=True)
+  print output
+  outfile = open(output_json, 'w')
+  outfile.write(output)
 
 if __name__ == '__main__':
   sys.exit(main())
