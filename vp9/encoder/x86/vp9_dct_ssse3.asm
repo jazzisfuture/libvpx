0a9c36526f0ed9354b6656af2ce56500afd6b5d1
Jingning Han <jingning@google.com>
2014-04-23 09:13:45 -0700

    Enable SSSE3 implementation of 8x8 forward 2D-DCT
    WIP
    
    Change-Id: I0f12435857e9425ed7ce12541344dfa16837f4f4

---
+++ vp9/encoder/x86/vp9_dct_ssse3.asm
@@ -0,0 +1,36 @@
+;
+;  Copyright (c) 2014 The WebM project authors. All Rights Reserved.
+;
+;  Use of this source code is governed by a BSD-style license
+;  that can be found in the LICENSE file in the root of the source
+;  tree. An additional intellectual property rights grant can be found
+;  in the file PATENTS.  All contributing project authors may
+;  be found in the AUTHORS file in the root of the source tree.
+;
+%include "third_party/x86inc/x86inc.asm"
+
+SECTION_RODATA
+pw_11585x2: times 8 dw 23170
+
+SECTION .text
+
+%if ARCH_X86_64
+INIT_XMM ssse3
+cglobal fdct8x8, 3, 5, 13, input, output, stride
+
+  lea                r3, [2 * strideq]
+  lea                r4, [4 * strideq]
+  mova               m0, [inputq]
+  mova               m1, [inputq + r3]
+  lea                inputq, [inputq + r4]
+  mova               m2, [inputq]
+  mova               m3, [inputq + r3]
+  lea                inputq, [inputq + r4]
+  mova               m4, [inputq]
+  mova               m5, [inputq + r3]
+  lea                inputq, [inputq + r4]
+  mova               m6, [inputq]
+  mova               m7, [inputq + r3]
+
+  RET
+%endif
