49f997cc8790a41f4d4ca3b19ad9814cc6220f6e
Jim Bankoski <jimbankoski@google.com>
2014-05-07 17:00:51 -0700

    Circumvents : Possible win64 msvc compile issue?
    This proposed fix seems to circumvent.
    
    https://code.google.com/p/chromium/issues/detail?id=370882
    
    Some quick attempts at duplicating the bug in a very simple console app
    failed.  Apparently with optimize size ( -O1 and -Os ) the compiler
    attempts to convert the pointer to the union struct ( 2 shorts unioned
    with an int), into the actual value for that int and then attempts to
    de reference it.
    
    TODO(jbb) : Try harder to fix this and either file a bug against
    msvc or find a bug that seems likely to be.   Also watch for future
    versions to see if this is a problem.
    
    Change-Id: I03339f631f507cc008759e2fae4321bc10d30b91

--- vp8/common/findnearmv.h
+++ vp8/common/findnearmv.h
@@ -34,6 +34,7 @@ static void mv_bias(int refmb_ref_frame_sign_bias, int refframe, int_mv *mvp,
 
 #define LEFT_TOP_MARGIN (16 << 3)
 #define RIGHT_BOTTOM_MARGIN (16 << 3)
+
 static void vp8_clamp_mv2(int_mv *mv, const MACROBLOCKD *xd)
 {
     if (mv->as_mv.col < (xd->mb_to_left_edge - LEFT_TOP_MARGIN))
@@ -47,18 +48,19 @@ static void vp8_clamp_mv2(int_mv *mv, const MACROBLOCKD *xd)
         mv->as_mv.row = xd->mb_to_bottom_edge + RIGHT_BOTTOM_MARGIN;
 }
 
-static void vp8_clamp_mv(int_mv *mv, int mb_to_left_edge, int mb_to_right_edge,
-                         int mb_to_top_edge, int mb_to_bottom_edge)
+static void vp8_clamp_mv(MV *mv, int mb_to_left_edge, int mb_to_right_edge,
+    int mb_to_top_edge, int mb_to_bottom_edge)
 {
-    mv->as_mv.col = (mv->as_mv.col < mb_to_left_edge) ?
-        mb_to_left_edge : mv->as_mv.col;
-    mv->as_mv.col = (mv->as_mv.col > mb_to_right_edge) ?
-        mb_to_right_edge : mv->as_mv.col;
-    mv->as_mv.row = (mv->as_mv.row < mb_to_top_edge) ?
-        mb_to_top_edge : mv->as_mv.row;
-    mv->as_mv.row = (mv->as_mv.row > mb_to_bottom_edge) ?
-        mb_to_bottom_edge : mv->as_mv.row;
+    mv->col = (mv->col < mb_to_left_edge) ?
+    mb_to_left_edge : mv->col;
+    mv->col = (mv->col > mb_to_right_edge) ?
+    mb_to_right_edge : mv->col;
+    mv->row = (mv->row < mb_to_top_edge) ?
+    mb_to_top_edge : mv->row;
+    mv->row = (mv->row > mb_to_bottom_edge) ?
+    mb_to_bottom_edge : mv->row;
 }
+
 static unsigned int vp8_check_mv_bounds(int_mv *mv, int mb_to_left_edge,
                                 int mb_to_right_edge, int mb_to_top_edge,
                                 int mb_to_bottom_edge)
