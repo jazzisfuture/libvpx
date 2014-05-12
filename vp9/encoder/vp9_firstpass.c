9a64c58b1cec0d17eb0637998883c912b4013c31
Minghai Shang <minghai@google.com>
2014-05-12 11:30:27 -0700

    [spatial svc] Remove some restrictions that are needed to improve the quality
    Change-Id: I76a48b03388a8c5cc74b871deb836cd92263b306

--- vp9/encoder/vp9_firstpass.c
+++ vp9/encoder/vp9_firstpass.c
@@ -2302,18 +2302,21 @@ void vp9_rc_get_second_pass_params(VP9_COMP *cpi) {
     // Define next KF group and assign bits to it.
     this_frame_copy = this_frame;
     find_next_key_frame(cpi, &this_frame_copy);
-    // Don't place key frame in any enhancement layers in spatial svc
-    if (cpi->use_svc && cpi->svc.number_temporal_layers == 1) {
-      lc->is_key_frame = 1;
-      if (cpi->svc.spatial_layer_id > 0) {
-        cm->frame_type = INTER_FRAME;
+  } else {
+    cm->frame_type = INTER_FRAME;
+  }
+
+  if (cpi->use_svc && cpi->svc.number_temporal_layers == 1) {
+    if (cpi->svc.spatial_layer_id == 0) {
+      lc->is_key_frame = (cm->frame_type == KEY_FRAME);
+    } else {
+      cm->frame_type = INTER_FRAME;
+      lc->is_key_frame = cpi->svc.layer_context[0].is_key_frame;
+
+      if (lc->is_key_frame) {
+        cpi->ref_frame_flags &= (~VP9_LAST_FLAG);
       }
     }
-  } else {
-    if (cpi->use_svc && cpi->svc.number_temporal_layers == 1) {
-      lc->is_key_frame = 0;
-    }
-    cm->frame_type = INTER_FRAME;
   }
 
   // Is this frame a GF / ARF? (Note: a key frame is always also a GF).
