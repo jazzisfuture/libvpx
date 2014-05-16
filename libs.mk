52fad966a334e55270a38bfc77a93ac417835d46
Tom Finegan <tomfinegan@google.com>
2014-05-15 20:52:47 -0700

    Add make target for running example tests.
    Change-Id: I0d0dfda96b99cefb5ef5c16df4a024de5bf8007e

--- libs.mk
+++ libs.mk
@@ -556,3 +556,25 @@ else
 utiltest:
 	@echo Unit tests must be enabled to make the utiltest target.
 endif
+
+##
+## Example tests.
+##
+ifeq ($(CONFIG_UNIT_TESTS),yes)
+TEST_BIN_PATH = examples
+ifeq ($(CONFIG_MSVS),yes)
+# MSVC will build both Debug and Release configurations of tools in a
+# sub directory named for the current target. Assume the user wants to
+# run the Release tools, and assign TEST_BIN_PATH accordingly.
+# TODO(tomfinegan): Is this adequate for ARM?
+# TODO(tomfinegan): Support running the debug versions of tools?
+TEST_BIN_PATH := $(TGT_OS:win64=x64)/Release
+endif
+exampletest: testdata
+	$(qexec)$(SRC_PATH_BARE)/test/examples.sh \
+		--test-data-path $(LIBVPX_TEST_DATA_PATH) \
+		--bin-path $(TEST_BIN_PATH)
+else
+exampletest:
+	@echo Unit tests must be enabled to make the exampletest target.
+endif
