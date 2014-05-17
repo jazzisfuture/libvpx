aa68c33a044dcf4b62c86800164a0f80152ea4a5
Jingning Han <jingning@google.com>
2014-05-16 17:04:39 -0700

    16x16 forward 2D-DCT SSSE3 implementation
    WIP
    
    Change-Id: Ib24b680d4ec4132ec4bdd859e371e9139b97e3c8

--- vp9/encoder/vp9_encodemb.c
+++ vp9/encoder/vp9_encodemb.c
@@ -24,6 +24,9 @@
 #include "vp9/encoder/vp9_rdopt.h"
 #include "vp9/encoder/vp9_tokenize.h"
 
+#include "vpx_ports/x86.h"
+#include <stdio.h>
+
 struct optimize_ctx {
   ENTROPY_CONTEXT ta[MAX_MB_PLANE][16];
   ENTROPY_CONTEXT tl[MAX_MB_PLANE][16];
@@ -313,9 +316,7 @@ void vp9_xform_quant(MACROBLOCK *x, int plane, int block,
   uint16_t *const eob = &p->eobs[block];
   const int diff_stride = 4 * num_4x4_blocks_wide_lookup[plane_bsize];
   int i, j;
-  int16_t *src_diff;
-
-  int16_t buf_input[256], buf_output[256];
+  const int16_t *src_diff;
 
   txfrm_block_to_raster_xy(plane_bsize, tx_size, block, &i, &j);
   src_diff = &p->src_diff[4 * (j * diff_stride + i)];
@@ -329,17 +330,26 @@ void vp9_xform_quant(MACROBLOCK *x, int plane, int block,
                            scan_order->iscan);
       break;
     case TX_16X16:
+    {
+      unsigned int a, b;
+      static unsigned int cycles = 0;
+      static int count = 0;
 
-      for (j = 0; j < 16; ++j)
-        vpx_memcpy(&buf_input[j * 16], &src_diff[j * diff_stride], 16 * sizeof(int16_t));
-
+      vp9_clear_system_state();
+      a = x86_readtsc();
       vp9_fdct16x16_ssse3(src_diff, coeff, diff_stride);
+      b = x86_readtsc();
 
-      for (j = 0; j < 8; ++j) {
-        for (i = 0; i < 8; ++i)
-          assert(buf_input[j * 16 + i] == src_diff[j * diff_stride + i]);
+      cycles += (b - a);
+      ++count;
+
+      if (count == 10000) {
+        fprintf(stderr, "cycles: %d\n", cycles);
+        cycles = 0;
+        count = 0;
       }
 
+    }
       vp9_quantize_b(coeff, 256, x->skip_block, p->zbin, p->round,
                      p->quant, p->quant_shift, qcoeff, dqcoeff,
                      pd->dequant, p->zbin_extra, eob,
