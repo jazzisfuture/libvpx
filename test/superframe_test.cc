b3990019c9380522abf30b8efe76451fbbcfa3c4
hkuang <hkuang@google.com>
2014-05-21 11:17:18 -0700

    Correct the unit test for superframe.
    A superframe is a bunch of frames that bundled as one frame. It is mostly
    used to combine one or more non-displayable frames and one displayable frame.
    
    If a frame that passed to decoder_decode is not a superframe, there
    should be only one displayable frame in it. If a frame that passed to
    decoder_decode is a superframe, then superframe index should be presented
    with the frame. Then each frame's size could be retrieved from that.
    
    The original test try to get rid of the superframe index and send a chunk
    of frames into the decoder_decode which should not be supported by the decoder.
    
    Change-Id: If9a29ecb290c3c9cee66c092c06f4356714d84e1

--- test/superframe_test.cc
+++ test/superframe_test.cc
@@ -41,40 +41,6 @@ class SuperframeTest : public ::libvpx_test::EncoderTest,
     }
   }
 
-  virtual const vpx_codec_cx_pkt_t * MutateEncoderOutputHook(
-      const vpx_codec_cx_pkt_t *pkt) {
-    if (pkt->kind != VPX_CODEC_CX_FRAME_PKT)
-      return pkt;
-
-    const uint8_t *buffer = reinterpret_cast<uint8_t*>(pkt->data.frame.buf);
-    const uint8_t marker = buffer[pkt->data.frame.sz - 1];
-    const int frames = (marker & 0x7) + 1;
-    const int mag = ((marker >> 3) & 3) + 1;
-    const unsigned int index_sz = 2 + mag  * frames;
-    if ((marker & 0xe0) == 0xc0 &&
-        pkt->data.frame.sz >= index_sz &&
-        buffer[pkt->data.frame.sz - index_sz] == marker) {
-      // frame is a superframe. strip off the index.
-      if (modified_buf_)
-        delete[] modified_buf_;
-      modified_buf_ = new uint8_t[pkt->data.frame.sz - index_sz];
-      memcpy(modified_buf_, pkt->data.frame.buf,
-             pkt->data.frame.sz - index_sz);
-      modified_pkt_ = *pkt;
-      modified_pkt_.data.frame.buf = modified_buf_;
-      modified_pkt_.data.frame.sz -= index_sz;
-
-      sf_count_++;
-      last_sf_pts_ = pkt->data.frame.pts;
-      return &modified_pkt_;
-    }
-
-    // Make sure we do a few frames after the last SF
-    abort_ |= sf_count_ > sf_count_max_ &&
-              pkt->data.frame.pts - last_sf_pts_ >= 5;
-    return pkt;
-  }
-
   int sf_count_;
   int sf_count_max_;
   vpx_codec_cx_pkt_t modified_pkt_;
