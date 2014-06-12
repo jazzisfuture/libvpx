5ea12577c2d6deb1333e0b01ae343be9ea63c73f
Deb Mukherjee <debargha@google.com>
2014-06-12 16:53:13 -0700

    Adds support for reading and writing 10/12-bit y4m
    The y4m extension used is the same as the one used in ffmpeg/x264.
    The patch is taken from the highbitdepth branch.
    
    Also adds basic unit tests for y4m header parsing and md5 check
    of the raw frame data.
    
    Change-Id: Ie2794daf6dbafd2f128464f9b9da520fc54c0dd6

---
+++ test/y4m_test.cc
@@ -0,0 +1,127 @@
+/*
+ *  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <string>
+#include "test/md5_helper.h"
+#include "test/y4m_video_source.h"
+#include "third_party/googletest/src/include/gtest/gtest.h"
+#include "./vpx_config.h"
+
+namespace {
+
+using std::string;
+
+static const unsigned int kWidth = 160;
+static const unsigned int kHeight = 90;
+static const unsigned int kFrames = 10;
+
+class Y4mVideoSourceTest
+    : public ::testing::Test, public ::libvpx_test::Y4mVideoSource {
+ protected:
+  Y4mVideoSourceTest() : Y4mVideoSource("", 0, 0) {}
+
+  virtual void Init(const std::string &file_name,
+                    unsigned int start, int limit) {
+    file_name_ = file_name;
+    start_ = start;
+    limit_ = limit;
+    Begin();
+  }
+
+  void HeaderChecks(unsigned int bit_depth, vpx_img_fmt_t fmt) {
+    ASSERT_TRUE(input_file_ != NULL);
+    ASSERT_EQ(y4m_.pic_w, (int)kWidth);
+    ASSERT_EQ(y4m_.pic_h, (int)kHeight);
+    ASSERT_EQ(img()->d_w, kWidth);
+    ASSERT_EQ(img()->d_h, kHeight);
+    ASSERT_EQ(y4m_.vpx_bit_depth, bit_depth);
+    ASSERT_EQ(y4m_.vpx_fmt, fmt);
+    if (fmt == VPX_IMG_FMT_I420 || fmt == VPX_IMG_FMT_I42016) {
+      ASSERT_EQ(y4m_.vpx_bps, (int)y4m_.vpx_bit_depth * 3 / 2);
+      ASSERT_EQ(img()->x_chroma_shift, 1U);
+      ASSERT_EQ(img()->y_chroma_shift, 1U);
+    }
+    if (fmt == VPX_IMG_FMT_I422 || fmt == VPX_IMG_FMT_I42216) {
+      ASSERT_EQ(y4m_.vpx_bps, (int)y4m_.vpx_bit_depth * 2);
+      ASSERT_EQ(img()->x_chroma_shift, 1U);
+      ASSERT_EQ(img()->y_chroma_shift, 0U);
+    }
+    if (fmt == VPX_IMG_FMT_I444 || fmt == VPX_IMG_FMT_I44416) {
+      ASSERT_EQ(y4m_.vpx_bps, (int)y4m_.vpx_bit_depth * 3);
+      ASSERT_EQ(img()->x_chroma_shift, 0U);
+      ASSERT_EQ(img()->y_chroma_shift, 0U);
+    }
+  }
+
+  void Md5Check(const string &expected_md5) {
+    ASSERT_TRUE(input_file_ != NULL);
+    libvpx_test::MD5 md5;
+    for (unsigned int i = start_; i < limit_; i++) {
+      md5.Add(img());
+      Next();
+    }
+    ASSERT_EQ(string(md5.Get()), expected_md5);
+  }
+};
+
+TEST_F(Y4mVideoSourceTest, Y4m420_8) {
+  Init("park_joy_90p_8_420.y4m", 0, kFrames);
+  HeaderChecks(8, VPX_IMG_FMT_I420);
+  Md5Check("e5406275b9fc6bb3436c31d4a05c1cab");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m422_8) {
+  Init("park_joy_90p_8_422.y4m", 0, kFrames);
+  HeaderChecks(8, VPX_IMG_FMT_I422);
+  Md5Check("284a47a47133b12884ec3a14e959a0b6");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m444_8) {
+  Init("park_joy_90p_8_444.y4m", 0, kFrames);
+  HeaderChecks(8, VPX_IMG_FMT_I444);
+  Md5Check("90517ff33843d85de712fd4fe60dbed0");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m420_10) {
+  Init("park_joy_90p_10_420.y4m", 0, kFrames);
+  HeaderChecks(10, VPX_IMG_FMT_I42016);
+  Md5Check("63f21f9f717d8b8631bd2288ee87137b");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m422_10) {
+  Init("park_joy_90p_10_422.y4m", 0, kFrames);
+  HeaderChecks(10, VPX_IMG_FMT_I42216);
+  Md5Check("48ab51fb540aed07f7ff5af130c9b605");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m444_10) {
+  Init("park_joy_90p_10_444.y4m", 0, kFrames);
+  HeaderChecks(10, VPX_IMG_FMT_I44416);
+  Md5Check("067bfd75aa85ff9bae91fa3e0edd1e3e");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m420_12) {
+  Init("park_joy_90p_12_420.y4m", 0, kFrames);
+  HeaderChecks(12, VPX_IMG_FMT_I42016);
+  Md5Check("9e6d8f6508c6e55625f6b697bc461cef");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m422_12) {
+  Init("park_joy_90p_12_422.y4m", 0, kFrames);
+  HeaderChecks(12, VPX_IMG_FMT_I42216);
+  Md5Check("b239c6b301c0b835485be349ca83a7e3");
+}
+
+TEST_F(Y4mVideoSourceTest, Y4m444_12) {
+  Init("park_joy_90p_12_444.y4m", 0, kFrames);
+  HeaderChecks(12, VPX_IMG_FMT_I44416);
+  Md5Check("5a6481a550821dab6d0192f5c63845e9");
+}
+}  // namespace
