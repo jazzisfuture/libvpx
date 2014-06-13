bf26f434869387778966f622afd4fa29ba898115
Tim Kopp <tkopp@google.com>
2014-06-13 13:58:57 -0700

    bugfix: s/stdint.h/vpx\/vpx_int.h
    Also removed unused #include.
    
    Change-Id: I852bba3e85e794f1d4af854c45c16a23a787e6a3

--- vp9/encoder/vp9_denoiser.c
+++ vp9/encoder/vp9_denoiser.c
@@ -8,8 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include <stdio.h>
-#include <stdint.h>
+#include "vpx/vpx_integer.h"
 #include "vp9/encoder/vp9_denoiser.h"
 #include "vpx_scale/yv12config.h"
 
