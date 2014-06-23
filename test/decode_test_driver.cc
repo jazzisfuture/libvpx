3c9bf7c0e3021d3da50893a0e0ec55fdea04f538
hkuang <hkuang@google.com>
2014-06-23 14:59:20 -0700

    Add unit test to test user_priv parameter.
    Change-Id: I6ba6171e43e0a43331ee0a7b698590b143979c44

--- test/decode_test_driver.cc
+++ test/decode_test_driver.cc
@@ -33,6 +33,17 @@ vpx_codec_err_t Decoder::DecodeFrame(const uint8_t *cxdata, size_t size) {
   return res_dec;
 }
 
+vpx_codec_err_t Decoder::DecodeFrame(const uint8_t *cxdata, size_t size,
+                                     void *user_priv) {
+  vpx_codec_err_t res_dec;
+  InitOnce();
+  REGISTER_STATE_CHECK(
+      res_dec = vpx_codec_decode(&decoder_,
+                                 cxdata, static_cast<unsigned int>(size),
+                                 user_priv, 0));
+  return res_dec;
+}
+
 void DecoderTest::RunLoop(CompressedVideoSource *video) {
   vpx_codec_dec_cfg_t dec_cfg = {0};
   Decoder* const decoder = codec_->CreateDecoder(dec_cfg, 0);
