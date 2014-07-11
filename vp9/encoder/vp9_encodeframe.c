b0a63e945f850de15d94e1fe0d30426ee8e4efec
Jingning Han <jingning@google.com>
2014-07-11 09:49:36 -0700

    Allow more coding flexibility in key frame coding
    This commit relaxes the encoding mode constraints on key frame
    coding. It improves the key frame coding performance in speed 5 and
    up.
    
    Change-Id: I114315c2b467174bb1f135f4ab2c1f328c8c65be

--- vp9/encoder/vp9_encodeframe.c
+++ vp9/encoder/vp9_encodeframe.c
@@ -3508,8 +3508,8 @@ static void encode_superblock(VP9_COMP *cpi, TOKENEXTRA **t, int output_enabled,
       vp9_setup_pre_planes(xd, ref, cfg, mi_row, mi_col,
                            &xd->block_refs[ref]->sf);
     }
-    vp9_build_inter_predictors_sb(xd, mi_row, mi_col, MAX(bsize, BLOCK_8X8));
 
+    vp9_build_inter_predictors_sb(xd, mi_row, mi_col, MAX(bsize, BLOCK_8X8));
     if (!x->skip) {
       mbmi->skip = 1;
       vp9_encode_sb(x, MAX(bsize, BLOCK_8X8));
