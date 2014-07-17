432485adace97f466c32995e9677fe0c45e8e07b
hkuang <hkuang@google.com>
2014-07-17 14:25:41 -0700

    Add segmentation map array for current and last frame segmentation.
    Change-Id: I442ddff36b5de9cb8a7eb59e225744c78f4492d8

--- vp9/common/vp9_entropymode.c
+++ vp9/common/vp9_entropymode.c
@@ -439,8 +439,12 @@ void vp9_setup_past_independence(VP9_COMMON *cm) {
   int i;
   vp9_clearall_segfeatures(&cm->seg);
   cm->seg.abs_delta = SEGMENT_DELTADATA;
+
   if (cm->last_frame_seg_map)
     vpx_memset(cm->last_frame_seg_map, 0, (cm->mi_rows * cm->mi_cols));
+
+  if (cm->current_frame_seg_map)
+    vpx_memset(cm->current_frame_seg_map, 0, (cm->mi_rows * cm->mi_cols));
 
   // Reset the mode ref deltas for loop filter
   vp9_zero(lf->last_ref_deltas);
