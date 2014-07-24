32fb4576bf85301d4fe819f415e6b9eafe283a68
Marco Paniconi <marpan@google.com>
2014-07-24 12:54:38 -0700

    vp8: Add an aggressive denoising mode.
    Change-Id: Ie4686e1b15af6bcc8d59d585bbeb996f38224522

--- vp8/encoder/denoising.h
+++ vp8/encoder/denoising.h
@@ -39,16 +39,40 @@ enum vp8_denoiser_filter_state {
   kFilterNonZeroMV
 };
 
+typedef struct {
+  // Scale factor on sse threshold above which no denoising is done.
+  unsigned int scale_sse_thresh;
+  // Scale factor on motion magnitude threshold above which no
+  // denoising is done.
+  unsigned int scale_motion_thresh;
+  // Scale factor to bias to ZEROMV for denoising.
+  unsigned int denoise_mv_bias;
+  // Scale factor to bias to ZEROMV for coding mode selection.
+  unsigned int pickmode_mv_bias;
+  // Quantizer threshold below which we use the segmentation map to switch off
+  // loop filter for blocks that have been coded as ZEROMV-LAST a certain number
+  // (consec_zerolast) of consecutive frames. Not that the delta-QP is set to 0
+  // when segmentation map is used for shutting off loop filter.
+  unsigned int qp_thresh;
+  // Threshold for number of consecutive frames for blocks coded as ZEROMV-LAST.
+  unsigned int consec_zerolast;
+  // Threshold for spatial blur, done on whole frame prior to encoding.
+  // 0 means no spatial blurring is done.
+  unsigned int spatial_blur;
+} DENOISE_PARAMETERS;
+
 typedef struct vp8_denoiser
 {
     YV12_BUFFER_CONFIG yv12_running_avg[MAX_REF_FRAMES];
     YV12_BUFFER_CONFIG yv12_mc_running_avg;
     unsigned char* denoise_state;
     int num_mb_cols;
+    int aggressive_mode;
+    DENOISE_PARAMETERS denoise_pars;
 } VP8_DENOISER;
 
 int vp8_denoiser_allocate(VP8_DENOISER *denoiser, int width, int height,
-                          int num_mb_rows, int num_mb_cols);
+                          int num_mb_rows, int num_mb_cols, int mode);
 
 void vp8_denoiser_free(VP8_DENOISER *denoiser);
 
@@ -63,6 +87,8 @@ void vp8_denoiser_denoise_mb(VP8_DENOISER *denoiser,
                              int mb_col,
                              int block_index,
                              int uv_denoise);
+
+void vp8_denoiser_set_parameters(VP8_DENOISER *denoiser);
 
 #ifdef __cplusplus
 }  // extern "C"
