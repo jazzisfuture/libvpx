f08569d48cee29d364d10829d749115b5aaa753f
levytamar82 <tamar.levy@intel.com>
2014-07-24 00:20:19 -0700

    Fix bug 807
    in the sub_pixel_*variance* function the dst is aligned to 16 bytes and not
    to 32 bytes - now load unaligned data
    
    Change-Id: I2e0b9745543697efc56fefa32857ea10117af135

--- test/variance_test.cc
+++ test/variance_test.cc
@@ -707,8 +707,7 @@ INSTANTIATE_TEST_CASE_P(
 #endif
 
 #if HAVE_AVX2
-// TODO(jzern): these prototypes can be removed after the avx2 versions are
-// reenabled in vp9_rtcd_defs.pl.
+
 extern "C" {
 unsigned int vp9_sub_pixel_variance32x32_avx2(
     const uint8_t *src_ptr, int source_stride, int xoffset, int yoffset,
@@ -743,7 +742,7 @@ const vp9_subpixvariance_fn_t subpel_variance32x32_avx2 =
 const vp9_subpixvariance_fn_t subpel_variance64x64_avx2 =
     vp9_sub_pixel_variance64x64_avx2;
 INSTANTIATE_TEST_CASE_P(
-    DISABLED_AVX2, VP9SubpelVarianceTest,
+    AVX2, VP9SubpelVarianceTest,
     ::testing::Values(make_tuple(5, 5, subpel_variance32x32_avx2),
                       make_tuple(6, 6, subpel_variance64x64_avx2)));
 
@@ -752,7 +751,7 @@ const vp9_subp_avg_variance_fn_t subpel_avg_variance32x32_avx2 =
 const vp9_subp_avg_variance_fn_t subpel_avg_variance64x64_avx2 =
     vp9_sub_pixel_avg_variance64x64_avx2;
 INSTANTIATE_TEST_CASE_P(
-    DISABLED_AVX2, VP9SubpelAvgVarianceTest,
+    AVX2, VP9SubpelAvgVarianceTest,
     ::testing::Values(make_tuple(5, 5, subpel_avg_variance32x32_avx2),
                       make_tuple(6, 6, subpel_avg_variance64x64_avx2)));
 #endif  // HAVE_AVX2
