b2fb658b25cc07602d56cbd1d844ef7d016ad100
Hangyu Kuang <hkuang@google.com>
2014-07-30 20:43:40 -0700

    Implenment frame parallel decode. Disable by default.
    TODO: 1. Add Unit test to test all test vectors.
          2. Add unit test that exercise all the superframe combination.
          3. Performance tunning.
    
    Change-Id: Ia0a549aaa3e83b5a17b31d8299aa496ea4f21e3e

--- vp9/decoder/vp9_dthread.h
+++ vp9/decoder/vp9_dthread.h
@@ -19,7 +19,7 @@ struct VP9Common;
 struct VP9Decoder;
 
 typedef struct TileWorkerData {
-  struct VP9Common *cm;
+  struct VP9Decoder *pbi;
   vp9_reader bit_reader;
   DECLARE_ALIGNED(16, struct macroblockd, xd);
 
@@ -55,6 +55,18 @@ typedef struct FrameWorkerData {
   // It is used to make a copy of the compressed data.
   uint8_t *scratch_buffer;
   size_t scratch_buffer_size;
+
+  // Following variable could be obtained in other way, but is shadowed here
+  // to considerably reduce access time.
+  VP9Worker *pre_worker;
+
+#if CONFIG_MULTITHREAD
+  pthread_mutex_t stats_mutex;
+  pthread_cond_t stats_cond;
+#endif
+
+  int frame_context_ready;  // Current frame's context is ready to read.
+  int decoding_done;        // Finished decoding current frame.
 } FrameWorkerData;
 
 // Allocate memory for loopfilter row synchronization.
@@ -70,5 +82,19 @@ void vp9_loop_filter_frame_mt(YV12_BUFFER_CONFIG *frame,
                               struct VP9Common *cm,
                               int frame_filter_level,
                               int y_only);
+
+// Wait until ref_buf has been decoded to (row, col) position.
+// Note: worker may already finish decoding ref_buf and release it in order to
+// start decoding next frame. So need to check whether worker is still decoding
+// ref_buf.
+void vp9_frameworker_wait(VP9Worker *const worker, RefCntBuffer *const ref_buf, int row, int col);
+
+// FrameWorker broadcasts its decoding progress so other workers that are
+// waiting on it can resume decoding.
+void vp9_frameworker_broadcast(RefCntBuffer *const buf, int row, int col);
+
+// Copy necessary decoding context from src worker to dst worker.
+void vp9_frameworker_copy_context(VP9Worker *const dst_worker,
+                                  const VP9Worker *const src_worker);
 
 #endif  // VP9_DECODER_VP9_DTHREAD_H_
