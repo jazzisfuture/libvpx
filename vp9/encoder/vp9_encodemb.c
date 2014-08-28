f31bb81d8a2cfc183379644ca9d881eb2205aae7
Jingning Han <jingning@google.com>
2014-08-28 09:09:37 -0700

    Extend block level sse to support multiple txfm blocks
    This commit extends the sse and forward transform computation flag
    to support the case 64x64 blocks where there are 4 32x32 2D-DCT
    blocks.
    
    Change-Id: I86a3e805dfaa0f3abd812f590520c71aa0e40473

--- vp9/encoder/vp9_encodemb.c
+++ vp9/encoder/vp9_encodemb.c
@@ -476,13 +476,13 @@ static void encode_block(int plane, int block, BLOCK_SIZE plane_bsize,
   }
 
   if (!x->skip_recode) {
-    if (x->skip_txfm[plane] == 0) {
+    if (x->skip_txfm[(plane << 2) + (block >> (tx_size << 1))] == 0) {
       // full forward transform and quantization
       if (x->quant_fp)
         vp9_xform_quant_fp(x, plane, block, plane_bsize, tx_size);
       else
         vp9_xform_quant(x, plane, block, plane_bsize, tx_size);
-    } else if (x->skip_txfm[plane] == 2) {
+    } else if (x->skip_txfm[(plane << 2) + (block >> (tx_size << 1))] == 2) {
       // fast path forward transform and quantization
       vp9_xform_quant_dc(x, plane, block, plane_bsize, tx_size);
     } else {
