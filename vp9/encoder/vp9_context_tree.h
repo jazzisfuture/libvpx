b1f3ec3fc1e7ef11f8ed11a1aeb214e680b303fb
Jingning Han <jingning@google.com>
2014-08-28 09:09:37 -0700

    Extend block level sse to support multiple txfm blocks
    This commit extends the sse and forward transform computation flag
    to support the case 64x64 blocks where there are 4 32x32 2D-DCT
    blocks.
    
    Change-Id: I86a3e805dfaa0f3abd812f590520c71aa0e40473

--- vp9/encoder/vp9_context_tree.h
+++ vp9/encoder/vp9_context_tree.h
@@ -33,7 +33,7 @@ typedef struct {
   int is_coded;
   int num_4x4_blk;
   int skip;
-  int skip_txfm[MAX_MB_PLANE];
+  int skip_txfm[MAX_MB_PLANE << 8];
   int best_mode_index;
   int hybrid_pred_diff;
   int comp_pred_diff;
