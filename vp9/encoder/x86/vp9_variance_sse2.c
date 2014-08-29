1c42acf8042522c82a5fd1c5c8264921b8ae6193
Dmitry Kovalev <dkovalev@google.com>
2014-08-29 14:07:46 -0700

    8x8
    Change-Id: I66e99fcd48504be55fd930e0d54721a41077e780

--- vp9/encoder/x86/vp9_variance_sse2.c
+++ vp9/encoder/x86/vp9_variance_sse2.c
@@ -15,6 +15,9 @@
 #include "vp9/encoder/vp9_variance.h"
 #include "vpx_ports/mem.h"
 
+#include <stdio.h>
+#include <time.h>
+
 typedef unsigned int (*variance_fn_t) (const unsigned char *src, int src_stride,
                                        const unsigned char *ref, int ref_stride,
                                        unsigned int *sse, int *sum);
@@ -47,6 +50,46 @@ unsigned int vp9_get4x4var_sse2(const uint8_t *src, int src_stride,
   vsum = _mm_add_epi32(vsum, _mm_srli_si128(vsum, 8));
   vsum = _mm_add_epi32(vsum, _mm_srli_si128(vsum, 4));
   *sse = _mm_cvtsi128_si32(vsum);
+
+  return 0;
+}
+
+#define READ64_XXX(p, stride, i) \
+  _mm_loadl_epi64((const __m128i *)(p + (i) * stride))
+
+unsigned int vp9_get8x8var_sse2_xxx(const uint8_t *src, int src_stride,
+                                    const uint8_t *ref, int ref_stride,
+                                    unsigned int *sse, int *sum) {
+  const __m128i zero = _mm_setzero_si128();
+  __m128i vsum = _mm_setzero_si128();
+  __m128i vsse = _mm_setzero_si128();
+  int i;
+
+  for (i = 0; i < 8; i += 2) {
+    const __m128i src0 = _mm_unpacklo_epi8(READ64_XXX(src, src_stride, i), zero);
+    const __m128i ref0 = _mm_unpacklo_epi8(READ64_XXX(ref, ref_stride, i), zero);
+    const __m128i diff0 = _mm_sub_epi16(src0, ref0);
+
+    const __m128i src1 = _mm_unpacklo_epi8(READ64_XXX(src, src_stride, i + 1), zero);
+    const __m128i ref1 = _mm_unpacklo_epi8(READ64_XXX(ref, ref_stride, i + 1), zero);
+    const __m128i diff1 = _mm_sub_epi16(src1, ref1);
+
+    vsum = _mm_add_epi16(vsum, diff0);
+    vsum = _mm_add_epi16(vsum, diff1);
+    vsse = _mm_add_epi32(vsse, _mm_madd_epi16(diff0, diff0));
+    vsse = _mm_add_epi32(vsse, _mm_madd_epi16(diff1, diff1));
+  }
+
+  // sum
+  vsum = _mm_add_epi16(vsum, _mm_srli_si128(vsum, 8));
+  vsum = _mm_add_epi16(vsum, _mm_srli_si128(vsum, 4));
+  vsum = _mm_add_epi16(vsum, _mm_srli_si128(vsum, 2));
+  *sum = (int16_t)_mm_extract_epi16(vsum, 0);
+
+  // sse
+  vsse = _mm_add_epi32(vsse, _mm_srli_si128(vsse, 8));
+  vsse = _mm_add_epi32(vsse, _mm_srli_si128(vsse, 4));
+  *sse = _mm_cvtsi128_si32(vsse);
 
   return 0;
 }
@@ -110,8 +153,27 @@ unsigned int vp9_variance8x8_sse2(const unsigned char *src, int src_stride,
                                   const unsigned char *ref, int ref_stride,
                                   unsigned int *sse) {
   int sum;
-  variance_sse2(src, src_stride, ref, ref_stride, 8, 8,
-                sse, &sum, vp9_get8x8var_sse2, 8);
+
+  const int N = 100000;
+  clock_t start = clock();
+  for (int i = 0; i < N; ++i) {
+    variance(src, src_stride, ref, ref_stride, 8, 8, sse, &sum);
+  }
+  float t0 = (float)(clock() - start) / CLOCKS_PER_SEC;
+
+  start = clock();
+  for (int i = 0; i < N; ++i) {
+    vp9_get8x8var_sse2(src, src_stride, ref, ref_stride, sse, &sum);
+  }
+  float t1 = (float)(clock() - start) / CLOCKS_PER_SEC;
+
+  start = clock();
+  for (int i = 0; i < N; ++i) {
+    vp9_get8x8var_sse2_xxx(src, src_stride, ref, ref_stride, sse, &sum);
+  }
+  float t2 = (float)(clock() - start) / CLOCKS_PER_SEC;
+  printf("8x8: c %f vs old %f vs new %f\n", t0, t1, t2);
+
   return *sse - (((unsigned int)sum * sum) >> 6);
 }
 
