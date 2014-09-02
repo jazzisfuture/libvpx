6969e7334aff60f969d6928c1e209b7f234bb025
hkuang <hkuang@google.com>
2014-09-02 11:28:18 -0700

    [DO NOT SUBMIT] Test remove memset.
    Change-Id: I29ff9a0f0bda3157148bb6243db885a467f6826c

--- vpx_scale/generic/yv12config.c
+++ vpx_scale/generic/yv12config.c
@@ -178,7 +178,6 @@ int vp9_realloc_frame_buffer(YV12_BUFFER_CONFIG *ybf,
       // This memset is needed for fixing valgrind error from C loop filter
       // due to access uninitialized memory in frame border. It could be
       // removed if border is totally removed.
-      vpx_memset(fb->data, 0, fb->size);
 
       ybf->buffer_alloc = (uint8_t *)yv12_align_addr(fb->data, 32);
     } else if (frame_size > ybf->buffer_alloc_sz) {
