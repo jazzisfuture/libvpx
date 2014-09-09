65460133c25e76f1f9a0448d976080ce1e4cf6d2
Jia Jia <jia.jia@linaro.org>
2014-09-09 08:18:49 +0000

    vp9: neon: convolve: use neon intrinsics version for convolve8 and convolve8_avg.
    Change-Id: Iaeecbf215cd03abc3709c5d28c3d1ca201d56ff9

--- vp9/common/arm/neon/vp9_convolve_neon.c
+++ vp9/common/arm/neon/vp9_convolve_neon.c
@@ -12,6 +12,22 @@
 #include "vp9/common/vp9_common.h"
 #include "vpx_ports/mem.h"
 
+void vp9_convolve8_horiz_neon(const uint8_t *src, ptrdiff_t src_stride,
+                              uint8_t *dst, ptrdiff_t dst_stride,
+                              const int16_t *filter_x, int x_step_q4,
+                              const int16_t *filter_y, int y_step_q4,
+                              int w, int h);
+void vp9_convolve8_vert_neon(const uint8_t *src, ptrdiff_t src_stride,
+                             uint8_t *dst, ptrdiff_t dst_stride,
+                             const int16_t *filter_x, int x_step_q4,
+                             const int16_t *filter_y, int y_step_q4,
+                             int w, int h);
+void vp9_convolve8_avg_vert_neon(const uint8_t *src, ptrdiff_t src_stride,
+                                 uint8_t *dst, ptrdiff_t dst_stride,
+                                 const int16_t *filter_x, int x_step_q4,
+                                 const int16_t *filter_y, int y_step_q4,
+                                 int w, int h);
+
 void vp9_convolve8_neon(const uint8_t *src, ptrdiff_t src_stride,
                         uint8_t *dst, ptrdiff_t dst_stride,
                         const int16_t *filter_x, int x_step_q4,
