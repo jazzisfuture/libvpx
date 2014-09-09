67eaeb739fdde7f50d489e2f3395a2c712d2cc57
Jia Jia <jia.jia@linaro.org>
2014-09-09 08:35:43 +0000

    vp9: neon: iht: use neon intrinsics version for iht4x4 and iht8x8.
    Change-Id: I0f1383728f73e92552e1afee0b1d22b88dbba29f

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -386,16 +386,16 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
   $vp9_idct4x4_1_add_neon_asm=vp9_idct4x4_1_add_neon;
 
   add_proto qw/void vp9_idct4x4_16_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride";
-  specialize qw/vp9_idct4x4_16_add sse2 neon_asm dspr2/;
-  $vp9_idct4x4_16_add_neon_asm=vp9_idct4x4_16_add_neon;
+  specialize qw/vp9_idct4x4_16_add sse2 neon dspr2/;
+  $vp9_idct4x4_16_add_neon=vp9_idct4x4_16_add_neon;
 
   add_proto qw/void vp9_idct8x8_1_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride";
   specialize qw/vp9_idct8x8_1_add sse2 neon_asm dspr2/;
   $vp9_idct8x8_1_add_neon_asm=vp9_idct8x8_1_add_neon;
 
   add_proto qw/void vp9_idct8x8_64_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride";
-  specialize qw/vp9_idct8x8_64_add sse2 neon_asm dspr2/, "$ssse3_x86_64";
-  $vp9_idct8x8_64_add_neon_asm=vp9_idct8x8_64_add_neon;
+  specialize qw/vp9_idct8x8_64_add sse2 neon dspr2/, "$ssse3_x86_64";
+  $vp9_idct8x8_64_add_neon=vp9_idct8x8_64_add_neon;
 
   add_proto qw/void vp9_idct8x8_12_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride";
   specialize qw/vp9_idct8x8_12_add sse2 neon_asm dspr2/, "$ssse3_x86_64";
@@ -426,12 +426,12 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
   $vp9_idct32x32_1_add_neon_asm=vp9_idct32x32_1_add_neon;
 
   add_proto qw/void vp9_iht4x4_16_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, int tx_type";
-  specialize qw/vp9_iht4x4_16_add sse2 neon_asm dspr2/;
-  $vp9_iht4x4_16_add_neon_asm=vp9_iht4x4_16_add_neon;
+  specialize qw/vp9_iht4x4_16_add sse2 neon dspr2/;
+  $vp9_iht4x4_16_add_neon=vp9_iht4x4_16_add_neon;
 
   add_proto qw/void vp9_iht8x8_64_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, int tx_type";
-  specialize qw/vp9_iht8x8_64_add sse2 neon_asm dspr2/;
-  $vp9_iht8x8_64_add_neon_asm=vp9_iht8x8_64_add_neon;
+  specialize qw/vp9_iht8x8_64_add sse2 neon dspr2/;
+  $vp9_iht8x8_64_add_neon=vp9_iht8x8_64_add_neon;
 
   add_proto qw/void vp9_iht16x16_256_add/, "const tran_low_t *input, uint8_t *output, int pitch, int tx_type";
   specialize qw/vp9_iht16x16_256_add sse2 dspr2/;
@@ -444,7 +444,6 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
   add_proto qw/void vp9_iwht4x4_16_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride";
   specialize qw/vp9_iwht4x4_16_add/;
 }
-
 
 # High bitdepth functions
 if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
