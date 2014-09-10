fdb38a997ec9f9764aa109f7e5b54b2ceebbcb25
Marco <marpan@google.com>
2014-09-10 10:12:45 -0700

    vp8: Update to the adaptive denoising mode.
    Allow for option to apply spatial blur for temporal
    denoising, under the aggressive denoising mode.
    
    Change-Id: I41c5fdc0b6cf32d8f8d1d4236b25fa5aa406e89e

--- vp8/common/postproc.h
+++ vp8/common/postproc.h
@@ -33,6 +33,12 @@ extern "C" {
 int vp8_post_proc_frame(struct VP8Common *oci, YV12_BUFFER_CONFIG *dest,
                         vp8_ppflags_t *flags);
 
+void vp8_de_noise_yonly_for_temporaldenoise(struct VP8Common *oci,
+                                            YV12_BUFFER_CONFIG *source,
+                                            YV12_BUFFER_CONFIG  *post,
+                                            int q,
+                                            int low_var_thresh,
+                                            int flag);
 
 void vp8_de_noise(struct VP8Common           *oci,
                   YV12_BUFFER_CONFIG         *source,
