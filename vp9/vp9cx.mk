4c39a8e16d5a1d8317709d995cf8af873349dc70
JackyChen <jackychen@google.com>
2014-09-15 14:39:48 -0700

    Add SSE2 code for block 16x16 in VP9 denoiser.
    This SSE2 code is inherited from VP8 denoiser. In VP8, there are
    only 16x16 blocks in denoiser, but in VP9, there are different size
    of blocks. For each size, the SSE code will be different in some way.
    By adding this SSE2 code, the speed improvement is around 20%, vary for
    different clips.
    
    Change-Id: I03d2c4577afa3d016ad98381936ff1d1c679d3b7

--- vp9/vp9cx.mk
+++ vp9/vp9cx.mk
@@ -23,6 +23,7 @@ VP9_CX_SRCS-yes += encoder/vp9_context_tree.h
 VP9_CX_SRCS-yes += encoder/vp9_cost.h
 VP9_CX_SRCS-yes += encoder/vp9_cost.c
 VP9_CX_SRCS-yes += encoder/vp9_dct.c
+
 VP9_CX_SRCS-$(CONFIG_VP9_TEMPORAL_DENOISING) += encoder/vp9_denoiser.c
 VP9_CX_SRCS-$(CONFIG_VP9_TEMPORAL_DENOISING) += encoder/vp9_denoiser.h
 VP9_CX_SRCS-yes += encoder/vp9_encodeframe.c
@@ -119,6 +120,10 @@ VP9_CX_SRCS-$(ARCH_X86_64) += encoder/x86/vp9_ssim_opt_x86_64.asm
 
 VP9_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp9_dct_sse2.c
 VP9_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp9_dct32x32_sse2.c
+
+ifeq ($(CONFIG_VP9_TEMPORAL_DENOISING),yes)
+VP9_CX_SRCS-$(HAVE_SSE2) += encoder/x86/vp9_denoiser_sse2.c
+endif
 
 VP9_CX_SRCS-$(HAVE_AVX2) += encoder/x86/vp9_dct32x32_avx2.c
 VP9_CX_SRCS-$(HAVE_AVX2) += encoder/x86/vp9_dct_avx2.c
