b0befad42f1c8440ba71d253f43ad06de893d834
Marco <marpan@google.com>
2014-10-07 11:43:51 -0700

    Reduce the intra_cost_penalty for non-rd mode.
    From some visual quality tests (at speed 6 on RTC clips),
    this helps the quality in uncovered areas.
    
    Made the factor reduction only in vp9_pickmode to make
    it specific for non-rd mode.
    
    Change-Id: Ice0f5895e4765e21cda3fd9661ce5de504cc8027

--- vp9/encoder/vp9_pickmode.c
+++ vp9/encoder/vp9_pickmode.c
@@ -483,7 +483,7 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
   unsigned int var_y = UINT_MAX;
   unsigned int sse_y = UINT_MAX;
   const int intra_cost_penalty = vp9_get_intra_cost_penalty(
-      cm->base_qindex, cm->y_dc_delta_q, cm->bit_depth);
+      cm->base_qindex, cm->y_dc_delta_q, cm->bit_depth) >> 2;
   const int64_t inter_mode_thresh = RDCOST(x->rdmult, x->rddiv,
                                            intra_cost_penalty, 0);
   const int intra_mode_cost = 50;
