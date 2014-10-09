b2fed04862c2575b928f074adda5c6fe03a8a71c
Marco <marpan@google.com>
2014-10-09 16:01:52 -0700

    Reduce the intra_cost_penalty for non-rd mode.
    Made the factor reduction only in vp9_pickmode. to make
    it specific for non-rd mode for now.
    
    Visual tests show some improvement at Speed 6, for RTC clips.
    Numerical results should about ~1%/2% increase in PSNR/SSIM.
    
    Change-Id: Ice0f5895e4765e21cda3fd9661ce5de504cc8027

--- vp9/encoder/vp9_pickmode.c
+++ vp9/encoder/vp9_pickmode.c
@@ -486,8 +486,9 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
   // var_y and sse_y are saved to be used in skipping checking
   unsigned int var_y = UINT_MAX;
   unsigned int sse_y = UINT_MAX;
+  const int reduction_fac = (bsize <= BLOCK_16X16) ? 4 : 1;
   const int intra_cost_penalty = vp9_get_intra_cost_penalty(
-      cm->base_qindex, cm->y_dc_delta_q, cm->bit_depth);
+      cm->base_qindex, cm->y_dc_delta_q, cm->bit_depth) / reduction_fac;
   const int64_t inter_mode_thresh = RDCOST(x->rdmult, x->rddiv,
                                            intra_cost_penalty, 0);
   const int intra_mode_cost = 50;
