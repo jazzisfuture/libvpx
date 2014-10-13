3c8754b4c75dde30ddb5059ae3f0ea32b01bfc8b
Rakesh Kumar TS <rakeshkumar.ts@ittiam.com>
2014-10-13 19:45:19 +0530

    VP9 encoder NEON optimizations
    Add vp9_reconintra_neon.c
    - vp9_dc_predictor_8x8_neon
    - vp9_dc_predictor_16x16_neon
    - vp9_dc_predictor_32x32_neon
    
    Renamed file to vp9_dc_reconintra_neon.c. Fixed include issue.
    
    In testing this change does not provide any perf benefit for
    640x480 files.
    Tested on Nexus 7, built with ndk r10d, gcc 4.9.
    
    Change-Id: I015ad4a8c171670aba2ef3a101c5fbf10d0e8008

--- vp9/vp9_common.mk
+++ vp9/vp9_common.mk
@@ -136,6 +136,7 @@ endif
 VP9_COMMON_SRCS-$(HAVE_NEON_ASM) += common/arm/neon/vp9_mb_lpf_neon$(ASM)
 VP9_COMMON_SRCS-$(HAVE_NEON_ASM) += common/arm/neon/vp9_save_reg_neon$(ASM)
 
+VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_dc_reconintra_neon.c
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_iht4x4_add_neon.c
 VP9_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/vp9_iht8x8_add_neon.c
 
