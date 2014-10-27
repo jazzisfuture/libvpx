6f0e158af2f46e37e00249028602a4ee33d4e7e3
Harinarayanan K K <harinarayanan.kk@ittiam.com>
2014-10-27 11:26:26 +0530

    VP9 encoder NEON optimizations
    Add vp9_dct_32x32_neon.asm
    - vp9_fdct32x32_rd_neon
    Add vp9_dct_ht_4x4_neon.asm
    - vp9_fht4x4_neon
    - vp9_fdct4x4_neon
    
    Fixed merge issue. Fixed line endings. Preserved Neon registers.
    Fixed armv8 targets.
    
    On Nexus 7 speed -5 saw about a 8% increase in perf.
    Speeds -6, -7, and -8, perf did not change.
    Tested on Nexus 7, built with ndk r10d, gcc 4.9.
    
    Change-Id: I878a4e2de0a44a203ccad3accb512d78d0de0a09

--- vp9/common/vp9_rtcd_defs.pl
+++ vp9/common/vp9_rtcd_defs.pl
@@ -1213,7 +1213,8 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
   specialize qw/vp9_fdct32x32_rd sse2/;
 } else {
   add_proto qw/void vp9_fht4x4/, "const int16_t *input, tran_low_t *output, int stride, int tx_type";
-  specialize qw/vp9_fht4x4 sse2/;
+  specialize qw/vp9_fht4x4 sse2 neon_asm/;
+  $vp9_fht4x4_neon_asm=vp9_fht4x4_neon;
 
   add_proto qw/void vp9_fht8x8/, "const int16_t *input, tran_low_t *output, int stride, int tx_type";
   specialize qw/vp9_fht8x8 sse2/;
@@ -1228,7 +1229,8 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
   specialize qw/vp9_fdct4x4_1 sse2/;
 
   add_proto qw/void vp9_fdct4x4/, "const int16_t *input, tran_low_t *output, int stride";
-  specialize qw/vp9_fdct4x4 sse2/;
+  specialize qw/vp9_fdct4x4 sse2 neon_asm/;
+  $vp9_fdct4x4_neon_asm=vp9_fdct4x4_neon;
 
   add_proto qw/void vp9_fdct8x8_1/, "const int16_t *input, tran_low_t *output, int stride";
   specialize qw/vp9_fdct8x8_1 sse2 neon/;
@@ -1249,7 +1251,8 @@ if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
   specialize qw/vp9_fdct32x32 sse2 avx2/;
 
   add_proto qw/void vp9_fdct32x32_rd/, "const int16_t *input, tran_low_t *output, int stride";
-  specialize qw/vp9_fdct32x32_rd sse2 avx2/;
+  specialize qw/vp9_fdct32x32_rd sse2 avx2 neon_asm/;
+  $vp9_fdct32x32_rd_neon_asm=vp9_fdct32x32_rd_neon;
 }
 
 #
