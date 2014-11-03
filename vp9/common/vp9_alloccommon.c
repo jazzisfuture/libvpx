e03e0abd126e2236a4891eff4ff3f16f386bf226
hkuang <hkuang@google.com>
2014-11-03 12:25:18 -0800

    Fix the memory leak due to missing free frame_mvs.
    Change-Id: I2ceee7341d906259002c0ea31ea009ae32c04bfd

--- vp9/common/vp9_alloccommon.c
+++ vp9/common/vp9_alloccommon.c
@@ -88,11 +88,11 @@ void vp9_free_ref_frame_buffers(VP9_COMMON *cm) {
 
   for (i = 0; i < FRAME_BUFFERS; ++i) {
     vp9_free_frame_buffer(&cm->frame_bufs[i].buf);
+    vpx_free(cm->frame_bufs[i].mvs);
 
     if (cm->frame_bufs[i].ref_count > 0 &&
         cm->frame_bufs[i].raw_frame_buffer.data != NULL) {
       cm->release_fb_cb(cm->cb_priv, &cm->frame_bufs[i].raw_frame_buffer);
-      vpx_free(cm->frame_bufs[i].mvs);
       cm->frame_bufs[i].ref_count = 0;
     }
   }
