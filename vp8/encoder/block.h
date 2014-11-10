dfd78b3dddbadb53a7d8c2ca41d790cc0e38dd0a
Marco <marpan@google.com>
2014-11-10 13:07:05 -0800

    Various updates to vp8.
    Change-Id: Iee00e2247f428c99a840134b5521e652195e50d9

--- vp8/encoder/block.h
+++ vp8/encoder/block.h
@@ -125,6 +125,8 @@ typedef struct macroblock
 
     int optimize;
     int q_index;
+    int is_skin;
+    int denoise_zeromv;
 
 #if CONFIG_TEMPORAL_DENOISING
     int increase_denoising;
@@ -162,6 +164,8 @@ typedef struct macroblock
     void (*quantize_b)(BLOCK *b, BLOCKD *d);
     void (*quantize_b_pair)(BLOCK *b1, BLOCK *b2, BLOCKD *d0, BLOCKD *d1);
 
+    unsigned int mbs_zero_last_dot_suppress;
+    int zero_last_dot_suppress;
 } MACROBLOCK;
 
 
