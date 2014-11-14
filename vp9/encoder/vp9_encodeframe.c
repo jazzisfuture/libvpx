0cb8c57b88c25575cc3a5d2fea898a87f3b16dc9
Spencer Egart <spencere@google.com>
2014-11-14 11:55:58 -0800

    Created COMPOUND_MODES experiment.
    COMPOUND_MODES experiment encodes separate MV modes for each frame in a compound
    reference prediction.  Added modes NEAR_NEARMV, NEAREST_NEARESTMV, ZERO_ZEROMV,
    NEW_NEWMV, NEAREST_NEARMV, NEAR_NEARESTMV, NEW_NEARESTMV, NEAR_NEWMV,
    NEW_NEARMV, and NEAREST_NEWMV.
    
    Results:
    derflr +0.228
    
    Change-Id: I719e8a34826bf1f1fe3988dac5733a845a89ef2b

--- vp9/encoder/vp9_encodeframe.c
+++ vp9/encoder/vp9_encodeframe.c
@@ -4655,7 +4655,11 @@ static void sum_intra_stats(FRAME_COUNTS *counts,
 static int get_zbin_mode_boost(const MB_MODE_INFO *mbmi, int enabled) {
   if (enabled) {
     if (is_inter_block(mbmi)) {
+#if CONFIG_COMPOUND_MODES
+      if (mbmi->mode == ZEROMV || mbmi->mode == ZERO_ZEROMV) {
+#else
       if (mbmi->mode == ZEROMV) {
+#endif  // CONFIG_COMPOUND_MODES
         return mbmi->ref_frame[0] != LAST_FRAME ? GF_ZEROMV_ZBIN_BOOST
                                                 : LF_ZEROMV_ZBIN_BOOST;
       } else {
