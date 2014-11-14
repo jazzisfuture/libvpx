0f32dd51b207bced0fe8ca44edf7b2cddca138bf
Adrian Grange <agrange@google.com>
2014-11-14 15:53:55 -0800

    Rationalize reference frame buffer handling
    Change-Id: Ie28d185cefe3fd512fe233c47ea5dab637ec3d78

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -2448,17 +2448,25 @@ static void loopfilter_frame(VP9_COMP *cpi, VP9_COMMON *cm) {
 
 void vp9_scale_references(VP9_COMP *cpi) {
   VP9_COMMON *cm = &cpi->common;
+  MACROBLOCKD *const xd = &cpi->mb.e_mbd;
   MV_REFERENCE_FRAME ref_frame;
   const VP9_REFFRAME ref_mask[3] = {VP9_LAST_FLAG, VP9_GOLD_FLAG, VP9_ALT_FLAG};
 
   for (ref_frame = LAST_FRAME; ref_frame <= ALTREF_FRAME; ++ref_frame) {
-    // Need to convert from VP9_REFFRAME to index into ref_mask (subtract 1).
     if (cpi->ref_frame_flags & ref_mask[ref_frame - 1]) {
       const int idx = cm->ref_frame_map[get_ref_frame_idx(cpi, ref_frame)];
-      const YV12_BUFFER_CONFIG *const ref = &cm->frame_bufs[idx].buf;
-
+      YV12_BUFFER_CONFIG *const buf = &cm->frame_bufs[idx].buf;
+      RefBuffer *const ref_buf = &cm->frame_refs[ref_frame - 1];
+      ref_buf->buf = buf;
+      ref_buf->idx = idx;
 #if CONFIG_VP9_HIGHBITDEPTH
-      if (ref->y_crop_width != cm->width || ref->y_crop_height != cm->height) {
+      vp9_setup_scale_factors_for_frame(&ref_buf->sf,
+                                        buf->y_crop_width, buf->y_crop_height,
+                                        cm->width, cm->height,
+                                        (buf->flags & YV12_FLAG_HIGHBITDEPTH) ?
+                                            1 : 0);
+
+      if (buf->y_crop_width != cm->width || buf->y_crop_height != cm->height) {
         const int new_fb = get_free_fb(cm);
         cm->cur_frame = &cm->frame_bufs[new_fb];
         vp9_realloc_frame_buffer(&cm->frame_bufs[new_fb].buf,
@@ -2466,16 +2474,20 @@ void vp9_scale_references(VP9_COMP *cpi) {
                                  cm->subsampling_x, cm->subsampling_y,
                                  cm->use_highbitdepth,
                                  VP9_ENC_BORDER_IN_PIXELS, NULL, NULL, NULL);
-        scale_and_extend_frame(ref, &cm->frame_bufs[new_fb].buf,
+        scale_and_extend_frame(buf, &cm->frame_bufs[new_fb].buf,
                                (int)cm->bit_depth);
 #else
-      if (ref->y_crop_width != cm->width || ref->y_crop_height != cm->height) {
+      vp9_setup_scale_factors_for_frame(&ref_buf->sf,
+                                        buf->y_crop_width, buf->y_crop_height,
+                                        cm->width, cm->height);
+
+      if (buf->y_crop_width != cm->width || buf->y_crop_height != cm->height) {
         const int new_fb = get_free_fb(cm);
         vp9_realloc_frame_buffer(&cm->frame_bufs[new_fb].buf,
                                  cm->width, cm->height,
                                  cm->subsampling_x, cm->subsampling_y,
                                  VP9_ENC_BORDER_IN_PIXELS, NULL, NULL, NULL);
-        scale_and_extend_frame(ref, &cm->frame_bufs[new_fb].buf);
+        scale_and_extend_frame(buf, &cm->frame_bufs[new_fb].buf);
 #endif  // CONFIG_VP9_HIGHBITDEPTH
         cpi->scaled_ref_idx[ref_frame - 1] = new_fb;
         if (cm->frame_bufs[new_fb].mvs == NULL ||
@@ -2491,10 +2503,17 @@ void vp9_scale_references(VP9_COMP *cpi) {
         cpi->scaled_ref_idx[ref_frame - 1] = idx;
         ++cm->frame_bufs[idx].ref_count;
       }
+
+      // AWG - Do we need this? (scale_and_extend_frame has already been called)
+      //if (vp9_is_scaled(&ref_buf->sf)) {
+      //  vp9_extend_frame_borders(buf);
+      //}
     } else {
       cpi->scaled_ref_idx[ref_frame - 1] = INVALID_REF_BUFFER_IDX;
     }
   }
+
+  set_ref_ptrs(cm, xd, LAST_FRAME, LAST_FRAME);
 }
 
 static void release_scaled_references(VP9_COMP *cpi) {
