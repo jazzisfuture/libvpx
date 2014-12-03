f5cb9bf03a1ac9f32f299a5a48e93dcb09d30cb4
Jingning Han <jingning@google.com>
2014-12-02 18:16:06 -0800

    Rework coeff probability model update for rtc coding
    This commit reworks the ONE_LOOP_REDUCED coefficient probability
    model update process. It allows model update for every coefficient
    across the spectrum at a coarser resolution, instead of performing
    precise update only for certain subset of probability models.
    
    The overall runtime remains nearly same (<1% change) for speed -6.
    The compression performance is improved by 7.5% in PSNR for speed
    -5 and 4.57% for speed -6, respectively.
    
    Change-Id: Ifb17136382ee7e39a9f34ff4a4f09a753125c8d1

--- vp9/encoder/vp9_subexp.h
+++ vp9/encoder/vp9_subexp.h
@@ -32,6 +32,11 @@ int vp9_prob_diff_update_savings_search_model(const unsigned int *ct,
                                               vp9_prob *bestp,
                                               vp9_prob upd);
 
+int vp9_prob_diff_search_appx(const unsigned int *ct,
+                              const vp9_prob *oldp,
+                              vp9_prob *bestp,
+                              vp9_prob upd);
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
