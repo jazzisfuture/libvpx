d2e0b880b67fadb0668f39d8096787f79ea0167e
Marco <marpan@google.com>
2015-01-15 14:51:15 -0800

    Allow for H and V intra modes for block sizes <= 16X16, for speed 6.
    Change-Id: I343b28682c6b7ebe851f9f48e4fafd1d71f38ffd

--- vp9/encoder/vp9_pickmode.c
+++ vp9/encoder/vp9_pickmode.c
@@ -874,8 +874,9 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
 
     for (i = 0; i < 4; ++i) {
       const PREDICTION_MODE this_mode = intra_mode_list[i];
-      if (!((1 << this_mode) & cpi->sf.intra_y_mode_mask[intra_tx_size]))
+      if (!((1 << this_mode) & cpi->sf.intra_y_mode_mask[bsize]))
         continue;
+
       args.mode = this_mode;
       args.rate = 0;
       args.dist = 0;
@@ -896,6 +897,8 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
         mbmi->ref_frame[0] = INTRA_FRAME;
         mbmi->uv_mode = this_mode;
         mbmi->mv[0].as_int = INVALID_MV;
+        mode_rd_thresh = best_mode_skip_txfm ? rd_threshes[this_mode] << 1 :
+                                               rd_threshes[this_mode];
       }
     }
 
