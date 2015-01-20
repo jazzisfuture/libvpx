89bc7838df520df4961bca58f04933e805abde2c
Paul Wilkins <paulwilkins@google.com>
2015-01-20 15:23:57 -0800

    Adjust active maxq for GF groups.
    In this patch the active max Q is adjusted for each GF
    group based on the vbr bit allocation and raw first pass
    group error.
    
    This will tend to give a lower q for very easy sections
    and a higher value for very hard sections. As such it is
    expected to help improve quality in some of the easier
    sections.
    
    This will almost certainly hurt overall psnr but help
    average. However, most of our current reported visual
    problems are in relatively easy sections which seem not
    to be getting enough bits.
    
    Change-Id: Icc015499cebbf2a45054a05e8e31f3dfb43f944a

--- vp9/encoder/vp9_firstpass.h
+++ vp9/encoder/vp9_firstpass.h
@@ -106,14 +106,19 @@ typedef struct {
 
   // Projected total bits available for a key frame group of frames
   int64_t kf_group_bits;
-
+  
   // Error score of frames still to be coded in kf group
   int64_t kf_group_error_left;
+
+  // The fraction for a kf groups total bits allocated to the inter frames
+  double kfgroup_inter_fraction;
+
   int sr_update_lag;
 
   int kf_zeromotion_pct;
   int last_kfgroup_zeromotion_pct;
   int gf_zeromotion_pct;
+  int baseline_worst_quality;
   int active_worst_quality;
   int extend_minq;
   int extend_maxq;
