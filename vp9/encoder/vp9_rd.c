fccdaeed73798eaeff27147f086f2d3d066c4a73
Jingning Han <jingning@google.com>
2015-01-23 11:35:45 -0800

    Fix adaptive mode search threshold
    This reduces the speed 2 runtime regression (close to 20% slower
    for pedestrian 1080p at 2000 kbps). The compression metric remains
    the same.
    
    Change-Id: I3277534226ed5298e8937d302fe0327881f90245

--- vp9/encoder/vp9_rd.c
+++ vp9/encoder/vp9_rd.c
@@ -566,9 +566,9 @@ void vp9_set_rd_speed_thresholds(VP9_COMP *cpi) {
 
   rd->thresh_mult[THR_DC] += 1000;
 
-  rd->thresh_mult[THR_NEWMV] += 1000;
-  rd->thresh_mult[THR_NEWA] += 1000;
-  rd->thresh_mult[THR_NEWG] += 1000;
+  rd->thresh_mult[THR_NEWMV] += 600;
+  rd->thresh_mult[THR_NEWA] += 600;
+  rd->thresh_mult[THR_NEWG] += 600;
 
   // Adjust threshold only in real time mode, which only uses last
   // reference frame.
