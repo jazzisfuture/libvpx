048b2b6a1bfbef05b60393fdd2e229999ddd7604
Tom Finegan <tomfinegan@google.com>
2015-02-04 12:39:23 -0800

    Xcode: Fix includes in examples.
    The current file's directory, ".", is treated much more literally
    when building libvpx examples with Xcode than it is with make, and
    clang cannot find common include files included via "./" when those
    files actually reside one directory up in the tree.
    
    Change-Id: I5f66a026282e35d80248ca4052ebb882b859172e

---
+++ xcode_project/vpxharness/vpxharness/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vpxharness
+//
+//  Created by Tom Finegan on 2/4/15.
+//  Copyright (c) 2015 Google. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
