2d7b1da5ec1e7cbdac927fa90d9550a677cb1692
Deb Mukherjee <debargha@google.com>
2015-02-06 17:44:49 -0800

    Adds code for corner detection and ransac
    The new code is to start experiments with global motion models.
    The corner detection code is fast_9.
    Three flavors of ransac are used to estimate either a
    homography (8-param), or an affine model (6-param) or a
    rotation-zoom only affine model (4-param).
    
    Change-Id: I3f9788ec2dc0635cbc65f5c66c6ea8853cfcf2dd

---
+++ vp9/encoder/vp9_corner_match.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2015 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be
+ *  found  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+int determine_correspondence(unsigned char *frm,
+                             int *frm_corners, int num_frm_corners,
+                             unsigned char *ref,
+                             int *ref_corners, int num_ref_corners,
+                             int width, int height,
+                             int frm_stride, int ref_stride,
+                             int *correspondence_pts);
