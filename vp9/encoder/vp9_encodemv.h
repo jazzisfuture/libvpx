af45c7e2e64dce2392511c2cf679769ed0a80238
Zoe Liu <zoeliu@google.com>
2015-02-09 15:54:56 -0800

    Changed mv ref to nearestmv always for sub8x8
    Change-Id: I8e3f4aaa8553ba8c445369692e079db5ce282593

--- vp9/encoder/vp9_encodemv.h
+++ vp9/encoder/vp9_encodemv.h
@@ -28,7 +28,13 @@ void vp9_encode_mv(VP9_COMP *cpi, vp9_writer* w, const MV* mv, const MV* ref,
 void vp9_build_nmv_cost_table(int *mvjoint, int *mvcost[2],
                               const nmv_context* mvctx, int usehp);
 
+#if CONFIG_NEWMVREF_SUB8X8
+void vp9_update_mv_count(VP9_COMMON *cm, const MACROBLOCKD *xd
+                         , const TileInfo *const tile,
+                         int mi_row, int mi_col);
+#else
 void vp9_update_mv_count(VP9_COMMON *cm, const MACROBLOCKD *xd);
+#endif  // CONFIG_NEWMVREF_SUB8X8
 
 #ifdef __cplusplus
 }  // extern "C"
