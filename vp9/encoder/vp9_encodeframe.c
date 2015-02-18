b5330ecc1b6715963c21a47c62a00073d03d7dd0
Adrian Grange <agrange@google.com>
2015-02-18 09:40:34 -0800

    Make encoder buffer allocation dynamic
    Change-Id: Id1afe98de98db4ae8b2dfefed7889c3b28c68582

--- vp9/encoder/vp9_encodeframe.c
+++ vp9/encoder/vp9_encodeframe.c
@@ -145,12 +145,15 @@ static unsigned int get_sby_perpixel_diff_variance(VP9_COMP *cpi,
                                                    int mi_row, int mi_col,
                                                    BLOCK_SIZE bs) {
   const YV12_BUFFER_CONFIG *last = get_ref_frame_buffer(cpi, LAST_FRAME);
-  const uint8_t* last_y = &last->y_buffer[mi_row * MI_SIZE * last->y_stride +
-                                              mi_col * MI_SIZE];
-  unsigned int sse;
-  const unsigned int var = cpi->fn_ptr[bs].vf(ref->buf, ref->stride,
-                                              last_y, last->y_stride, &sse);
-  return ROUND_POWER_OF_TWO(var, num_pels_log2_lookup[bs]);
+  if (last != NULL) {
+    const uint8_t* last_y = &last->y_buffer[mi_row * MI_SIZE * last->y_stride +
+                                                mi_col * MI_SIZE];
+    unsigned int sse;
+    const unsigned int var = cpi->fn_ptr[bs].vf(ref->buf, ref->stride,
+                                                last_y, last->y_stride, &sse);
+    return ROUND_POWER_OF_TWO(var, num_pels_log2_lookup[bs]);
+  }
+  return UINT_MAX;
 }
 
 static BLOCK_SIZE get_rd_var_based_fixed_partition(VP9_COMP *cpi, MACROBLOCK *x,
@@ -542,7 +545,7 @@ static void choose_partitioning(VP9_COMP *cpi,
   s = x->plane[0].src.buf;
   sp = x->plane[0].src.stride;
 
-  if (!is_key_frame) {
+  if (!is_key_frame && yv12 != NULL) {
     MB_MODE_INFO *mbmi = &xd->mi[0].src_mi->mbmi;
     unsigned int var = 0, sse;
     vp9_setup_pre_planes(xd, 0, yv12, mi_row, mi_col,
@@ -3995,6 +3998,7 @@ static void encode_superblock(VP9_COMP *cpi, ThreadData *td,
     int ref;
     const int is_compound = has_second_ref(mbmi);
     for (ref = 0; ref < 1 + is_compound; ++ref) {
+      // TODO(agrange) What happens if get_ref_frame_buffer returns NULL?
       YV12_BUFFER_CONFIG *cfg = get_ref_frame_buffer(cpi,
                                                      mbmi->ref_frame[ref]);
       vp9_setup_pre_planes(xd, ref, cfg, mi_row, mi_col,
