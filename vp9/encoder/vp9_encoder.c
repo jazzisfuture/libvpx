530bddb30634a4e1a1af3eb79ccb92531b67aad5
Adrian Grange <agrange@google.com>
2015-03-04 15:14:09 -0800

    Remove width/height params from vp9_alloc_ref_frame_buffers
    Change-Id: I06be8df9d2c62976935dd9e2ed1e002785c34d2e

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -497,7 +497,7 @@ static void alloc_raw_frame_buffers(VP9_COMP *cpi) {
 
 static void alloc_ref_frame_buffers(VP9_COMP *cpi) {
   VP9_COMMON *const cm = &cpi->common;
-  if (vp9_alloc_ref_frame_buffers(cm, cm->width, cm->height))
+  if (vp9_alloc_ref_frame_buffers(cm))
     vpx_internal_error(&cm->error, VPX_CODEC_MEM_ERROR,
                        "Failed to allocate frame buffers");
 }
