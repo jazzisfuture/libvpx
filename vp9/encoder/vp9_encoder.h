991e4109a943d9c65b1eecf28b2f61756410f568
Marco <marpan@google.com>
2015-03-18 13:55:19 -0700

    Force_split on 16x16 blocks in variance partition.
    Force split on 16x16 block based on the minmax over the 8x8 sub-blocks.
    
    Also some modification to force split on 32x32 and 64x64 block, and add exit
    condiiton of choose_partition (with very safe threshold) based on sad used
    to select reference frame.
    
    Somw visual improvement near moving boundaries, ~0.3% gain in avg. psnr/ssim.
    
    Change-Id: I4759bb181251ac41517cd45e326ce2997dadb577

--- vp9/encoder/vp9_encoder.h
+++ vp9/encoder/vp9_encoder.h
@@ -464,6 +464,8 @@ typedef struct VP9_COMP {
   int64_t vbp_threshold_32x32;
   int64_t vbp_threshold_16x16;
   int64_t vbp_threshold_8x8;
+  int64_t vbp_threshold_sad;
+  int64_t vbp_threshold_minmax;
   BLOCK_SIZE vbp_bsize_min;
 
   // Multi-threading
