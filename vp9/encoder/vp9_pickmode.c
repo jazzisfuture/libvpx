14a43c5189de6776bba709b6c2d081fdd68e3dca
Jingning Han <jingning@google.com>
2015-03-31 12:18:55 -0700

    Refactor block_yrd function for RTC mode
    Use local variable when it provides speed benefits. Remove
    redundant computation of the effective src diff block buffer
    address. The CPU cycle percentage of block_yrd in the overall
    profile goes from 1.57% to 1.22%.
    
    Change-Id: I4aab241a36abcef7fd8adf74aed5e12b8b88e0ef

--- vp9/encoder/vp9_pickmode.c
+++ vp9/encoder/vp9_pickmode.c
@@ -359,41 +359,39 @@ static void block_yrd(VP9_COMP *cpi, MACROBLOCK *x, int *rate, int64_t *dist,
         tran_low_t *const coeff = BLOCK_OFFSET(p->coeff, block);
         tran_low_t *const qcoeff = BLOCK_OFFSET(p->qcoeff, block);
         tran_low_t *const dqcoeff = BLOCK_OFFSET(pd->dqcoeff, block);
-        uint16_t *const eob = &p->eobs[block];
+        uint16_t eob;
         const int diff_stride = 4 * num_4x4_blocks_wide_lookup[bsize];
-        int i, j;
         const int16_t *src_diff;
         int64_t this_sse;
-        txfrm_block_to_raster_xy(bsize, tx_size, block, &i, &j);
-        src_diff = &p->src_diff[4 * (j * diff_stride + i)];
+        src_diff = &p->src_diff[(r * diff_stride + c) << 2];
 
         switch (tx_size) {
           case TX_32X32:
             vp9_fdct32x32_rd(src_diff, coeff, diff_stride);
             vp9_quantize_fp_32x32(coeff, 1024, x->skip_block, p->zbin,
                                   p->round_fp, p->quant_fp, p->quant_shift,
-                                  qcoeff, dqcoeff, pd->dequant, eob,
+                                  qcoeff, dqcoeff, pd->dequant, &eob,
                                   scan_order->scan, scan_order->iscan);
             break;
           case TX_16X16:
             vp9_hadamard_16x16(src_diff, diff_stride, (int16_t *)coeff);
             vp9_quantize_fp(coeff, 256, x->skip_block, p->zbin, p->round_fp,
                             p->quant_fp, p->quant_shift, qcoeff, dqcoeff,
-                            pd->dequant, eob,
+                            pd->dequant, &eob,
                             scan_order->scan, scan_order->iscan);
             break;
           case TX_8X8:
             vp9_hadamard_8x8(src_diff, diff_stride, (int16_t *)coeff);
             vp9_quantize_fp(coeff, 64, x->skip_block, p->zbin, p->round_fp,
                             p->quant_fp, p->quant_shift, qcoeff, dqcoeff,
-                            pd->dequant, eob,
+                            pd->dequant, &eob,
                             scan_order->scan, scan_order->iscan);
             break;
           case TX_4X4:
             x->fwd_txm4x4(src_diff, coeff, diff_stride);
             vp9_quantize_fp(coeff, 16, x->skip_block, p->zbin, p->round_fp,
                             p->quant_fp, p->quant_shift, qcoeff, dqcoeff,
-                            pd->dequant, eob,
+                            pd->dequant, &eob,
                             scan_order->scan, scan_order->iscan);
             break;
           default:
@@ -403,13 +401,13 @@ static void block_yrd(VP9_COMP *cpi, MACROBLOCK *x, int *rate, int64_t *dist,
 
         *dist += vp9_block_error(coeff, dqcoeff, step << 4, &this_sse) >> shift;
 
-        if (*eob == 1)
+        if (eob == 1)
           *rate += (int)abs(qcoeff[0]);
-        else if (*eob > 1)
+        else if (eob > 1)
           *rate += (int)vp9_satd((const int16_t *)qcoeff, step << 4);
 
         *sse += (this_sse >> shift);
-        *skippable &= (*eob == 0);
+        *skippable &= (eob == 0);
       }
       block += step;
     }
