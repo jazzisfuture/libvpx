65260860768604787c8af78f8ff556768b2bf2f2
Frank Galligan <fgalligan@google.com>
2015-03-31 08:56:22 -0700

    WIP: Refactor read_inter_segment_id
    Change-Id: Ia3163ae46217b6960db04f739285158f0145b4f8

--- vp9/decoder/vp9_decodemv.c
+++ vp9/decoder/vp9_decodemv.c
@@ -147,25 +147,28 @@ static int read_inter_segment_id(VP9_COMMON *const cm, MACROBLOCKD *const xd,
   struct segmentation *const seg = &cm->seg;
   MB_MODE_INFO *const mbmi = &xd->mi[0].src_mi->mbmi;
   const BLOCK_SIZE bsize = mbmi->sb_type;
-  int predicted_segment_id, segment_id;
+  int segment_id;
 
   if (!seg->enabled)
     return 0;  // Default for disabled segmentation
 
-  predicted_segment_id = cm->last_frame_seg_map ?
-      vp9_get_segment_id(cm, cm->last_frame_seg_map, bsize, mi_row, mi_col) : 0;
-
   if (!seg->update_map) {
     copy_segment_id(cm, cm->last_frame_seg_map, cm->current_frame_seg_map,
                     bsize, mi_row, mi_col);
-    return predicted_segment_id;
+    if (!cm->last_frame_seg_map) {
+      return 0;
+    }
+    return vp9_get_segment_id(cm, cm->last_frame_seg_map, bsize, mi_row, mi_col);
   }
 
   if (seg->temporal_update) {
     const vp9_prob pred_prob = vp9_get_pred_prob_seg_id(seg, xd);
     mbmi->seg_id_predicted = vp9_read(r, pred_prob);
-    segment_id = mbmi->seg_id_predicted ? predicted_segment_id
-                                        : read_segment_id(r, seg);
+    if (mbmi->seg_id_predicted) {
+      segment_id = vp9_get_segment_id(cm, cm->last_frame_seg_map, bsize, mi_row, mi_col);
+    } else {
+      segment_id = read_segment_id(r, seg);
+    }
   } else {
     segment_id = read_segment_id(r, seg);
   }
