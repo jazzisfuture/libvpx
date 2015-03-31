c3757dc58957ccee79dd64c0e01cf4766dc09bd8
Frank Galligan <fgalligan@google.com>
2015-03-31 10:00:57 -0700

    WIP: Refactor seg id.
    Change-Id: I15addd452a5dffbf02edfd20664629d3cb2fad02

--- vp9/common/vp9_pred_common.c
+++ vp9/common/vp9_pred_common.c
@@ -362,19 +362,14 @@ int vp9_get_tx_size_context(const MACROBLOCKD *xd) {
   return (above_ctx + left_ctx) > max_tx_size;
 }
 
-int vp9_get_segment_id(const VP9_COMMON *cm, const uint8_t *segment_ids,
-                       BLOCK_SIZE bsize, int mi_row, int mi_col) {
-  const int mi_offset = mi_row * cm->mi_cols + mi_col;
-  const int bw = num_8x8_blocks_wide_lookup[bsize];
-  const int bh = num_8x8_blocks_high_lookup[bsize];
-  const int xmis = MIN(cm->mi_cols - mi_col, bw);
-  const int ymis = MIN(cm->mi_rows - mi_row, bh);
+int vp9_get_segment_id(const uint8_t *segment_ids,
+                       int mi_offset, int mi_cols, int xmis, int ymis) {
   int x, y, segment_id = INT_MAX;
 
   for (y = 0; y < ymis; y++)
     for (x = 0; x < xmis; x++)
       segment_id = MIN(segment_id,
-                       segment_ids[mi_offset + y * cm->mi_cols + x]);
+                       segment_ids[mi_offset + y * mi_cols + x]);
 
   assert(segment_id >= 0 && segment_id < MAX_SEGMENTS);
   return segment_id;
