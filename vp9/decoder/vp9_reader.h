1bbbe95c41209892e553ef9bf20a51e72e0b866d
Jim Bankoski <jimbankoski@google.com>
2015-04-28 08:24:37 -0700

    WIP:  Partition Read Expansion Revisited
    Took frank's patch and expanded vp9_read to avoid an extra comparison
    that might have been well predicted.
    
    Change-Id: I30e05a9243d2cca39e68ec306d627b2127ab00eb

--- vp9/decoder/vp9_reader.h
+++ vp9/decoder/vp9_reader.h
@@ -89,6 +89,92 @@ static INLINE int vp9_read(vp9_reader *r, int prob) {
 
   return bit;
 }
+#define VP9_READ_VARS(r) \
+  BD_VALUE bigsplit; \
+  BD_VALUE value = r->value; \
+  int count = r->count; \
+  unsigned int range = r->range; \
+  unsigned int split;
+
+#define VP9_READ_BIT(r, prob) \
+  split = (range * prob + (256 - prob)) >> CHAR_BIT; \
+  if (count < 0) { \
+    VP9_READ_STORE(r) \
+    vp9_reader_fill(r); \
+    value = r->value; \
+    count = r->count; \
+    range = r->range; \
+  } \
+  bigsplit = (BD_VALUE)split << (BD_VALUE_SIZE - CHAR_BIT);
+
+#define VP9_READ_SET (value >= bigsplit)
+
+
+static INLINE void vp9_read_normalize(BD_VALUE *value, unsigned int *range,
+                                      unsigned int *count) {
+  register unsigned int shift = vp9_norm[*range];
+  *range <<= shift;
+  *value <<= shift;
+  *count -= shift;
+}
+static INLINE void vp9_read_one(BD_VALUE *value, unsigned int *range,
+                                unsigned int *count, unsigned int *split,
+                                unsigned int *bigsplit) {
+  *range = *split;
+  *value -= *bigsplit;
+  vp9_read_normalize(value, range, count);
+}
+
+static INLINE void vp9_read_zed(BD_VALUE *value, unsigned int *range,
+                                unsigned int *count, unsigned int *split,
+                                unsigned int *bigsplit) {
+  *range = *split;
+  vp9_read_normalize(value, range, count);
+}
+static INLINE void vp9_read_store(BD_VALUE *value, unsigned int *range,
+                                unsigned int *count,
+                                BD_VALUE val, unsigned int rng,
+                                unsigned int cnt) {
+  *value = val;
+  *range = rng;
+  *count = cnt;
+}
+
+#define VP9_READ_NORMALIZE \
+  { \
+    register unsigned int shift = vp9_norm[range]; \
+    range <<= shift; \
+    value <<= shift; \
+    count -= shift; \
+  }
+#define VP9_READ_STORE(r) \
+  r->value = value; \
+  r->count = count; \
+  r->range = range;
+
+#define VP9_READ_ADJUST_FOR_ONE(r) \
+  range = range - split; \
+  value = value - bigsplit; \
+  VP9_READ_NORMALIZE
+
+#define VP9_READ_ADJUST_FOR_ZERO \
+  range = split; \
+  VP9_READ_NORMALIZE
+
+
+static INLINE int vp9_readz(vp9_reader *r, int prob) {
+  int bit = 0;
+  VP9_READ_VARS(r);
+  VP9_READ_BIT(r, prob);
+  bit = VP9_READ_SET;
+  if (bit) {
+    VP9_READ_ADJUST_FOR_ONE(r)
+  } else {
+    VP9_READ_ADJUST_FOR_ZERO;
+  }
+  VP9_READ_STORE(r)
+  return bit;
+}
 
 static INLINE int vp9_read_bit(vp9_reader *r) {
   return vp9_read(r, 128);  // vp9_prob_half
