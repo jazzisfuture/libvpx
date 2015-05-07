21a3aab844326179c0730a84e59ee3cf8b7d54de
Scott LaVarnway <slavarnway@google.com>
2015-05-07 15:41:13 -0700

    Reducing size of MODE_INFO struct
    Reduced size from 124 bytes to 88 bytes.  For decode only builds,
    it is reduced to 52 bytes.
    
    Change-Id: If9e6b92285459425fa086ab5a743d0a598a69de3

--- vp9/encoder/vp9_encodeframe.c
+++ vp9/encoder/vp9_encodeframe.c
@@ -367,7 +367,7 @@ void sum_2_variances(const var *a, const var *b, var *r) {
 }
 
 static void fill_variance_tree(void *data, BLOCK_SIZE bsize) {
-  variance_node node;
+  variance_node node = {0};
   tree_to_node(data, bsize, &node);
   sum_2_variances(node.split[0], node.split[1], &node.part_variances->horz[0]);
   sum_2_variances(node.split[2], node.split[3], &node.part_variances->horz[1]);
