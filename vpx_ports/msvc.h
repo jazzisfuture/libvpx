791e225a6f031fa73a004a9cc6eb46e438b4f952
Johann <johannkoenig@google.com>
2015-05-07 16:41:33 -0700

    Don't #define snprintf in VS 2015 or higher.
    In VS 2015 and higher snprintf is supplied and therefore vsnprintf
    doesn't need to be defined. This also avoids problems caused by
    _snprintf being different from snprintf.
    
    This fixes a build break with VS 2015 and improves security.
    
    Originally submitted via chromium by brucedawson@chromium.org
    https://codereview.chromium.org/1055603003
    
    Additionally break this MSVC-specific tweak to a new file, which will
    become the home of all such MSVC-specific things.
    
    This requires adding a dependency on msvc.h to every example which uses
    args.c and tools_common.h
    
    Change-Id: I35b5f8e7ea00f6627403aabc9ea79b0412557a99

---
+++ vpx_ports/msvc.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2015 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef VPX_PORTS_MSVC_H_
+#define VPX_PORTS_MSVC_H_
+
+#include "./vpx_config.h"
+
+#ifdef _MSC_VER
+# include <math.h>  // the ceil() definition must precede intrin.h
+# if _MSC_VER > 1310 && (defined(_M_X64) || defined(_M_IX86))
+#  include <intrin.h>
+#  define USE_MSC_INTRINSICS
+# endif  // _MSC_VER > 1310 && (defined(_M_X64) || defined(_M_IX86)
+# if _MSC_VER < 1900  // VS2015 provides snprintf
+#  define snprintf _snprintf
+# endif  // _MSC_VER < 1900
+#endif  // _MSC_VER
+
+#endif  // VPX_PORTS_MSVC_H_
