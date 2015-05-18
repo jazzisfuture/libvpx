efcf13929b460b88e0dca96de8ac06e394eedf12
Pieter Kapsenberg <pkapsenb@google.com>
2015-05-18 12:14:12 -0700

    Removing unnecessary syntax remaping for mc filter type
    Change-Id: Iba067eb2bcf519dc5776976c3ab0a694ff3feb12

--- vp9/encoder/vp9_bitstream.c
+++ vp9/encoder/vp9_bitstream.c
@@ -1853,11 +1853,9 @@ static void encode_txfm_probs(VP9_COMMON *cm, vp9_writer *w) {
 
 static void write_interp_filter(INTERP_FILTER filter,
                                 struct vp9_write_bit_buffer *wb) {
-  const int filter_to_literal[] = { 1, 0, 2, 3 };
-
-  vp9_wb_write_bit(wb, filter == SWITCHABLE);
+   vp9_wb_write_bit(wb, filter == SWITCHABLE);
   if (filter != SWITCHABLE)
-    vp9_wb_write_literal(wb, filter_to_literal[filter], 2);
+    vp9_wb_write_literal(wb, filter, 2);
 }
 
 static void fix_interp_filter(VP9_COMMON *cm) {
