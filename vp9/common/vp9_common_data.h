cfa0765514397c34781e5e8d705330e675022de7
Scott LaVarnway <slavarnway@google.com>
2015-07-03 09:53:16 -0700

    WIP: VP9: Eliminate bsize/subsize from decode_partition
    Change-Id: Ia7de18d25a49f116f22837b442b75f82debb1e89

--- vp9/common/vp9_common_data.h
+++ vp9/common/vp9_common_data.h
@@ -32,7 +32,7 @@ extern const BLOCK_SIZE subsize_lookup[PARTITION_TYPES][BLOCK_SIZES];
 extern const TX_SIZE max_txsize_lookup[BLOCK_SIZES];
 extern const BLOCK_SIZE txsize_to_bsize[TX_SIZES];
 extern const TX_SIZE tx_mode_to_biggest_tx_size[TX_MODES];
-extern const BLOCK_SIZE ss_size_lookup[BLOCK_SIZES][2][2];
+extern const BLOCK_SIZE ss_size_lookup[BLOCK_SIZES + 1][2][2];
 
 #ifdef __cplusplus
 }  // extern "C"
