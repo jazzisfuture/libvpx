d8510b65ab7e3159aa881c3d58c722a8407f3cd2
Jim Bankoski <jimbankoski@google.com>
2015-07-07 17:01:18 -0700

    Fill buffer / possible speed up
    A new thing to try - eliminates byte by byte read from bool decoder,  and
    eliminates all the in loop changes for shift, buffer and count.
    
    Change-Id: Id89241977103fc3b973e4ed172a5cbf246998e5d

--- vp9/decoder/vp9_reader.c
+++ vp9/decoder/vp9_reader.c
@@ -7,10 +7,12 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
+#include <stdlib.h>
 
 #include "vpx_ports/mem.h"
 #include "vpx_mem/vpx_mem.h"
 
+#include "./vpx_config.h"
 #include "vp9/decoder/vp9_reader.h"
 
 int vp9_reader_init(vp9_reader *r,
@@ -32,6 +34,47 @@ int vp9_reader_init(vp9_reader *r,
     return vp9_read_bit(r) != 0;  // marker bit
   }
 }
+#if CONFIG_BIG_ENDIAN
+#define BIGENDIFY64(X) (X)
+#define BIGENDIFY32(X) (X)
+#else
+#if defined(__GNUC__)
+  #define BIGENDIFY64(X) __builtin_bswap64(X)
+#elif defined(_MSC_VER)
+  #define BIGENDIFY64(X) _byteswap_uint64(X)
+#else
+  #define BIGENDIFY64(X) bswap_64(X)
+#endif
+
+#if defined(__GNUC__)
+  #define BIGENDIFY32(X) __builtin_bswap32(X)
+#elif defined(_MSC_VER)
+  #define BIGENDIFY32(X) _byteswap_uint32(X)
+#else
+  #define BIGENDIFY32(X) bswap_32(X)
+#endif
+#endif
+uint64_t bswap_64(uint64_t v) {
+  uint64_t nv = 0;
+  int bytes = 8;
+  while (bytes--) {
+    nv <<= 8;
+    nv |= (v & 0xff);
+    v >>= 8;
+  }
+  return nv;
+}
+uint32_t bswap_32(uint32_t v) {
+  uint32_t nv = 0;
+  int bytes = 4;
+  // This while loop can be replaced with a single pshuffb command on intel.
+  while (bytes--) {
+    nv <<= 8;
+    nv |= (v & 0xff);
+    v >>= 8;
+  }
+  return nv;
+}
 
 void vp9_reader_fill(vp9_reader *r) {
   const uint8_t *const buffer_end = r->buffer_end;
@@ -39,11 +82,10 @@ void vp9_reader_fill(vp9_reader *r) {
   const uint8_t *buffer_start = buffer;
   BD_VALUE value = r->value;
   int count = r->count;
-  int shift = BD_VALUE_SIZE - CHAR_BIT - (count + CHAR_BIT);
-  int loop_end = 0;
   const size_t bytes_left = buffer_end - buffer;
   const size_t bits_left = bytes_left * CHAR_BIT;
-  const int x = (int)(shift + CHAR_BIT - bits_left);
+  int shift = BD_VALUE_SIZE - CHAR_BIT - (count + CHAR_BIT);
+  const int bits_over = (int)(shift + CHAR_BIT - bits_left);
 
   if (r->decrypt_cb) {
     size_t n = MIN(sizeof(r->clear_buffer), bytes_left);
@@ -52,13 +94,20 @@ void vp9_reader_fill(vp9_reader *r) {
     buffer_start = r->clear_buffer;
   }
 
-  if (x >= 0) {
+  if (bits_over < 0) {
+  #if UINTPTR_MAX == 0xffffffffffffffff
+      BD_VALUE big_endian_values = BIGENDIFY64(*((const BD_VALUE *) buffer));
+  #else
+      BD_VALUE big_endian_values = BIGENDIFY32(*((const BD_VALUE *) buffer));
+  #endif
+      int bits = (shift & 0xfffffff8) + CHAR_BIT;
+      BD_VALUE nv = big_endian_values >> (BD_VALUE_SIZE - bits);
+      count += bits;
+      buffer += (bits >> 3);
+      value = r->value | (nv << (shift & 0x7));
+  } else {
     count += LOTS_OF_BITS;
-    loop_end = x;
-  }
-
-  if (x < 0 || bits_left) {
-    while (shift >= loop_end) {
+    while (shift >= bits_over) {
       count += CHAR_BIT;
       value |= (BD_VALUE)*buffer++ << shift;
       shift -= CHAR_BIT;
