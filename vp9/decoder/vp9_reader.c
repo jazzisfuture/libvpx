613505e05003b6adac1bbb3797b573cea66eb9cc
Jim Bankoski <jimbankoski@google.com>
2015-07-14 09:19:01 -0700

    Fill buffer speed up
    Eliminates the byte by byte read from bool decoder,  by reading
    in a size_t and then shifting it into place.
    
    Change-Id: I0ed8c7b6f942847e79cc90105dc1d2b5b3deb0d6

--- vp9/decoder/vp9_reader.c
+++ vp9/decoder/vp9_reader.c
@@ -7,11 +7,16 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
+#include <stdlib.h>
 
 #include "vpx_ports/mem.h"
 #include "vpx_mem/vpx_mem.h"
 
+#include "./vpx_config.h"
 #include "vp9/decoder/vp9_reader.h"
+
+#include "vpx_util/endian_inl.h"
+
 
 int vp9_reader_init(vp9_reader *r,
                     const uint8_t *buffer,
@@ -39,11 +44,9 @@ void vp9_reader_fill(vp9_reader *r) {
   const uint8_t *buffer_start = buffer;
   BD_VALUE value = r->value;
   int count = r->count;
-  int shift = BD_VALUE_SIZE - CHAR_BIT - (count + CHAR_BIT);
-  int loop_end = 0;
   const size_t bytes_left = buffer_end - buffer;
   const size_t bits_left = bytes_left * CHAR_BIT;
-  const int x = (int)(shift + CHAR_BIT - bits_left);
+  int shift = BD_VALUE_SIZE - CHAR_BIT - (count + CHAR_BIT);
 
   if (r->decrypt_cb) {
     size_t n = MIN(sizeof(r->clear_buffer), bytes_left);
@@ -51,17 +54,34 @@ void vp9_reader_fill(vp9_reader *r) {
     buffer = r->clear_buffer;
     buffer_start = r->clear_buffer;
   }
+  if (bits_left > BD_VALUE_SIZE) {
+      const int bits = (shift & 0xfffffff8) + CHAR_BIT;
+      BD_VALUE nv;
+      BD_VALUE big_endian_values;
+      memcpy(&big_endian_values, buffer, sizeof(BD_VALUE));
+#if SIZE_MAX == 0xffffffffffffffffULL
+        big_endian_values = HToBE64(big_endian_values);
+#else
+        big_endian_values = HToBE32(big_endian_values);
+#endif
+      nv = big_endian_values >> (BD_VALUE_SIZE - bits);
+      count += bits;
+      buffer += (bits >> 3);
+      value = r->value | (nv << (shift & 0x7));
+  } else {
+    const int bits_over = (int)(shift + CHAR_BIT - bits_left);
+    int loop_end = 0;
+    if (bits_over >= 0) {
+      count += LOTS_OF_BITS;
+      loop_end = bits_over;
+    }
 
-  if (x >= 0) {
-    count += LOTS_OF_BITS;
-    loop_end = x;
-  }
-
-  if (x < 0 || bits_left) {
-    while (shift >= loop_end) {
-      count += CHAR_BIT;
-      value |= (BD_VALUE)*buffer++ << shift;
-      shift -= CHAR_BIT;
+    if (bits_over < 0 || bits_left) {
+      while (shift >= loop_end) {
+        count += CHAR_BIT;
+        value |= (BD_VALUE)*buffer++ << shift;
+        shift -= CHAR_BIT;
+      }
     }
   }
 
