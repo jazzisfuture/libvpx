f9934f6afd5680913aa88ed93b7ab1719ec4d3f9
Scott LaVarnway <slavarnway@google.com>
2015-07-20 09:47:37 -0700

    WIP: VP9 ssse3 scaled vertical convolve
    Change-Id: I71d5994e21813554a927d35ebcc26bf7a68984fd

--- vp9/common/x86/vp9_asm_stubs.c
+++ vp9/common/x86/vp9_asm_stubs.c
@@ -74,8 +74,8 @@ FUN_CONV_1D(avg_vert, y_step_q4, filter_y, v, src - src_stride * 3, avg_, sse2);
 //                             const int16_t *filter_x, int x_step_q4,
 //                             const int16_t *filter_y, int y_step_q4,
 //                             int w, int h);
-FUN_CONV_2D(, sse2);
-FUN_CONV_2D(avg_ , sse2);
+FUN_CONV_2D(, sse2, );
+FUN_CONV_2D(avg_ , sse2, );
 
 #if CONFIG_VP9_HIGHBITDEPTH && ARCH_X86_64
 highbd_filter8_1dfunction vp9_highbd_filter_block1d16_v8_sse2;
@@ -156,7 +156,7 @@ HIGH_FUN_CONV_1D(avg_vert, y_step_q4, filter_y, v, src - src_stride * 3, avg_,
 //                                    const int16_t *filter_x, int x_step_q4,
 //                                    const int16_t *filter_y, int y_step_q4,
 //                                    int w, int h, int bd);
-HIGH_FUN_CONV_2D(, sse2);
-HIGH_FUN_CONV_2D(avg_ , sse2);
+HIGH_FUN_CONV_2D(, sse2, );
+HIGH_FUN_CONV_2D(avg_ , sse2, );
 #endif  // CONFIG_VP9_HIGHBITDEPTH && ARCH_X86_64
 #endif  // HAVE_SSE2
