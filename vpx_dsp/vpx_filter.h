7c769ff7c3eb1726479945c5205118a6f47ac78c
Zoe Liu <zoeliu@google.com>
2015-07-22 10:40:42 -0700

    Code refactor on InterpKernel
    It in essence refactors the code for both the interpolation
    filtering and the convolution. This change includes the moving
    of all the files as well as the changing of the code from vp9_
    prefix to vpx_ prefix accordingly, for underneath architectures:
    (1) x86;
    (2) arm/neon; and
    (3) mips/msa.
    The work on mips/drsp2 will be done in a separate change list.
    
    Change-Id: Ic3ce7fb7f81210db7628b373c73553db68793c46

---
+++ vpx_dsp/vpx_filter.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2011 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef VPX_DSP_FILTER_H_
+#define VPX_DSP_FILTER_H_
+
+#include "./vpx_config.h"
+#include "vpx/vpx_integer.h"
+#include "vpx_ports/mem.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FILTER_BITS 7
+
+#define SUBPEL_BITS 4
+#define SUBPEL_MASK ((1 << SUBPEL_BITS) - 1)
+#define SUBPEL_SHIFTS (1 << SUBPEL_BITS)
+#define SUBPEL_TAPS 8
+
+typedef int16_t InterpKernel[SUBPEL_TAPS];
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // VPX_DSP_FILTER_H_
