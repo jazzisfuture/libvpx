dd4ec25afd6a626129063fd893d25965f5b48a68
Zoe Liu <zoeliu@google.com>
2015-07-28 10:52:24 -0700

    Refactor mips/dspr2 on convolution.
    Change-Id: If59a39d5a92c261537342726f94bb7f7f26dfff3

--- vpx_dsp/mips/vpx_convolve2_avg_dspr2.c
+++ vpx_dsp/mips/vpx_convolve2_avg_dspr2.c
@@ -11,30 +11,28 @@
 #include <assert.h>
 #include <stdio.h>
 
-#include "./vpx_config.h"
-#include "./vp9_rtcd.h"
-#include "vp9/common/vp9_common.h"
-#include "vpx/vpx_integer.h"
+#include "./vpx_dsp_rtcd.h"
 #include "vpx_ports/mem.h"
-#include "vp9/common/vp9_convolve.h"
-#include "vp9/common/mips/dspr2/vp9_common_dspr2.h"
+#include "vpx_dsp/vpx_dsp_common.h"
+#include "vpx_dsp/vpx_convolve.h"
+#include "vpx_dsp/mips/vpx_common_dspr2.h"
 
 #if HAVE_DSPR2
-static void convolve_bi_vert_4_dspr2(const uint8_t *src,
-                                     int32_t src_stride,
-                                     uint8_t *dst,
-                                     int32_t dst_stride,
-                                     const int16_t *filter_y,
-                                     int32_t w,
-                                     int32_t h) {
+static void convolve_bi_avg_vert_4_dspr2(const uint8_t *src,
+                                         int32_t src_stride,
+                                         uint8_t *dst,
+                                         int32_t dst_stride,
+                                         const int16_t *filter_y,
+                                         int32_t w,
+                                         int32_t h) {
   int32_t       x, y;
   const uint8_t *src_ptr;
   uint8_t       *dst_ptr;
-  uint8_t       *cm = vp9_ff_cropTbl;
+  uint8_t       *cm = vpx_ff_cropTbl;
   uint32_t      vector4a = 64;
   uint32_t      load1, load2;
   uint32_t      p1, p2;
-  uint32_t      scratch1;
+  uint32_t      scratch1, scratch2;
   uint32_t      store1, store2;
   int32_t       Temp1, Temp2;
   const int16_t *filter = &filter_y[3];
@@ -66,7 +64,6 @@ static void convolve_bi_vert_4_dspr2(const uint8_t *src,
 
           "preceu.ph.qbr    %[scratch1],  %[load1]                        \n\t"
           "preceu.ph.qbr    %[p1],        %[load2]                        \n\t"
-
           "precrq.ph.w      %[p2],        %[p1],          %[scratch1]     \n\t" /* pixel 2 */
           "append           %[p1],        %[scratch1],    16              \n\t" /* pixel 1 */
 
@@ -75,7 +72,6 @@ static void convolve_bi_vert_4_dspr2(const uint8_t *src,
 
           "preceu.ph.qbl    %[scratch1],  %[load1]                        \n\t"
           "preceu.ph.qbl    %[p1],        %[load2]                        \n\t"
-
           "precrq.ph.w      %[p2],        %[p1],          %[scratch1]     \n\t" /* pixel 2 */
           "append           %[p1],        %[scratch1],    16              \n\t" /* pixel 1 */
 
@@ -85,30 +81,39 @@ static void convolve_bi_vert_4_dspr2(const uint8_t *src,
           "extp             %[Temp1],     $ac0,           31              \n\t"
           "extp             %[Temp2],     $ac1,           31              \n\t"
 
+          "lbu              %[scratch1],  0(%[dst_ptr])                   \n\t"
+          "lbu              %[scratch2],  1(%[dst_ptr])                   \n\t"
+
           "lbux             %[store1],    %[Temp1](%[cm])                 \n\t"
+          "addqh_r.w        %[store1],    %[store1],      %[scratch1]     \n\t" /* pixel 1 */
           "extp             %[Temp1],     $ac2,           31              \n\t"
 
           "lbux             %[store2],    %[Temp2](%[cm])                 \n\t"
+          "addqh_r.w        %[store2],    %[store2],      %[scratch2]     \n\t" /* pixel 2 */
           "extp             %[Temp2],     $ac3,           31              \n\t"
+          "lbu              %[scratch1],  2(%[dst_ptr])                   \n\t"
 
           "sb               %[store1],    0(%[dst_ptr])                   \n\t"
           "sb               %[store2],    1(%[dst_ptr])                   \n\t"
+          "lbu              %[scratch2],  3(%[dst_ptr])                   \n\t"
 
           "lbux             %[store1],    %[Temp1](%[cm])                 \n\t"
           "lbux             %[store2],    %[Temp2](%[cm])                 \n\t"
+          "addqh_r.w        %[store1],    %[store1],      %[scratch1]     \n\t" /* pixel 3 */
+          "addqh_r.w        %[store2],    %[store2],      %[scratch2]     \n\t" /* pixel 4 */
 
           "sb               %[store1],    2(%[dst_ptr])                   \n\t"
           "sb               %[store2],    3(%[dst_ptr])                   \n\t"
 
           : [load1] "=&r" (load1), [load2] "=&r" (load2),
             [p1] "=&r" (p1), [p2] "=&r" (p2),
-            [scratch1] "=&r" (scratch1),
+            [scratch1] "=&r" (scratch1), [scratch2] "=&r" (scratch2),
             [Temp1] "=&r" (Temp1), [Temp2] "=&r" (Temp2),
             [store1] "=&r" (store1), [store2] "=&r" (store2),
             [src_ptr] "+r" (src_ptr)
-          : [filter45] "r" (filter45),[vector4a] "r" (vector4a),
-            [src_stride] "r" (src_stride),
-            [cm] "r" (cm), [dst_ptr] "r" (dst_ptr)
+          : [filter45] "r" (filter45), [vector4a] "r" (vector4a),
+            [src_stride] "r" (src_stride), [cm] "r" (cm),
+            [dst_ptr] "r" (dst_ptr)
       );
     }
 
@@ -118,30 +123,31 @@ static void convolve_bi_vert_4_dspr2(const uint8_t *src,
   }
 }
 
-static void convolve_bi_vert_64_dspr2(const uint8_t *src,
-                                      int32_t src_stride,
-                                      uint8_t *dst,
-                                      int32_t dst_stride,
-                                      const int16_t *filter_y,
-                                      int32_t h) {
+static void convolve_bi_avg_vert_64_dspr2(const uint8_t *src,
+                                          int32_t src_stride,
+                                          uint8_t *dst,
+                                          int32_t dst_stride,
+                                          const int16_t *filter_y,
+                                          int32_t h) {
   int32_t       x, y;
   const uint8_t *src_ptr;
   uint8_t       *dst_ptr;
-  uint8_t       *cm = vp9_ff_cropTbl;
+  uint8_t       *cm = vpx_ff_cropTbl;
   uint32_t      vector4a = 64;
   uint32_t      load1, load2;
   uint32_t      p1, p2;
-  uint32_t      scratch1;
+  uint32_t      scratch1, scratch2;
   uint32_t      store1, store2;
   int32_t       Temp1, Temp2;
   const int16_t *filter = &filter_y[3];
-  uint32_t      filter45;
+  uint32_t filter45;;
 
   filter45 = ((const int32_t *)filter)[0];
 
   for (y = h; y--;) {
     /* prefetch data to cache memory */
     prefetch_store(dst + dst_stride);
+    prefetch_store(dst + dst_stride + 32);
 
     for (x = 0; x < 64; x += 4) {
       src_ptr = src + x;
@@ -163,7 +169,6 @@ static void convolve_bi_vert_64_dspr2(const uint8_t *src,
 
           "preceu.ph.qbr    %[scratch1],  %[load1]                        \n\t"
           "preceu.ph.qbr    %[p1],        %[load2]                        \n\t"
-
           "precrq.ph.w      %[p2],        %[p1],          %[scratch1]     \n\t" /* pixel 2 */
           "append           %[p1],        %[scratch1],    16              \n\t" /* pixel 1 */
 
@@ -172,7 +177,6 @@ static void convolve_bi_vert_64_dspr2(const uint8_t *src,
 
           "preceu.ph.qbl    %[scratch1],  %[load1]                        \n\t"
           "preceu.ph.qbl    %[p1],        %[load2]                        \n\t"
-
           "precrq.ph.w      %[p2],        %[p1],          %[scratch1]     \n\t" /* pixel 2 */
           "append           %[p1],        %[scratch1],    16              \n\t" /* pixel 1 */
 
@@ -182,30 +186,39 @@ static void convolve_bi_vert_64_dspr2(const uint8_t *src,
           "extp             %[Temp1],     $ac0,           31              \n\t"
           "extp             %[Temp2],     $ac1,           31              \n\t"
 
+          "lbu              %[scratch1],  0(%[dst_ptr])                   \n\t"
+          "lbu              %[scratch2],  1(%[dst_ptr])                   \n\t"
+
           "lbux             %[store1],    %[Temp1](%[cm])                 \n\t"
+          "addqh_r.w        %[store1],    %[store1],      %[scratch1]     \n\t" /* pixel 1 */
           "extp             %[Temp1],     $ac2,           31              \n\t"
 
           "lbux             %[store2],    %[Temp2](%[cm])                 \n\t"
+          "addqh_r.w        %[store2],    %[store2],      %[scratch2]     \n\t" /* pixel 2 */
           "extp             %[Temp2],     $ac3,           31              \n\t"
+          "lbu              %[scratch1],  2(%[dst_ptr])                   \n\t"
 
           "sb               %[store1],    0(%[dst_ptr])                   \n\t"
           "sb               %[store2],    1(%[dst_ptr])                   \n\t"
+          "lbu              %[scratch2],  3(%[dst_ptr])                   \n\t"
 
           "lbux             %[store1],    %[Temp1](%[cm])                 \n\t"
           "lbux             %[store2],    %[Temp2](%[cm])                 \n\t"
+          "addqh_r.w        %[store1],    %[store1],      %[scratch1]     \n\t" /* pixel 3 */
+          "addqh_r.w        %[store2],    %[store2],      %[scratch2]     \n\t" /* pixel 4 */
 
           "sb               %[store1],    2(%[dst_ptr])                   \n\t"
           "sb               %[store2],    3(%[dst_ptr])                   \n\t"
 
           : [load1] "=&r" (load1), [load2] "=&r" (load2),
             [p1] "=&r" (p1), [p2] "=&r" (p2),
-            [scratch1] "=&r" (scratch1),
+            [scratch1] "=&r" (scratch1), [scratch2] "=&r" (scratch2),
             [Temp1] "=&r" (Temp1), [Temp2] "=&r" (Temp2),
             [store1] "=&r" (store1), [store2] "=&r" (store2),
             [src_ptr] "+r" (src_ptr)
-          : [filter45] "r" (filter45),[vector4a] "r" (vector4a),
-            [src_stride] "r" (src_stride),
-            [cm] "r" (cm), [dst_ptr] "r" (dst_ptr)
+          : [filter45] "r" (filter45), [vector4a] "r" (vector4a),
+            [src_stride] "r" (src_stride), [cm] "r" (cm),
+            [dst_ptr] "r" (dst_ptr)
       );
     }
 
@@ -215,11 +228,11 @@ static void convolve_bi_vert_64_dspr2(const uint8_t *src,
   }
 }
 
-void vp9_convolve2_vert_dspr2(const uint8_t *src, ptrdiff_t src_stride,
-                              uint8_t *dst, ptrdiff_t dst_stride,
-                              const int16_t *filter_x, int x_step_q4,
-                              const int16_t *filter_y, int y_step_q4,
-                              int w, int h) {
+void vpx_convolve2_avg_vert_dspr2(const uint8_t *src, ptrdiff_t src_stride,
+                                  uint8_t *dst, ptrdiff_t dst_stride,
+                                  const int16_t *filter_x, int x_step_q4,
+                                  const int16_t *filter_y, int y_step_q4,
+                                  int w, int h) {
   if (16 == y_step_q4) {
     uint32_t pos = 38;
 
@@ -233,34 +246,34 @@ void vp9_convolve2_vert_dspr2(const uint8_t *src, ptrdiff_t src_stride,
     prefetch_store(dst);
 
     switch (w) {
-      case 4 :
-      case 8 :
-      case 16 :
-      case 32 :
-        convolve_bi_vert_4_dspr2(src, src_stride,
-                                 dst, dst_stride,
-                                 filter_y, w, h);
+      case 4:
+      case 8:
+      case 16:
+      case 32:
+        convolve_bi_avg_vert_4_dspr2(src, src_stride,
+                                     dst, dst_stride,
+                                     filter_y, w, h);
         break;
-      case 64 :
+      case 64:
         prefetch_store(dst + 32);
-        convolve_bi_vert_64_dspr2(src, src_stride,
-                                  dst, dst_stride,
-                                  filter_y, h);
+        convolve_bi_avg_vert_64_dspr2(src, src_stride,
+                                      dst, dst_stride,
+                                      filter_y, h);
         break;
       default:
-        vp9_convolve8_vert_c(src, src_stride,
+        vpx_convolve8_avg_vert_c(src, src_stride,
+                                 dst, dst_stride,
+                                 filter_x, x_step_q4,
+                                 filter_y, y_step_q4,
+                                 w, h);
+        break;
+    }
+  } else {
+    vpx_convolve8_avg_vert_c(src, src_stride,
                              dst, dst_stride,
                              filter_x, x_step_q4,
                              filter_y, y_step_q4,
                              w, h);
-        break;
-    }
-  } else {
-    vp9_convolve8_vert_c(src, src_stride,
-                         dst, dst_stride,
-                         filter_x, x_step_q4,
-                         filter_y, y_step_q4,
-                         w, h);
   }
 }
 #endif
