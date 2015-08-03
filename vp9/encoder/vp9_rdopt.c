2acc9e5c28e80eef3ca964a0745e86dd60acb74b
Zoe Liu <zoeliu@google.com>
2015-08-03 14:26:36 -0700

    New experiment added: NEW_EDGE
    It is to add one more option for the WEDGE_PARTITION that searches for
    separate motion vectors along one wedge option over the same reference
    frame.
    
    Currently only LAST_FRAME and GOLDEN_FRAME are considered for the same
    ref but different compound motion vector mode, namely NEW_NEW2MV.
    
    Change-Id: I28acdaa7534089725adc9290b5f1d8a72670e268

--- vp9/encoder/vp9_rdopt.c
+++ vp9/encoder/vp9_rdopt.c
@@ -140,6 +140,15 @@ static const MODE_DEFINITION vp9_mode_order[MAX_MODES] = {
 
   {ZERO_ZEROMV,    {LAST_FRAME,   ALTREF_FRAME}},
   {ZERO_ZEROMV,    {GOLDEN_FRAME, ALTREF_FRAME}},
+
+#if CONFIG_NEW_WEDGE
+  {NEW_NEW2MV,     {LAST_FRAME,   LAST_FRAME}},
+  {NEW_NEW2MV,     {GOLDEN_FRAME, GOLDEN_FRAME}},
+// NOTE (zoeliu): May consider ALTREF_FRAME later as we save 1 bit by not
+// considering it currently.
+// {NEW_NEW2MV,     {ALTREF_FRAME, ALTREF_FRAME}},
+#endif  // CONFIG_NEW_WEDGE
+
 #else
   {NEARMV,    {LAST_FRAME,   ALTREF_FRAME}},
   {NEWMV,     {LAST_FRAME,   ALTREF_FRAME}},
@@ -3997,13 +4006,24 @@ static void estimate_ref_frame_costs(const VP9_COMMON *cm,
     }
     if (cm->reference_mode != SINGLE_REFERENCE) {
       vp9_prob ref_comp_p = vp9_get_pred_prob_comp_ref_p(cm, xd);
+#if CONFIG_NEW_WEDGE && CONFIG_NEW_INTER
+      vp9_prob ref_same_comp_p = vp9_get_pred_prob_comp_same_ref_p(cm, xd);
+#endif  // CONFIG_NEW_WEDGE && CONFIG_NEW_INTER
+
       unsigned int base_cost = vp9_cost_bit(intra_inter_p, 1);
 
       if (cm->reference_mode == REFERENCE_MODE_SELECT)
         base_cost += vp9_cost_bit(comp_inter_p, 1);
 
+#if CONFIG_NEW_WEDGE && CONFIG_NEW_INTER
+      ref_costs_comp[LAST_FRAME]   = base_cost + vp9_cost_bit(ref_comp_p, 0)
+                                     + vp9_cost_bit(ref_same_comp_p, 1);
+      ref_costs_comp[GOLDEN_FRAME] = base_cost + vp9_cost_bit(ref_comp_p, 1)
+                                     + vp9_cost_bit(ref_same_comp_p, 1);
+#else
       ref_costs_comp[LAST_FRAME]   = base_cost + vp9_cost_bit(ref_comp_p, 0);
       ref_costs_comp[GOLDEN_FRAME] = base_cost + vp9_cost_bit(ref_comp_p, 1);
+#endif  // CONFIG_NEW_WEDGE && CONFIG_NEW_INTER
     } else {
       ref_costs_comp[LAST_FRAME]   = 512;
       ref_costs_comp[GOLDEN_FRAME] = 512;
@@ -4560,7 +4580,12 @@ static void do_masked_motion_search_indexed(VP9_COMP *cpi, MACROBLOCK *x,
   vp9_generate_masked_weight(wedge_index, sb_type, h, w, mask, mask_stride);
   // vp9_generate_hard_mask(wedge_index, sb_type, h, w, mask, mask_stride);
 
+#if CONFIG_NEW_WEDGE
+  // NOTE: 3 - same reference frame but 2 separate mvs
+  if (which == 0 || which == 2 || which == 3)
+#else
   if (which == 0 || which == 2)
+#endif  // CONFIG_NEW_WEDGE
     do_masked_motion_search(cpi, x, mask, mask_stride, bsize,
                             mi_row, mi_col, &tmp_mv[0], &rate_mv[0],
 #if CONFIG_NEW_INTER
@@ -4568,11 +4593,26 @@ static void do_masked_motion_search_indexed(VP9_COMP *cpi, MACROBLOCK *x,
 #endif  // CONFIG_NEW_INTER
                             0);
 
+#if CONFIG_NEW_WEDGE
+  // NOTE: 3 - same reference frame but 2 separate mvs
+  if (which == 1 || which == 2 || which == 3) {
+#else
   if (which == 1 || which == 2) {
+#endif  // CONFIG_NEW_WEDGE
     for (i = 0; i < h; ++i)
       for (j = 0; j < w; ++j)
         mask[i * mask_stride + j] = 64 - mask[i * mask_stride + j];
 
+#if CONFIG_NEW_WEDGE
+    if (which == 3)
+      do_masked_motion_search(cpi, x, mask, mask_stride, bsize,
+                              mi_row, mi_col, &tmp_mv[1], &rate_mv[1],
+#if CONFIG_NEW_INTER
+                              &ref_mv[0],
+#endif  // CONFIG_NEW_INTER
+                              0);
+    else
+#endif  // CONFIG_NEW_WEDGE
     do_masked_motion_search(cpi, x, mask, mask_stride, bsize,
                             mi_row, mi_col, &tmp_mv[1], &rate_mv[1],
 #if CONFIG_NEW_INTER
@@ -4829,7 +4869,7 @@ static int64_t handle_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
 
 #if !(CONFIG_INTERINTRA || CONFIG_WEDGE_PARTITION)
       *rate2 += rate_mv;
-#endif
+#endif  // !(CONFIG_INTERINTRA || CONFIG_WEDGE_PARTITION)
     } else {
       int_mv tmp_mv;
 
@@ -5052,16 +5092,19 @@ static int64_t handle_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
     int64_t best_rd_nowedge = INT64_MAX;
     int64_t best_rd_wedge = INT64_MAX;
     int wedge_types;
+
     mbmi->use_wedge_interinter = 0;
     rs = vp9_cost_bit(cm->fc.wedge_interinter_prob[bsize], 0);
     vp9_build_inter_predictors_sb(xd, mi_row, mi_col, bsize);
     model_rd_for_sb(cpi, bsize, x, xd, &rate_sum, &dist_sum, NULL, NULL);
     rd = RDCOST(x->rdmult, x->rddiv, rs + rate_mv_tmp + rate_sum, dist_sum);
     best_rd_nowedge = rd;
+
     mbmi->use_wedge_interinter = 1;
     rs = get_wedge_bits(bsize) * 256 +
         vp9_cost_bit(cm->fc.wedge_interinter_prob[bsize], 1);
     wedge_types = (1 << get_wedge_bits(bsize));
+
     if (have_newmv_in_inter_mode(this_mode)) {
       int_mv tmp_mv[2];
       int rate_mvs[2], tmp_rate_mv = 0;
@@ -5097,6 +5140,21 @@ static int64_t handle_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
                                         ref_mv, 1);
         tmp_rate_mv = rate_mvs[1];
         mbmi->mv[1].as_int = tmp_mv[1].as_int;
+#if CONFIG_NEW_WEDGE
+      } else if (this_mode == NEW_NEW2MV) {
+        // NOTE: To search for 2 separate mvs along the wedge over the same
+        // reference frame.
+        do_masked_motion_search_indexed(cpi, x, mbmi->interinter_wedge_index,
+                                        bsize, mi_row, mi_col, tmp_mv, rate_mvs,
+                                        ref_mv, 3);
+        tmp_rate_mv = rate_mvs[0] + rate_mvs[1];
+        mbmi->mv[0].as_int = tmp_mv[0].as_int;
+        mbmi->mv[1].as_int = tmp_mv[1].as_int;
+        if (mbmi->mv[0].as_int == mbmi->mv[1].as_int) {
+          // NOTE: If same reference and same mvs along the wedge, then skip.
+          tmp_rate_mv = INT64_MAX;
+        }
+#endif  // CONFIG_NEW_WEDGE
       }
 #else
       do_masked_motion_search_indexed(cpi, x, mbmi->interinter_wedge_index,
@@ -5106,9 +5164,19 @@ static int64_t handle_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
       mbmi->mv[0].as_int = tmp_mv[0].as_int;
       mbmi->mv[1].as_int = tmp_mv[1].as_int;
 #endif  // CONFIG_NEW_INTER
+
+#if CONFIG_NEW_WEDGE
+      if (tmp_rate_mv == INT64_MAX) {
+        rd = INT64_MAX;
+      } else {
+#endif  // CONFIG_NEW_WEDGE
       vp9_build_inter_predictors_sb(xd, mi_row, mi_col, bsize);
       model_rd_for_sb(cpi, bsize, x, xd, &rate_sum, &dist_sum, NULL, NULL);
       rd = RDCOST(x->rdmult, x->rddiv, rs + tmp_rate_mv + rate_sum, dist_sum);
+#if CONFIG_NEW_WEDGE
+      }
+#endif  // CONFIG_NEW_WEDGE
+
       if (rd < best_rd_wedge) {
         best_rd_wedge = rd;
       } else {
@@ -5116,6 +5184,7 @@ static int64_t handle_inter_mode(VP9_COMP *cpi, MACROBLOCK *x,
         mbmi->mv[1].as_int = cur_mv[1].as_int;
         tmp_rate_mv = rate_mv_tmp;
       }
+
       if (best_rd_wedge < best_rd_nowedge) {
         mbmi->use_wedge_interinter = 1;
         mbmi->interinter_wedge_index = best_wedge_index;
