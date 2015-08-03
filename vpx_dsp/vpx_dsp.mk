7db4adf0b4bb1d448f4536921378baf37faf9893
Jingning Han <jingning@google.com>
2015-08-03 10:17:45 -0700

    Add common_dspr2.c file to vpx_dsp/mips
    Move the declaration of commonly referenced variables to
    vpx_dsp/mips/common_dspr2.c.
    
    Change-Id: Ia51287b02e2ac5cfae0fba98c721f0810618f28e

--- vpx_dsp/vpx_dsp.mk
+++ vpx_dsp/vpx_dsp.mk
@@ -54,6 +54,11 @@ DSP_SRCS-$(HAVE_DSPR2)  += mips/intrapred8_dspr2.c
 DSP_SRCS-$(HAVE_DSPR2)  += mips/intrapred16_dspr2.c
 endif  # CONFIG_VP9
 
+ifeq ($(CONFIG_VP9),yes)
+DSP_SRCS-$(HAVE_DSPR2)  += mips/common_dspr2.h
+DSP_SRCS-$(HAVE_DSPR2)  += mips/common_dspr2.c
+endif  # CONFIG_VP9
+
 # interpolation filters
 DSP_SRCS-yes += vpx_convolve.c
 DSP_SRCS-yes += vpx_convolve.h
