3bfb7cc8f0e4710e5d5e772ee90fb69dd0e34733
Jingning Han <jingning@google.com>
2015-08-03 10:50:32 -0700

    Move inverse transfrom dspr2 from vp9 to vpx_dsp
    WIP
    
    Change-Id: Ia9cf7c31cab4ba3dd6b9bb668c4b3e84bd55cf69

--- vpx_dsp/mips/inv_txfm_dspr2.h
+++ vpx_dsp/mips/inv_txfm_dspr2.h
@@ -8,8 +8,8 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef VP9_COMMON_MIPS_DSPR2_VP9_COMMON_DSPR2_H_
-#define VP9_COMMON_MIPS_DSPR2_VP9_COMMON_DSPR2_H_
+#ifndef VPX_DSP_MIPS_INV_TXFM_DSPR2_H_
+#define VPX_DSP_MIPS_INV_TXFM_DSPR2_H_
 
 #include <assert.h>
 
@@ -56,4 +56,4 @@ void vp9_idct32_cols_add_blk_dspr2(int16_t *input, uint8_t *dest,
 }  // extern "C"
 #endif
 
-#endif  // VP9_COMMON_MIPS_DSPR2_VP9_COMMON_DSPR2_H_
+#endif  // VPX_DSP_MIPS_INV_TXFM_DSPR2_H_
