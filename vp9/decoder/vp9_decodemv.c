090cf0f73b4c1974cc51c5a6ca3d0e153740a51f
Zoe Liu <zoeliu@google.com>
2015-08-03 14:26:36 -0700

    New experiment added: NEW_EDGE
    It is to add one more option for the WEDGE_PARTITION that searches for
    separate motion vectors along one wedge option over the same reference
    frame.
    
    Currently only LAST_FRAME and GOLDEN_FRAME are considered for the same
    ref. The same compound inter mode NEW_NEWMV is reused, coupled with the
    same reference frame choice: LAST_FRAME/LAST_FRAME, GOLDEN_FRAME/
    GOLDEN_FRAME. Note that no wedge partition is available for the sub8x8
    block size option.
    
    Change-Id: I28acdaa7534089725adc9290b5f1d8a72670e268

--- vp9/decoder/vp9_decodemv.c
+++ vp9/decoder/vp9_decodemv.c
@@ -700,11 +700,28 @@ static void read_ref_frames(VP9_COMMON *const cm, MACROBLOCKD *const xd,
     // FIXME(rbultje) I'm pretty sure this breaks segmentation ref frame coding
     if (mode == COMPOUND_REFERENCE) {
       const int idx = cm->ref_frame_sign_bias[cm->comp_fixed_ref];
+
+#if CONFIG_NEW_INTER && CONFIG_WEDGE_PARTITION && CONFIG_NEW_WEDGE
+      const int ctx0 = vp9_get_pred_context_comp_same_ref_p(xd);
+      const int bit0 = vp9_read(r, fc->comp_same_ref_prob[ctx0]);
+#endif  // CONFIG_NEW_INTER && CONFIG_WEDGE_PARTITION && CONFIG_NEW_WEDGE
+
       const int ctx = vp9_get_pred_context_comp_ref_p(cm, xd);
       const int bit = vp9_read(r, fc->comp_ref_prob[ctx]);
-      if (!cm->frame_parallel_decoding_mode)
+
+      if (!cm->frame_parallel_decoding_mode) {
+#if CONFIG_NEW_INTER && CONFIG_WEDGE_PARTITION && CONFIG_NEW_WEDGE
+        ++counts->comp_same_ref[ctx0][bit0];
+#endif  // CONFIG_NEW_INTER && CONFIG_WEDGE_PARTITION && CONFIG_NEW_WEDGE
         ++counts->comp_ref[ctx][bit];
+      }
+
+#if CONFIG_NEW_INTER && CONFIG_WEDGE_PARTITION && CONFIG_NEW_WEDGE
+      // bit0 == 1: Two reference frames are identical.
+      ref_frame[idx] = bit0 ? cm->comp_var_ref[bit] : cm->comp_fixed_ref;
+#else
       ref_frame[idx] = cm->comp_fixed_ref;
+#endif  // CONFIG_NEW_INTER && CONFIG_WEDGE_PARTITION && CONFIG_NEW_WEDGE
       ref_frame[!idx] = cm->comp_var_ref[bit];
     } else if (mode == SINGLE_REFERENCE) {
       const int ctx0 = vp9_get_pred_context_single_ref_p1(xd);
