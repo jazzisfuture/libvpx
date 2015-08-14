1b26dab68e3342a6496f5d677f82bac925404f39
Yaowu Xu <yaowu@google.com>
2015-08-14 12:30:10 -0700

    write correct codecID for vp10 webm file
    Change-Id: Ifeb6073c4a687a73540a0f8aef0d834f4712c193

--- webmenc.cc
+++ webmenc.cc
@@ -49,7 +49,8 @@ void write_webm_file_header(struct EbmlGlobal *glob,
       static_cast<mkvmuxer::VideoTrack*>(
           segment->GetTrackByNumber(video_track_id));
   video_track->SetStereoMode(stereo_fmt);
-  video_track->set_codec_id(fourcc == VP8_FOURCC ? "V_VP8" : "V_VP9");
+  video_track->set_codec_id(fourcc == VP8_FOURCC ?
+      "V_VP8" : (fourcc == VP9_FOURCC ? "V_VP9" :"V_VP10"));
   if (par->numerator > 1 || par->denominator > 1) {
     // TODO(fgalligan): Add support of DisplayUnit, Display Aspect Ratio type
     // to WebM format.
