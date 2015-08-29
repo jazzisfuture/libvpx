5b8428810ec46deadbbc4616f0f397c7771f34ae
Angie Chiang <angiebird@google.com>
2015-08-28 20:39:10 -0700

    add range_check for fdct in vp10
    Unify the style of fdct4() fdct8() fdct16()
    Add fdct32()
    Add range_check() at each stage
    Add unit test at ../../test/vp10_dct_test.cc
    
    Change-Id: I9e912b2c5683862e65c5a21abc3e1c260cca4576

---
+++ test/vp10_dct_test.cc
@@ -0,0 +1,128 @@
+
+#include "third_party/googletest/src/include/gtest/gtest.h"
+#include "vp10/encoder/dct.c"
+#include "test/acm_random.h"
+
+using libvpx_test::ACMRandom;
+
+namespace {
+void reference_dct_1d(double *in, double *out, int size) {
+  const double PI = 3.141592653589793238462643383279502884;
+  const double kInvSqrt2 = 0.707106781186547524400844362104;
+  for(int k = 0; k < size; k++) {
+      out[k] = 0; // initialize out[k]
+      for(int n = 0; n < size; n++) {
+        out[k] += in[n] * cos(PI * (2 * n + 1) * k / (2*size));
+      }
+      if(k == 0)
+          out[k] = out[k] * kInvSqrt2;
+  }
+}
+
+
+TEST(vp10_dct_test, fdct4) {
+  int size = 4;
+  tran_low_t input[4];
+  tran_low_t output[4];
+  double refInput[4];
+  double refOutput[4];
+
+  ACMRandom rnd(ACMRandom::DeterministicSeed());
+
+  for(int r= 0; r< 10000; r++) {
+    for(int i = 0; i < size; i++) {
+      input[i] = rnd.Rand8() - rnd.Rand8(); 
+      refInput[i] = (double)input[i];
+    }
+
+    fdct4(input, output);
+    reference_dct_1d(refInput, refOutput, size);
+
+    for(int i = 0; i < size; i++) {
+      EXPECT_LE(abs(output[i]-(tran_low_t)round(refOutput[i])), 1);
+    }
+  }
+
+}
+
+TEST(vp10_dct_test, fdct8) {
+  int size = 8;
+  tran_low_t input[8];
+  tran_low_t output[8];
+  double refInput[8];
+  double refOutput[8];
+
+  ACMRandom rnd(ACMRandom::DeterministicSeed());
+
+  for(int r = 0; r < 10000; r++) {
+    for(int i = 0; i < size; i++) {
+      input[i] = rnd.Rand8() - rnd.Rand8(); 
+      refInput[i] = (double)input[i];
+    }
+
+    fdct8(input, output);
+    reference_dct_1d(refInput, refOutput, size);
+
+    for(int i = 0; i < size; i++) {
+      EXPECT_LE(abs(output[i]-(tran_low_t)round(refOutput[i])), 1);
+    }
+  }
+
+}
+
+TEST(vp10_dct_test, fdct16) {
+  int size = 16;
+  tran_low_t input[16];
+  tran_low_t output[16];
+  double refInput[16];
+  double refOutput[16];
+
+  ACMRandom rnd(ACMRandom::DeterministicSeed());
+
+  for(int r = 0; r < 10000; r++) {
+    for(int i = 0; i < size; i++) {
+      input[i] = rnd.Rand8() - rnd.Rand8(); 
+      refInput[i] = (double)input[i];
+    }
+
+    fdct16(input, output);
+    reference_dct_1d(refInput, refOutput, size);
+
+    for(int i = 0; i < size; i++) {
+      EXPECT_LE(abs(output[i]-(tran_low_t)round(refOutput[i])), 2);
+    }
+  }
+
+}
+
+TEST(vp10_dct_test, fdct32) {
+  int size = 32;
+  tran_low_t input[32];
+  tran_low_t output[32];
+  double refInput[32];
+  double refOutput[32];
+
+  ACMRandom rnd(ACMRandom::DeterministicSeed());
+
+  for(int r = 0; r < 10000; r++) {
+    for(int i = 0; i < size; i++) {
+      input[i] = rnd.Rand8() - rnd.Rand8(); 
+      refInput[i] = (double)input[i];
+    }
+
+    fdct32(input, output);
+    reference_dct_1d(refInput, refOutput, size);
+
+    for(int i = 0; i < size; i++) {
+      EXPECT_LE(abs(output[i]-(tran_low_t)round(refOutput[i])), 4);
+    }
+  }
+
+}
+
+}
+
+
+
+
+
