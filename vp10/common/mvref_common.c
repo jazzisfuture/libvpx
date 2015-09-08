0c8da86ff204ebee1317e0673879324c67409460
Ronald S. Bultje <rsbultje@gmail.com>
2015-09-08 09:14:37 -0400

    vp10: remove double MV value check.
    This has virtually no effect on coding efficiency, but it is more
    logical from a theoretical perspective (since it makes no sense to
    me that you would exclude a MV from a list just because it's sign-
    inversed value is identical to a value already in a list), and it
    also makes the code simpler (it removes a duplicate value check in
    cases where signbias is equal between the two MVs being compared).
    
    See issue 662.
    
    Change-Id: I23e607c6de150b9f11d1372fb2868b813c322d37

--- vp10/common/mvref_common.c
+++ vp10/common/mvref_common.c
@@ -125,8 +125,7 @@ static void find_mv_refs_idx(const VP10_COMMON *cm, const MACROBLOCKD *xd,
     }
 
     if (prev_frame_mvs->ref_frame[1] > INTRA_FRAME &&
-        prev_frame_mvs->ref_frame[1] != ref_frame &&
-        prev_frame_mvs->mv[1].as_int != prev_frame_mvs->mv[0].as_int) {
+        prev_frame_mvs->ref_frame[1] != ref_frame) {
       int_mv mv = prev_frame_mvs->mv[1];
       if (ref_sign_bias[prev_frame_mvs->ref_frame[1]] !=
           ref_sign_bias[ref_frame]) {
