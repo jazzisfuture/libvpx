996e2ec094b5c96dd8247942ea7216205bcb2162
Marco <marpan@chromium.org>
2015-09-08 08:47:42 -0700

    Add cyclic refresh parameters to svc-layer context.
    For 1 pass CBR spatial-SVC:
    Add cyclic refresh parameters to the svc-layer context.
    
    This allows cyclic refresh (aq-mode=3) to be applied to
    the whole super-frame (all spatial layers).
    This gives a performance improvement for spatial layer encoding.
    
    Addd the aq_mode mode on/off setting as command line option.
    
    Change-Id: Ib9c3b5ba3cb7851bfb8c37d4f911664bef38e165

--- vp9/encoder/vp9_aq_cyclicrefresh.c
+++ vp9/encoder/vp9_aq_cyclicrefresh.c
@@ -452,11 +452,10 @@ void vp9_cyclic_refresh_setup(VP9_COMP *const cpi) {
   const int apply_cyclic_refresh  = apply_cyclic_refresh_bitrate(cm, rc);
   if (cm->current_video_frame == 0)
     cr->low_content_avg = 0.0;
-  // Don't apply refresh on key frame or enhancement layer frames.
+  // Don't apply refresh on key frame or temporal enhancement layer frames.
   if (!apply_cyclic_refresh ||
       (cm->frame_type == KEY_FRAME) ||
-      (cpi->svc.temporal_layer_id > 0) ||
-      (cpi->svc.spatial_layer_id > 0)) {
+      (cpi->svc.temporal_layer_id > 0)) {
     // Set segmentation map to 0 and disable.
     unsigned char *const seg_map = cpi->segmentation_map;
     memset(seg_map, 0, cm->mi_rows * cm->mi_cols);
