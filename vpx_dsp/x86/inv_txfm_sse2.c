54e3370e6237fb6d88d13d84e8bb5d443a596979
Scott LaVarnway <slavarnway@google.com>
2015-09-24 15:34:50 -0700

    test
    _34 and _135 c versions
    
    Change-Id: If0d549b375d0efa924793a3c6392605f0df8c95b

--- vpx_dsp/x86/inv_txfm_sse2.c
+++ vpx_dsp/x86/inv_txfm_sse2.c
@@ -3028,6 +3028,24 @@ void vpx_idct16x16_10_add_sse2(const int16_t *input, uint8_t *dest,
   stp1_31 = stp2_31; \
 }
 
+
+// 2_D: Calculate the results and store them to destination.
+#define CALC_FINAL_ADD(idx, in_a, in_b) \
+{                                                     \
+  const __m128i a = _mm_add_epi16(in_a, in_b); \
+  const __m128i b = _mm_adds_epi16(a, final_rounding); \
+  const __m128i c = _mm_srai_epi16(b, 6); \
+  RECON_AND_STORE(dest + idx * stride, c); \
+}
+
+#define CALC_FINAL_SUB(idx, in_a, in_b) \
+{                                                     \
+  const __m128i a = _mm_sub_epi16(in_a, in_b); \
+  const __m128i b = _mm_adds_epi16(a, final_rounding); \
+  const __m128i c = _mm_srai_epi16(b, 6); \
+  RECON_AND_STORE(dest + idx * stride, c); \
+}
+
 // Only upper-left 8x8 has non-zero coeff
 void vpx_idct32x32_34_add_sse2(const int16_t *input, uint8_t *dest,
                                int stride) {
@@ -3090,16 +3108,16 @@ void vpx_idct32x32_34_add_sse2(const int16_t *input, uint8_t *dest,
   in[6] = _mm_load_si128((const __m128i *)(input + 192));
   in[7] = _mm_load_si128((const __m128i *)(input + 224));
 
-  for (i = 8; i < 32; ++i) {
-    in[i] = _mm_setzero_si128();
-  }
+//  for (i = 8; i < 32; ++i) {
+//    in[i] = _mm_setzero_si128();
+//  }
 
   array_transpose_8x8(in, in);
   // TODO(hkuang): Following transposes are unnecessary. But remove them will
   // lead to performance drop on some devices.
-  array_transpose_8x8(in + 8, in + 8);
-  array_transpose_8x8(in + 16, in + 16);
-  array_transpose_8x8(in + 24, in + 24);
+//  array_transpose_8x8(in + 8, in + 8);
+//  array_transpose_8x8(in + 16, in + 16);
+//  array_transpose_8x8(in + 24, in + 24);
 
   IDCT32_34
 
@@ -3136,53 +3154,313 @@ void vpx_idct32x32_34_add_sse2(const int16_t *input, uint8_t *dest,
   col[29] = _mm_sub_epi16(stp1_2, stp1_29);
   col[30] = _mm_sub_epi16(stp1_1, stp1_30);
   col[31] = _mm_sub_epi16(stp1_0, stp1_31);
+
   for (i = 0; i < 4; i++) {
-    int j;
     const __m128i zero = _mm_setzero_si128();
     // Transpose 32x8 block to 8x32 block
     array_transpose_8x8(col + i * 8, in);
     IDCT32_34
 
     // 2_D: Calculate the results and store them to destination.
-    in[0] = _mm_add_epi16(stp1_0, stp1_31);
-    in[1] = _mm_add_epi16(stp1_1, stp1_30);
-    in[2] = _mm_add_epi16(stp1_2, stp1_29);
-    in[3] = _mm_add_epi16(stp1_3, stp1_28);
-    in[4] = _mm_add_epi16(stp1_4, stp1_27);
-    in[5] = _mm_add_epi16(stp1_5, stp1_26);
-    in[6] = _mm_add_epi16(stp1_6, stp1_25);
-    in[7] = _mm_add_epi16(stp1_7, stp1_24);
-    in[8] = _mm_add_epi16(stp1_8, stp1_23);
-    in[9] = _mm_add_epi16(stp1_9, stp1_22);
-    in[10] = _mm_add_epi16(stp1_10, stp1_21);
-    in[11] = _mm_add_epi16(stp1_11, stp1_20);
-    in[12] = _mm_add_epi16(stp1_12, stp1_19);
-    in[13] = _mm_add_epi16(stp1_13, stp1_18);
-    in[14] = _mm_add_epi16(stp1_14, stp1_17);
-    in[15] = _mm_add_epi16(stp1_15, stp1_16);
-    in[16] = _mm_sub_epi16(stp1_15, stp1_16);
-    in[17] = _mm_sub_epi16(stp1_14, stp1_17);
-    in[18] = _mm_sub_epi16(stp1_13, stp1_18);
-    in[19] = _mm_sub_epi16(stp1_12, stp1_19);
-    in[20] = _mm_sub_epi16(stp1_11, stp1_20);
-    in[21] = _mm_sub_epi16(stp1_10, stp1_21);
-    in[22] = _mm_sub_epi16(stp1_9, stp1_22);
-    in[23] = _mm_sub_epi16(stp1_8, stp1_23);
-    in[24] = _mm_sub_epi16(stp1_7, stp1_24);
-    in[25] = _mm_sub_epi16(stp1_6, stp1_25);
-    in[26] = _mm_sub_epi16(stp1_5, stp1_26);
-    in[27] = _mm_sub_epi16(stp1_4, stp1_27);
-    in[28] = _mm_sub_epi16(stp1_3, stp1_28);
-    in[29] = _mm_sub_epi16(stp1_2, stp1_29);
-    in[30] = _mm_sub_epi16(stp1_1, stp1_30);
-    in[31] = _mm_sub_epi16(stp1_0, stp1_31);
+    CALC_FINAL_ADD(0, stp1_0, stp1_31);
+    CALC_FINAL_ADD(1, stp1_1, stp1_30);
+    CALC_FINAL_ADD(2, stp1_2, stp1_29);
+    CALC_FINAL_ADD(3, stp1_3, stp1_28);
+    CALC_FINAL_ADD(4, stp1_4, stp1_27);
+    CALC_FINAL_ADD(5, stp1_5, stp1_26);
+    CALC_FINAL_ADD(6, stp1_6, stp1_25);
+    CALC_FINAL_ADD(7, stp1_7, stp1_24);
+    CALC_FINAL_ADD(8, stp1_8, stp1_23);
+    CALC_FINAL_ADD(9, stp1_9, stp1_22);
+    CALC_FINAL_ADD(10, stp1_10, stp1_21);
+    CALC_FINAL_ADD(11, stp1_11, stp1_20);
+    CALC_FINAL_ADD(12, stp1_12, stp1_19);
+    CALC_FINAL_ADD(13, stp1_13, stp1_18);
+    CALC_FINAL_ADD(14, stp1_14, stp1_17);
+    CALC_FINAL_ADD(15, stp1_15, stp1_16);
+    CALC_FINAL_SUB(16, stp1_15, stp1_16);
+    CALC_FINAL_SUB(17, stp1_14, stp1_17);
+    CALC_FINAL_SUB(18, stp1_13, stp1_18);
+    CALC_FINAL_SUB(19, stp1_12, stp1_19);
+    CALC_FINAL_SUB(20, stp1_11, stp1_20);
+    CALC_FINAL_SUB(21, stp1_10, stp1_21);
+    CALC_FINAL_SUB(22, stp1_9, stp1_22);
+    CALC_FINAL_SUB(23, stp1_8, stp1_23);
+    CALC_FINAL_SUB(24, stp1_7, stp1_24);
+    CALC_FINAL_SUB(25, stp1_6, stp1_25);
+    CALC_FINAL_SUB(26, stp1_5, stp1_26);
+    CALC_FINAL_SUB(27, stp1_4, stp1_27);
+    CALC_FINAL_SUB(28, stp1_3, stp1_28);
+    CALC_FINAL_SUB(29, stp1_2, stp1_29);
+    CALC_FINAL_SUB(30, stp1_1, stp1_30);
+    CALC_FINAL_SUB(31, stp1_0, stp1_31);
 
-    for (j = 0; j < 32; ++j) {
-      // Final rounding and shift
-      in[j] = _mm_adds_epi16(in[j], final_rounding);
-      in[j] = _mm_srai_epi16(in[j], 6);
-      RECON_AND_STORE(dest + j * stride, in[j]);
-    }
+    dest += 8;
+  }
+}
+
+void vpx_idct32x32_135_add_sse2(const int16_t *input, uint8_t *dest,
+                                 int stride) {
+  const __m128i rounding = _mm_set1_epi32(DCT_CONST_ROUNDING);
+  const __m128i final_rounding = _mm_set1_epi16(1 << 5);
+  const __m128i zero = _mm_setzero_si128();
+
+  // idct constants for each stage
+  const __m128i stg1_0 = pair_set_epi16(cospi_31_64, -cospi_1_64);
+  const __m128i stg1_1 = pair_set_epi16(cospi_1_64, cospi_31_64);
+  const __m128i stg1_2 = pair_set_epi16(cospi_15_64, -cospi_17_64);
+  const __m128i stg1_3 = pair_set_epi16(cospi_17_64, cospi_15_64);
+  const __m128i stg1_4 = pair_set_epi16(cospi_23_64, -cospi_9_64);
+  const __m128i stg1_5 = pair_set_epi16(cospi_9_64, cospi_23_64);
+  const __m128i stg1_6 = pair_set_epi16(cospi_7_64, -cospi_25_64);
+  const __m128i stg1_7 = pair_set_epi16(cospi_25_64, cospi_7_64);
+  const __m128i stg1_8 = pair_set_epi16(cospi_27_64, -cospi_5_64);
+  const __m128i stg1_9 = pair_set_epi16(cospi_5_64, cospi_27_64);
+  const __m128i stg1_10 = pair_set_epi16(cospi_11_64, -cospi_21_64);
+  const __m128i stg1_11 = pair_set_epi16(cospi_21_64, cospi_11_64);
+  const __m128i stg1_12 = pair_set_epi16(cospi_19_64, -cospi_13_64);
+  const __m128i stg1_13 = pair_set_epi16(cospi_13_64, cospi_19_64);
+  const __m128i stg1_14 = pair_set_epi16(cospi_3_64, -cospi_29_64);
+  const __m128i stg1_15 = pair_set_epi16(cospi_29_64, cospi_3_64);
+
+  const __m128i stg2_0 = pair_set_epi16(cospi_30_64, -cospi_2_64);
+  const __m128i stg2_1 = pair_set_epi16(cospi_2_64, cospi_30_64);
+  const __m128i stg2_2 = pair_set_epi16(cospi_14_64, -cospi_18_64);
+  const __m128i stg2_3 = pair_set_epi16(cospi_18_64, cospi_14_64);
+  const __m128i stg2_4 = pair_set_epi16(cospi_22_64, -cospi_10_64);
+  const __m128i stg2_5 = pair_set_epi16(cospi_10_64, cospi_22_64);
+  const __m128i stg2_6 = pair_set_epi16(cospi_6_64, -cospi_26_64);
+  const __m128i stg2_7 = pair_set_epi16(cospi_26_64, cospi_6_64);
+
+  const __m128i stg3_0 = pair_set_epi16(cospi_28_64, -cospi_4_64);
+  const __m128i stg3_1 = pair_set_epi16(cospi_4_64, cospi_28_64);
+  const __m128i stg3_2 = pair_set_epi16(cospi_12_64, -cospi_20_64);
+  const __m128i stg3_3 = pair_set_epi16(cospi_20_64, cospi_12_64);
+  const __m128i stg3_4 = pair_set_epi16(-cospi_4_64, cospi_28_64);
+  const __m128i stg3_5 = pair_set_epi16(cospi_28_64, cospi_4_64);
+  const __m128i stg3_6 = pair_set_epi16(-cospi_28_64, -cospi_4_64);
+  const __m128i stg3_8 = pair_set_epi16(-cospi_20_64, cospi_12_64);
+  const __m128i stg3_9 = pair_set_epi16(cospi_12_64, cospi_20_64);
+  const __m128i stg3_10 = pair_set_epi16(-cospi_12_64, -cospi_20_64);
+
+  const __m128i stg4_0 = pair_set_epi16(cospi_16_64, cospi_16_64);
+  const __m128i stg4_1 = pair_set_epi16(cospi_16_64, -cospi_16_64);
+  const __m128i stg4_2 = pair_set_epi16(cospi_24_64, -cospi_8_64);
+  const __m128i stg4_3 = pair_set_epi16(cospi_8_64, cospi_24_64);
+  const __m128i stg4_4 = pair_set_epi16(-cospi_8_64, cospi_24_64);
+  const __m128i stg4_5 = pair_set_epi16(cospi_24_64, cospi_8_64);
+  const __m128i stg4_6 = pair_set_epi16(-cospi_24_64, -cospi_8_64);
+
+  const __m128i stg6_0 = pair_set_epi16(-cospi_16_64, cospi_16_64);
+
+  __m128i in[32], col[128];
+  __m128i stp1_0, stp1_1, stp1_2, stp1_3, stp1_4, stp1_5, stp1_6, stp1_7,
+          stp1_8, stp1_9, stp1_10, stp1_11, stp1_12, stp1_13, stp1_14, stp1_15,
+          stp1_16, stp1_17, stp1_18, stp1_19, stp1_20, stp1_21, stp1_22,
+          stp1_23, stp1_24, stp1_25, stp1_26, stp1_27, stp1_28, stp1_29,
+          stp1_30, stp1_31;
+  __m128i stp2_0, stp2_1, stp2_2, stp2_3, stp2_4, stp2_5, stp2_6, stp2_7,
+          stp2_8, stp2_9, stp2_10, stp2_11, stp2_12, stp2_13, stp2_14, stp2_15,
+          stp2_16, stp2_17, stp2_18, stp2_19, stp2_20, stp2_21, stp2_22,
+          stp2_23, stp2_24, stp2_25, stp2_26, stp2_27, stp2_28, stp2_29,
+          stp2_30, stp2_31;
+  __m128i tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+  int i, j, i32;
+
+  for (i = 0; i < 2; i++) {
+    i32 = (i << 5);
+    // First 1-D idct
+    // Load input data.
+//    LOAD_DQCOEFF(in[0], input);
+  //  LOAD_DQCOEFF(in[8], input);
+    in[0] = _mm_load_si128((const __m128i *)(input + ( 0 * 8)));
+    in[8] = _mm_load_si128((const __m128i *)(input + ( 1 * 8)));
+//    LOAD_DQCOEFF(in[16], input);
+  //  LOAD_DQCOEFF(in[24], input);
+    in[16] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + ( 2 * 8)));
+    in[24] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + ( 3 * 8)));
+//    LOAD_DQCOEFF(in[1], input);
+  //  LOAD_DQCOEFF(in[9], input);
+    in[1] = _mm_load_si128((const __m128i *)(input + ( 4 * 8)));
+    in[9] = _mm_load_si128((const __m128i *)(input + ( 5 * 8)));
+//    LOAD_DQCOEFF(in[17], input);
+  //  LOAD_DQCOEFF(in[25], input);
+    in[17] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + ( 6 * 8)));
+    in[25] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + ( 7 * 8)));
+//    LOAD_DQCOEFF(in[2], input);
+  //  LOAD_DQCOEFF(in[10], input);
+    in[2] = _mm_load_si128((const __m128i *)(input + ( 8 * 8)));
+    in[10] = _mm_load_si128((const __m128i *)(input + ( 9 * 8)));
+//    LOAD_DQCOEFF(in[18], input);
+  //  LOAD_DQCOEFF(in[26], input);
+    in[18] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (10 * 8)));
+    in[26] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (11 * 8)));
+//    LOAD_DQCOEFF(in[3], input);
+//    LOAD_DQCOEFF(in[11], input);
+    in[3] = _mm_load_si128((const __m128i *)(input + (12 * 8)));
+    in[11] = _mm_load_si128((const __m128i *)(input + (13 * 8)));
+//    LOAD_DQCOEFF(in[19], input);
+  //  LOAD_DQCOEFF(in[27], input);
+    in[19] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (14 * 8)));
+    in[27] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (15 * 8)));
+//    LOAD_DQCOEFF(in[4], input);
+  //  LOAD_DQCOEFF(in[12], input);
+    in[4] = _mm_load_si128((const __m128i *)(input + (16 * 8)));
+    in[12] = _mm_load_si128((const __m128i *)(input + (17 * 8)));
+//    LOAD_DQCOEFF(in[20], input);
+  //  LOAD_DQCOEFF(in[28], input);
+    in[20] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (18 * 8)));
+    in[28] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (19 * 8)));
+//    LOAD_DQCOEFF(in[5], input);
+//    LOAD_DQCOEFF(in[13], input);
+    in[5] = _mm_load_si128((const __m128i *)(input + (20 * 8)));
+    in[13] = _mm_load_si128((const __m128i *)(input + (21 * 8)));
+//    LOAD_DQCOEFF(in[21], input);
+  //  LOAD_DQCOEFF(in[29], input);
+    in[21] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (22 * 8)));
+    in[29] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (23 * 8)));
+//    LOAD_DQCOEFF(in[6], input);
+  //  LOAD_DQCOEFF(in[14], input);
+    in[6] = _mm_load_si128((const __m128i *)(input + (24 * 8)));
+    in[14] = _mm_load_si128((const __m128i *)(input + (25 * 8)));
+//    LOAD_DQCOEFF(in[22], input);
+  //  LOAD_DQCOEFF(in[30], input);
+    in[22] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (26 * 8)));
+    in[30] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (27 * 8)));
+//    LOAD_DQCOEFF(in[7], input);
+  //  LOAD_DQCOEFF(in[15], input);
+    in[7] = _mm_load_si128((const __m128i *)(input + (28 * 8)));
+    in[15] = _mm_load_si128((const __m128i *)(input + (29 * 8)));
+//    LOAD_DQCOEFF(in[23], input);
+  //  LOAD_DQCOEFF(in[31], input);
+    in[23] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (30 * 8)));
+    in[31] = _mm_setzero_si128(); //_mm_load_si128((const __m128i *)(input + (31 * 8)));
+    input += 32 * 8;
+
+    // Transpose 32x8 block to 8x32 block
+    array_transpose_8x8(in, in);
+    array_transpose_8x8(in + 8, in + 8);
+//    array_transpose_8x8(in + 16, in + 16);
+//    array_transpose_8x8(in + 24, in + 24);
+
+    IDCT32
+
+    // 1_D: Store 32 intermediate results for each 8x32 block.
+    col[i32 + 0] = _mm_add_epi16(stp1_0, stp1_31);
+    col[i32 + 1] = _mm_add_epi16(stp1_1, stp1_30);
+    col[i32 + 2] = _mm_add_epi16(stp1_2, stp1_29);
+    col[i32 + 3] = _mm_add_epi16(stp1_3, stp1_28);
+    col[i32 + 4] = _mm_add_epi16(stp1_4, stp1_27);
+    col[i32 + 5] = _mm_add_epi16(stp1_5, stp1_26);
+    col[i32 + 6] = _mm_add_epi16(stp1_6, stp1_25);
+    col[i32 + 7] = _mm_add_epi16(stp1_7, stp1_24);
+    col[i32 + 8] = _mm_add_epi16(stp1_8, stp1_23);
+    col[i32 + 9] = _mm_add_epi16(stp1_9, stp1_22);
+    col[i32 + 10] = _mm_add_epi16(stp1_10, stp1_21);
+    col[i32 + 11] = _mm_add_epi16(stp1_11, stp1_20);
+    col[i32 + 12] = _mm_add_epi16(stp1_12, stp1_19);
+    col[i32 + 13] = _mm_add_epi16(stp1_13, stp1_18);
+    col[i32 + 14] = _mm_add_epi16(stp1_14, stp1_17);
+    col[i32 + 15] = _mm_add_epi16(stp1_15, stp1_16);
+    col[i32 + 16] = _mm_sub_epi16(stp1_15, stp1_16);
+    col[i32 + 17] = _mm_sub_epi16(stp1_14, stp1_17);
+    col[i32 + 18] = _mm_sub_epi16(stp1_13, stp1_18);
+    col[i32 + 19] = _mm_sub_epi16(stp1_12, stp1_19);
+    col[i32 + 20] = _mm_sub_epi16(stp1_11, stp1_20);
+    col[i32 + 21] = _mm_sub_epi16(stp1_10, stp1_21);
+    col[i32 + 22] = _mm_sub_epi16(stp1_9, stp1_22);
+    col[i32 + 23] = _mm_sub_epi16(stp1_8, stp1_23);
+    col[i32 + 24] = _mm_sub_epi16(stp1_7, stp1_24);
+    col[i32 + 25] = _mm_sub_epi16(stp1_6, stp1_25);
+    col[i32 + 26] = _mm_sub_epi16(stp1_5, stp1_26);
+    col[i32 + 27] = _mm_sub_epi16(stp1_4, stp1_27);
+    col[i32 + 28] = _mm_sub_epi16(stp1_3, stp1_28);
+    col[i32 + 29] = _mm_sub_epi16(stp1_2, stp1_29);
+    col[i32 + 30] = _mm_sub_epi16(stp1_1, stp1_30);
+    col[i32 + 31] = _mm_sub_epi16(stp1_0, stp1_31);
+  }
+
+  for (; i < 4; i++) {
+    i32 = (i << 5);
+    col[i32 + 0] = _mm_setzero_si128();
+    col[i32 + 1] = _mm_setzero_si128();
+    col[i32 + 2] = _mm_setzero_si128();
+    col[i32 + 3] = _mm_setzero_si128();
+    col[i32 + 4] = _mm_setzero_si128();
+    col[i32 + 5] = _mm_setzero_si128();
+    col[i32 + 6] = _mm_setzero_si128();
+    col[i32 + 7] = _mm_setzero_si128();
+    col[i32 + 8] = _mm_setzero_si128();
+    col[i32 + 9] = _mm_setzero_si128();
+    col[i32 + 10] = _mm_setzero_si128();
+    col[i32 + 11] = _mm_setzero_si128();
+    col[i32 + 12] = _mm_setzero_si128();
+    col[i32 + 13] = _mm_setzero_si128();
+    col[i32 + 14] = _mm_setzero_si128();
+    col[i32 + 15] = _mm_setzero_si128();
+    col[i32 + 16] = _mm_setzero_si128();
+    col[i32 + 17] = _mm_setzero_si128();
+    col[i32 + 18] = _mm_setzero_si128();
+    col[i32 + 19] = _mm_setzero_si128();
+    col[i32 + 20] = _mm_setzero_si128();
+    col[i32 + 21] = _mm_setzero_si128();
+    col[i32 + 22] = _mm_setzero_si128();
+    col[i32 + 23] = _mm_setzero_si128();
+    col[i32 + 24] = _mm_setzero_si128();
+    col[i32 + 25] = _mm_setzero_si128();
+    col[i32 + 26] = _mm_setzero_si128();
+    col[i32 + 27] = _mm_setzero_si128();
+    col[i32 + 28] = _mm_setzero_si128();
+    col[i32 + 29] = _mm_setzero_si128();
+    col[i32 + 30] = _mm_setzero_si128();
+    col[i32 + 31] = _mm_setzero_si128();
+  }
+
+  for (i = 0; i < 4; i++) {
+    // Second 1-D idct
+    j = i << 3;
+
+    // Transpose 32x8 block to 8x32 block
+    array_transpose_8x8(col + j, in);
+    array_transpose_8x8(col + j + 32, in + 8);
+    array_transpose_8x8(col + j + 64, in + 16);
+    array_transpose_8x8(col + j + 96, in + 24);
+
+    IDCT32
+
+    // 2_D: Calculate the results and store them to destination.
+    CALC_FINAL_ADD(0, stp1_0, stp1_31);
+    CALC_FINAL_ADD(1, stp1_1, stp1_30);
+    CALC_FINAL_ADD(2, stp1_2, stp1_29);
+    CALC_FINAL_ADD(3, stp1_3, stp1_28);
+    CALC_FINAL_ADD(4, stp1_4, stp1_27);
+    CALC_FINAL_ADD(5, stp1_5, stp1_26);
+    CALC_FINAL_ADD(6, stp1_6, stp1_25);
+    CALC_FINAL_ADD(7, stp1_7, stp1_24);
+    CALC_FINAL_ADD(8, stp1_8, stp1_23);
+    CALC_FINAL_ADD(9, stp1_9, stp1_22);
+    CALC_FINAL_ADD(10, stp1_10, stp1_21);
+    CALC_FINAL_ADD(11, stp1_11, stp1_20);
+    CALC_FINAL_ADD(12, stp1_12, stp1_19);
+    CALC_FINAL_ADD(13, stp1_13, stp1_18);
+    CALC_FINAL_ADD(14, stp1_14, stp1_17);
+    CALC_FINAL_ADD(15, stp1_15, stp1_16);
+    CALC_FINAL_SUB(16, stp1_15, stp1_16);
+    CALC_FINAL_SUB(17, stp1_14, stp1_17);
+    CALC_FINAL_SUB(18, stp1_13, stp1_18);
+    CALC_FINAL_SUB(19, stp1_12, stp1_19);
+    CALC_FINAL_SUB(20, stp1_11, stp1_20);
+    CALC_FINAL_SUB(21, stp1_10, stp1_21);
+    CALC_FINAL_SUB(22, stp1_9, stp1_22);
+    CALC_FINAL_SUB(23, stp1_8, stp1_23);
+    CALC_FINAL_SUB(24, stp1_7, stp1_24);
+    CALC_FINAL_SUB(25, stp1_6, stp1_25);
+    CALC_FINAL_SUB(26, stp1_5, stp1_26);
+    CALC_FINAL_SUB(27, stp1_4, stp1_27);
+    CALC_FINAL_SUB(28, stp1_3, stp1_28);
+    CALC_FINAL_SUB(29, stp1_2, stp1_29);
+    CALC_FINAL_SUB(30, stp1_1, stp1_30);
+    CALC_FINAL_SUB(31, stp1_0, stp1_31);
 
     dest += 8;
   }
