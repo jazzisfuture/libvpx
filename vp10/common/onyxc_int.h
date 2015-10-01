f36d1fb420b05dffc355ef4aa6c91f4f8abac350
Ronald S. Bultje <rsbultje@gmail.com>
2015-10-01 16:30:10 -0400

    vp10: make segmentation probs use generic probability model.
    Locate them (code-wise) in frame_context, and have them be updated
    as any other probability using the subexp forward and adaptive bw
    updates.
    
    See issue 1040 point 1.
    
    TODOs:
    - real-world default probabilities
    - why is counts sometimes NULL in the decoder? Does that mean bw
      adaptivity updates only work on some frames? (I haven't looked
      very closely yet, maybe this is a red herring.)
    
    Change-Id: I23b57b4e5e7574b75f16eb64823b29c22fbab42e

--- vp10/common/onyxc_int.h
+++ vp10/common/onyxc_int.h
@@ -252,6 +252,9 @@ typedef struct VP10Common {
 
   struct loopfilter lf;
   struct segmentation seg;
+#if !CONFIG_MISC_FIXES
+  struct segmentation_probs segp;
+#endif
 
   int frame_parallel_decode;  // frame-based threading.
 
