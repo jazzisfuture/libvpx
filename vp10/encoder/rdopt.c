29f1f4161b488738f80d700e177d29c5e085bbb0
hui su <huisu@google.com>
2015-10-15 18:04:50 -0700

    VP10: some changes to palette mode
    Account for rounding in distortion calculation in k-means;
    carry out rounding before duplicates removal of base colors;
    replace numbers with macros;
    use prefix increment.
    
    Slight coding gain (<0.1%) on screen_content testset.
    
    Change-Id: Ie8bd241266da6b82c7b2874befc3a0c72b4fcd8c

--- vp10/encoder/rdopt.c
+++ vp10/encoder/rdopt.c
@@ -821,7 +821,8 @@ void rd_pick_palette_intra_sby(VP10_COMP *cpi, MACROBLOCK *x, BLOCK_SIZE bsize,
       vp10_k_means(data, centroids, indices, pre_indices, rows * cols,
                    n, 1, max_itr);
       vp10_insertion_sort(centroids, n);
-
+      for (i = 0; i < n; ++i)
+        centroids[i] = round(centroids[i]);
       // remove duplicates
       i = 1;
       k = n;
@@ -846,7 +847,7 @@ void rd_pick_palette_intra_sby(VP10_COMP *cpi, MACROBLOCK *x, BLOCK_SIZE bsize,
       else
 #endif  // CONFIG_VP9_HIGHBITDEPTH
         for (i = 0; i < k; ++i)
-          pmi->palette_colors[i] = clip_pixel((int)round(centroids[i]));
+          pmi->palette_colors[i] = clip_pixel((int)round(centroids[i]));;
       pmi->palette_size[0] = k;
 
       vp10_calc_indices(data, centroids, indices, rows * cols, k, 1);
