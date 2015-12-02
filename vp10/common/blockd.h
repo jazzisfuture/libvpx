c1ade8bb601206520f20faf640eb79153128ac7c
hui su <huisu@google.com>
2015-12-01 17:19:52 -0800

    Misc. changes
    Change-Id: Iee829003a20f32d6185a08bab2bd4201806be2b3

--- vp10/common/blockd.h
+++ vp10/common/blockd.h
@@ -347,7 +347,7 @@ static const int ext_tx_used_inter[EXT_TX_SETS_INTER][TX_TYPES] = {
 #endif  // CONFIG_EXT_TX
 
 #if CONFIG_EXT_INTRA
-#define ALLOW_FILTER_INTRA_MODES 1
+#define ALLOW_FILTER_INTRA_MODES 0
 #define ANGLE_STEP 3
 #define MAX_ANGLE_DELTAS 3
 #define ANGLE_FAST_SEARCH 1
