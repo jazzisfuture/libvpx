79aca9ab8e1832a14bb0716ac5a3663b1df2a7df
Marco <marpan@google.com>
2016-01-04 14:52:27 -0800

    Vidyo patch: Changes to the scalability code.
    Change-Id: I3e883aa04db521cfa026a0b12c9478ea35a344c9

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -3317,7 +3317,7 @@ static void encode_without_recode_loop(VP9_COMP *cpi,
       cpi->oxcf.content == VP9E_CONTENT_SCREEN)
     vp9_avg_source_sad(cpi);
 
-  if (frame_is_intra_only(cm) == 0) {
+  if (frame_is_intra_only(cm) == 0 && !is_one_pass_cbr_svc(cpi)) {
     vp9_scale_references(cpi);
   }
 
