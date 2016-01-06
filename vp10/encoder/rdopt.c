1dc47d197f680209f6cb8e99610936734555bd0d
Debargha Mukherjee <debargha@google.com>
2016-01-06 14:36:13 -0800

    Some fixes on tx size/type selection
    For ext_tx experiment.
    
    Change-Id: Ie37b9b456b09bde8b606fb978fee4cca8d0326b7

--- vp10/encoder/rdopt.c
+++ vp10/encoder/rdopt.c
@@ -806,8 +806,7 @@ static void choose_largest_tx_size(VP10_COMP *cpi, MACROBLOCK *x,
 #if CONFIG_EXT_TX
   ext_tx_set = get_ext_tx_set(mbmi->tx_size, bs, is_inter);
 
-  if (is_inter &&
-      get_ext_tx_types(mbmi->tx_size, bs, is_inter) > 1 &&
+  if (get_ext_tx_types(mbmi->tx_size, bs, is_inter) > 1 &&
       !xd->lossless[mbmi->segment_id]) {
     for (tx_type = 0; tx_type < TX_TYPES; ++tx_type) {
       if (is_inter) {
@@ -1007,10 +1006,10 @@ static void choose_tx_size_from_rd(VP10_COMP *cpi, MACROBLOCK *x,
         if (is_inter) {
           if (ext_tx_set > 0)
             r += cpi->inter_tx_type_costs[ext_tx_set]
-                                         [mbmi->tx_size][mbmi->tx_type];
+                                         [n][mbmi->tx_type];
         } else {
           if (ext_tx_set > 0 && ALLOW_INTRA_EXT_TX)
-            r += cpi->intra_tx_type_costs[ext_tx_set][mbmi->tx_size]
+            r += cpi->intra_tx_type_costs[ext_tx_set][n]
                                          [mbmi->mode][mbmi->tx_type];
         }
       }
