8032cc75ed4061d75e806b7edb1941d4bdf30d40
Alex Converse <aconverse@google.com>
2016-02-01 10:02:54 -0800

    Use VP9_PROB_COST_SHIFT in mv error cost functions
    Change-Id: Ic69ceb423f6bab8f7b18e1d972ba83e341ba6e26

--- vp9/encoder/vp9_mcomp.c
+++ vp9/encoder/vp9_mcomp.c
@@ -86,10 +86,8 @@ static int mv_err_cost(const MV *mv, const MV *ref,
   if (mvcost) {
     const MV diff = { mv->row - ref->row,
                       mv->col - ref->col };
-    // TODO(aconverse): See if this needs to be shift needs to by tied to
-    // VP9_PROB_COST_SHIFT.
     return ROUND_POWER_OF_TWO((unsigned)mv_cost(&diff, mvjcost, mvcost) *
-                                  error_per_bit, 13);
+                                  error_per_bit, VP9_PROB_COST_SHIFT + 5);
   }
   return 0;
 }
@@ -98,10 +96,10 @@ static int mvsad_err_cost(const MACROBLOCK *x, const MV *mv, const MV *ref,
                           int error_per_bit) {
   const MV diff = { mv->row - ref->row,
                     mv->col - ref->col };
-  // TODO(aconverse): See if this needs to be shift needs to by tied to
-  // VP9_PROB_COST_SHIFT.
-  return ROUND_POWER_OF_TWO((unsigned)mv_cost(&diff, x->nmvjointsadcost,
-                                      x->nmvsadcost) * error_per_bit, 8);
+  return ROUND_POWER_OF_TWO(
+      (unsigned)mv_cost(&diff, x->nmvjointsadcost, x->nmvsadcost) *
+          error_per_bit,
+      VP9_PROB_COST_SHIFT);
 }
 
 void vp9_init_dsmotion_compensation(search_site_config *cfg, int stride) {
