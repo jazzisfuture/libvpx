3fde4598c258af6ec2ce1bee75b095c733860103
Zoe Liu <zoeliu@google.com>
2016-02-04 09:47:46 -0800

    Added an experiment "bidir_pred" for backward prediction
    Three major parts have been implemented:
    (1) Added BRF_UPDATE, LASTNRF_UPDATE, and NRF_UPDATE in firstpass.c;
    (2) Added the handling for the scenario of
    "cpi->common.show_existing_frame == 1" at the encoder;
    (3) Added a new reference frame of BWDREF_FRAME.
    
    Current implementation has not worked with the experiment of EXT_REFS
    yet.
    
    Change-Id: I95f69242b68f8385e8948ec88dc3246da415f1b3

--- vp10/encoder/denoiser.c
+++ vp10/encoder/denoiser.c
@@ -389,8 +389,11 @@ void vp10_denoiser_update_frame_info(VP9_DENOISER *denoiser,
                                     FRAME_TYPE frame_type,
 #if CONFIG_EXT_REFS
                                     int refresh_last_frames[LAST_REF_FRAMES],
-#else
+#else  // CONFIG_EXT_REFS
                                     int refresh_last_frame,
+#if CONFIG_BIDIR_PRED
+                                    int refresh_bwd_ref_frame,
+#endif  // CONFIG_BIDIR_PRED
 #endif  // CONFIG_EXT_REFS
                                     int refresh_alt_ref_frame,
                                     int refresh_golden_frame) {
@@ -422,11 +425,17 @@ void vp10_denoiser_update_frame_info(VP9_DENOISER *denoiser,
                         &denoiser->running_avg_y[INTRA_FRAME]);
     }
   }
-#else
+#else  // CONFIG_EXT_REFS
   if (refresh_last_frame) {
     swap_frame_buffer(&denoiser->running_avg_y[LAST_FRAME],
                       &denoiser->running_avg_y[INTRA_FRAME]);
   }
+#if CONFIG_BIDIR_PRED
+  if (refresh_bwd_ref_frame) {
+    swap_frame_buffer(&denoiser->running_avg_y[BWDREF_FRAME],
+                      &denoiser->running_avg_y[INTRA_FRAME]);
+  }
+#endif  // CONFIG_BIDIR_PRED
 #endif  // CONFIG_EXT_REFS
 }
 
