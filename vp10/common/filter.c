5f759a32afcd01a10fa8ff4e4b90e1da2dc5157e
Angie Chiang <angiebird@google.com>
2016-02-04 16:13:18 -0800

    add convolution function with adjustable length
    Change-Id: I1a5b1e15a188ef11594d0c6ac0dbd42aac59cfca

--- vp10/common/filter.c
+++ vp10/common/filter.c
@@ -201,3 +201,18 @@ const InterpKernel *vp10_intra_filter_kernels[INTRA_FILTERS] = {
     sub_pel_filters_8smooth,  // INTRA_FILTER_8TAP_SMOOTH
 };
 #endif  // CONFIG_EXT_INTRA
+
+static const InterpFilterSet
+vp10_interp_filter_set_list[SWITCHABLE_FILTERS + 1] = {
+  {(const int16_t*)sub_pel_filters_8, SUBPEL_TAPS, SUBPEL_SHIFTS},
+  {(const int16_t*)sub_pel_filters_8smooth, SUBPEL_TAPS, SUBPEL_SHIFTS},
+  {(const int16_t*)sub_pel_filters_8sharp, SUBPEL_TAPS, SUBPEL_SHIFTS},
+#if CONFIG_EXT_INTERP && SWITCHABLE_FILTERS == 4
+  {(const int16_t*)sub_pel_filters_8smooth2, SUBPEL_TAPS, SUBPEL_SHIFTS},
+#endif
+  {(const int16_t*)bilinear_filters, SUBPEL_TAPS, SUBPEL_SHIFTS}
+};
+
+InterpFilterSet vp10_get_interp_filter_set(const INTERP_FILTER interp_filter) {
+  return vp10_interp_filter_set_list[interp_filter];
+}
