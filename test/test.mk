1bc4467830249c16a9f95b1aff9874fa7f60e051
Yaowu Xu <yaowu@google.com>
2016-02-09 10:35:15 -0800

    Add a test to verify VPXSSIM computation for HBD inputs
    Change-Id: I61dc0f43d073b62d0eab8cd7471c2d76e03379bf

--- test/test.mk
+++ test/test.mk
@@ -135,6 +135,9 @@ LIBVPX_TEST_SRCS-yes                   += vp9_encoder_parms_get_to_decoder.cc
 endif
 
 LIBVPX_TEST_SRCS-yes                   += convolve_test.cc
+ifeq ($(CONFIG_VP9_HIGHBITDEPTH),yes)
+LIBVPX_TEST_SRCS-yes                   += hbd_metrics_test.cc
+endif
 LIBVPX_TEST_SRCS-yes                   += lpf_8_test.cc
 LIBVPX_TEST_SRCS-yes                   += vp9_intrapred_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP9_DECODER) += vp9_decrypt_test.cc
