d27f50eef55aa018495f716718e41aa1e3f7737c
James Zern <jzern@google.com>
2016-02-11 20:02:53 -0800

    vpx_lpf_horizontal_8: remove unused count param
    Change-Id: I48741e167a7b09b7c9ad3bfc1c4b88ef1029ae46

--- vpx_dsp/arm/loopfilter_8_neon.c
+++ vpx_dsp/arm/loopfilter_8_neon.c
@@ -268,16 +268,12 @@ void vpx_lpf_horizontal_8_neon(
         int pitch,
         const uint8_t *blimit,
         const uint8_t *limit,
-        const uint8_t *thresh,
-        int count) {
+        const uint8_t *thresh) {
     int i;
     uint8_t *s, *psrc;
     uint8x8_t dblimit, dlimit, dthresh;
     uint8x8_t d0u8, d1u8, d2u8, d3u8, d4u8, d5u8, d6u8, d7u8;
     uint8x8_t d16u8, d17u8, d18u8;
-
-    if (count == 0)  // end_vpx_mblf_h_edge
-        return;
 
     dblimit = vld1_u8(blimit);
     dlimit = vld1_u8(limit);
