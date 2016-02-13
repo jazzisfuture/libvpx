8a438fb4e72280273380dfc1d2f23b61f5183d6c
Debargha Mukherjee <debargha@google.com>
2016-02-12 16:44:33 -0800

    Extends ext-tx to support 32x32 masked transforms
    Adds new 32x32 masked 1-d transforms that combine 1-D length-16
    DCT with length-16 identity transforms.
    
    To be continued in subsequent patches.
    
    Change-Id: I0b4f66492d44c079b3c3b531ba48a97201de1484

--- vpx_dsp/txfm_common.h
+++ vpx_dsp/txfm_common.h
@@ -63,4 +63,6 @@ static const tran_high_t sinpi_2_9 = 9929;
 static const tran_high_t sinpi_3_9 = 13377;
 static const tran_high_t sinpi_4_9 = 15212;
 
+static const tran_high_t Sqrt2 = 23170;
+
 #endif  // VPX_DSP_TXFM_COMMON_H_
