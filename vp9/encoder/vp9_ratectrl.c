f4932966e03191ae4aaab4cae68a2e043b117339
Marco <marpan@google.com>
2016-02-17 12:03:57 -0800

    vp9: 1 pass vbr mode: Allow usage of lookahead for real-time.
    Allow usage of lookahead for VBR in real-time mode.
    Usage is for fast checking of motion content and scene cuts
    on current and future frames (given by lag_in_frames).
    
    For testing...DO NOT COMMIT.
    
    Change-Id: I960d57dfc89de121c4824b9a9bf88d2814e74b56

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -339,9 +339,12 @@ void vp9_rc_init(const VP9EncoderConfig *oxcf, int pass, RATE_CONTROL *rc) {
   rc->total_target_vs_actual = 0;
   rc->avg_intersize_gfint = 0;
   rc->avg_frame_low_motion = 0;
-  rc->high_source_sad = 0;
   rc->count_last_scene_change = 0;
-  rc->avg_source_sad = 0;
+  rc->high_source_sad_lagindex = 0;
+  for (i = 0; i < MAX_LAG_BUFFERS; ++i) {
+    rc->high_source_sad[i] = 0;
+    rc->avg_source_sad[i] = 0;
+  }
 
   rc->frames_since_key = 8;  // Sensible default for first frame.
   rc->this_key_frame_forced = 0;
@@ -2081,86 +2084,147 @@ int vp9_resize_one_pass_cbr(VP9_COMP *cpi) {
 // Compute average source sad (temporal sad: between current source and
 // previous source) over a subset of superblocks. Use this is detect big changes
 // in content and allow rate control to react.
-// TODO(marpan): Superblock sad is computed again in variance partition for
-// non-rd mode (but based on last reconstructed frame). Should try to reuse
-// these computations.
+// This function also handles special case of lag_in_frames, to measure content
+// level in #future frames set by the lag_in_frames.
 void vp9_avg_source_sad(VP9_COMP *cpi) {
   VP9_COMMON * const cm = &cpi->common;
   RATE_CONTROL *const rc = &cpi->rc;
-  rc->high_source_sad = 0;
+  rc->high_source_sad[0] = 0;
   if (cpi->Last_Source != NULL &&
       cpi->Last_Source->y_width == cpi->Source->y_width &&
       cpi->Last_Source->y_height == cpi->Source->y_height) {
-    const uint8_t *src_y = cpi->Source->y_buffer;
-    const int src_ystride = cpi->Source->y_stride;
-    const uint8_t *last_src_y = cpi->Last_Source->y_buffer;
-    const int last_src_ystride = cpi->Last_Source->y_stride;
-    int sbi_row, sbi_col;
-    const BLOCK_SIZE bsize = BLOCK_64X64;
+    YV12_BUFFER_CONFIG *frames[MAX_LAG_BUFFERS] = {NULL};
+    uint8_t *src_y = cpi->Source->y_buffer;
+    int src_ystride = cpi->Source->y_stride;
+    uint8_t *last_src_y = cpi->Last_Source->y_buffer;
+    int last_src_ystride = cpi->Last_Source->y_stride;
+    int frames_to_check = 1;
+    int start_frame = 0;
+    int frame = 0;
     uint32_t min_thresh = 4000;
     float thresh = 8.0f;
-    // Loop over sub-sample of frame, and compute average sad over 64x64 blocks.
-    uint64_t avg_sad = 0;
-    int num_samples = 0;
-    int sb_cols = (cm->mi_cols + MI_BLOCK_SIZE - 1) / MI_BLOCK_SIZE;
-    int sb_rows = (cm->mi_rows + MI_BLOCK_SIZE - 1) / MI_BLOCK_SIZE;
-    for (sbi_row = 0; sbi_row < sb_rows; sbi_row ++) {
-      for (sbi_col = 0; sbi_col < sb_cols; sbi_col ++) {
-        // Checker-board pattern, ignore boundary.
-        if ((sbi_row > 0 && sbi_col > 0) &&
-            (sbi_row < sb_rows - 1 && sbi_col < sb_cols - 1) &&
-            ((sbi_row % 2 == 0 && sbi_col % 2 == 0) ||
-            (sbi_row % 2 != 0 && sbi_col % 2 != 0))) {
-          num_samples++;
-          avg_sad += cpi->fn_ptr[bsize].sdf(src_y,
-                                            src_ystride,
-                                            last_src_y,
-                                            last_src_ystride);
-        }
-        src_y += 64;
-        last_src_y += 64;
-      }
-      src_y += (src_ystride << 6) - (sb_cols << 6);
-      last_src_y += (last_src_ystride << 6) - (sb_cols << 6);
-    }
-    if (num_samples > 0)
-      avg_sad = avg_sad / num_samples;
-    // Set high_source_sad flag if we detect very high increase in avg_sad
-    // between current and the previous frame value(s). Use a minimum threshold
-    // for cases where there is small change from content that is completely
-    // static.
     if (cpi->oxcf.rc_mode == VPX_VBR) {
       min_thresh = 60000;
       thresh = 2.1f;
     }
-    if (avg_sad >
-        VPXMAX(min_thresh, (unsigned int)(rc->avg_source_sad  * thresh)) &&
-        rc->frames_since_key > 1)
-      rc->high_source_sad = 1;
-    else
-      rc->high_source_sad = 0;
-    if (avg_sad > 0 || cpi->oxcf.rc_mode == VPX_CBR)
-      rc->avg_source_sad = (3 * rc->avg_source_sad + avg_sad) >> 2;
-    // For VBR, under scene change/high content change, force golden refresh.
-    if (cpi->oxcf.rc_mode == VPX_VBR &&
-        rc->high_source_sad &&
-        rc->frames_to_key > 3 &&
-        rc->count_last_scene_change > 4 &&
-        cpi->ext_refresh_frame_flags_pending == 0) {
-      int target;
-      cpi->refresh_golden_frame = 1;
-      rc->gfu_boost = DEFAULT_GF_BOOST >> 1;
-      rc->baseline_gf_interval = VPXMIN(20,
-          VPXMAX(10, rc->baseline_gf_interval));
-      rc->frames_till_gf_update_due = rc->baseline_gf_interval;
-      if (rc->frames_till_gf_update_due > rc->frames_to_key)
-        rc->frames_till_gf_update_due = rc->frames_to_key;
-      target = calc_pframe_target_size_one_pass_vbr(cpi);
-      vp9_rc_set_frame_target(cpi, target);
-      rc->count_last_scene_change = 0;
-      rc->avg_intersize_gfint = 0;
+    if (cpi->oxcf.lag_in_frames > 0) {
+      int frames_to_buffer = (cm->current_video_frame == 1) ?
+          cpi->oxcf.lag_in_frames - 2: 2;
+      start_frame = cpi->oxcf.lag_in_frames - 2;
+      for (frame = 0; frame < frames_to_buffer; ++frame) {
+        const int lagframe_idx = start_frame - frame;
+        struct lookahead_entry *buf = vp9_lookahead_peek(cpi->lookahead,
+                                                         lagframe_idx);
+        frames[frame] = &buf->img;
+      }
+    }
+    if (cpi->oxcf.lag_in_frames > 0) {
+      if (frames[frame] == NULL || frames[frame + 1] == NULL)
+        return;
+      frames_to_check = (cm->current_video_frame == 1) ?
+          cpi->oxcf.lag_in_frames - 2: 1;
+       // Shift data back one frame: the avg_sad for this current frame
+       // is the value of frame#1 (first future frame) from previous frame.
+       if (cm->current_video_frame > 1) {
+         for (frame = 0; frame < cpi->oxcf.lag_in_frames - 1; ++frame) {
+           rc->avg_source_sad[frame] = rc->avg_source_sad[frame + 1];
+           rc->high_source_sad[frame] = rc->high_source_sad[frame + 1];
+         }
+       }
+    }
+    for (frame = 0; frame < frames_to_check; ++frame) {
+      int sbi_row, sbi_col;
+      const int lagframe_idx = start_frame - frame;
+      const BLOCK_SIZE bsize = BLOCK_64X64;
+      // Loop over sub-sample of frame, & compute average sad over 64x64 blocks.
+      uint64_t avg_sad = 0;
+      int num_samples = 0;
+      int sb_cols = (cm->mi_cols + MI_BLOCK_SIZE - 1) / MI_BLOCK_SIZE;
+      int sb_rows = (cm->mi_rows + MI_BLOCK_SIZE - 1) / MI_BLOCK_SIZE;
+      if (cpi->oxcf.lag_in_frames > 0) {
+        src_y = frames[frame]->y_buffer;
+        src_ystride = frames[frame]->y_stride;
+        last_src_y = frames[frame + 1]->y_buffer;
+        last_src_ystride = frames[frame + 1]->y_stride;
+      }
+      for (sbi_row = 0; sbi_row < sb_rows; sbi_row ++) {
+        for (sbi_col = 0; sbi_col < sb_cols; sbi_col ++) {
+          // Checker-board pattern, ignore boundary.
+          if ((sbi_row > 0 && sbi_col > 0) &&
+              (sbi_row < sb_rows - 1 && sbi_col < sb_cols - 1) &&
+              ((sbi_row % 2 == 0 && sbi_col % 2 == 0) ||
+              (sbi_row % 2 != 0 && sbi_col % 2 != 0))) {
+            num_samples++;
+            avg_sad += cpi->fn_ptr[bsize].sdf(src_y,
+                                              src_ystride,
+                                              last_src_y,
+                                              last_src_ystride);
+          }
+          src_y += 64;
+          last_src_y += 64;
+        }
+        src_y += (src_ystride << 6) - (sb_cols << 6);
+        last_src_y += (last_src_ystride << 6) - (sb_cols << 6);
+      }
+      if (num_samples > 0)
+        avg_sad = avg_sad / num_samples;
+      // Keep track of avg_sad of the (future) frames. For the current frame
+      // (lagfrmae_idx == 0) avg_sad is recursive average from past frames.
+      if (lagframe_idx > 0)
+        rc->avg_source_sad[lagframe_idx] = avg_sad;
+      // Set high_source_sad flag if we detect very high increase in avg_sad
+      // between current and the previous frame value(s). Use minimum threshold
+      // for cases where there is small change from content that is completely
+      // static.
+      if (avg_sad >
+          VPXMAX(min_thresh,
+                 (unsigned int)(rc->avg_source_sad[lagframe_idx]  * thresh)) &&
+          rc->frames_since_key > 1)
+        rc->high_source_sad[lagframe_idx] = 1;
+      else
+        rc->high_source_sad[lagframe_idx] = 0;
+      if (lagframe_idx == 0 && (avg_sad > 0 || cpi->oxcf.rc_mode == VPX_CBR))
+        rc->avg_source_sad[0] = (3 * rc->avg_source_sad[0] + avg_sad) >> 2;
+    }
+    if (cpi->oxcf.lag_in_frames > 0) {
+      // Find the first future frame with scene change, and get the
+      // avg_source_sad of future frames.
+      int num_frames = 0;
+      uint64_t avg_sad_lag = 0;
+      for (frame = 1; frame < cpi->oxcf.lag_in_frames - 1; ++frame) {
+        if (rc->high_source_sad[frame] == 1) {
+          rc->high_source_sad_lagindex = frame;
+        } else {
+          avg_sad_lag += rc->avg_source_sad[frame];
+          num_frames++;
+        }
+      }
+      avg_sad_lag = avg_sad_lag / num_frames;
+      // TODO(marpan): Add logic for gf boost/interval based on these variables.
     } else {
-      rc->count_last_scene_change++;
+      // For VBR, under scene change/high content change, force golden refresh.
+      // Use lagframe_idx = 0 for this (current frame).
+      // TODO(marpan): Factor in position of next scene cut in future frames.
+      if (cpi->oxcf.rc_mode == VPX_VBR &&
+          rc->high_source_sad[0] &&
+          rc->frames_to_key > 3 &&
+          rc->count_last_scene_change > 4 &&
+          cpi->ext_refresh_frame_flags_pending == 0) {
+        int target;
+        cpi->refresh_golden_frame = 1;
+        rc->gfu_boost = DEFAULT_GF_BOOST >> 1;
+        rc->baseline_gf_interval = VPXMIN(20,
+            VPXMAX(10, rc->baseline_gf_interval));
+        rc->frames_till_gf_update_due = rc->baseline_gf_interval;
+        if (rc->frames_till_gf_update_due > rc->frames_to_key)
+          rc->frames_till_gf_update_due = rc->frames_to_key;
+        target = calc_pframe_target_size_one_pass_vbr(cpi);
+        vp9_rc_set_frame_target(cpi, target);
+        rc->count_last_scene_change = 0;
+        rc->avg_intersize_gfint = 0;
+      } else {
+        rc->count_last_scene_change++;
+      }
     }
   }
 }
