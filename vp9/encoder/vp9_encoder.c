fcd163cdaf8212643619283b24892098afab9043
Yaowu Xu <yaowu@google.com>
2016-02-21 18:49:01 -0800

    Move psnrhvs function declaration to psnr.h
    From "ssim.h"
    
    Change-Id: Ie53378794149ef8a844b4eb47ad4f08579de4b60

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -4460,7 +4460,7 @@ int vp9_get_compressed_data(VP9_COMP *cpi, unsigned int *frame_flags,
       {
         double y, u, v, frame_all;
         frame_all = vpx_psnrhvs(cpi->Source, cm->frame_to_show, &y, &u, &v,
-                                bit_depth);
+                                bit_depth, in_bit_depth);
         adjust_image_stat(y, u, v, frame_all, &cpi->psnrhvs);
       }
     }
