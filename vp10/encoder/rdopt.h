80bdc7d4148e9e7f1719f5ff624a6625f2d8a570
Yunqing Wang <yunqingwang@google.com>
2016-03-10 11:07:50 -0800

    Add high-precision sub-pixel search as a speed feature
    Using the up-sampled reference frames in sub-pixel motion search is
    enabled as a speed feature for good-quality mode speed 0 and speed 1.
    
    Change-Id: Ieb454bf8c646ddb99e87bd64c8e74dbd78d84a50

--- vp10/encoder/rdopt.h
+++ vp10/encoder/rdopt.h
@@ -103,7 +103,6 @@ void vp10_build_prediction_by_left_preds(VP10_COMP *cpi,
                                          int tmp_stride[MAX_MB_PLANE]);
 #endif  // CONFIG_OBMC
 
-#if CONFIG_AFFINE_MOTION
 static INLINE const YV12_BUFFER_CONFIG *get_upsampled_ref(VP10_COMP *cpi,
                                                           const int ref) {
   // Use up-sampled reference frames.
@@ -117,7 +116,6 @@ static INLINE const YV12_BUFFER_CONFIG *get_upsampled_ref(VP10_COMP *cpi,
 
   return &cpi->upsampled_ref_bufs[cpi->upsampled_ref_idx[ref_idx]].buf;
 }
-#endif
 
 #ifdef __cplusplus
 }  // extern "C"
