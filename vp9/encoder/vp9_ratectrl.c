af0fa1fbe74d16adc2b069ed838db999e8f63fb2
Marco <marpan@google.com>
2016-03-28 15:14:55 -0700

    vp9: Adjust gf period for 1 pass vbr mode.
    Only affects 1 pass vbr mode.
    
    Some code cleanup to better adjust boost parameters,
    and some adjustment gf interval for near (up-coming) key frames
    (more even spacing near key frame and avoid gf on scene cut if its
    close to key frame).
    
    Small increase in metrics: AvgPSNR/SSIM up by ~0.29/24% on ytlive set.
    
    Also code cleanup for aq-mode under vbr, and leave some nominal settings
    for boost parameters in aq-mode for now (aq-mode is off by default for ytlive).
    
    Change-Id: I817fc878e45ea0ddef5c118cd60fca8b63ad64cf

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -1476,6 +1476,27 @@ static int calc_iframe_target_size_one_pass_vbr(const VP9_COMP *const cpi) {
   return vp9_rc_clamp_iframe_target_size(cpi, target);
 }
 
+static void set_gf_intervalboost_one_pass_vbr(VP9_COMP *const cpi) {
+  RATE_CONTROL *const rc = &cpi->rc;
+  if (cpi->oxcf.aq_mode == CYCLIC_REFRESH_AQ) {
+    vp9_cyclic_refresh_update_parameters_vbr(cpi);
+  } else {
+    // TODO(marpan): Adjust this based on overshoot in last gf_interval.
+    rc->gfu_boost = DEFAULT_GF_BOOST;
+    rc->baseline_gf_interval = (rc->min_gf_interval + rc->max_gf_interval) >> 1;
+    if (rc->high_source_sad) {
+      rc->gfu_boost = DEFAULT_GF_BOOST >> 1;
+      rc->baseline_gf_interval =
+          VPXMIN(20, VPXMAX(10, rc->baseline_gf_interval));
+    }
+    // Reset the gf interval to make more equal spacing for up-coming key frame.
+    if ((rc->frames_to_key <= 7 * rc->baseline_gf_interval >> 2) &&
+        (rc->frames_to_key > rc->baseline_gf_interval)) {
+      rc->baseline_gf_interval = rc->frames_to_key >> 1;
+    }
+  }
+}
+
 void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
   VP9_COMMON *const cm = &cpi->common;
   RATE_CONTROL *const rc = &cpi->rc;
@@ -1496,12 +1517,7 @@ void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
     cm->frame_type = INTER_FRAME;
   }
   if (rc->frames_till_gf_update_due == 0) {
-    if (cpi->oxcf.aq_mode == CYCLIC_REFRESH_AQ && cpi->oxcf.pass == 0) {
-      vp9_cyclic_refresh_set_golden_update(cpi);
-    } else {
-      rc->baseline_gf_interval =
-          (rc->min_gf_interval + rc->max_gf_interval) / 2;
-    }
+    set_gf_intervalboost_one_pass_vbr(cpi);
     rc->frames_till_gf_update_due = rc->baseline_gf_interval;
     // NOTE: frames_till_gf_update_due must be <= frames_to_key.
     if (rc->frames_till_gf_update_due > rc->frames_to_key) {
@@ -1512,15 +1528,12 @@ void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
     }
     cpi->refresh_golden_frame = 1;
     rc->source_alt_ref_pending = USE_ALTREF_FOR_ONE_PASS;
-    rc->gfu_boost = DEFAULT_GF_BOOST;
   }
   if (cm->frame_type == KEY_FRAME)
     target = calc_iframe_target_size_one_pass_vbr(cpi);
   else
     target = calc_pframe_target_size_one_pass_vbr(cpi);
   vp9_rc_set_frame_target(cpi, target);
-  if (cpi->oxcf.aq_mode == CYCLIC_REFRESH_AQ && cpi->oxcf.pass == 0)
-    vp9_cyclic_refresh_update_parameters(cpi);
 }
 
 static int calc_pframe_target_size_one_pass_cbr(const VP9_COMP *cpi) {
@@ -2086,14 +2099,14 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
     if (avg_sad > 0 || cpi->oxcf.rc_mode == VPX_CBR)
       rc->avg_source_sad = (rc->avg_source_sad + avg_sad) >> 1;
     // For VBR, under scene change/high content change, force golden refresh.
+    // Don't force refresh if key frame is coming within a few frames.
     if (cpi->oxcf.rc_mode == VPX_VBR &&
         rc->high_source_sad &&
+        rc->frames_to_key > 3 &&
         cpi->ext_refresh_frame_flags_pending == 0) {
       int target;
       cpi->refresh_golden_frame = 1;
-      rc->gfu_boost = DEFAULT_GF_BOOST >> 1;
-      rc->baseline_gf_interval = VPXMIN(20,
-          VPXMAX(10, rc->baseline_gf_interval));
+      set_gf_intervalboost_one_pass_vbr(cpi);
       rc->frames_till_gf_update_due = rc->baseline_gf_interval;
       if (rc->frames_till_gf_update_due > rc->frames_to_key)
         rc->frames_till_gf_update_due = rc->frames_to_key;
