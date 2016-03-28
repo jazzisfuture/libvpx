d87760e3b86308a82bb19b61aee5e271a2e484f0
Marco <marpan@google.com>
2016-03-28 15:14:55 -0700

    vp9: Adjust gf period and delta-qp for 1 pass vbr mode.
    Adjustment is based on average QP, and key-frame spacing.
    Also, increase threshold in scene-cut detection.
    
    Change-Id: I817fc878e45ea0ddef5c118cd60fca8b63ad64cf

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -1449,6 +1449,21 @@ static int calc_iframe_target_size_one_pass_vbr(const VP9_COMP *const cpi) {
   return vp9_rc_clamp_iframe_target_size(cpi, target);
 }
 
+static void set_gf_interval_one_pass_vbr(VP9_COMP *const cpi) {
+  VP9_COMMON *const cm = &cpi->common;
+  RATE_CONTROL *const rc = &cpi->rc;
+  if (cpi->oxcf.aq_mode == CYCLIC_REFRESH_AQ) {
+    vp9_cyclic_refresh_update_parameters_vbr(cpi);
+  } else {
+    rc->baseline_gf_interval =
+        (rc->min_gf_interval + rc->max_gf_interval) >> 1;
+    // Increase the gf interval unless the average QP is very low.
+    if (cm->current_video_frame > 30 &&
+        rc->avg_frame_qindex[INTER_FRAME] > (rc->worst_quality >> 2))
+      rc->baseline_gf_interval = (rc->min_gf_interval + rc->max_gf_interval);
+  }
+}
+
 void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
   VP9_COMMON *const cm = &cpi->common;
   RATE_CONTROL *const rc = &cpi->rc;
@@ -1469,12 +1484,7 @@ void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
     cm->frame_type = INTER_FRAME;
   }
   if (rc->frames_till_gf_update_due == 0) {
-    if (cpi->oxcf.aq_mode == CYCLIC_REFRESH_AQ && cpi->oxcf.pass == 0) {
-      vp9_cyclic_refresh_set_golden_update(cpi);
-    } else {
-      rc->baseline_gf_interval =
-          (rc->min_gf_interval + rc->max_gf_interval) / 2;
-    }
+    set_gf_interval_one_pass_vbr(cpi);
     rc->frames_till_gf_update_due = rc->baseline_gf_interval;
     // NOTE: frames_till_gf_update_due must be <= frames_to_key.
     if (rc->frames_till_gf_update_due > rc->frames_to_key) {
@@ -1484,6 +1494,9 @@ void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
       rc->constrained_gf_group = 0;
     }
     cpi->refresh_golden_frame = 1;
+    // Don't update golden frame if key frame is coming within a few frames.
+    if (rc->frames_to_key < 4)
+      cpi->refresh_golden_frame = 0;
     rc->source_alt_ref_pending = USE_ALTREF_FOR_ONE_PASS;
     rc->gfu_boost = DEFAULT_GF_BOOST;
   }
@@ -1492,8 +1505,6 @@ void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
   else
     target = calc_pframe_target_size_one_pass_vbr(cpi);
   vp9_rc_set_frame_target(cpi, target);
-  if (cpi->oxcf.aq_mode == CYCLIC_REFRESH_AQ && cpi->oxcf.pass == 0)
-    vp9_cyclic_refresh_update_parameters(cpi);
 }
 
 static int calc_pframe_target_size_one_pass_cbr(const VP9_COMP *cpi) {
@@ -2047,7 +2058,7 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
     // for cases where there is small change from content that is completely
     // static.
     if (cpi->oxcf.rc_mode == VPX_VBR) {
-      min_thresh = 30000;
+      min_thresh = 40000;
       thresh = 2.0f;
     }
     if (avg_sad >
@@ -2065,7 +2076,7 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
         cpi->ext_refresh_frame_flags_pending == 0) {
       int target;
       cpi->refresh_golden_frame = 1;
-      rc->frames_till_gf_update_due = rc->baseline_gf_interval;
+      rc->frames_till_gf_update_due = rc->baseline_gf_interval >> 1;
       if (rc->frames_till_gf_update_due > rc->frames_to_key)
         rc->frames_till_gf_update_due = rc->frames_to_key;
       rc->gfu_boost = DEFAULT_GF_BOOST;
