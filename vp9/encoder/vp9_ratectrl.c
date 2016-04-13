99366e7a747b0b5c628fa9946a9759356189c8f1
Marco <marpan@google.com>
2016-04-13 11:18:16 -0700

    vp9: Add contraint to spacing between scene-cut detections.
    Change only affects 1 pass vbr real-time mode.
    
    Change-Id: I1917e748d845e244812d11aec2a9d755372ec182

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -1492,6 +1492,7 @@ void vp9_rc_get_one_pass_vbr_params(VP9_COMP *cpi) {
     rc->frames_to_key = cpi->oxcf.key_freq;
     rc->kf_boost = DEFAULT_KF_BOOST;
     rc->source_alt_ref_active = 0;
+    rc->count_last_scene_change = 0;
   } else {
     cm->frame_type = INTER_FRAME;
   }
@@ -2079,15 +2080,20 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
     }
     if (avg_sad >
         VPXMAX(min_thresh, (unsigned int)(rc->avg_source_sad  * thresh)) &&
-        rc->frames_since_key > 1)
+        rc->frames_since_key > 1) {
       rc->high_source_sad = 1;
-    else
+      rc->count_last_scene_change = 0;
+    }
+    else {
       rc->high_source_sad = 0;
+      rc->count_last_scene_change++;
+    }
     if (avg_sad > 0 || cpi->oxcf.rc_mode == VPX_CBR)
       rc->avg_source_sad = (rc->avg_source_sad + avg_sad) >> 1;
     // For VBR, under scene change/high content change, force golden refresh.
     if (cpi->oxcf.rc_mode == VPX_VBR &&
         rc->high_source_sad &&
+        rc->count_last_scene_change > 4 &&
         cpi->ext_refresh_frame_flags_pending == 0) {
       int target;
       cpi->refresh_golden_frame = 1;
