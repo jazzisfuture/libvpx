39506add8f3826fb35d96c9795d2db2ae3e52f19
Marco <marpan@google.com>
2016-04-20 15:53:20 -0700

    vp9: 1 pass vbr: Adjust gf boost/interval on motion level.
    Change-Id: If700be1ce849c187ed031221ea7cabbc8d91ec2b

--- vp9/encoder/vp9_ratectrl.h
+++ vp9/encoder/vp9_ratectrl.h
@@ -163,6 +163,7 @@ typedef struct {
   int high_source_sad;
   int count_last_scene_change;
   int avg_intersize_gfint;
+  int cnt_zeromv_gfint;
 } RATE_CONTROL;
 
 struct VP9_COMP;
@@ -278,6 +279,8 @@ void vp9_set_target_rate(struct VP9_COMP *cpi);
 int vp9_resize_one_pass_cbr(struct VP9_COMP *cpi);
 
 void vp9_avg_source_sad(struct VP9_COMP *cpi);
+
+void vp9_compute_amount_motion(struct VP9_COMP *const cpi);
 
 int vp9_encodedframe_overshoot(struct VP9_COMP *cpi, int frame_size, int *q);
 
