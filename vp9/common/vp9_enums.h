5107c6dbcce031923060128d5fe8923a7b2ffc27
hui su <huisu@google.com>
2016-05-05 12:04:42 -0700

    Add vp9 encoder API VP9E_GET_LEVEL to provide bitstream level
    Change-Id: I1ef3df0192491035728fe9d5eb25cc66dc2965de

--- vp9/common/vp9_enums.h
+++ vp9/common/vp9_enums.h
@@ -140,6 +140,26 @@ typedef uint8_t PREDICTION_MODE;
 #define COMP_INTER_CONTEXTS 5
 #define REF_CONTEXTS 5
 
+#define VP9_LEVELS 14
+typedef enum {
+  LEVEL_UNKNOWN = 0,
+  LEVEL_1 = 10,
+  LEVEL_1_1 = 11,
+  LEVEL_2 = 20,
+  LEVEL_2_1 = 21,
+  LEVEL_3 = 30,
+  LEVEL_3_1 = 31,
+  LEVEL_4 = 40,
+  LEVEL_4_1 = 41,
+  LEVEL_5 = 50,
+  LEVEL_5_1 = 51,
+  LEVEL_5_2 = 52,
+  LEVEL_6 = 60,
+  LEVEL_6_1 = 61,
+  LEVEL_6_2 = 62,
+  LEVEL_MAX = 255
+} VP9_LEVEL;
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
