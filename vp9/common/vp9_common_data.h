d8979f47665243c6b93857a7d5c63077dc48fd9b
hui su <huisu@google.com>
2016-05-05 12:04:42 -0700

    Add vp9 encoder API VP9E_GET_LEVEL to provide bitstream level
    Change-Id: I1ef3df0192491035728fe9d5eb25cc66dc2965de

--- vp9/common/vp9_common_data.h
+++ vp9/common/vp9_common_data.h
@@ -18,6 +18,49 @@
 extern "C" {
 #endif
 
+#define CPB_WINDOW_SIZE 4
+#define FRAME_WINDOW_SIZE 128
+#define SAMPLE_RATE_GRACE_P 0.015
+
+typedef struct {
+  VP9_LEVEL level;
+  uint64_t max_luma_sample_rate;
+  uint32_t max_luma_picture_size;
+  double average_bitrate;  // in kilobits per second
+  double max_cpb_size;  // in kilobits
+  double compression_ratio;
+  uint8_t max_col_tiles;
+  uint32_t min_altref_distance;
+  uint8_t max_ref_frame_buffers;
+} VP9_LEVEL_SPEC;
+
+typedef struct {
+  int64_t ts;  // timestamp
+  uint32_t luma_samples;
+  uint32_t size;  // in bytes
+} FRAME_RECORD;
+
+typedef struct {
+  FRAME_RECORD buf[FRAME_WINDOW_SIZE];
+  uint8_t start;
+  uint8_t len;
+} FRAME_WINDOW_BUFFER;
+
+typedef struct {
+  uint8_t seen_first_altref;
+  uint32_t frames_since_last_altref;
+  uint64_t total_compressed_size;  // in bytes
+  uint64_t total_uncompressed_size;  // in bytes
+  double time_encoded;  // in seconds
+  FRAME_WINDOW_BUFFER frame_window_buffer;
+  int ref_refresh_map;
+} VP9_LEVEL_STATS;
+
+typedef struct {
+  VP9_LEVEL_STATS level_stats;
+  VP9_LEVEL_SPEC level_spec;
+} VP9_LEVEL_INFO;
+
 extern const uint8_t b_width_log2_lookup[BLOCK_SIZES];
 extern const uint8_t b_height_log2_lookup[BLOCK_SIZES];
 extern const uint8_t mi_width_log2_lookup[BLOCK_SIZES];
@@ -33,6 +76,7 @@ extern const TX_SIZE max_txsize_lookup[BLOCK_SIZES];
 extern const BLOCK_SIZE txsize_to_bsize[TX_SIZES];
 extern const TX_SIZE tx_mode_to_biggest_tx_size[TX_MODES];
 extern const BLOCK_SIZE ss_size_lookup[BLOCK_SIZES][2][2];
+extern const VP9_LEVEL_SPEC vp9_level_defs[VP9_LEVELS];
 
 #ifdef __cplusplus
 }  // extern "C"
