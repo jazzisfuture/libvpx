1cc193326d066dc03638e67cb773fc2b8a2cc1ac
Yaowu Xu <yaowu@google.com>
2016-05-06 14:21:06 -0700

    Make parameter types consistent
    This fixes compiler warnings from MSVC.
    
    Change-Id: Iaac0e994869561371295578a893f766493ce0544

--- vp10/encoder/x86/highbd_fwd_txfm_sse4.c
+++ vp10/encoder/x86/highbd_fwd_txfm_sse4.c
@@ -122,7 +122,7 @@ static INLINE void write_buffer_4x4(__m128i *res, tran_low_t *output) {
 // Note:
 //  We implement vp10_fwd_txfm2d_4x4(). This function is kept here since
 //  vp10_highbd_fht4x4_c() is not removed yet
-void vp10_highbd_fht4x4_sse4_1(const int16_t *input, tran_low_t *output,
+void vp10_highbd_fht4x4_sse4_1(const int16_t *input, int *output,
                                int stride, int tx_type) {
   (void)input;
   (void)output;
@@ -205,8 +205,8 @@ static void fadst4x4_sse4_1(__m128i *in, int bit) {
   in[3] = _mm_unpackhi_epi64(v1, v3);
 }
 
-void vp10_fwd_txfm2d_4x4_sse4_1(const int16_t *input, tran_low_t *coeff,
-                                int input_stride, int tx_type,
+void vp10_fwd_txfm2d_4x4_sse4_1(const int16_t *input, int *coeff,
+                                const int input_stride, int tx_type,
                                 const int bd) {
   __m128i in[4];
   const TXFM_2D_CFG *cfg = NULL;
@@ -918,7 +918,7 @@ static void fadst8x8_sse4_1(__m128i *in, __m128i *out, int bit) {
 }
 
 void vp10_fwd_txfm2d_8x8_sse4_1(const int16_t *input, tran_low_t *coeff,
-                                int stride, int tx_type, int bd) {
+                                const int stride, int tx_type, const int bd) {
   __m128i in[16], out[16];
   const TXFM_2D_CFG *cfg = NULL;
 
