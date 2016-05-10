a0d929e19b159844fa2f97be3be63b4ef0691a94
Kyle Siefring <kylesiefring@gmail.com>
2016-05-10 17:56:37 -0400

    Add 31bit pairs before unpacking in x86 block error code
    AVX2 code provides a very minor performance improvement.
    
    Change-Id: I5ca8c7f7775585a17fe09d6bbfc25e1f2955eb0a

--- vp9/encoder/x86/vp9_error_intrin_avx2.c
+++ vp9/encoder/x86/vp9_error_intrin_avx2.c
@@ -17,7 +17,7 @@ int64_t vp9_block_error_avx2(const int16_t *coeff,
                              const int16_t *dqcoeff,
                              intptr_t block_size,
                              int64_t *ssz) {
-  __m256i sse_reg, ssz_reg, coeff_reg, dqcoeff_reg;
+  __m256i sse_reg, ssz_reg, coeff_reg1, dqcoeff_reg1, coeff_reg2, dqcoeff_reg2;
   __m256i exp_dqcoeff_lo, exp_dqcoeff_hi, exp_coeff_lo, exp_coeff_hi;
   __m256i sse_reg_64hi, ssz_reg_64hi;
   __m128i sse_reg128, ssz_reg128;
@@ -25,31 +25,62 @@ int64_t vp9_block_error_avx2(const int16_t *coeff,
   int i;
   const __m256i zero_reg = _mm256_set1_epi16(0);
 
-  // init sse and ssz registerd to zero
-  sse_reg = _mm256_set1_epi16(0);
-  ssz_reg = _mm256_set1_epi16(0);
-
-  for (i = 0 ; i < block_size ; i+= 16) {
+  // if the block size is only 16 then use the code that loads only 32 bytes
+  if (block_size == 16) {
     // load 32 bytes from coeff and dqcoeff
-    coeff_reg = _mm256_loadu_si256((const __m256i *)(coeff + i));
-    dqcoeff_reg = _mm256_loadu_si256((const __m256i *)(dqcoeff + i));
+    coeff_reg1 = _mm256_loadu_si256((const __m256i *)(coeff));
+    dqcoeff_reg1 = _mm256_loadu_si256((const __m256i *)(dqcoeff));
     // dqcoeff - coeff
-    dqcoeff_reg = _mm256_sub_epi16(dqcoeff_reg, coeff_reg);
+    dqcoeff_reg1 = _mm256_sub_epi16(dqcoeff_reg1, coeff_reg1);
     // madd (dqcoeff - coeff)
-    dqcoeff_reg = _mm256_madd_epi16(dqcoeff_reg, dqcoeff_reg);
+    dqcoeff_reg1 = _mm256_madd_epi16(dqcoeff_reg1, dqcoeff_reg1);
     // madd coeff
-    coeff_reg = _mm256_madd_epi16(coeff_reg, coeff_reg);
-    // expand each double word of madd (dqcoeff - coeff) to quad word
-    exp_dqcoeff_lo = _mm256_unpacklo_epi32(dqcoeff_reg, zero_reg);
-    exp_dqcoeff_hi = _mm256_unpackhi_epi32(dqcoeff_reg, zero_reg);
-    // expand each double word of madd (coeff) to quad word
-    exp_coeff_lo = _mm256_unpacklo_epi32(coeff_reg, zero_reg);
-    exp_coeff_hi = _mm256_unpackhi_epi32(coeff_reg, zero_reg);
-    // add each quad word of madd (dqcoeff - coeff) and madd (coeff)
-    sse_reg = _mm256_add_epi64(sse_reg, exp_dqcoeff_lo);
-    ssz_reg = _mm256_add_epi64(ssz_reg, exp_coeff_lo);
-    sse_reg = _mm256_add_epi64(sse_reg, exp_dqcoeff_hi);
-    ssz_reg = _mm256_add_epi64(ssz_reg, exp_coeff_hi);
+    coeff_reg1 = _mm256_madd_epi16(coeff_reg1, coeff_reg1);
+    // save the higher 64 bit of each 128 bit lane
+    dqcoeff_reg2 = _mm256_srli_si256(dqcoeff_reg1, 8);
+    coeff_reg2 = _mm256_srli_si256(coeff_reg1, 8);
+    // add the higher 64 bit to the low 64 bit
+    dqcoeff_reg1 = _mm256_add_epi32(dqcoeff_reg1, dqcoeff_reg2);
+    coeff_reg1 = _mm256_add_epi32(coeff_reg1, coeff_reg2);
+    // expand each double word in the lower 64 bits to quad word
+    sse_reg = _mm256_unpacklo_epi32(dqcoeff_reg1, zero_reg);
+    ssz_reg  = _mm256_unpacklo_epi32(coeff_reg1, zero_reg);
+  } else {
+    // init sse and ssz registerd to zero
+    sse_reg = _mm256_set1_epi16(0);
+    ssz_reg = _mm256_set1_epi16(0);
+
+    for (i = 0; i < block_size; i+= 32) {
+      // load 64 bytes from coeff and dqcoeff
+      coeff_reg1 = _mm256_loadu_si256((const __m256i *)(coeff + i));
+      dqcoeff_reg1 = _mm256_loadu_si256((const __m256i *)(dqcoeff + i));
+      coeff_reg2 = _mm256_loadu_si256((const __m256i *)(coeff + i + 16));
+      dqcoeff_reg2 = _mm256_loadu_si256((const __m256i *)(dqcoeff + i + 16));
+      // dqcoeff - coeff
+      dqcoeff_reg1 = _mm256_sub_epi16(dqcoeff_reg1, coeff_reg1);
+      dqcoeff_reg2 = _mm256_sub_epi16(dqcoeff_reg2, coeff_reg2);
+      // madd (dqcoeff - coeff)
+      dqcoeff_reg1 = _mm256_madd_epi16(dqcoeff_reg1, dqcoeff_reg1);
+      dqcoeff_reg2 = _mm256_madd_epi16(dqcoeff_reg2, dqcoeff_reg2);
+      // madd coeff
+      coeff_reg1 = _mm256_madd_epi16(coeff_reg1, coeff_reg1);
+      coeff_reg2 = _mm256_madd_epi16(coeff_reg2, coeff_reg2);
+      // add the first madd (dqcoeff - coeff) with the second
+      dqcoeff_reg1 = _mm256_add_epi32(dqcoeff_reg1, dqcoeff_reg2);
+      // add the first madd (coeff) with the second
+      coeff_reg1 = _mm256_add_epi32(coeff_reg1, coeff_reg2);
+      // expand each double word of madd (dqcoeff - coeff) to quad word
+      exp_dqcoeff_lo = _mm256_unpacklo_epi32(dqcoeff_reg1, zero_reg);
+      exp_dqcoeff_hi = _mm256_unpackhi_epi32(dqcoeff_reg1, zero_reg);
+      // expand each double word of madd (coeff) to quad word
+      exp_coeff_lo = _mm256_unpacklo_epi32(coeff_reg1, zero_reg);
+      exp_coeff_hi = _mm256_unpackhi_epi32(coeff_reg1, zero_reg);
+      // add each quad word of madd (dqcoeff - coeff) and madd (coeff)
+      sse_reg = _mm256_add_epi64(sse_reg, exp_dqcoeff_lo);
+      ssz_reg = _mm256_add_epi64(ssz_reg, exp_coeff_lo);
+      sse_reg = _mm256_add_epi64(sse_reg, exp_dqcoeff_hi);
+      ssz_reg = _mm256_add_epi64(ssz_reg, exp_coeff_hi);
+    }
   }
   // save the higher 64 bit of each 128 bit lane
   sse_reg_64hi = _mm256_srli_si256(sse_reg, 8);
