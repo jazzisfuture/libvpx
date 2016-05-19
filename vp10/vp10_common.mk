613758cde65a297f0a96fc6df1fece1d5f8cd3d8
Yi Luo <luoyi@google.com>
2016-05-19 14:13:07 -0700

    HBD inverse HT 8x8 and 16x16 sse4.1 optimization
    - Covers tx_type: DCT_DCT, DCT_ADST, ADST_DCT, ADST_ADST.
    - Encoding speed improves ~27% on crowd_run_1080p_12.
    - Merge 4x4, 8x8, 16x16 unit tests in one test file.
    
    Change-Id: I058ef5254d068a9523a826480c78ebbdd231824c

--- vp10/vp10_common.mk
+++ vp10/vp10_common.mk
@@ -113,6 +113,10 @@ VP10_COMMON_SRCS-$(HAVE_SSE4_1) += common/x86/vp10_fwd_txfm1d_sse4.c
 VP10_COMMON_SRCS-$(HAVE_SSE4_1) += common/x86/vp10_fwd_txfm2d_sse4.c
 
 ifneq ($(CONFIG_VP9_HIGHBITDEPTH),yes)
+VP10_COMMON_SRCS-$(HAVE_SSE4_1) += common/x86/highbd_txfm_utility_sse4.h
+endif
+
+ifneq ($(CONFIG_VP9_HIGHBITDEPTH),yes)
 VP10_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/iht4x4_add_neon.c
 VP10_COMMON_SRCS-$(HAVE_NEON) += common/arm/neon/iht8x8_add_neon.c
 endif
