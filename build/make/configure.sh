c9c3fc9b2b6f87d102a6eb8e75fd67a170156fdc
Johann <johannkoenig@google.com>
2016-06-10 14:59:26 -0700

    Parse codec options in order of occurrence.
    Each time a codec is enabled or disabled with the umbrella
    --enable-vpN flag, set the encoder and decoder configurations as well.
    This was done as a post-processing step but doing that lost the order of
    the arguments.
    
    Because the _feature function has been overridden to accomplish this
    set_all must be used directly to clean up the umbrella configuration.
    
    BUG=webm:1205
    
    Change-Id: Ic629bfdd06acc04bc5a7227309f36bba54dad8b1

--- build/make/configure.sh
+++ build/make/configure.sh
@@ -186,11 +186,29 @@ add_extralibs() {
 # Boolean Manipulation Functions
 #
 enable_feature(){
-  set_all yes $*
+  case "$1" in
+    vp8|vp9|vp10)
+      set_all yes $1
+      set_all yes $1_encoder
+      set_all yes $1_decoder
+      ;;
+    *)
+      set_all yes $*
+      ;;
+  esac
 }
 
 disable_feature(){
-  set_all no $*
+  case "$1" in
+    vp8|vp9|vp10)
+      set_all no $1
+      set_all no $1_encoder
+      set_all no $1_decoder
+      ;;
+    *)
+      set_all no $*
+      ;;
+  esac
 }
 
 enabled(){
