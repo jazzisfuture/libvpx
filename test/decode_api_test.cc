f6f256ad4d52f1c5fa13960af97e720ae65e0592
Vignesh Venkatasubramanian <vigneshv@google.com>
2016-06-22 10:24:27 -0700

    vp9: Fix potential SEGV in decoder_peek_si_internal
    decoder_peek_si_internal could potentially read more bytes than
    what actually exists in the input buffer. We check for the buffer
    size to be at least 8, but we try to read up to 10 bytes in the
    worst case. A well crafted file could thus cause a segfault.
    Likely change that introduced this bug was:
    https://chromium-review.googlesource.com/#/c/70439 (git hash:
    7c43fb6)
    
    BUG=chromium:621095
    
    Change-Id: Id74880cfdded44caaa45bbdbaac859c09d3db752

--- test/decode_api_test.cc
+++ test/decode_api_test.cc
@@ -146,6 +146,30 @@ TEST(DecodeAPI, Vp9InvalidDecode) {
   TestVp9Controls(&dec);
   EXPECT_EQ(VPX_CODEC_OK, vpx_codec_destroy(&dec));
 }
+
+TEST(DecodeAPI, Vp9PeekSI) {
+  const vpx_codec_iface_t *const codec = &vpx_codec_vp9_dx_algo;
+  // The first 9 bytes are valid and the rest of the bytes are made up. Until
+  // size 10, this should return VPX_CODEC_UNSUP_BITSTREAM and after that it
+  // should return VPX_CODEC_CORRUPT_FRAME.
+  const uint8_t data[32] = {
+    0x85, 0xa4, 0xc1, 0xa1, 0x38, 0x81, 0xa3, 0x49,
+    0x83, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+  };
+
+  for (uint32_t data_sz = 8; data_sz <= 32; ++data_sz) {
+    vpx_codec_ctx_t dec;
+    EXPECT_EQ(VPX_CODEC_OK, vpx_codec_dec_init(&dec, codec, NULL, 0));
+    EXPECT_EQ((data_sz < 10) ?
+                  VPX_CODEC_UNSUP_BITSTREAM : VPX_CODEC_CORRUPT_FRAME,
+              vpx_codec_decode(&dec, data, data_sz, NULL, 0));
+    vpx_codec_iter_t iter = NULL;
+    EXPECT_EQ(NULL, vpx_codec_get_frame(&dec, &iter));
+    EXPECT_EQ(VPX_CODEC_OK, vpx_codec_destroy(&dec));
+  }
+}
 #endif  // CONFIG_VP9_DECODER
 
 }  // namespace
