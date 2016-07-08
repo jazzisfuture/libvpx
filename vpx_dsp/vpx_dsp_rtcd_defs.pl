26d02224476d045dad07316009a7c50f8e5e1697
Geza Lore <gezalore@gmail.com>
2016-07-08 14:10:53 +0100

    Optimize supertx decoding using blending functions.
    Added vpx_blend_{h,v}mask6b which take 1D masks and apply them
    horizontally (all rows use the same mask) or vertically (all
    columns use the same mask) directly. The optimized horizontal
    version falls back on the 2D vpx_blend_mask6 implementation
    at the moment. This can be improved upon if the function shows
    up high enough in a profile.
    
    Decoder speedup of up to 4% has been observed.
    
    Change-Id: I574aab64448a93e460fede1f535f242277fa7c16

--- vpx_dsp/vpx_dsp_rtcd_defs.pl
+++ vpx_dsp/vpx_dsp_rtcd_defs.pl
@@ -959,6 +959,27 @@ if ((vpx_config("CONFIG_VP9_ENCODER") eq "yes") || (vpx_config("CONFIG_VP10_ENCO
   }  # CONFIG_VP9_HIGHBITDEPTH
 }  # CONFIG_VP9_ENCODER || CONFIG_VP10_ENCODER
 
+if (vpx_config("CONFIG_VP10") eq "yes") {
+  #
+  # Blending with mask
+  #
+  add_proto qw/void vpx_blend_mask6/, "uint8_t *dst, uint32_t dst_stride,  uint8_t *src0, uint32_t src0_stride, uint8_t *src1, uint32_t src1_stride, const uint8_t *mask, uint32_t mask_stride, int h, int w, int suby, int subx";
+  add_proto qw/void vpx_blend_hmask6b/, "uint8_t *dst, uint32_t dst_stride,  uint8_t *src0, uint32_t src0_stride, uint8_t *src1, uint32_t src1_stride, const uint8_t *mask, int h, int w";
+  add_proto qw/void vpx_blend_vmask6b/, "uint8_t *dst, uint32_t dst_stride,  uint8_t *src0, uint32_t src0_stride, uint8_t *src1, uint32_t src1_stride, const uint8_t *mask, int h, int w";
+  specialize "vpx_blend_mask6", qw/sse4_1/;
+  specialize "vpx_blend_hmask6b", qw/sse4_1/;
+  specialize "vpx_blend_vmask6b", qw/sse4_1/;
+
+  if (vpx_config("CONFIG_VP9_HIGHBITDEPTH") eq "yes") {
+    add_proto qw/void vpx_highbd_blend_mask6/, "uint8_t *dst, uint32_t dst_stride,  uint8_t *src0, uint32_t src0_stride, uint8_t *src1, uint32_t src1_stride, const uint8_t *mask, uint32_t mask_stride, int h, int w, int suby, int subx, int bd";
+    add_proto qw/void vpx_highbd_blend_hmask6b/, "uint8_t *dst, uint32_t dst_stride,  uint8_t *src0, uint32_t src0_stride, uint8_t *src1, uint32_t src1_stride, const uint8_t *mask, int h, int w, int bd";
+    add_proto qw/void vpx_highbd_blend_vmask6b/, "uint8_t *dst, uint32_t dst_stride,  uint8_t *src0, uint32_t src0_stride, uint8_t *src1, uint32_t src1_stride, const uint8_t *mask, int h, int w, int bd";
+    specialize "vpx_highbd_blend_mask6", qw/sse4_1/;
+    specialize "vpx_highbd_blend_hmask6b", qw/sse4_1/;
+    specialize "vpx_highbd_blend_vmask6b", qw/sse4_1/;
+  }
+}  # CONFIG_VP10
+
 if (vpx_config("CONFIG_ENCODERS") eq "yes") {
 #
 # Block subtraction
---
+++ vpx_dsp/x86/blend_hmask6b_sse4.c
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <smmintrin.h>  // SSE4.1
+
+#include <assert.h>
+
+#include "vpx/vpx_integer.h"
+#include "vpx_ports/mem.h"
+#include "vpx_dsp/vpx_dsp_common.h"
+
+#include "vpx_dsp/x86/synonyms.h"
+
+#include "./vpx_dsp_rtcd.h"
+
+// To start out, just dispatch to the function using the 2D mask and
+// pass mask stride as 0. This can be improved upon if necessary.
+
+void vpx_blend_hmask6b_sse4_1(uint8_t *dst, uint32_t dst_stride,
+                              uint8_t *src0, uint32_t src0_stride,
+                              uint8_t *src1, uint32_t src1_stride,
+                              const uint8_t *mask, int h, int w) {
+  vpx_blend_mask6b_sse4_1(dst, dst_stride,
+                          src0, src0_stride,
+                          src1, src1_stride,
+                          mask, 0, h, w, 0, 0);
+}
+
+#if CONFIG_VP9_HIGHBITDEPTH
+void vpx_highbd_blend_hmask6b_sse4_1(uint8_t *dst_8, uint32_t dst_stride,
+                                     uint8_t *src0_8, uint32_t src0_stride,
+                                     uint8_t *src1_8, uint32_t src1_stride,
+                                     const uint8_t *mask, int h, int w, int bd) {
+  vpx_highbd_blend_mask6b_sse4_1(dst_8, dst_stride,
+                                 src0_8, src0_stride,
+                                 src1_8, src1_stride,
+                                 mask, 0, h, w, 0, 0, bd);
+}
+#endif  // CONFIG_VP9_HIGHBITDEPTH
