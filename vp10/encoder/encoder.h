1779c0d80d8944b75f207363c4b96565265e1255
Wei-ting Lin <weitinglin@google.com>
2016-07-12 13:19:45 -0700

    Allow OVERLAY frames to use the show_exsiting_frame flag
    ARF with zero strength temporal filter can be reused by setting the
    show_existing_frame = 1, and in this case, there is no need to
    refresh the reference frame buffer. However, we used the flag
    "refresh_golden_frame" as the identifier for the starting point of a gf
    group.
    
    Two new flags "is_arf_filter_off" and "is_gf_group_start" are used to track
    if the zero strength filter is used for a overlay frame. Therefore, we
    can still update rate-control parameter properly.
    
    Change-Id: I25971a760f6e1638d5147fe30488c48125512b1a

--- vp10/encoder/encoder.h
+++ vp10/encoder/encoder.h
@@ -614,7 +614,9 @@ typedef struct VP10_COMP {
 #if CONFIG_EXT_REFS
   int refresh_frame_mask;
   int existing_fb_idx_to_show;
-#endif  // CONFIG_EXT_REFS
+  int is_arf_filter_off;
+  int is_gf_group_start;
+#endif // CONFIG_EXT_REFS
 } VP10_COMP;
 
 void vp10_initialize_enc(void);
