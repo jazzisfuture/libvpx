ff0998696ba45985309c31d60f0124cbddc763e5
Yury Gitman <yuryg@google.com>
2016-07-15 11:42:21 -0700

    Add imwrite function to matx functions module
    I assume to use this function for debuging purposes
    
    Change-Id: I033ee8f6cec01282451ffe117d2bfa5621842df2

---
+++ vp9/common/vp9_matx_enums.c
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed  by a BSD-style license that can be
+ *  found in the LICENSE file in the root of the source tree. An additional
+ *  intellectual property  rights grant can  be found in the  file PATENTS.
+ *  All contributing  project authors may be  found in the AUTHORS  file in
+ *  the root of the source tree.
+ */
+
+const char* matx_type_format_strings[] = {
+    "%hhu",  // TYPE_8U
+    "%hhd",  // TYPE_8S
+     "%hu",  // TYPE_16U
+     "%hd",  // TYPE_16S
+      "%u",  // TYPE_32U
+      "%d",  // TYPE_32S
+    "%.4f",  // TYPE_32F
+    "%.4f"   // TYPE_64F
+};
