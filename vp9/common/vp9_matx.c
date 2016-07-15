1564dce00299ff35a6a10b04fd5b2ab85c9d89f6
Yury Gitman <yuryg@google.com>
2016-07-15 11:27:07 -0700

    Add templated MATX class for image processing
        I created this class for my own experiment on superframe adaptive
    quantization, but  the class is quite  general and may be  useful for
    others. It adds some  performance overhead, so I meant to  use it for
    images that are at least 3000 pixels large.
    
    Change-Id: I16865b6f7d1e760bf39cd1f42920832ec347e777

---
+++ vp9/common/vp9_matx.c
@@ -0,0 +1,173 @@
+/*
+ *  Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed  by a BSD-style license that can be
+ *  found in the LICENSE file in the root of the source tree. An additional
+ *  intellectual property  rights grant can  be found in the  file PATENTS.
+ *  All contributing  project authors may be  found in the AUTHORS  file in
+ *  the root of the source tree.
+ */
+
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "vpx_dsp/vpx_dsp_common.h"
+#include "vpx/vpx_integer.h"
+#include "vpx_mem/vpx_mem.h"
+
+#include "vp9/common/vp9_matx_enums.h"
+#include "vp9/common/vp9_matx.h"
+
+#define vpx_runtime_assert(expression) assert(expression)
+
+#define VP9_MATX_NBYTES(type, self) \
+    (self)->rows*(self)->stride*sizeof(type)
+
+#define VP9_MATX_REALLOC(type, self) \
+  (self)->data = vpx_realloc((self)->data, VP9_MATX_NBYTES(type, self));
+
+void vp9_matx_init(MATX_PTR const _self) {
+  struct MATX* const self = (struct MATX*) _self;
+
+  self->data = NULL;
+  self->is_wrapper = 0;
+
+  self->rows = 0;
+  self->cols = 0;
+  self->stride = 0;
+  self->cn = -1;
+  self->typeid = MATX_NO_TYPE;
+}
+
+void vp9_matx_create(MATX_PTR _self,
+                     int rows,
+                     int cols,
+                     int stride,
+                     int cn,
+                     MATX_TYPE typeid) {
+  struct MATX* const self = (struct MATX*) _self;
+
+  uint8_t reallocation_not_needed = 1;
+
+  if (self->rows != rows) {
+    self->rows = rows;
+    reallocation_not_needed = 0;
+  }
+
+  if (self->cols != cols) {
+    self->cols = cols;
+    reallocation_not_needed = 0;
+  }
+
+  if (self->stride != stride && stride != 0) {
+    self->stride = stride;
+    reallocation_not_needed = 0;
+  }
+
+  if (!stride)
+    self->stride = cols*cn;
+
+  if (self->cn != cn || self->typeid != typeid) {
+    self->typeid = typeid;
+    self->cn = cn;
+    reallocation_not_needed = 0;
+  }
+
+  if (reallocation_not_needed)
+    return;
+
+  vpx_runtime_assert(!self->is_wrapper);
+  vpx_runtime_assert(self->stride >= self->cols*self->cn);
+
+  switch (self->typeid) {
+    case TYPE_8U:
+      VP9_MATX_REALLOC(uint8_t, self);
+      break;
+    case TYPE_8S:
+      VP9_MATX_REALLOC(int8_t, self);
+      break;
+    case TYPE_16U:
+      VP9_MATX_REALLOC(uint16_t, self);
+      break;
+    case TYPE_16S:
+      VP9_MATX_REALLOC(int16_t, self);
+      break;
+    case TYPE_32U:
+      VP9_MATX_REALLOC(uint32_t, self);
+      break;
+    case TYPE_32S:
+      VP9_MATX_REALLOC(int32_t, self);
+      break;
+    case TYPE_32F:
+      VP9_MATX_REALLOC(float, self);
+      break;
+    case TYPE_64F:
+      VP9_MATX_REALLOC(double, self);
+      break;
+    default:
+      vpx_runtime_assert(0 /* matx: inapprorpiate type */);
+  }
+
+  vpx_runtime_assert(self->data != NULL);
+}
+
+void vp9_mat8u_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_8U);
+}
+
+void vp9_mat8s_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_8S);
+}
+
+void vp9_mat16u_create(MATX_PTR const self,
+                       int rows,
+                       int cols,
+                       int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_16U);
+}
+
+void vp9_mat16s_create(MATX_PTR const self,
+                       int rows,
+                       int cols,
+                       int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_16S);
+}
+
+void vp9_mat32u_create(MATX_PTR const self,
+                       int rows,
+                       int cols,
+                       int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_32U);
+}
+
+void vp9_mat32s_create(MATX_PTR const self,
+                       int rows,
+                       int cols,
+                       int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_32S);
+}
+
+void vp9_mat32f_create(MATX_PTR const self,
+                       int rows,
+                       int cols,
+                       int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_32F);
+}
+
+void vp9_mat64f_create(MATX_PTR const self,
+                       int rows,
+                       int cols,
+                       int stride, int cn) {
+  vp9_matx_create(self, rows, cols, stride, cn, TYPE_64F);
+}
+
+void vp9_matx_destroy(MATX_PTR _self) {
+  vpx_free(((struct MATX*) _self)->data);
+}
---
+++ vp9/common/vp9_matx.h
@@ -0,0 +1,227 @@
+/*
+ *  Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed  by a BSD-style license that can be
+ *  found in the LICENSE file in the root of the source tree. An additional
+ *  intellectual property  rights grant can  be found in the  file PATENTS.
+ *  All contributing  project authors may be  found in the AUTHORS  file in
+ *  the root of the source tree.
+ */
+
+/*
+ *  \file vp9_matx.h
+ *
+ *  This file contains generic templated matrix class
+ *  designed like OpenCV Mat class,  but much simpler
+ */
+
+#ifndef VP9_COMMON_VP9_MATX_H_
+#define VP9_COMMON_VP9_MATX_H_
+
+#include "vpx/vpx_integer.h"
+#include "./vp9_matx_enums.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define _DEFINE_MATX(typesuffix)  \
+struct MATX ## typesuffix {       \
+  int rows;                       \
+  int cols;                       \
+  int stride;                     \
+  int cn;                         \
+  MATX_TYPE typeid;               \
+                                  \
+  uint8_t is_wrapper;             \
+                                  \
+  ELEMTYPE##typesuffix *data;     \
+};
+
+#define DEFINE_MATX(typesuffix) \
+  _DEFINE_MATX(typesuffix)
+
+
+/*! Templated image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX();
+
+/*! 8bit unsigned image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_8U);
+
+/*! 8bit signed image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_8S);
+
+/*! 16bit unsigned image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_16U);
+
+/*! 16bit signed image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_16S);
+
+/*! 32bit unsigned image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_32U);
+
+/*! 32bit signed image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_32S);
+
+/*! 32bit floating point image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_32F);
+
+/*! 64bit floating point image/matrix class
+ *  (inspired by OpenCV Matx class) */
+
+DEFINE_MATX(_64F);
+
+
+/*!\brief Constructor
+ *
+ * \param    self    Instance of the class
+ */
+void vp9_matx_init(MATX_PTR const self);
+
+/*!\brief Create new matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ * \param    typeid  Element type for the created matrix
+ */
+void vp9_matx_create(MATX_PTR const self,
+                     int rows,
+                     int cols,
+                     int stride,
+                     int cn,
+                     MATX_TYPE typeid);
+
+/*!\brief Create new uint8_t matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat8u_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Create new int8_t matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat8s_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Create new uint16_t matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat16u_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Create new int16_t matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat16s_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Create new uint32_t matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat32u_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Create new int32_t matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat32s_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Create new float matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat32f_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Create new double matrix or update existing
+ *
+ * \param    self    Instance of the class
+ * \param    rows    Number of rows
+ * \param    cols    Number of cols
+ * \param    stride  Stride in elements
+ * \param    cn      Number of channels
+ */
+void vp9_mat64f_create(MATX_PTR const self,
+                      int rows,
+                      int cols,
+                      int stride, int cn);
+
+/*!\brief Destructor
+ *
+ * \param    self    Instance of the class
+ */
+void vp9_matx_destroy(MATX_PTR const self);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // VP9_COMMON_VP9_MATX_H_
