674f26eb1f85e23b21c89c85884b6282759980c8
Yury Gitman <yuryg@google.com>
2016-07-18 12:17:58 -0700

    Add --alt-ref-aq=<int> option
    In the future this option will activate adaptive quantization special
    for altref frames. Encoder will  create the adaptive quantization map
    on the basis of lookahead buffers similarity which is the estimate of
    the future motion compensation performance.
    
    Change-Id: Ia0088b3babb0f9a4899c79d8d819947ba5a03df2

--- vpx/vp8cx.h
+++ vpx/vp8cx.h
@@ -399,6 +399,19 @@ enum vp8e_enc_control_id {
    */
   VP9E_SET_AQ_MODE,
 
+  /*!\brief Codec control function to enable/disable special mode for altref
+   *        adaptive quantization. You can use it with --aq-mode concurrently.
+   *
+   * Enable  special adaptive  quantization for  altref frames  based on  their
+   * expected  prediction quality  for  the future  frames. Any non-zero  value
+   * enables  option controls  the  delta-quantization  step between  segments.
+   * Minus one enables the option and determine quantization step automatically
+   * from rate control.
+   *
+   * Supported in codecs: VP9
+   */
+  VP9E_SET_ALT_REF_AQ,
+
   /*!\brief Codec control function to enable/disable periodic Q boost.
    *
    * One VP9 encoder speed feature is to enable quality boost by lowering
@@ -784,6 +797,9 @@ VPX_CTRL_USE_TYPE(VP9E_SET_FRAME_PARALLEL_DECODING, unsigned int)
 
 VPX_CTRL_USE_TYPE(VP9E_SET_AQ_MODE, unsigned int)
 #define VPX_CTRL_VP9E_SET_AQ_MODE
+
+VPX_CTRL_USE_TYPE(VP9E_SET_ALT_REF_AQ, unsigned int)
+#define VPX_CTRL_VP9E_SET_ALT_REF_AQ
 
 VPX_CTRL_USE_TYPE(VP9E_SET_FRAME_PERIODIC_BOOST, unsigned int)
 #define VPX_CTRL_VP9E_SET_FRAME_PERIODIC_BOOST
