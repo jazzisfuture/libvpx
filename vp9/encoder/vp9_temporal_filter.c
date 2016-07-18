3c65e28b8dacde3558402ebd9f019f12e811757c
Yury Gitman <yuryg@google.com>
2016-07-18 15:45:20 -0700

    Upload segmentation map to the cpi->alt_ref_aq
    I use  here simple  though reasonable approach  creating segmentation
    map as a number of nonzero frames blurred together to produce current
    pixel.  I  also experimented with  non-binary weights (actually  0, 1
    and 2 like in the altref  frame creation process itself), but results
    are similar in average.
    
    Change-Id: Id490079daf086cf3b7b54eab3f0a92461b8dce45

--- vp9/encoder/vp9_temporal_filter.c
+++ vp9/encoder/vp9_temporal_filter.c
@@ -15,6 +15,7 @@
 #include "vp9/common/vp9_onyxc_int.h"
 #include "vp9/common/vp9_quant_common.h"
 #include "vp9/common/vp9_reconinter.h"
+#include "vp9/encoder/vp9_alt_ref_aq.h"
 #include "vp9/encoder/vp9_extend.h"
 #include "vp9/encoder/vp9_firstpass.h"
 #include "vp9/encoder/vp9_mcomp.h"
@@ -281,6 +282,7 @@ static void temporal_filter_iterate_c(VP9_COMP *cpi,
   MACROBLOCKD *mbd = &cpi->td.mb.e_mbd;
   YV12_BUFFER_CONFIG *f = frames[alt_ref_index];
   uint8_t *dst1, *dst2;
+
 #if CONFIG_VP9_HIGHBITDEPTH
   DECLARE_ALIGNED(16, uint16_t, predictor16[16 * 16 * 3]);
   DECLARE_ALIGNED(16, uint8_t, predictor8[16 * 16 * 3]);
@@ -293,7 +295,51 @@ static void temporal_filter_iterate_c(VP9_COMP *cpi,
 
   // Save input state
   uint8_t *input_buffer[MAX_MB_PLANE];
+
+  uint32_t filter_weight_lower_thresh = 0;
   int i;
+
+  struct MATX_8U segm_map;
+
+  segm_map.rows = 0;
+  segm_map.cols = 0;
+  segm_map.data = NULL;
+  segm_map.stride = 0;
+
+  if (cpi->oxcf.alt_ref_aq) {
+    int bitrate = cpi->rc.avg_frame_bandwidth / 40;
+
+    // Encoder blurs lookahead buffers together into
+    // altref frame using integer weights from the set
+    // of 0,1, and 2. Thus, the most straightforward
+    // way to estimate prediction quality of some altref
+    // block is to consider either number of any nonzero
+    // weights or just number of weights equal to 2.
+    // Second option is more conservative and I empirically
+    // found that it is better for high bitrates.
+    // -------------------------------------------------
+    // TODO(yuryg): I should change this condition
+    // to work on a per-frame basis.
+    // Also, I can consider something more continuous.
+    if (bitrate > ALT_REF_AQ_SWITCH_WEIGHTS_BOUNDARY)
+      filter_weight_lower_thresh = 0;
+    else
+      filter_weight_lower_thresh = 1;
+
+    segm_map.data = vpx_malloc(mb_rows * mb_cols * sizeof(uint8_t));
+
+    // I want zero to be the smallest value finally
+    memset(segm_map.data, 255, mb_rows * mb_cols * sizeof(uint8_t));
+
+    segm_map.rows = mb_rows;
+    segm_map.cols = mb_cols;
+    segm_map.stride = mb_cols;
+
+    assert(frame_count <= ALT_REF_MAX_FRAMES);
+
+    vp9_alt_ref_aq_set_nsegments(cpi->alt_ref_aq, frame_count);
+  }
+
 #if CONFIG_VP9_HIGHBITDEPTH
   if (mbd->cur_buf->flags & YV12_FLAG_HIGHBITDEPTH) {
     predictor = CONVERT_TO_BYTEPTR(predictor16);
@@ -355,6 +401,9 @@ static void temporal_filter_iterate_c(VP9_COMP *cpi,
           // is to weight all MBs equal.
           filter_weight = err < thresh_low ? 2 : err < thresh_high ? 1 : 0;
         }
+
+        if (cpi->oxcf.alt_ref_aq && filter_weight > filter_weight_lower_thresh)
+          ++segm_map.data[mb_row * segm_map.stride + mb_col];
 
         if (filter_weight != 0) {
           // Construct the predictors
@@ -560,6 +609,14 @@ static void temporal_filter_iterate_c(VP9_COMP *cpi,
     }
     mb_y_offset += 16 * (f->y_stride - mb_cols);
     mb_uv_offset += mb_uv_height * f->uv_stride - mb_uv_width * mb_cols;
+  }
+
+  if (cpi->oxcf.alt_ref_aq) {
+    int height = cpi->common.mi_rows;
+    int width = cpi->common.mi_cols;
+
+    vp9_alt_ref_aq_upload_map(cpi->alt_ref_aq, &segm_map, height, width);
+    vpx_free(segm_map.data);
   }
 
   // Restore input state
