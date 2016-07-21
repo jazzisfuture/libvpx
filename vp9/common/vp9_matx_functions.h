31b1bfb70d2a07c04f5a2868bd3535cea58236d1
Yury Gitman <yuryg@google.com>
2016-07-21 11:00:38 -0700

    Add matx image processing module into common
    While  matx.c/matx.h  contain   structure  definition  and  assisting
    methods,  I  mean matx_functions.c/matx_functions.h  contain  various
    image  processing methods  not semantically  binded to  the structure
    itself. Later  if  it  grows  large  I will  split  it  into  several
    submodules.
    
    Change-Id: I117738763ba01f9b33b07a1c4573044037480bef

---
+++ vp9/common/vp9_matx_functions.h
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed  by a BSD-style license that can be
+ *  found in the LICENSE file in the root of the source tree. An additional
+ *  intellectual property  rights grant can  be found in the  file PATENTS.
+ *  All contributing  project authors may be  found in the AUTHORS  file in
+ *  the root of the source tree.
+ */
+
+/*
+ *  \file vp9_matx_functions.h
+ *
+ *  This file contains image processing functions for MATX class
+ */
+
+#ifndef VP9_COMMON_VP9_MATX_FUNCTIONS_H_
+#define VP9_COMMON_VP9_MATX_FUNCTIONS_H_
+
+#include <assert.h>
+#include "vp9/common/vp9_matx_enums.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MATX_SUFFIX 8u
+#include "vp9/common/vp9_matx_functions_h.def"
+
+struct MATX;
+
+// ...
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // VP9_COMMON_VP9_MATX_FUNCTIONS_H_
