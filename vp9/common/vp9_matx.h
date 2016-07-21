101a7562fcefb0a9bbe0e01a2b3e386129df3095
Yury Gitman <yuryg@google.com>
2016-07-21 10:36:50 -0700

    Add templated MATX class for image processing
    I created this  class for my own experiment on  altref frame adaptive
    quantization, but  the class is quite  general and may be  useful for
    others. It adds some  performance overhead, so I meant to  use it for
    images that are at least 3000 pixels large.
    
    Change-Id: I16865b6f7d1e760bf39cd1f42920832ec347e777

---
+++ vp9/common/vp9_matx.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed  by a BSD-style license that can be
+ *  found in the LICENSE file in the root of the source tree. An additional
+ *  intellectual property  rights grant can  be found in the  file PATENTS.
+ *  All contributing  project authors may be  found in the AUTHORS  file in
+ *  the root of the source tree.
+ */
+
+/*
+ *  \file vp9_matx.h
+ *
+ *  This file contains generic templated matrix class
+ *  designed like OpenCV Mat class,  but much simpler
+ */
+
+#ifndef VP9_COMMON_VP9_MATX_H_
+#define VP9_COMMON_VP9_MATX_H_
+
+#include "./vp9_matx_enums.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MATX_SUFFIX x
+#include "./vp9_matx_struct.def"
+
+/*!\brief Constructor
+ *
+ * \param    self    Instance of the class
+ */
+void vp9_matx_init(MATX_PTR const self);
+
+/*!\brief Destructor
+ *
+ * \param    self    Instance of the class
+ */
+void vp9_matx_destroy(MATX_PTR const self);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // VP9_COMMON_VP9_MATX_H_
