a006a7c9a1b85dacec8de10a0f07c21a83707b03
Yury Gitman <yuryg@google.com>
2016-07-21 10:39:23 -0700

    Add matx_affirm function
    I use this function to assert in runtime image characteristics. It is
    more or less inspired by OpenCV Matx::create method.
    
    Change-Id: I27d15a15894677ccaceea121805553c4b7796067

--- vp9/common/vp9_matx.c
+++ vp9/common/vp9_matx.c
@@ -32,6 +32,53 @@ void vp9_matx_init(MATX_PTR const _self) {
   self->typeid = MATX_NO_TYPE;
 }
 
+void vp9_matx_affirm(MATX_PTR _self,
+                     int rows,
+                     int cols,
+                     int stride,
+                     int cn,
+                     MATX_TYPE typeid) {
+  struct MATX* const self = (struct MATX*) _self;
+
+  uint8_t reallocation_not_needed = 1;
+
+  if (self->rows != rows) {
+    self->rows = rows;
+    reallocation_not_needed = 0;
+  }
+
+  if (self->cols != cols) {
+    self->cols = cols;
+    reallocation_not_needed = 0;
+  }
+
+  if (self->stride != stride && stride > 0) {
+    self->stride = stride;
+    reallocation_not_needed = 0;
+  }
+
+  if (stride <= 0)
+    self->stride = cols*cn;
+
+  if (self->cn != cn || self->typeid != typeid) {
+    self->typeid = typeid;
+    self->cn = cn;
+    reallocation_not_needed = 0;
+  }
+
+  if (reallocation_not_needed)
+    return;
+
+  assert(self->stride >= self->cols*self->cn);
+
+  switch (self->typeid) {
+    default:
+      assert(0 /* matx: inapprorpiate type */);
+  }
+
+  assert(self->data != NULL);
+}
+
 void vp9_matx_destroy(MATX_PTR _self) {
   vpx_free(((struct MATX*) _self)->data);
 }
