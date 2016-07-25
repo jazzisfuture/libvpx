8fa419bb73f6f201328e7e6d7c46d1439936ceac
Yaowu Xu <yaowu@google.com>
2016-07-25 15:39:46 -0700

    Remove vp8, vp9 folders
    Change-Id: I09b8acd22d031ece52e1fee18b998349bf1cf06b

--- test/test.mk
+++ test/test.mk
@@ -10,6 +10,7 @@ LIBVPX_TEST_SRCS-yes += test_vectors.h
 LIBVPX_TEST_SRCS-yes += util.h
 LIBVPX_TEST_SRCS-yes += video_source.h
 LIBVPX_TEST_SRCS-yes += transform_test_base.h
+LIBVPX_TEST_SRCS-yes += function_equivalence_test.h
 
 ##
 ## BLACK BOX TESTS
@@ -142,7 +143,7 @@ LIBVPX_TEST_SRCS-yes                   += vp9_boolcoder_test.cc
 LIBVPX_TEST_SRCS-yes                   += vp9_encoder_parms_get_to_decoder.cc
 endif
 
-LIBVPX_TEST_SRCS-yes                   += convolve_test.cc
+#LIBVPX_TEST_SRCS-yes                   += convolve_test.cc
 LIBVPX_TEST_SRCS-yes                   += lpf_8_test.cc
 LIBVPX_TEST_SRCS-yes                   += vp9_intrapred_test.cc
 LIBVPX_TEST_SRCS-$(CONFIG_VP9_DECODER) += vp9_decrypt_test.cc
