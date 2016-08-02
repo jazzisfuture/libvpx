bcb84cc739451d209143aef86c362b6c9beaff76
Yury Gitman <yuryg@google.com>
2016-08-02 16:51:09 -0700

    Add uploading of the altref segm map to the cpi
    Current version seems to work  and produces about 1.4% better average
    bitrate for the  same SSIM quality for the  stdhd dataset. However, I
    think there is still an opportunity for better gain.
    
    Change-Id: I74931393573b7865d1b2af205fc90865ed000736

--- vp9/encoder/vp9_alt_ref_aq.c
+++ vp9/encoder/vp9_alt_ref_aq.c
@@ -115,12 +115,22 @@ static void vp9_alt_ref_aq_update_segmentation_map(struct ALT_REF_AQ *self) {
   }
 }
 
+static void vp9_alt_ref_aq_set_single_delta(struct ALT_REF_AQ *const self,
+                                            const struct VP9_COMP *cpi) {
+  int total_delta = cpi->rc.avg_frame_qindex[1] - cpi->common.base_qindex;
+  float ndeltas = VPXMIN(self->_nsteps, MAX_SEGMENTS - 1);
+
+  // Don't ask me why it is (... + self->DELTA_SHRINK). It just works better.
+  self->single_delta = total_delta / VPXMAX(ndeltas + self->delta_shrink, 1);
+}
+
 // set up histogram (I use it for filtering zero-area segments out)
 static void vp9_alt_ref_aq_setup_histogram(struct ALT_REF_AQ *const self) {
   struct MATX_8U segm_map;
   int i, j;
 
   segm_map = self->segmentation_map;
+
   array_zerofill(self->_segment_hist, self->nsegments);
 
   for (i = 0; i < segm_map.rows; ++i) {
@@ -135,7 +145,10 @@ struct ALT_REF_AQ *vp9_alt_ref_aq_create() {
   struct ALT_REF_AQ *self = vpx_malloc(sizeof(struct ALT_REF_AQ));
 
   self->aq_mode = LOOKAHEAD_AQ;
+
   self->nsegments = -1;
+  self->_nsteps = -1;
+  self->single_delta = -1;
 
   memset(self->_segment_hist, 0, sizeof(self->_segment_hist));
 
@@ -152,9 +165,15 @@ struct ALT_REF_AQ *vp9_alt_ref_aq_create() {
   self->NUM_NONZERO_SEGMENTS = 1;
   self->SEGMENT_MIN_AREA = 0;
 
+  self->DELTA_SHRINK = 2.0;
+
+  // TODO(yuryg): this may be an overfitting
+  self->SINGLE_SEGMENT_DELTA_SHRINK = 1.5;
+
   // This is just initiallization, allocation
   // is going to happen on the first request
   vp9_matx_init(&self->segmentation_map);
+  vp9_matx_init(&self->cpi_segmentation_map);
 
   return (struct ALT_REF_AQ *)self;
 }
@@ -162,12 +181,17 @@ struct ALT_REF_AQ *vp9_alt_ref_aq_create() {
 static void vp9_alt_ref_aq_process_map(struct ALT_REF_AQ *const self) {
   int i, j;
 
+  self->_nsteps = self->nsegments - 1;
+
   vp9_alt_ref_aq_setup_histogram(self);
 
   assert(self->nsegments > 0);
 
   // super special case, e.g., riverbed sequence
-  if (self->nsegments == 1) self->segment_deltas[0] = 1;
+  if (self->nsegments == 1) {
+    self->segment_deltas[0] = 1;
+    self->delta_shrink = self->SINGLE_SEGMENT_DELTA_SHRINK;
+  }
 
   if (self->nsegments == 1) return;
 
@@ -180,13 +204,16 @@ static void vp9_alt_ref_aq_process_map(struct ALT_REF_AQ *const self) {
     for (i = 0; self->_segment_hist[i] <= 0; ++i) {
     }
     self->segment_deltas[0] = self->nsegments - i - 1;
+
+    self->nsegments = 1;
+    self->delta_shrink = self->SINGLE_SEGMENT_DELTA_SHRINK;
   } else {
     vp9_alt_ref_aq_compress_segment_hist(self);
     vp9_alt_ref_aq_setup_segment_deltas(self);
-    vp9_alt_ref_aq_update_number_of_segments(self);
-  }
 
-  if (j == 1) self->nsegments = 1;
+    vp9_alt_ref_aq_update_number_of_segments(self);
+    self->delta_shrink = self->DELTA_SHRINK;
+  }
 
   if (self->nsegments == 1) return;
 
@@ -203,6 +230,7 @@ static void vp9_alt_ref_aq_self_destroy(struct ALT_REF_AQ *const self) {
 
 void vp9_alt_ref_aq_destroy(struct ALT_REF_AQ *const self) {
   vp9_matx_destroy(&self->segmentation_map);
+  vp9_matx_destroy(&self->cpi_segmentation_map);
   vp9_alt_ref_aq_self_destroy(self);
 }
 
@@ -217,20 +245,75 @@ void vp9_alt_ref_aq_set_nsegments(struct ALT_REF_AQ *const self,
 
 void vp9_alt_ref_aq_setup_mode(struct ALT_REF_AQ *const self,
                                struct VP9_COMP *const cpi) {
-  (void)cpi;
+  VPX_SWAP(AQ_MODE, self->aq_mode, cpi->oxcf.aq_mode);
   vp9_alt_ref_aq_process_map(self);
 }
 
 // set basic segmentation to the altref's one
 void vp9_alt_ref_aq_setup_map(struct ALT_REF_AQ *const self,
                               struct VP9_COMP *const cpi) {
-  (void)cpi;
-  (void)self;
+  int guard = 0, i, j;
+
+  assert(self->segmentation_map.rows == cpi->common.mi_rows);
+  assert(self->segmentation_map.cols == cpi->common.mi_cols);
+
+  assert_gray8_image(&self->segmentation_map);
+
+  vp9_mat8u_wrap(&self->cpi_segmentation_map, cpi->common.mi_rows,
+                 cpi->common.mi_cols,
+                 // logically, it should be mi_stride,
+                 // but it is how it was before me
+                 cpi->common.mi_cols, 1, cpi->segmentation_map);
+
+  if (self->single_delta >= 0) guard = 1;
+
+  // clear down mmx registers (only need for x86 arch)
+  vpx_clear_system_state();
+
+  // set single quantizer step between segments
+  vp9_alt_ref_aq_set_single_delta(self, cpi);
+
+  // TODO(yuryg): this is probably not nice for rate control,
+  if (self->nsegments == 1) {
+    float qdelta = self->single_delta * self->segment_deltas[0];
+    cpi->common.base_qindex += (int)(qdelta + 1e-2);
+  }
+
+  // TODO(yuryg): do I need this?
+  if (self->nsegments == 1) vp9_mat8u_zerofill(&self->cpi_segmentation_map);
+
+  if (self->nsegments == 1) return;
+
+  // set cpi segmentation to altref one
+  cpi->common.seg.abs_delta = SEGMENT_DELTADATA;
+
+  vp9_enable_segmentation(&cpi->common.seg);
+  vp9_clearall_segfeatures(&cpi->common.seg);
+
+  for (i = 1, j = 1; i < self->nsegments; ++i) {
+    int qdelta = (int)(self->single_delta * self->segment_deltas[i] + 1e-2f);
+
+    vp9_enable_segfeature(&cpi->common.seg, j, SEG_LVL_ALT_Q);
+    vp9_set_segdata(&cpi->common.seg, j++, SEG_LVL_ALT_Q, qdelta);
+  }
+
+  if (guard == 1) return;
+
+  // TODO(yuryg): avoid this copy
+  vp9_mat8u_copy_to(&self->segmentation_map, &self->cpi_segmentation_map);
 }
 
 // restore cpi->aq_mode
 void vp9_alt_ref_aq_unset_all(struct ALT_REF_AQ *const self,
                               struct VP9_COMP *const cpi) {
-  (void)cpi;
-  (void)self;
+  cpi->force_update_segmentation = 1;
+
+  self->nsegments = -1;
+  self->_nsteps = -1;
+  self->single_delta = -1;
+
+  VPX_SWAP(AQ_MODE, self->aq_mode, cpi->oxcf.aq_mode);
+
+  // TODO(yuryg): may be it is better to move this to encoder.c
+  if (cpi->oxcf.aq_mode == NO_AQ) vp9_disable_segmentation(&cpi->common.seg);
 }
