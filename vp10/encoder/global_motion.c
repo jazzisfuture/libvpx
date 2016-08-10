936bcbae8ee7baa73f442fc2079bd07154422929
Sarah Parker <sarahparker@google.com>
2016-08-09 17:40:53 -0700

    WIP Implement global motion parameter computation
    This computes global motion parameters between 2 frames by
    matching corresponding points using FAST feature and then
    fitting a model using RANSAC.
    
    Change-Id: Ib6664df44090e8cfa4db9f2f9e0556931ccfe5c8

--- vp10/encoder/global_motion.c
+++ vp10/encoder/global_motion.c
@@ -18,17 +18,104 @@
 
 #include "vp10/encoder/segmentation.h"
 #include "vp10/encoder/global_motion.h"
+#include "vp10/encoder/corner_detect.h"
+#include "vp10/encoder/corner_match.h"
+#include "vp10/encoder/ransac.h"
 
-int compute_global_motion_feature_based(struct VP10_COMP *cpi,
-                                        TransformationType type,
+#define MAX_CORNERS 4096
+#define MIN_INLIER_PROB 0.1
+
+INLINE ransacType get_ransacType(TransformationType type) {
+  switch (type) {
+    case HOMOGRAPHY:
+      return ransacHomography;
+    case AFFINE:
+      return ransacAffine;
+    case ROTZOOM:
+      return ransacRotZoom;
+    case TRANSLATION:
+      return ransacTranslation;
+    default:
+      assert(0);
+      return NULL;
+  }
+}
+
+// computes global motion parameters by fitting a model using RANSAC
+static int compute_global_motion_params(TransformationType type,
+                                        double *correspondences,
+                                        int num_correspondences,
+                                        double *params,
+                                        int *inlier_map) {
+  int i, result;
+  double *mp, *matched_points;
+  double *cp = correspondences;
+  int num_inliers = 0;
+  ransacType ransac = get_ransacType(type);
+  if (ransac == NULL)
+    return 0;
+  matched_points =
+      (double *)malloc(4 * num_correspondences * sizeof(*matched_points));
+
+  for (mp = matched_points, cp = correspondences, i = 0;
+       i < num_correspondences; ++i) {
+    *mp++ = *cp++;
+    *mp++ = *cp++;
+    *mp++ = *cp++;
+    *mp++ = *cp++;
+  }
+  result = ransac(matched_points, num_correspondences,
+                  &num_inliers, inlier_map, params);
+  if (!result && num_inliers < MIN_INLIER_PROB * num_correspondences) {
+    result = 1;
+    num_inliers = 0;
+  }
+  if (!result) {
+    for (i = 0; i < num_correspondences; ++i) {
+      inlier_map[i] = inlier_map[i] - 1;
+    }
+  }
+  free(matched_points);
+  return num_inliers;
+}
+
+int compute_global_motion_feature_based(TransformationType type,
                                         YV12_BUFFER_CONFIG *frm,
                                         YV12_BUFFER_CONFIG *ref,
-                                        double inlier_prob, double *H) {
-  (void)cpi;
-  (void)type;
-  (void)frm;
-  (void)ref;
-  (void)inlier_prob;
-  (void)H;
-  return 0;
+                                        double *params) {
+  int num_frm_corners, num_ref_corners;
+  int num_correspondences;
+  double *correspondences;
+  int num_inliers;
+  int frm_corners[2 * MAX_CORNERS], ref_corners[2 * MAX_CORNERS];
+  int *inlier_map = NULL;
+
+  // compute interest points in images using FAST features
+  num_frm_corners = FastCornerDetect(frm->y_buffer, frm->y_width,
+                                     frm->y_height, frm->y_stride,
+                                     frm_corners, MAX_CORNERS);
+  num_ref_corners = FastCornerDetect(ref->y_buffer, ref->y_width,
+                                     ref->y_height, ref->y_stride,
+                                     ref_corners, MAX_CORNERS);
+
+  // find correspondences between the two images
+  correspondences = (double *) malloc(num_frm_corners * 4 *
+                                      sizeof(*correspondences));
+  num_correspondences = determine_correspondence(frm->y_buffer,
+                                                 (int*)frm_corners,
+                                                 num_frm_corners,
+                                                 ref->y_buffer,
+                                                 (int*)ref_corners,
+                                                 num_ref_corners,
+                                                 frm->y_width, frm->y_height,
+                                                 frm->y_stride, ref->y_stride,
+                                                 correspondences);
+
+  inlier_map = (int *) malloc(num_correspondences * sizeof(*inlier_map));
+  num_inliers = compute_global_motion_params(type, correspondences,
+                                             num_correspondences, params,
+                                             inlier_map);
+  free(correspondences);
+  free(inlier_map);
+  return (num_inliers > 0);
 }
