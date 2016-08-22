d5d4feb85da151a892d2eb66c9a170c777cadc09
Yaowu Xu <yaowu@google.com>
2016-08-22 16:08:15 -0700

    Port folder renaming changes from AOM
    Manually cherry-picked commits:
    ceef058 libvpx->libaom part2
    3d26d91 libvpx -> libaom
    cfea7dd vp10/ -> av1/
    3a8eff7 Fix a build issue for a test
    bf4202e Rename vpx to aom
    
    Change-Id: I1b0eb5a40796e3aaf41c58984b4229a439a597dc

--- test/twopass_encoder.sh
+++ test/twopass_encoder.sh
@@ -8,7 +8,7 @@
 ##  in the file PATENTS.  All contributing project authors may
 ##  be found in the AUTHORS file in the root of the source tree.
 ##
-##  This file tests the libvpx twopass_encoder example. To add new tests to this
+##  This file tests the libaom twopass_encoder example. To add new tests to this
 ##  file, do the following:
 ##    1. Write a shell function (this is your test).
 ##    2. Add the function to twopass_encoder_tests (on a new line).
@@ -18,7 +18,7 @@
 # Environment check: $YUV_RAW_INPUT is required.
 twopass_encoder_verify_environment() {
   if [ ! -e "${YUV_RAW_INPUT}" ]; then
-    echo "Libvpx test data must exist in LIBVPX_TEST_DATA_PATH."
+    echo "Libvpx test data must exist in LIBAOM_TEST_DATA_PATH."
     return 1
   fi
 }
@@ -26,7 +26,7 @@ twopass_encoder_verify_environment() {
 # Runs twopass_encoder using the codec specified by $1 with a frame limit of
 # 100.
 twopass_encoder() {
-  local encoder="${LIBVPX_BIN_PATH}/twopass_encoder${VPX_TEST_EXE_SUFFIX}"
+  local encoder="${LIBAOM_BIN_PATH}/twopass_encoder${VPX_TEST_EXE_SUFFIX}"
   local codec="$1"
   local output_file="${VPX_TEST_OUTPUT_DIR}/twopass_encoder_${codec}.ivf"
 
