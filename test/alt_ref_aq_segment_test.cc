ff5df5b93daa79b585559c8a09a5f6d20ea08c36
Yury Gitman <yuryg@google.com>
2016-08-22 16:10:22 -0700

    Add tests for --alt-ref-aq --aq-mode combinations
    Change-Id: I57aeefb90a1ce896eadc05c2378a3bedab41bf7e

--- test/alt_ref_aq_segment_test.cc
+++ test/alt_ref_aq_segment_test.cc
@@ -15,24 +15,26 @@
 
 namespace {
 
-class AqSegmentTest
+class AltRefAqSegmentTest
     : public ::libvpx_test::EncoderTest,
       public ::libvpx_test::CodecTestWith2Params<libvpx_test::TestMode, int> {
  protected:
-  AqSegmentTest() : EncoderTest(GET_PARAM(0)) {}
-  virtual ~AqSegmentTest() {}
+  AltRefAqSegmentTest() : EncoderTest(GET_PARAM(0)) {}
+  virtual ~AltRefAqSegmentTest() {}
 
   virtual void SetUp() {
     InitializeConfig();
     SetMode(GET_PARAM(1));
     set_cpu_used_ = GET_PARAM(2);
     aq_mode_ = 0;
+    alt_ref_aq_mode_ = 0;
   }
 
   virtual void PreEncodeFrameHook(::libvpx_test::VideoSource *video,
                                   ::libvpx_test::Encoder *encoder) {
     if (video->frame() == 1) {
       encoder->Control(VP8E_SET_CPUUSED, set_cpu_used_);
+      encoder->Control(VP9E_SET_ALT_REF_AQ, alt_ref_aq_mode_);
       encoder->Control(VP9E_SET_AQ_MODE, aq_mode_);
       encoder->Control(VP8E_SET_MAX_INTRA_BITRATE_PCT, 100);
     }
@@ -40,21 +42,44 @@ class AqSegmentTest
 
   int set_cpu_used_;
   int aq_mode_;
+  int alt_ref_aq_mode_;
 };
 
-// Validate that this AQ segmentation mode (AQ=1, variance_ap)
+// Validate that this ALT_REF_AQ/AQ segmentation mode
+// (ALT_REF_AQ=0, AQ=0/no_aq)
 // encodes and decodes without a mismatch.
-TEST_P(AqSegmentTest, TestNoMisMatchAQ1) {
+TEST_P(AltRefAqSegmentTest, TestNoMisMatchAltRefAQ0) {
   cfg_.rc_min_quantizer = 8;
   cfg_.rc_max_quantizer = 56;
   cfg_.rc_end_usage = VPX_CBR;
-  cfg_.g_lag_in_frames = 0;
+  cfg_.rc_buf_initial_sz = 500;
+  cfg_.rc_buf_optimal_sz = 500;
+  cfg_.rc_buf_sz = 1000;
+  cfg_.rc_target_bitrate = 300;
+
+  aq_mode_ = 0;
+  alt_ref_aq_mode_ = 1;
+
+  ::libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
+                                       30, 1, 0, 100);
+
+  ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
+}
+
+// Validate that this ALT_REF_AQ/AQ segmentation mode
+// (ALT_REF_AQ=0, AQ=1/variance_aq)
+// encodes and decodes without a mismatch.
+TEST_P(AltRefAqSegmentTest, TestNoMisMatchAltRefAQ1) {
+  cfg_.rc_min_quantizer = 8;
+  cfg_.rc_max_quantizer = 56;
+  cfg_.rc_end_usage = VPX_CBR;
   cfg_.rc_buf_initial_sz = 500;
   cfg_.rc_buf_optimal_sz = 500;
   cfg_.rc_buf_sz = 1000;
   cfg_.rc_target_bitrate = 300;
 
   aq_mode_ = 1;
+  alt_ref_aq_mode_ = 1;
 
   ::libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
                                        30, 1, 0, 100);
@@ -62,19 +87,20 @@ TEST_P(AqSegmentTest, TestNoMisMatchAQ1) {
   ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
 }
 
-// Validate that this AQ segmentation mode (AQ=2, complexity_aq)
+// Validate that this ALT_REF_AQ/AQ segmentation mode
+// (ALT_REF_AQ=0, AQ=2/complexity_aq)
 // encodes and decodes without a mismatch.
-TEST_P(AqSegmentTest, TestNoMisMatchAQ2) {
+TEST_P(AltRefAqSegmentTest, TestNoMisMatchAltRefAQ2) {
   cfg_.rc_min_quantizer = 8;
   cfg_.rc_max_quantizer = 56;
   cfg_.rc_end_usage = VPX_CBR;
-  cfg_.g_lag_in_frames = 0;
   cfg_.rc_buf_initial_sz = 500;
   cfg_.rc_buf_optimal_sz = 500;
   cfg_.rc_buf_sz = 1000;
   cfg_.rc_target_bitrate = 300;
 
   aq_mode_ = 2;
+  alt_ref_aq_mode_ = 1;
 
   ::libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
                                        30, 1, 0, 100);
@@ -82,19 +108,20 @@ TEST_P(AqSegmentTest, TestNoMisMatchAQ2) {
   ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
 }
 
-// Validate that this AQ segmentation mode (AQ=3, cyclic_refresh_aq)
+// Validate that this ALT_REF_AQ/AQ segmentation mode
+// (ALT_REF_AQ=0, AQ=3/cyclicrefresh_aq)
 // encodes and decodes without a mismatch.
-TEST_P(AqSegmentTest, TestNoMisMatchAQ3) {
+TEST_P(AltRefAqSegmentTest, TestNoMisMatchAltRefAQ3) {
   cfg_.rc_min_quantizer = 8;
   cfg_.rc_max_quantizer = 56;
   cfg_.rc_end_usage = VPX_CBR;
-  cfg_.g_lag_in_frames = 0;
   cfg_.rc_buf_initial_sz = 500;
   cfg_.rc_buf_optimal_sz = 500;
   cfg_.rc_buf_sz = 1000;
   cfg_.rc_target_bitrate = 300;
 
   aq_mode_ = 3;
+  alt_ref_aq_mode_ = 1;
 
   ::libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
                                        30, 1, 0, 100);
@@ -102,7 +129,28 @@ TEST_P(AqSegmentTest, TestNoMisMatchAQ3) {
   ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
 }
 
-VP9_INSTANTIATE_TEST_CASE(AqSegmentTest,
+// Validate that this ALT_REF_AQ/AQ segmentation mode
+// (ALT_REF_AQ=0, AQ=4/equator360_aq)
+// encodes and decodes without a mismatch.
+TEST_P(AltRefAqSegmentTest, TestNoMisMatchAltRefAQ4) {
+  cfg_.rc_min_quantizer = 8;
+  cfg_.rc_max_quantizer = 56;
+  cfg_.rc_end_usage = VPX_CBR;
+  cfg_.rc_buf_initial_sz = 500;
+  cfg_.rc_buf_optimal_sz = 500;
+  cfg_.rc_buf_sz = 1000;
+  cfg_.rc_target_bitrate = 300;
+
+  aq_mode_ = 4;
+  alt_ref_aq_mode_ = 1;
+
+  ::libvpx_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
+                                       30, 1, 0, 100);
+
+  ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
+}
+
+VP9_INSTANTIATE_TEST_CASE(AltRefAqSegmentTest,
                           ::testing::Values(::libvpx_test::kRealTime,
                                             ::libvpx_test::kOnePassGood),
                           ::testing::Range(3, 9));
