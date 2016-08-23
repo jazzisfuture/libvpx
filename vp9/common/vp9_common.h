00c44dfe9c149ad8fcab5e66cdfe2a7c63c8b325
Yury Gitman <yuryg@google.com>
2016-08-22 18:52:44 -0700

    Correct CHECK_MEM_ERROR macro (release branch)
    The previous macro doesn't work with &cpi->common as a first argument
    
    Change-Id: Iddf7a1f5d56d7abafd9b2b8707aa611d349e7a68

--- vp9/common/vp9_common.h
+++ vp9/common/vp9_common.h
@@ -60,7 +60,7 @@ static INLINE int get_unsigned_bits(unsigned int num_values) {
   do {                                                    \
     lval = (expr);                                        \
     if (!lval)                                            \
-      vpx_internal_error(&cm->error, VPX_CODEC_MEM_ERROR, \
+      vpx_internal_error(&(cm)->error, VPX_CODEC_MEM_ERROR, \
                          "Failed to allocate " #lval);    \
   } while (0)
 #endif
