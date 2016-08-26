d4c809ad610ee20ca17cfab390a3c78f0c4ed306
Jim Bankoski <jimbankoski@google.com>
2016-08-26 14:31:29 -0700

    Avoid TSAN warnings in VP8.
    This is a slightly reworked version of:
    https://chromium-review.googlesource.com/#/c/320797/
    
    It avoids all TSAN warnings rather than most.
    
    Change-Id: I9c600b8a3b13e8a26feb49512480d7844231cf14

--- vp8/encoder/onyx_int.h
+++ vp8/encoder/onyx_int.h
@@ -505,6 +505,8 @@ typedef struct VP8_COMP {
 
 #if CONFIG_MULTITHREAD
   /* multithread data */
+  pthread_mutex_t *pmutex;
+  pthread_mutex_t mt_mutex; /* mutex for b_multi_threaded */
   int *mt_current_mb_col;
   int mt_sync_range;
   int b_multi_threaded;
@@ -519,7 +521,7 @@ typedef struct VP8_COMP {
 
   /* events */
   sem_t *h_event_start_encoding;
-  sem_t h_event_end_encoding;
+  sem_t *h_event_end_encoding;
   sem_t h_event_start_lpf;
   sem_t h_event_end_lpf;
 #endif
