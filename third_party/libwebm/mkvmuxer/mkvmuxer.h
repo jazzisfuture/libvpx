e22ce38d49d57e8bbda819ffca8d1729d9422529
Yaowu Xu <yaowu@google.com>
2016-08-30 14:01:10 -0700

    Port renaming changes from AOMedia
    Cherry-Picked the following commits:
    0defd8f Changed "WebM" to "AOMedia" & "webm" to "aomedia"
    54e6676 Replace "VPx" by "AVx"
    5082a36 Change "Vpx" to "Avx"
    7df44f1 Replace "Vp9" w/ "Av1"
    967f722 Remove kVp9CodecId
    828f30c Change "Vp8" to "AOM"
    030b5ff AUTHORS regenerated
    2524cae Add ref-mv experimental flag
    016762b Change copyright notice to AOMedia form
    81e5526 Replace vp9 w/ av1
    9b94565 Add missing files
    fa8ca9f Change "vp9" to "av1"
    ec838b7  Convert "vp8" to "aom"
    80edfa0 Change "VP9" to "AV1"
    d1a11fb Change "vp8" to "aom"
    7b58251 Point to WebM test data
    dd1a5c8 Replace "VP8" with "AOM"
    ff00fc0 Change "VPX" to "AOM"
    01dee0b Change "vp10" to "av1" in source code
    cebe6f0 Convert "vpx" to "aom"
    17b0567 rename vp10*.mk to av1_*.mk
    fe5f8a8 rename files vp10_* to av1_*
    
    Change-Id: I6fc3d18eb11fc171e46140c836ad5339cf6c9419

--- third_party/libwebm/mkvmuxer/mkvmuxer.h
+++ third_party/libwebm/mkvmuxer/mkvmuxer.h
@@ -668,7 +668,7 @@ class Tracks {
   static const char kOpusCodecId[];
   static const char kVorbisCodecId[];
   static const char kVp8CodecId[];
-  static const char kVp9CodecId[];
+  static const char kAv1CodecId[];
   static const char kVp10CodecId[];
 
   Tracks();
