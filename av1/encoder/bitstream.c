8d3923338dd38238d2906bd0a70ea3699b14cc70
Yaowu Xu <yaowu@google.com>
2016-09-01 18:06:57 -0700

    Change to use AOM copyright notice
    Change-Id: I2b2b70e756b7eb9611b7b33b7d5f19b3b30e0a50

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -1,11 +1,12 @@
 /*
- *  Copyright (c) 2010 The WebM project authors. All Rights Reserved.
+ * Copyright (c) 2016, Alliance for Open Media. All rights reserved
  *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
 #include <assert.h>
@@ -3387,7 +3388,7 @@ static uint32_t write_compressed_header(AV1_COMP *cpi, uint8_t *data) {
       prob_diff_update(av1_intra_mode_tree, cm->fc->y_mode_prob[i],
                        counts->y_mode[i], INTRA_MODES, header_bc);
 
-    aom_write_nmv_probs(cm, cm->allow_high_precision_mv, header_bc,
+    av1_write_nmv_probs(cm, cm->allow_high_precision_mv, header_bc,
 #if CONFIG_REF_MV
                         counts->mv);
 #else
