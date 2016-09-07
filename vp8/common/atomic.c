47665bd9aa8348b4c35a8876813673e0b60680a2
Alexander Potapenko <glider@google.com>
2016-09-07 11:59:52 +0200

    vp8: atomic operations implementation
    Introduce the following 32-bit atomic operations:
     - vpx_nobarrier_load_32()
     - vpx_nobarrier_store_32()
     - vpx_acquire_load_32()
     - vpx_release_store_32()
    
    Where available these are implemented using stdatomic.h, otherwise we
    fall back to volatile 32-bit integer accesses and explicit memory
    barriers.
    
    Change-Id: I2c74fbd682639cd4ac61cced32845aa186251509

---
+++ vp8/common/atomic.c
@@ -0,0 +1,6 @@
+#include "vp8/common/atomic.h"
+
+int vpx_nobarrier_load_32(atomic_int *x);
+void vpx_nobarrier_store_32(atomic_int *x, int v);
+int vpx_acquire_load_32(atomic_int *x);
+void vpx_release_store_32(atomic_int *x, int v);
