67ba6cae0a43e0a44a766c85568968f0048f2311
Yaowu Xu <yaowu@google.com>
2016-09-08 16:28:23 -0700

    third_party/libwebm: use aomedia copyright notice
    Change-Id: I6d94e357db4fca8226b83351cc8c43b8af1ece54

--- third_party/libwebm/mkvparser/mkvreader.cc
+++ third_party/libwebm/mkvparser/mkvreader.cc
@@ -1,10 +1,12 @@
-// Copyright (c) 2010 The WebM project authors. All Rights Reserved.
+//
+// Copyright (c) 2016, Alliance for Open Media. All rights reserved
 //
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file in the root of the source
 // tree. An additional intellectual property rights grant can be found
 // in the file PATENTS.  All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
+
 #include "mkvparser/mkvreader.h"
 
 #include <cassert>
