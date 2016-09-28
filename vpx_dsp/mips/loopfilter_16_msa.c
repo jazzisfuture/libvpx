900b734af4e545c44ce3e08918c322fb05233977
Linfeng Zhang <linfengz@google.com>
2016-09-28 15:20:01 -0700

    Unify loopfilter function names
    Rename vpx_lpf_horizontal_edge_8() to vpx_lpf_horizontal_16().
    Rename vpx_lpf_horizontal_edge_16() to vpx_lpf_horizontal_16_dual().
    
    Change-Id: I798ca8fbbd657d06d3db2bfb0fb3321168f49e52

--- vpx_dsp/mips/loopfilter_16_msa.c
+++ vpx_dsp/mips/loopfilter_16_msa.c
@@ -643,14 +643,14 @@ static void mb_lpf_horizontal_edge(uint8_t *src, int32_t pitch,
   }
 }
 
-void vpx_lpf_horizontal_edge_8_msa(uint8_t *src, int32_t pitch,
-                                   const uint8_t *b_limit_ptr,
-                                   const uint8_t *limit_ptr,
-                                   const uint8_t *thresh_ptr) {
+void vpx_lpf_horizontal_16_msa(uint8_t *src, int32_t pitch,
+                               const uint8_t *b_limit_ptr,
+                               const uint8_t *limit_ptr,
+                               const uint8_t *thresh_ptr) {
   mb_lpf_horizontal_edge(src, pitch, b_limit_ptr, limit_ptr, thresh_ptr, 1);
 }
 
-void vpx_lpf_horizontal_edge_16_msa(uint8_t *src, int32_t pitch,
+void vpx_lpf_horizontal_16_dual_msa(uint8_t *src, int32_t pitch,
                                     const uint8_t *b_limit_ptr,
                                     const uint8_t *limit_ptr,
                                     const uint8_t *thresh_ptr) {
