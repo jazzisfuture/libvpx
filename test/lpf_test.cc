3db368e1c74c6b1724f0a1b7e86f2e18cead02ce
Linfeng Zhang <linfengz@google.com>
2016-10-05 11:33:52 -0700

    [vpx highbd lpf NEON 6/6] vertical 16
    BUG=webm:1300
    
    Change-Id: I29d0b482d66f05e278325ddebcf108fbf0b6e222

--- test/lpf_test.cc
+++ test/lpf_test.cc
@@ -527,7 +527,11 @@ INSTANTIATE_TEST_CASE_P(
                       make_tuple(&vpx_highbd_lpf_vertical_4_neon,
                                  &vpx_highbd_lpf_vertical_4_c, 8),
                       make_tuple(&vpx_highbd_lpf_vertical_8_neon,
-                                 &vpx_highbd_lpf_vertical_8_c, 8)));
+                                 &vpx_highbd_lpf_vertical_8_c, 8),
+                      make_tuple(&vpx_highbd_lpf_vertical_16_neon,
+                                 &vpx_highbd_lpf_vertical_16_c, 8),
+                      make_tuple(&vpx_highbd_lpf_vertical_16_dual_neon,
+                                 &vpx_highbd_lpf_vertical_16_dual_c, 8)));
 INSTANTIATE_TEST_CASE_P(
     NEON, Loop8Test9Param,
     ::testing::Values(make_tuple(&vpx_highbd_lpf_horizontal_4_dual_neon,
