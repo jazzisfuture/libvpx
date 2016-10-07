123f7bd03c9e19aed3b073868ac1ed130314b858
Yi Luo <luoyi@google.com>
2016-10-07 15:02:33 -0700

    Zero high 128b YMM registers to avoid SSE-AVX transition penalties
    - Remove redundant function prototype declarations.
    
    Change-Id: I90f85fcb15a7a2c49ee068300be6ffe9c68d371c

--- aom_dsp/x86/variance_avx2.c
+++ aom_dsp/x86/variance_avx2.c
@@ -8,6 +8,8 @@
  * Media Patent License 1.0 was not distributed with this source code in the
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
+
+#include <immintrin.h>
 #include "./aom_dsp_rtcd.h"
 
 typedef void (*get_var_avx2)(const uint8_t *src, int src_stride,
@@ -45,6 +47,7 @@ unsigned int aom_variance16x16_avx2(const uint8_t *src, int src_stride,
   int sum;
   variance_avx2(src, src_stride, ref, ref_stride, 16, 16, sse, &sum,
                 aom_get16x16var_avx2, 16);
+  _mm256_zeroupper();
   return *sse - (((uint32_t)((int64_t)sum * sum)) >> 8);
 }
 
@@ -53,6 +56,7 @@ unsigned int aom_mse16x16_avx2(const uint8_t *src, int src_stride,
                                unsigned int *sse) {
   int sum;
   aom_get16x16var_avx2(src, src_stride, ref, ref_stride, sse, &sum);
+  _mm256_zeroupper();
   return *sse;
 }
 
@@ -62,6 +66,7 @@ unsigned int aom_variance32x16_avx2(const uint8_t *src, int src_stride,
   int sum;
   variance_avx2(src, src_stride, ref, ref_stride, 32, 16, sse, &sum,
                 aom_get32x32var_avx2, 32);
+  _mm256_zeroupper();
   return *sse - (((int64_t)sum * sum) >> 9);
 }
 
@@ -71,6 +76,7 @@ unsigned int aom_variance32x32_avx2(const uint8_t *src, int src_stride,
   int sum;
   variance_avx2(src, src_stride, ref, ref_stride, 32, 32, sse, &sum,
                 aom_get32x32var_avx2, 32);
+  _mm256_zeroupper();
   return *sse - (((int64_t)sum * sum) >> 10);
 }
 
@@ -80,6 +86,7 @@ unsigned int aom_variance64x64_avx2(const uint8_t *src, int src_stride,
   int sum;
   variance_avx2(src, src_stride, ref, ref_stride, 64, 64, sse, &sum,
                 aom_get32x32var_avx2, 32);
+  _mm256_zeroupper();
   return *sse - (((int64_t)sum * sum) >> 12);
 }
 
@@ -89,6 +96,7 @@ unsigned int aom_variance64x32_avx2(const uint8_t *src, int src_stride,
   int sum;
   variance_avx2(src, src_stride, ref, ref_stride, 64, 32, sse, &sum,
                 aom_get32x32var_avx2, 32);
+  _mm256_zeroupper();
   return *sse - (((int64_t)sum * sum) >> 11);
 }
 
@@ -116,6 +124,7 @@ unsigned int aom_sub_pixel_variance64x64_avx2(const uint8_t *src,
                                       dst + 32, dst_stride, 64, &sse2);
   const int se = se1 + se2;
   *sse = sse1 + sse2;
+  _mm256_zeroupper();
   return *sse - (((int64_t)se * se) >> 12);
 }
 
@@ -126,6 +135,7 @@ unsigned int aom_sub_pixel_variance32x32_avx2(const uint8_t *src,
                                               unsigned int *sse) {
   const int se = aom_sub_pixel_variance32xh_avx2(
       src, src_stride, x_offset, y_offset, dst, dst_stride, 32, sse);
+  _mm256_zeroupper();
   return *sse - (((int64_t)se * se) >> 10);
 }
 
@@ -142,7 +152,7 @@ unsigned int aom_sub_pixel_avg_variance64x64_avx2(
   const int se = se1 + se2;
 
   *sse = sse1 + sse2;
-
+  _mm256_zeroupper();
   return *sse - (((int64_t)se * se) >> 12);
 }
 
@@ -152,5 +162,6 @@ unsigned int aom_sub_pixel_avg_variance32x32_avx2(
   // Process 32 elements in parallel.
   const int se = aom_sub_pixel_avg_variance32xh_avx2(
       src, src_stride, x_offset, y_offset, dst, dst_stride, sec, 32, 32, sse);
+  _mm256_zeroupper();
   return *sse - (((int64_t)se * se) >> 10);
 }
