48dacf2b2e511845081ecc7f19e0069c30411ed4
David Barker <david.barker@argondesign.com>
2016-10-13 15:10:14 +0100

    Enable test system to detect transforms misusing 'stride' parameter
    This would have caught the bug introduced in patch set 1 of
    https://chromium-review.googlesource.com/#/c/397378/
    
    Change-Id: I9c6d5d9c4c98aed5ac48c4fb1c4ff4131b0df1d5

--- test/fdct4x4_test.cc
+++ test/fdct4x4_test.cc
@@ -100,6 +100,7 @@ class Trans4x4DCT : public libaom_test::TransformTestBase,
     inv_txfm_ = GET_PARAM(1);
     tx_type_ = GET_PARAM(2);
     pitch_ = 4;
+    height_ = 4;
     fwd_txfm_ref = fdct4x4_ref;
     bit_depth_ = GET_PARAM(3);
     mask_ = (1 << bit_depth_) - 1;
