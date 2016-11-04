bb891393fb3eb4fd4d4179c1cf18081e79492908
Jingning Han <jingning@google.com>
2016-11-04 10:30:00 -0700

    Add min_tx_size variable to recursive transform block partition system
    Replace max_tx_size with min_tx_size for transform type decision.
    
    Change-Id: I64e39923a67903d52b381bd93eaac33b3400a201

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -1129,7 +1129,11 @@ static void write_tx_type(const AV1_COMMON *const cm,
 #endif
                           aom_writer *w) {
   const int is_inter = is_inter_block(mbmi);
+#if CONFIG_VAR_TX
+  const TX_SIZE tx_size = is_inter ? mbmi->min_tx_size : mbmi->tx_size;
+#else
   const TX_SIZE tx_size = mbmi->tx_size;
+#endif
   if (!FIXED_TX_TYPE) {
 #if CONFIG_EXT_TX
     const BLOCK_SIZE bsize = mbmi->sb_type;
@@ -1898,6 +1902,7 @@ static void write_modes_b(AV1_COMP *cpi, const TileInfo *const tile,
       (*tok)++;
     }
   }
+
 #if CONFIG_RD_DEBUG
   for (plane = 0; plane < MAX_MB_PLANE; ++plane) {
     if (m->mbmi.txb_coeff_cost[plane] != txb_coeff_cost[plane]) {
