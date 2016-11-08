83ef684f17b86c3363dfc07ab58dabd17d000430
Linfeng Zhang <linfengz@google.com>
2016-11-08 15:20:18 -0800

    Add high bitdepth 4x4 idct NEON intrinsics
    Change-Id: I4afc130effa05b8be2e9f982967216b1beb2ce4b

--- vpx_dsp/arm/idct4x4_add_neon.c
+++ vpx_dsp/arm/idct4x4_add_neon.c
@@ -13,43 +13,10 @@
 
 #include "./vpx_dsp_rtcd.h"
 #include "vpx_dsp/arm/idct_neon.h"
-#include "vpx_dsp/arm/transpose_neon.h"
 #include "vpx_dsp/txfm_common.h"
-
-static INLINE void idct4x4_16_kernel(const int16x4_t cospis, int16x8_t *s0,
-                                     int16x8_t *s1) {
-  int16x4_t d0, d1, d2, d3, d4, d5;
-  int16x8_t q4, q5;
-  int32x4_t q3, q0, q1, q2;
-
-  transpose_s16_4x4q(s0, s1);
-  d0 = vget_low_s16(*s0);
-  d1 = vget_high_s16(*s0);
-  d2 = vget_low_s16(*s1);
-  d3 = vget_high_s16(*s1);
-  d4 = vadd_s16(d0, d1);
-  d5 = vsub_s16(d0, d1);
-  q0 = vmull_lane_s16(d4, cospis, 2);
-  q1 = vmull_lane_s16(d5, cospis, 2);
-  q2 = vmull_lane_s16(d2, cospis, 3);
-  q3 = vmull_lane_s16(d2, cospis, 1);
-  q2 = vmlsl_lane_s16(q2, d3, cospis, 1);
-  q3 = vmlal_lane_s16(q3, d3, cospis, 3);
-  d0 = vqrshrn_n_s32(q0, 14);
-  d1 = vqrshrn_n_s32(q1, 14);
-  d2 = vqrshrn_n_s32(q2, 14);
-  d3 = vqrshrn_n_s32(q3, 14);
-  q4 = vcombine_s16(d0, d1);
-  q5 = vcombine_s16(d3, d2);
-  *s0 = vaddq_s16(q4, q5);
-  *s1 = vsubq_s16(q4, q5);
-}
 
 void vpx_idct4x4_16_add_neon(const tran_low_t *input, uint8_t *dest,
                              int dest_stride) {
-  DECLARE_ALIGNED(16, static const int16_t, cospi[4]) = {
-    0, (int16_t)cospi_8_64, (int16_t)cospi_16_64, (int16_t)cospi_24_64
-  };
   const int16x4_t cospis = vld1_s16(cospi);
   uint32x2_t dest01_u32 = vmov_n_u32(0);
   uint32x2_t dest32_u32 = vmov_n_u32(0);
