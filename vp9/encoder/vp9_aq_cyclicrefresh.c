5e28819fcf20e20149ca996c1ce8eaf7ad5bbc18
Marco <marpan@google.com>
2016-11-21 16:37:32 -0800

    vp9: Adjust cyclic refresh parameters for low bitrates.
    Increase the motion threshold and qp-delta for segment#2 boost.
    This can increase the frame-drop at low bitrates, but generally
    better spatial quality.
    
    Only affects real-time mode with aq-mode=3, at very low bitrates.
    
    Change-Id: I5ccb784667f70d0c27d369806b93b1f93d5605d1

--- vp9/encoder/vp9_aq_cyclicrefresh.c
+++ vp9/encoder/vp9_aq_cyclicrefresh.c
@@ -472,8 +472,9 @@ void vp9_cyclic_refresh_update_parameters(VP9_COMP *const cpi) {
   }
   // Adjust some parameters for low resolutions at low bitrates.
   if (cm->width <= 352 && cm->height <= 288 && rc->avg_frame_bandwidth < 3400) {
-    cr->motion_thresh = 4;
-    cr->rate_boost_fac = 10;
+    cr->motion_thresh = 8;
+    cr->rate_boost_fac = 13;
+    cr->percent_refresh = 10;
   }
   if (cpi->svc.spatial_layer_id > 0) {
     cr->motion_thresh = 4;
