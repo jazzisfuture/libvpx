dd6ddf57fe5faf088212c0ff4d31b397f794f272
Ranjit Kumar Tulabandu <ranjit.tulabandu@ittiam.com>
2016-12-21 16:58:28 +0530

    Fix for populating the output frame size in recode path
    Calculate the accurate estimate of output frame size to determine the
    need to recode. This fix is applicable for good mode, cpu-used >= 3
    preset
    
    Change-Id: Ic7dc44bfc91654b9451132e01109ebfb81442585

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -3700,17 +3700,15 @@ static void encode_with_recode_loop(VP9_COMP *cpi,
     // Dummy pack of the bitstream using up to date stats to get an
     // accurate estimate of output frame size to determine if we need
     // to recode.
-    if (cpi->sf.recode_loop >= ALLOW_RECODE_KFARFGF) {
-      save_coding_context(cpi);
-      if (!cpi->sf.use_nonrd_pick_mode)
-        vp9_pack_bitstream(cpi, dest, size);
+    save_coding_context(cpi);
+    if (!cpi->sf.use_nonrd_pick_mode)
+      vp9_pack_bitstream(cpi, dest, size);
 
-      rc->projected_frame_size = (int)(*size) << 3;
-      restore_coding_context(cpi);
+    rc->projected_frame_size = (int)(*size) << 3;
+    restore_coding_context(cpi);
 
-      if (frame_over_shoot_limit == 0)
-        frame_over_shoot_limit = 1;
-    }
+    if (frame_over_shoot_limit == 0)
+      frame_over_shoot_limit = 1;
 
     if (cpi->oxcf.rc_mode == VPX_Q) {
       loop = 0;
