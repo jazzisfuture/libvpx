b9667f7f76359ef4ef0fa6fed13019d53b2b7cb7
Jingning Han <jingning@google.com>
2017-01-26 15:00:04 -0800

    Fix real-time compression regression in hbd mode
    This commit resolves the compression performance regression in
    real-time encoding setting when high bit-depth mode is enabled.
    
    The current solution temporarily disables the SIMD implementations
    of vpx_satd, hadamard8x8, and hadamard16x16 in high bit-depth mode.
    
    The commit makes the coding results bit-wise identical between
    regular coding pipeline and high bit-depth at profile 0.
    
    BUG=webm:1365
    
    Change-Id: Icfb900821733749685370460a1a5a7e07f76f4bf

--- test/hadamard_test.cc
+++ test/hadamard_test.cc
@@ -144,6 +144,7 @@ TEST_P(Hadamard8x8Test, VaryStride) {
   }
 }
 
+#if !CONFIG_VP9_HIGHBITDEPTH
 INSTANTIATE_TEST_CASE_P(C, Hadamard8x8Test,
                         ::testing::Values(&vpx_hadamard_8x8_c));
 
@@ -161,6 +162,7 @@ INSTANTIATE_TEST_CASE_P(SSSE3, Hadamard8x8Test,
 INSTANTIATE_TEST_CASE_P(NEON, Hadamard8x8Test,
                         ::testing::Values(&vpx_hadamard_8x8_neon));
 #endif  // HAVE_NEON
+#endif
 
 class Hadamard16x16Test : public HadamardTestBase {};
 
@@ -205,6 +207,7 @@ TEST_P(Hadamard16x16Test, VaryStride) {
   }
 }
 
+#if !CONFIG_VP9_HIGHBITDEPTH
 INSTANTIATE_TEST_CASE_P(C, Hadamard16x16Test,
                         ::testing::Values(&vpx_hadamard_16x16_c));
 
@@ -217,4 +220,5 @@ INSTANTIATE_TEST_CASE_P(SSE2, Hadamard16x16Test,
 INSTANTIATE_TEST_CASE_P(NEON, Hadamard16x16Test,
                         ::testing::Values(&vpx_hadamard_16x16_neon));
 #endif  // HAVE_NEON
+#endif
 }  // namespace
