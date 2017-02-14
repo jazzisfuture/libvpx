7a52a52c52bc4123fb528113c53d23b518ad4132
Johann <johannkoenig@google.com>
2017-02-13 16:29:49 -0800

    Drop zbin_ptr and quant_shift_ptr
    vp9[_highbd]_quantize]_fp[_32x32] and vp9_fdct8x8_quant do not make use
    of these parameters.
    
    scan is used for C code and iscan is used for SIMD implementations.
    
    Change-Id: I908a0ff7d3febac33da97e0596e040ec7bc18ca5

--- vp9/encoder/x86/vp9_dct_ssse3.c
+++ vp9/encoder/x86/vp9_dct_ssse3.c
@@ -20,12 +20,16 @@
 
 void vp9_fdct8x8_quant_ssse3(
     const int16_t *input, int stride, tran_low_t *coeff_ptr, intptr_t n_coeffs,
-    int skip_block, const int16_t *zbin_ptr, const int16_t *round_ptr,
-    const int16_t *quant_ptr, const int16_t *quant_shift_ptr,
+    int skip_block, const int16_t *round_ptr,
+    const int16_t *quant_ptr,
     tran_low_t *qcoeff_ptr, tran_low_t *dqcoeff_ptr, const int16_t *dequant_ptr,
     uint16_t *eob_ptr, const int16_t *scan_ptr, const int16_t *iscan_ptr) {
   __m128i zero;
   int pass;
+
+  (void)scan_ptr;
+  (void)coeff_ptr;
+
   // Constants
   //    When we use them, in one case, they are all the same. In all others
   //    it's a pair of them that we need to repeat four times. This is done
@@ -51,11 +55,6 @@ void vp9_fdct8x8_quant_ssse3(
   __m128i in7 = _mm_load_si128((const __m128i *)(input + 7 * stride));
   __m128i *in[8];
   int index = 0;
-
-  (void)scan_ptr;
-  (void)zbin_ptr;
-  (void)quant_shift_ptr;
-  (void)coeff_ptr;
 
   // Pre-condition input (shift by two)
   in0 = _mm_slli_epi16(in0, 2);
