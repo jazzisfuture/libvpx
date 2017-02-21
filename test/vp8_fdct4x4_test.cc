51ed447b8f16e62d510232c5da3db66acdc4754e
Johann <johannkoenig@google.com>
2017-02-21 11:12:45 -0800

    vp8_fdct4x4 test: fix segfault again
    The input needs to be aligned. The output block is only used by the
    reference function which is C and requires not specific alignment.
    
    Change-Id: Ibd48a84c1785917a6a97c3689a05322abba486b4

--- test/vp8_fdct4x4_test.cc
+++ test/vp8_fdct4x4_test.cc
@@ -20,6 +20,7 @@
 #include "./vpx_config.h"
 #include "./vp8_rtcd.h"
 #include "test/acm_random.h"
+#include "test/buffer.h"
 #include "vpx/vpx_integer.h"
 #include "vpx_ports/mem.h"
 
@@ -71,6 +72,7 @@ void reference_idct4x4(const int16_t *input, int16_t *output) {
 }
 
 using libvpx_test::ACMRandom;
+using libvpx_test::Buffer;
 
 class FdctTest : public ::testing::TestWithParam<FdctFunc> {
  public:
@@ -86,7 +88,7 @@ class FdctTest : public ::testing::TestWithParam<FdctFunc> {
 
 TEST_P(FdctTest, SignBiasCheck) {
   int16_t test_input_block[16];
-  int16_t test_output_block[16];
+  DECLARE_ALIGNED(16, int16_t, test_output_block[16]);
   const int pitch = 8;
   int count_sign_block[16][2];
   const int count_test_block = 1000000;
@@ -155,9 +157,9 @@ TEST_P(FdctTest, RoundTripErrorCheck) {
   double total_error = 0;
   const int count_test_block = 1000000;
   for (int i = 0; i < count_test_block; ++i) {
-    DECLARE_ALIGNED(16, int16_t, test_input_block[16]);
-    DECLARE_ALIGNED(16, int16_t, test_output_block[16]);
-    int16_t test_temp_block[16];
+    int16_t test_input_block[16];
+    int16_t test_output_block[16];
+    DECLARE_ALIGNED(16, int16_t, test_temp_block[16]);
 
     // Initialize a test block with input range [-255, 255].
     for (int j = 0; j < 16; ++j) {
