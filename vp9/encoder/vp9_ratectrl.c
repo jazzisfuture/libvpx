16cda50e084d5334dc31b74204a4018fc6645d33
Marco <marpan@google.com>
2017-02-22 17:12:46 -0800

    vp9: 1 pass CBR: active_worst adjustment from content pre-anaylsis.
    Apply an adjustment factor downwards to active_worst/best factor if
    the average frame-level source sad (from pre-analysis/scene detection)
    is small.
    
    1-2% gain in avgPSNR/SSIM on RTC set for speed 7 and 8,
    a few clips go up ~2-3%.
    
    Change-Id: I9be0d7e346d34dec29bf4e5bb380a7bf81c8480a

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -352,6 +352,7 @@ void vp9_rc_init(const VP9EncoderConfig *oxcf, int pass, RATE_CONTROL *rc) {
   rc->count_last_scene_change = 0;
   rc->af_ratio_onepass_vbr = 10;
   rc->prev_avg_source_sad_lag = 0;
+  rc->low_source_sad = 0;
   rc->high_source_sad = 0;
   rc->high_source_sad_lagindex = -1;
   rc->alt_ref_gf_group = 0;
@@ -716,6 +717,9 @@ static int calc_active_worst_quality_one_pass_cbr(const VP9_COMP *cpi) {
     // Set to worst_quality if buffer is below critical level.
     active_worst_quality = rc->worst_quality;
   }
+  // If active_worst is high, lower it if current frame has low SAD.
+  if (rc->low_source_sad && active_worst_quality > (7 * rc->worst_quality) >> 3)
+    active_worst_quality = (7 * active_worst_quality) >> 3;
   return active_worst_quality;
 }
 
@@ -2209,6 +2213,7 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
   if (cm->use_highbitdepth) return;
 #endif
   rc->high_source_sad = 0;
+  rc->low_source_sad = 0;
   if (cpi->Last_Source != NULL &&
       cpi->Last_Source->y_width == cpi->Source->y_width &&
       cpi->Last_Source->y_height == cpi->Source->y_height) {
@@ -2221,6 +2226,7 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
     int frames_to_buffer = 1;
     int frame = 0;
     uint64_t avg_sad_current = 0;
+    uint64_t avg_source_sad_threshold = 10000;
     uint32_t min_thresh = 4000;
     float thresh = 8.0f;
     if (cpi->oxcf.rc_mode == VPX_VBR) {
@@ -2273,7 +2279,6 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
         int num_samples = 0;
         int sb_cols = (cm->mi_cols + MI_BLOCK_SIZE - 1) / MI_BLOCK_SIZE;
         int sb_rows = (cm->mi_rows + MI_BLOCK_SIZE - 1) / MI_BLOCK_SIZE;
-        uint64_t avg_source_sad_threshold = 10000;
         if (cpi->oxcf.lag_in_frames > 0) {
           src_y = frames[frame]->y_buffer;
           src_ystride = frames[frame]->y_stride;
@@ -2329,6 +2334,8 @@ void vp9_avg_source_sad(VP9_COMP *cpi) {
             rc->high_source_sad = 0;
           if (avg_sad > 0 || cpi->oxcf.rc_mode == VPX_CBR)
             rc->avg_source_sad[0] = (3 * rc->avg_source_sad[0] + avg_sad) >> 2;
+          if (rc->avg_source_sad[0] < avg_source_sad_threshold)
+            rc->low_source_sad = 1;
         } else {
           rc->avg_source_sad[lagframe_idx] = avg_sad;
         }
