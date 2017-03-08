075405982f0314c34837d9d5813ce0434574b536
Marco <marpan@google.com>
2017-03-08 10:57:48 -0800

    vp9: Fix to source_sad feature for SVC.
    Allow speed feature sf->use_source_sad to be used
    on highest spatial layer for SVC.
    
    Change-Id: I260eb0478902764f49f83e43b17024fe86ff3b22

--- vp9/encoder/vp9_speed_features.c
+++ vp9/encoder/vp9_speed_features.c
@@ -501,8 +501,11 @@ static void set_rt_speed_feature_framesize_independent(
       sf->mv.search_method = NSTEP;
       sf->mv.fullpel_search_step_param = 6;
     }
-    if (!cpi->use_svc && !cpi->resize_pending && !cpi->resize_state &&
-        !cpi->external_resize && cpi->oxcf.resize_mode == RESIZE_NONE)
+    if (!cpi->resize_pending && !cpi->resize_state && !cpi->external_resize &&
+        cpi->oxcf.resize_mode == RESIZE_NONE &&
+        (!cpi->use_svc ||
+         (cpi->use_svc &&
+          cpi->svc.spatial_layer_id == cpi->svc.number_spatial_layers - 1)))
       sf->use_source_sad = 1;
     if (sf->use_source_sad) {
       if (cpi->content_state_sb == NULL) {
