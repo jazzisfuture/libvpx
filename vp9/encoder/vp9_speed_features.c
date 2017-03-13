e54bbade461f866413d4f4e4eebdaf5af959f158
Jerome Jiang <jianj@google.com>
2017-03-13 15:27:02 -0700

    vp9: Using source sad for speedup for dynamic resizing.
    Only for speed >= 7.
    
    Change-Id: I3ac85fbb4023cf7e6f8333806b345b0174382a09

--- vp9/encoder/vp9_speed_features.c
+++ vp9/encoder/vp9_speed_features.c
@@ -501,8 +501,7 @@ static void set_rt_speed_feature_framesize_independent(
       sf->mv.search_method = NSTEP;
       sf->mv.fullpel_search_step_param = 6;
     }
-    if (!cpi->resize_pending && !cpi->resize_state && !cpi->external_resize &&
-        cpi->oxcf.resize_mode == RESIZE_NONE)
+    if (!cpi->resize_pending && !cpi->resize_state && !cpi->external_resize)
       sf->use_source_sad = 1;
     if (sf->use_source_sad) {
       // For SVC allocate for top layer.
