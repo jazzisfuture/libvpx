f1832a6d32ad81ebd8e26a2e7726e3055671069e
Marco <marpan@google.com>
2017-03-23 14:32:43 -0700

    vp9: Fix to condition on using source_sad for 1 pass real-time.
    Make the source_sad feature work properly for cases of VBR or
    screen_content with SVC.
    
    Change-Id: Iba5254fd8833fb11da521e00cc1317ec81d3f89b

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -3094,7 +3094,9 @@ static void encode_without_recode_loop(VP9_COMP *cpi, size_t *size,
                                        uint8_t *dest) {
   VP9_COMMON *const cm = &cpi->common;
   int q = 0, bottom_index = 0, top_index = 0;  // Dummy variables.
-  int compute_source_sad = cpi->sf.use_source_sad;
+  int compute_source_sad = cpi->sf.use_source_sad ||
+                           cpi->oxcf.content == VP9E_CONTENT_SCREEN ||
+                           cpi->oxcf.rc_mode == VPX_VBR;
 
   vpx_clear_system_state();
 
@@ -3178,9 +3180,7 @@ static void encode_without_recode_loop(VP9_COMP *cpi, size_t *size,
 
   if (cpi->oxcf.pass == 0 && cpi->oxcf.mode == REALTIME &&
       cpi->oxcf.speed >= 5 && cpi->resize_state == ORIG &&
-      (cpi->oxcf.content == VP9E_CONTENT_SCREEN ||
-       cpi->oxcf.rc_mode == VPX_VBR || compute_source_sad) &&
-      cm->show_frame)
+      compute_source_sad && cm->show_frame)
     vp9_avg_source_sad(cpi);
 
   // For 1 pass SVC, since only ZEROMV is allowed for upsampled reference
