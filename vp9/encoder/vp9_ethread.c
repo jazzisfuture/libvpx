fe219e3d5cf8ce431d3df404b604e21ba7c6d066
Yunqing Wang <yunqingwang@google.com>
2017-03-28 11:27:31 -0700

    Enhance the row mt sync read to accept the sync_range greater than 1
    The row mt sync read uses sync_range = 1, and wouldn't work if we want
    to use a sync_range that is greater than 1. To make it work, this sync
    read code is modified. Pass in col instead of col - 1 to make it
    consistent with other row mt code in VP9, and then add 1 in "while"
    codition.
    
    Change-Id: I4a0e487190ac5d47b8216368da12d80fec779c1a

--- vp9/encoder/vp9_ethread.c
+++ vp9/encoder/vp9_ethread.c
@@ -320,7 +320,7 @@ void vp9_row_mt_sync_read(VP9RowMTSync *const row_mt_sync, int r, int c) {
     pthread_mutex_t *const mutex = &row_mt_sync->mutex_[r - 1];
     pthread_mutex_lock(mutex);
 
-    while (c > row_mt_sync->cur_col[r - 1] - nsync) {
+    while (c > row_mt_sync->cur_col[r - 1] - nsync + 1) {
       pthread_cond_wait(&row_mt_sync->cond_[r - 1], mutex);
     }
     pthread_mutex_unlock(mutex);
