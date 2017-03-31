f9e580084b519d631502f5bc650ce474b073fdcf
Marco <marpan@google.com>
2017-03-30 17:08:44 -0700

    vp9: SVC: Fix an issue with artifact for denoising.
    Avoid early exit in mode selection for the golden reference mode
    on non-zero spatial layer.
    
    Change-Id: I793cf441341edf7c4a88b8ab1e1b22b3cb0eb508

--- vp9/encoder/vp9_pickmode.c
+++ vp9/encoder/vp9_pickmode.c
@@ -1587,14 +1587,6 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x, TileDataEnc *tile_data,
         !svc_force_zero_mode[GOLDEN_FRAME - 1] && !force_skip_low_temp_var))
     use_golden_nonzeromv = 0;
 
-#if CONFIG_VP9_TEMPORAL_DENOISING
-  // TODO(marpan): Allowing golden as the (spatial) reference for SVC with
-  // denoising causes bad artifact. Remove this condition when artifact issue
-  // is resolved.
-  if (cpi->use_svc && cpi->oxcf.noise_sensitivity > 0 && denoise_svc_pickmode)
-    usable_ref_frame = LAST_FRAME;
-#endif
-
   if (cpi->oxcf.speed >= 8 && !cpi->use_svc &&
       ((cpi->rc.frames_since_golden + 1) < x->last_sb_high_content ||
        x->last_sb_high_content > 40))
@@ -2013,7 +2005,8 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x, TileDataEnc *tile_data,
 
     // If early termination flag is 1 and at least 2 modes are checked,
     // the mode search is terminated.
-    if (best_early_term && idx > 0) {
+    if (best_early_term && idx > 0 &&
+        !(cpi->use_svc && svc->spatial_layer_id && ref_frame == GOLDEN_FRAME)) {
       x->skip = 1;
       break;
     }
@@ -2036,13 +2029,6 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x, TileDataEnc *tile_data,
         !(cpi->ref_frame_flags & flag_list[GOLDEN_FRAME]) ||
         (!cpi->svc.layer_context[cpi->svc.temporal_layer_id].is_key_frame &&
          svc_force_zero_mode[best_ref_frame - 1]);
-#if CONFIG_VP9_TEMPORAL_DENOISING
-    // TODO(marpan): Temporary fix to keep intra prediction on as we currently
-    // disallow golden as the (spatial) reference for SVC with denoising due to
-    // artifact issue, Remove this condition when artifact issue is resolved.
-    if (cpi->use_svc && cpi->oxcf.noise_sensitivity > 0 && denoise_svc_pickmode)
-      perform_intra_pred = 1;
-#endif
     inter_mode_thresh = (inter_mode_thresh << 1) + inter_mode_thresh;
   }
   if (cpi->oxcf.lag_in_frames > 0 && cpi->oxcf.rc_mode == VPX_VBR &&
