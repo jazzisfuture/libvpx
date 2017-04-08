87d62b33a67d6aa63aae69246a2528effbdef918
Luca Barbato <lu_zero@gentoo.org>
2017-04-08 22:41:41 +0000

    ppc: d45 predictor 32x32
    About 12x faster.
    
    Change-Id: I22c150256aefb4941861ab1f6c17d554fb694bed

--- test/test_intra_pred_speed.cc
+++ test/test_intra_pred_speed.cc
@@ -340,7 +340,7 @@ INTRA_PRED_TEST(VSX, TestIntraPred32,
                 vpx_dc_128_predictor_32x32_vsx,
                 vpx_v_predictor_32x32_vsx,
                 vpx_h_predictor_32x32_vsx,
-                NULL, NULL, NULL, NULL, NULL, NULL,
+                vpx_d45_predictor_32x32_vsx, NULL, NULL, NULL, NULL, NULL,
                 vpx_tm_predictor_32x32_vsx)
 #endif  // HAVE_VSX
 
