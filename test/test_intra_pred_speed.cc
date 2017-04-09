dc9dcb742bef7a95941137a824f82c2d11fe9790
Luca Barbato <lu_zero@gentoo.org>
2017-04-09 13:05:09 +0000

    ppc: dc predictor 8x8
    Slightly faster, the other dc predictors cannot be faster since
    the computation speedup is overwhelmed by the time spent reading
    dst to write just the 8x8 part.
    
    Change-Id: I94a0b50500adf8b7b6bb919dbf5c7adf5b9fba66

--- test/test_intra_pred_speed.cc
+++ test/test_intra_pred_speed.cc
@@ -313,8 +313,8 @@ INTRA_PRED_TEST(MSA, TestIntraPred32, vpx_dc_predictor_32x32_msa,
 #endif  // HAVE_MSA
 
 #if HAVE_VSX
-INTRA_PRED_TEST(VSX, TestIntraPred8, NULL, NULL, NULL, NULL, NULL, NULL,
-                vpx_d45_predictor_8x8_vsx, NULL, NULL, NULL, NULL,
+INTRA_PRED_TEST(VSX, TestIntraPred8, vpx_dc_predictor_8x8_vsx, NULL, NULL, NULL,
+                NULL, NULL, vpx_d45_predictor_8x8_vsx, NULL, NULL, NULL, NULL,
                 vpx_d63_predictor_8x8_vsx, vpx_tm_predictor_8x8_vsx)
 
 INTRA_PRED_TEST(VSX, TestIntraPred16, vpx_dc_predictor_16x16_vsx,
