ec9bc4cded2a933e01a9885b9a5b2326b663d4eb
Jerome Jiang <jianj@google.com>
2017-04-27 12:56:52 -0700

    vp9: Fix condition for disabling adaptive_rd_thresh.
    Add speed constrains for disabling adaptive_rd_thresh when
    row_mt_bit_exact is set.
    
    Change-Id: I2445115c2f9a2e46b8a0966031a0fea488d4964e

--- vp9/encoder/vp9_speed_features.c
+++ vp9/encoder/vp9_speed_features.c
@@ -647,8 +647,8 @@ void vp9_set_speed_features_framesize_dependent(VP9_COMP *cpi) {
   // With row based multi-threading, the following speed features
   // have to be disabled to guarantee that bitstreams encoded with single thread
   // and multiple threads match.
-  // It can be used since adaptive_rd_thresh is defined per-row for REALTIME.
-  if (oxcf->mode != REALTIME && cpi->row_mt_bit_exact)
+  // It can be used since adaptive_rd_thresh is defined per-row for non-rd.
+  if (oxcf->mode != REALTIME && oxcf->speed < 5 && cpi->row_mt_bit_exact)
     sf->adaptive_rd_thresh = 0;
 
   // This is only used in motion vector unit test.
@@ -803,8 +803,8 @@ void vp9_set_speed_features_framesize_independent(VP9_COMP *cpi) {
   // With row based multi-threading, the following speed features
   // have to be disabled to guarantee that bitstreams encoded with single thread
   // and multiple threads match.
-  // It can be used since adaptive_rd_thresh is defined per-row for REALTIME.
-  if (oxcf->mode != REALTIME && cpi->row_mt_bit_exact)
+  // It can be used since adaptive_rd_thresh is defined per-row for non-rd.
+  if (oxcf->mode != REALTIME && oxcf->speed < 5 && cpi->row_mt_bit_exact)
     sf->adaptive_rd_thresh = 0;
 
   // This is only used in motion vector unit test.
