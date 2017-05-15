9cb0f70a2fcb58210faa92269f643f8895727a1f
Johann <johannkoenig@google.com>
2017-05-15 10:39:44 -0700

    'protected' visibility unsupported on macho
    Mac builds must not specify 'protected' visibility. Then only support
    'default' and 'hidden'.
    
    https://developer.apple.com/library/content/documentation/DeveloperTools/Conceptual/CppRuntimeEnv/Articles/SymbolVisibility.html
    
    Change-Id: I94eccfaa29af0ddcc4a5c1c0e14cf63ef7146462

--- vpx_ports/mem.h
+++ vpx_ports/mem.h
@@ -23,7 +23,8 @@
 #define DECLARE_ALIGNED(n, typ, val) typ val
 #endif
 
-#if defined(__GNUC__) && __GNUC__ >= 4 && !defined(_WIN32)
+#if defined(__GNUC__) && __GNUC__ >= 4 && !defined(_WIN32) && \
+    !defined(__MACHO__)
 #define DECLARE_PROTECTED(decl) decl __attribute__((visibility("protected")))
 #else
 #define DECLARE_PROTECTED(decl) decl
