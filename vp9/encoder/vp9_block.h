eb1010bd2855a3a0253730e5abcaf42b44f40b29
Marco <marpan@google.com>
2017-06-05 15:53:01 -0700

    vp9: Modify mode selection in CBR mode for flat areas.
    For 1 pass CBR non-rd mode: modify the pickmode selection
    when the sse of zeromv-last is very small.
    
    Use an existing condition for SVC mode, and include
    conditioning on source_variance and block size (for non-SVC case).
    Rename/reuse an existing function to apply rdcost bias against
    large motion for flat areas.
    
    Neutral change on RTC and RTC_derf.
    
    Some visual improvement in reducing flashing caused by poor
    selection of golden reference or large motion vectors on big blocks
    in low contrast areas.
    
    Change-Id: Ib0dc5a415c967fc4e88eae2748c5f612fe4fadaa

--- vp9/encoder/vp9_block.h
+++ vp9/encoder/vp9_block.h
@@ -170,6 +170,8 @@ struct macroblock {
 
   uint8_t lowvar_highsumdiff;
 
+  uint8_t limit_mv_search;
+
   uint8_t last_sb_high_content;
 
   // For each superblock: saves the content value (e.g., low/high sad/sumdiff)
