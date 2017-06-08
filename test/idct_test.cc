0c38adb5455b2c628f2aec5ad766b43346510737
Johann <johannkoenig@google.com>
2017-06-08 10:09:23 -0700

    idct_test: don't use std::nothrow anymore
    Buffer now uses Init for allocation. Rely on the bad_alloc
    exception to fail
    
    Change-Id: I2bf2887e1064c9103d29c542d20365c0aea75d76

--- test/idct_test.cc
+++ test/idct_test.cc
@@ -30,11 +30,11 @@ class IDCTTest : public ::testing::TestWithParam<IdctFunc> {
   virtual void SetUp() {
     UUT = GetParam();
 
-    input = new (std::nothrow) Buffer<int16_t>(4, 4, 0);
+    input = new Buffer<int16_t>(4, 4, 0);
     ASSERT_TRUE(input->Init());
-    predict = new (std::nothrow) Buffer<uint8_t>(4, 4, 3);
+    predict = new Buffer<uint8_t>(4, 4, 3);
     ASSERT_TRUE(predict->Init());
-    output = new (std::nothrow) Buffer<uint8_t>(4, 4, 3);
+    output = new Buffer<uint8_t>(4, 4, 3);
     ASSERT_TRUE(output->Init());
   }
 
