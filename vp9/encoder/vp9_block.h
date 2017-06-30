14e87a74f60464655d2ee43b19c912db50f81764
James Zern <jzern@google.com>
2017-06-30 15:29:46 -0700

    cosmetics,vp9/: normalize inv/fwd_txfm naming
    + vpx_dsp/, test/
    
    itxfm -> inv_txfm, ftxfm -> fwd_txfm
    
    Change-Id: I3aacdb65143576d64cfe5c9b14dd358c17c1fe7e

--- vp9/encoder/vp9_block.h
+++ vp9/encoder/vp9_block.h
@@ -182,10 +182,10 @@ struct macroblock {
   uint8_t variance_low[25];
 
   void (*fwd_txfm4x4)(const int16_t *input, tran_low_t *output, int stride);
-  void (*itxfm_add)(const tran_low_t *input, uint8_t *dest, int stride,
+  void (*inv_txfm_add)(const tran_low_t *input, uint8_t *dest, int stride,
                     int eob);
 #if CONFIG_VP9_HIGHBITDEPTH
-  void (*highbd_itxfm_add)(const tran_low_t *input, uint16_t *dest, int stride,
+  void (*highbd_inv_txfm_add)(const tran_low_t *input, uint16_t *dest, int stride,
                            int eob, int bd);
 #endif
 };
