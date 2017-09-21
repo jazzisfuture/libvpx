f2bd47772cfe25b3f0a3450a611349d66093f946
Jerome Jiang <jianj@google.com>
2017-09-21 10:52:20 -0700

    Add unit test to expose vp8 bug when width is set odd.
    Change-Id: Ia518af5494a42e80949cf1165244fbed59606cf7

--- test/encode_api_test.cc
+++ test/encode_api_test.cc
@@ -10,13 +10,32 @@
 
 #include "third_party/googletest/src/include/gtest/gtest.h"
 
+#include "../tools_common.h"
+
 #include "./vpx_config.h"
+#include "test/codec_factory.h"
+#include "test/encode_test_driver.h"
+#include "test/i420_video_source.h"
+#include "test/util.h"
+#include "test/y4m_video_source.h"
+
 #include "vpx/vp8cx.h"
 #include "vpx/vpx_encoder.h"
+#include "vpx_mem/vpx_mem.h"
 
 namespace {
 
 #define NELEMENTS(x) static_cast<int>(sizeof(x) / sizeof(x[0]))
+
+static const VpxInterface vpx_encoders[] = {
+#if CONFIG_VP8_ENCODER
+  { "vp8", VP8_FOURCC, &vpx_codec_vp8_cx },
+#endif
+
+#if CONFIG_VP9_ENCODER
+  { "vp9", VP9_FOURCC, &vpx_codec_vp9_cx },
+#endif
+};
 
 TEST(EncodeAPI, InvalidParams) {
   static const vpx_codec_iface_t *kCodecs[] = {
@@ -76,6 +95,35 @@ TEST(EncodeAPI, HighBitDepthCapability) {
 #else
   EXPECT_EQ(vp9_caps & VPX_CODEC_CAP_HIGHBITDEPTH, 0);
 #endif
+#endif
+}
+
+TEST(EncodeAPI, DISABLED_ImageSizeSetting) {
+#if CONFIG_VP8_ENCODER
+  uint8_t *img_buf;
+  int width = 711;
+  int height = 360;
+  int bps = 12;
+  vpx_image_t img;
+  vpx_codec_ctx_t enc;
+  vpx_codec_enc_cfg_t cfg;
+  const VpxInterface *encoder = &vpx_encoders[0];
+  img_buf = reinterpret_cast<uint8_t *>(
+      vpx_calloc(width * height * bps / 8, sizeof(*img_buf)));
+  vpx_codec_enc_config_default(encoder->codec_interface(), &cfg, 0);
+
+  cfg.g_w = width;
+  cfg.g_h = height;
+
+  vpx_img_wrap(&img, VPX_IMG_FMT_I420, width, height, 1, img_buf);
+
+  vpx_codec_enc_init(&enc, encoder->codec_interface(), &cfg, 0);
+
+  vpx_codec_encode(&enc, &img, 0, 1, 0, 0);
+
+  vpx_free(img_buf);
+
+  vpx_codec_destroy(&enc);
 #endif
 }
 
