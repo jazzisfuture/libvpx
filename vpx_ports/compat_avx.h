0c32cb1fc5ab5789d55ce51789ddda7a98cfb81a
Kyle Siefring <kylesiefring@gmail.com>
2017-11-29 11:27:40 -0500

    Add shared macros for missing AVX/AVX2 intrinsics
    Change-Id: I877b19a3cea6bd12eec7b0e839e54fd87d98db46

---
+++ vpx_ports/compat_avx.h
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 2017 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef VPX_PORTS_COMPAT_AVX_H_
+#define VPX_PORTS_COMPAT_AVX_H_
+
+#define MM256_SET_M128I(a, b) \
+  _mm256_insertf128_si256(_mm256_castsi128_si256(b), (a), 1)
+#define MM256_SETR_M128I(a, b) MM256_SET_M128I((b), (a))
+
+#endif  // VPX_PORTS_COMPAT_AVX_H_
