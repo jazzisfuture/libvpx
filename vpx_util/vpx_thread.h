645260fb64b01499281c0b16d92e5a75d93e7483
Ashik Salim <ashikns@gmail.com>
2018-01-19 10:13:22 +0530

    remove USE_CREATE_THREAD
    CreateThread uses LPTHREAD_START_ROUTINE as a parameter type, which has
    DWORD as it's return type. DWORD corresponds to unsigned int, and the
    libvpx delegate has unsigned long as the delegate return type. This
    causes the compiler to throw an error.
    
    Change-Id: I741eedf49b9aed368c163eb3b2c1a2bd6151d2b3

--- vpx_util/vpx_thread.h
+++ vpx_util/vpx_thread.h
@@ -50,10 +50,6 @@ typedef struct {
 #define WINAPI_FAMILY_PARTITION(x) x
 #endif
 
-#if !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
-#define USE_CREATE_THREAD
-#endif
-
 //------------------------------------------------------------------------------
 // simplistic pthread emulation layer
 
@@ -75,17 +71,10 @@ static INLINE int pthread_create(pthread_t *const thread, const void *attr,
                                  unsigned int(__stdcall *start)(void *),
                                  void *arg) {
   (void)attr;
-#ifdef USE_CREATE_THREAD
-  *thread = CreateThread(NULL,          /* lpThreadAttributes */
-                         0,             /* dwStackSize */
-                         start, arg, 0, /* dwStackSize */
-                         NULL);         /* lpThreadId */
-#else
   *thread = (pthread_t)_beginthreadex(NULL,          /* void *security */
                                       0,             /* unsigned stack_size */
                                       start, arg, 0, /* unsigned initflag */
                                       NULL);         /* unsigned *thrdaddr */
-#endif
   if (*thread == NULL) return 1;
   SetThreadPriority(*thread, THREAD_PRIORITY_ABOVE_NORMAL);
   return 0;
