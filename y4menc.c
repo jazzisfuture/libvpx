1177551c6d9132b088f0407930780274a927f4ec
James Zern <jzern@google.com>
2018-04-09 16:07:40 -0700

    vpx_image: remove unused image formats
    libvpx only emits:
    VPX_IMG_FMT_{I420,I422,I440,I444,I42016,I42216,I44016,I44416}
    and additionally supports YV12 as input.
    
    interleaved yuv, rgb and alpha formats are unused.
    
    Change-Id: Ie2ab1099e950c6e696f475d46882f5c47a174042

--- y4menc.c
+++ y4menc.c
@@ -17,9 +17,7 @@ int y4m_write_file_header(char *buf, size_t len, int width, int height,
   const char *color;
   switch (bit_depth) {
     case 8:
-      color = fmt == VPX_IMG_FMT_444A
-                  ? "C444alpha\n"
-                  : fmt == VPX_IMG_FMT_I444
+      color = fmt == VPX_IMG_FMT_I444
                         ? "C444\n"
                         : fmt == VPX_IMG_FMT_I422 ? "C422\n" : "C420jpeg\n";
       break;
