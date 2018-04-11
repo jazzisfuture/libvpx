5b9175ebbc66456e9099cfd562e7612ca1555ec1
Marco Paniconi <marpan@google.com>
2018-04-11 10:59:34 -0700

    vp9: Intra-only frame for RTC.
    Use case is for layered (SVC) coding to allow higher
    resolution to continue decoding with temporal references,
    while base spatial layer is intra-only frame.
    
    Add control for setting a given frame to be intra-only.
    
    Made encoder changes to real-time path for encoding intra-only
    frame. The intra-only frame will be followed by the overlay/copy
    frame (with both packed in the same superframe).
    
    Added datarate unittest for encoding with first frame being
    intra-only (no key frame at start of stream), for:
    non-svc 1 layer and SVC with 3 spatial layers and 1 temporal.
    
    Made changes to sample encoder vp9_spatial_svc_encoder.c, to
    test inserting intra_only frame into sequence.
    
    Change-Id: I2f4f0106b2c4f51ce77aa2c1c6823ba83ff2f7a0
    Signed-off-by: Marco Paniconi <marpan@google.com>

--- vp9/vp9_cx_iface.c
+++ vp9/vp9_cx_iface.c
@@ -1520,6 +1520,14 @@ static vpx_codec_err_t ctrl_set_svc_frame_drop_layer(vpx_codec_alg_priv_t *ctx,
   return VPX_CODEC_OK;
 }
 
+static vpx_codec_err_t ctrl_set_intra_only_frame(vpx_codec_alg_priv_t *ctx,
+                                                 va_list args) {
+  VP9_COMP *const cpi = ctx->cpi;
+  int data = va_arg(args, int);
+  cpi->set_intra_only_frame = data;
+  return VPX_CODEC_OK;
+}
+
 static vpx_codec_err_t ctrl_register_cx_callback(vpx_codec_alg_priv_t *ctx,
                                                  va_list args) {
   vpx_codec_priv_output_cx_pkt_cb_pair_t *cbp =
@@ -1605,6 +1613,7 @@ static vpx_codec_ctrl_fn_map_t encoder_ctrl_maps[] = {
   { VP9E_ENABLE_MOTION_VECTOR_UNIT_TEST, ctrl_enable_motion_vector_unit_test },
   { VP9E_SET_SVC_INTER_LAYER_PRED, ctrl_set_svc_inter_layer_pred },
   { VP9E_SET_SVC_FRAME_DROP_LAYER, ctrl_set_svc_frame_drop_layer },
+  { VP9E_SET_INTRA_ONLY_FRAME, ctrl_set_intra_only_frame },
 
   // Getters
   { VP8E_GET_LAST_QUANTIZER, ctrl_get_quantizer },
