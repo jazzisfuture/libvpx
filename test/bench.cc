9a61317c7d6220550b914c7cf892acd1c168965b
Luc Trudeau <luc@trud.ca>
2018-05-22 14:16:15 -0400

    Robust Speed Tests
    To make speed tests more robust, tests are performed mulitple times and
    the median time is reported.
    
    To use robust speed tests, add AbstractBench as a parent to your test
    class, and implement the run() method (with the code you want to
    benchmark).
    
    Sample output for VP9QuantizeTest
    
      [    BENCH ]      Bypass calculations       4x4  165.8 ms ( ±1.0 ms )
      [    BENCH ]        Full calculations       4x4  165.8 ms ( ±0.9 ms )
      [    BENCH ]      Bypass calculations       8x8  129.7 ms ( ±0.9 ms )
      [    BENCH ]        Full calculations       8x8  130.3 ms ( ±1.4 ms )
      [    BENCH ]      Bypass calculations     16x16  110.3 ms ( ±1.4 ms )
      [    BENCH ]        Full calculations     16x16  110.1 ms ( ±0.9 ms )
    
    Change-Id: I1dd649754cb8c4c621eee2728198ea6a555f38b3

---
+++ test/bench.cc
@@ -0,0 +1,27 @@
+#include "test/bench.h"
+
+#include <stdio>
+#include <algorithm>
+
+void AbstractBench::runNTimes(int n) {
+  for (int r = 0; r < VPX_BENCH_ROBUST_ITER; r++) {
+    vpx_usec_timer timer;
+    vpx_usec_timer_start(&timer);
+    for (int j = 0; j < n; ++j) {
+      run();
+    }
+    vpx_usec_timer_mark(&timer);
+    times[r] = static_cast<int>(vpx_usec_timer_elapsed(&timer));
+  }
+}
+
+void AbstractBench::printMedian(const char *title) {
+  std::sort(times, times + VPX_BENCH_ROBUST_ITER);
+  const int med = times[VPX_BENCH_ROBUST_ITER >> 1];
+  int sad = 0;
+  for (int t = 0; t < VPX_BENCH_ROBUST_ITER; t++) {
+    sad += abs(times[t] - med);
+  }
+  printf("[%10s] %s %.1f ms ( ±%.1f ms )\n", "BENCH ", title, med / 1000.0,
+         sad / (VPX_BENCH_ROBUST_ITER * 1000.0));
+}
