84b09f5f050e570f7c5ae4f2130d821fdd038fa1
Alexandra Hájková <alexandra.khirnova@gmail.com>
2018-05-26 13:10:10 +0000

    ppc: add vp9_iht8x8_64_add_vsx
    Change-Id: I99a9535bf1ae58c494113fc88d9616bda202716a

--- vp9/common/ppc/vp9_idct_vsx.c
+++ vp9/common/ppc/vp9_idct_vsx.c
@@ -45,3 +45,40 @@ void vp9_iht4x4_16_add_vsx(const tran_low_t *input, uint8_t *dest, int stride,
 
   round_store4x4_vsx(in, out, dest, stride);
 }
+
+void vp9_iht8x8_64_add_vsx(const tran_low_t *input, uint8_t *dest, int stride,
+                           int tx_type) {
+  int16x8_t in[8], out[8];
+
+  // load input data
+  in[0] = load_tran_low(0, input);
+  in[1] = load_tran_low(8 * sizeof(*input), input);
+  in[2] = load_tran_low(2 * 8 * sizeof(*input), input);
+  in[3] = load_tran_low(3 * 8 * sizeof(*input), input);
+  in[4] = load_tran_low(4 * 8 * sizeof(*input), input);
+  in[5] = load_tran_low(5 * 8 * sizeof(*input), input);
+  in[6] = load_tran_low(6 * 8 * sizeof(*input), input);
+  in[7] = load_tran_low(7 * 8 * sizeof(*input), input);
+
+  switch (tx_type) {
+    case DCT_DCT:
+      idct8_vsx(in, out);
+      idct8_vsx(out, in);
+      break;
+    case ADST_DCT:
+      idct8_vsx(in, out);
+      iadst8_vsx(out, in);
+      break;
+    case DCT_ADST:
+      iadst8_vsx(in, out);
+      idct8_vsx(out, in);
+      break;
+    default:
+      assert(tx_type == ADST_ADST);
+      iadst8_vsx(in, out);
+      iadst8_vsx(out, in);
+      break;
+  }
+
+  round_store8x8_vsx(in, dest, stride);
+}
