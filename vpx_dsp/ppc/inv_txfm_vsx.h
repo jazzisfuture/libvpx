d85b154dd4e824702596303a2a96dcf24fca1db5
Alexandra Hájková <alexandra.khirnova@gmail.com>
2018-05-26 13:10:10 +0000

    ppc: add vp9_iht8x8_64_add_vsx
    Change-Id: I99a9535bf1ae58c494113fc88d9616bda202716a

--- vpx_dsp/ppc/inv_txfm_vsx.h
+++ vpx_dsp/ppc/inv_txfm_vsx.h
@@ -20,3 +20,7 @@ void round_store4x4_vsx(int16x8_t *in, int16x8_t *out, uint8_t *dest,
                         int stride);
 void idct4_vsx(int16x8_t *in, int16x8_t *out);
 void iadst4_vsx(int16x8_t *in, int16x8_t *out);
+
+void round_store8x8_vsx(int16x8_t *in, uint8_t *dest, int stride);
+void idct8_vsx(int16x8_t *in, int16x8_t *out);
+void iadst8_vsx(int16x8_t *in, int16x8_t *out);
