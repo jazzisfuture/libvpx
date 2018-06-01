68ac7c1cd25f6a3661af90322ccc9991a0f6dd18
Jerome Jiang <jianj@google.com>
2018-06-01 14:27:34 -0700

    vp9-svc: Allow usage of second (long term) temporal reference.
    Allow for second temporal reference for top spatial layer in SVC,
    when inter-layer prediction is disabled on INTER frames.
    The second temporal reference is labelled as the golden reference
    and the update/refresh of this reference buffer is only on base
    temporal layer superframes. For now the period of refresh is
    fixed at every 20 TL0 superframes.
    
    Average gain is ~4% on RTC set, several clips up
    by ~8-12%. Speed loss is about ~4% on mac.
    
    Feature is disabled as default for now.
    
    Change-Id: I2e5db5052c62dbe958a3b14be97d043823b7a529

--- vp9/encoder/vp9_svc_layercontext.h
+++ vp9/encoder/vp9_svc_layercontext.h
@@ -97,6 +97,10 @@ typedef struct SVC {
   int gld_fb_idx[VPX_MAX_LAYERS];
   int alt_fb_idx[VPX_MAX_LAYERS];
   int force_zero_mode_spatial_ref;
+  // Sequence level flag to enable second (long term) temporal reference.
+  int use_longterm_ref;
+  // Frame level flag to enable second (long term) temporal reference.
+  int use_longterm_ref_current_layer;
   int current_superframe;
   int non_reference_frame;
   int use_base_mv;
