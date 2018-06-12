8dbef6f31710f4f34630a80b70c61f21f96048a0
Marco Paniconi <marpan@google.com>
2018-06-12 11:50:29 -0700

    vp9-svc: Add support for spatial layer sync frames.
    Add encoder control to allow application to insert
    spatial layer sync frame. The sync frame disables
    temporal prediction for that spatial layer.
    
    This is useful for RTC application to have receiver
    start decoding a higher spatial layer, without inserting
    a key frame on base spatial layer.
    
    If the layer sync is requested on the base spatial layer
    this then force a key frame, otherwise it only disables
    the temporal reference for that spatial layer, allowing
    temporal prediciton to continue for the other layers.
    
    Added unittest for inserting 2 layer sync frames.
    
    Bump up ABI version.
    Change-Id: Id458acc400a77c853551f125c4e7b6d001991f03

--- vpx/vp8cx.h
+++ vpx/vp8cx.h
@@ -637,6 +637,15 @@ enum vp8e_enc_control_id {
    * Supported in codecs: VP9
    */
   VP9E_SET_SVC_GF_TEMPORAL_REF,
+
+  /*!\brief Codec control function to enable spatial layer sync frame, for any
+   * spatial layer. Enabling it for layer k means spatial layer k has no
+   * temporal prediction. Setting for base spatial layer has no effect. Default
+   * is off (0) for all spatial layers.
+   *
+   * Supported in codecs: VP9
+   */
+  VP9E_SET_SVC_SPATIAL_LAYER_SYNC,
 };
 
 /*!\brief vpx 1-D scaling mode
@@ -959,6 +968,9 @@ VPX_CTRL_USE_TYPE(VP9E_GET_SVC_REF_FRAME_CONFIG, vpx_svc_ref_frame_config_t *)
 
 VPX_CTRL_USE_TYPE(VP9E_SET_SVC_GF_TEMPORAL_REF, unsigned int)
 #define VPX_CTRL_VP9E_SET_SVC_GF_TEMPORAL_REF
+
+VPX_CTRL_USE_TYPE(VP9E_SET_SVC_SPATIAL_LAYER_SYNC, int *)
+#define VPX_CTRL_VP9E_SET_SVC_SPATIAL_LAYER_SYNC
 
 /*!\endcond */
 /*! @} - end defgroup vp8_encoder */
