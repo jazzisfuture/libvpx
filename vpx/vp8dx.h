76d4419848fb44f98257241e9058b5e3f61e4427
Supradeep T R <supradeep.tr@ittiam.com>
2018-06-12 13:57:39 +0530

    DO NO MERGE: Force Loopfilter Multi-Thread Optimization
    enabled lpf-opt and disabled 1562 test.
    
    Take the original loopfilter multi-thread optimization
    (dafe064289a917977439ab6f4f002b9946496084) along with the fixes for bugs
    1558 and 1562.
    
    Change-Id: I2ebfcc97418649df4b7d6e332ba758fc51b28e18

--- vpx/vp8dx.h
+++ vpx/vp8dx.h
@@ -132,6 +132,14 @@ enum vp8_dec_control_id {
    */
   VP9D_SET_ROW_MT,
 
+  /*!\brief Codec control function to set loopfilter optimization.
+   *
+   * 0 : off, 1 : on
+   *
+   * Supported in codecs: VP9
+   */
+  VP9D_SET_LPF_OPT,
+
   VP8_DECODER_CTRL_ID_MAX
 };
 
@@ -191,6 +199,8 @@ VPX_CTRL_USE_TYPE(VP9_DECODE_SVC_SPATIAL_LAYER, int)
 VPX_CTRL_USE_TYPE(VP9_SET_SKIP_LOOP_FILTER, int)
 #define VPX_CTRL_VP9_DECODE_SET_ROW_MT
 VPX_CTRL_USE_TYPE(VP9D_SET_ROW_MT, int)
+#define VPX_CTRL_VP9_SET_LPF_OPT
+VPX_CTRL_USE_TYPE(VP9D_SET_LPF_OPT, int)
 
 /*!\endcond */
 /*! @} - end defgroup vp8_decoder */
