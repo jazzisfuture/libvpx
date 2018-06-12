af6bdc60639cddee078d16a08362f1acdfd9249c
Marco Paniconi <marpan@google.com>
2018-06-12 11:50:29 -0700

    vp9-svc: Add support for spatial layer sync frames.
    Add encoder control to allow application to insert
    spatial layer sync frame. The sync frame disables
    temporal prediction for that spatial layer.
    
    This is useful for RTC application to have receiver
    start decoding a higher spatial layer, without inserting
    a key frame on base spatial layer.
    
    If the layer sync is requested on the base spatial layer
    this then force a key frame, otherwise it only disables
    the temporal reference for that spatial layer, allowing
    temporal prediciton to continue for the other layers.
    The inter-layer prediction for the sync frame is enabled.
    
    Added unittest for inserting 2 layer sync frames.
    
    Bump up ABI version.
    Change-Id: Id458acc400a77c853551f125c4e7b6d001991f03

--- vp9/vp9_cx_iface.c
+++ vp9/vp9_cx_iface.c
@@ -1547,6 +1547,15 @@ static vpx_codec_err_t ctrl_set_svc_gf_temporal_ref(vpx_codec_alg_priv_t *ctx,
   return VPX_CODEC_OK;
 }
 
+static vpx_codec_err_t ctrl_set_svc_spatial_layer_sync(
+    vpx_codec_alg_priv_t *ctx, va_list args) {
+  VP9_COMP *const cpi = ctx->cpi;
+  int *const data = va_arg(args, int *);
+  memcpy(&cpi->svc.spatial_layer_sync, data,
+         sizeof(data[0]) * cpi->svc.number_spatial_layers);
+  return VPX_CODEC_OK;
+}
+
 static vpx_codec_err_t ctrl_register_cx_callback(vpx_codec_alg_priv_t *ctx,
                                                  va_list args) {
   vpx_codec_priv_output_cx_pkt_cb_pair_t *cbp =
@@ -1633,6 +1642,7 @@ static vpx_codec_ctrl_fn_map_t encoder_ctrl_maps[] = {
   { VP9E_SET_SVC_INTER_LAYER_PRED, ctrl_set_svc_inter_layer_pred },
   { VP9E_SET_SVC_FRAME_DROP_LAYER, ctrl_set_svc_frame_drop_layer },
   { VP9E_SET_SVC_GF_TEMPORAL_REF, ctrl_set_svc_gf_temporal_ref },
+  { VP9E_SET_SVC_SPATIAL_LAYER_SYNC, ctrl_set_svc_spatial_layer_sync },
 
   // Getters
   { VP8E_GET_LAST_QUANTIZER, ctrl_get_quantizer },
