d42572019db34081a4c1b77fae32aef5878cb436
Zoe Liu <zoeliu@google.com>
2018-06-21 16:28:15 -0700

    Add extra altref option for hierarchical structure
    This CL is to hook up the implemented hierarchical strucutre
    construction as well as its corresponding bitrate allocation
    functionalities with the GF group defining.
    
    Currently the hierarchical structure is off by default. Hence this CL
    has no impact on the coding performance.
    
    Change-Id: I9e1ddfd877559e99072c23970f7fe103b64ed9ee

--- vp9/encoder/vp9_firstpass.h
+++ vp9/encoder/vp9_firstpass.h
@@ -41,7 +41,12 @@ typedef struct {
 
 #define INVALID_ROW -1
 
+// Length of the bi-predictive frame group (BFG)
+// NOTE: Currently each BFG contains one backward ref (BWF) frame plus a certain
+//       number of bi-predictive frames.
+#define BFG_INTERVAL 2
 #define MAX_EXT_ARFS 2
+#define MIN_EXT_ARF_INTERVAL 4
 
 typedef struct {
   double frame_mb_intra_factor;
@@ -209,6 +214,18 @@ void vp9_twopass_postencode_update(struct VP9_COMP *cpi);
 
 void calculate_coded_size(struct VP9_COMP *cpi, int *scaled_frame_width,
                           int *scaled_frame_height);
+
+static INLINE int get_number_of_extra_arfs(int interval, int arf_pending) {
+  assert(MAX_EXT_ARFS > 0);
+  if (arf_pending)
+    return interval >= MIN_EXT_ARF_INTERVAL * (MAX_EXT_ARFS + 1)
+               ? MAX_EXT_ARFS
+               : interval >= MIN_EXT_ARF_INTERVAL * MAX_EXT_ARFS
+                     ? MAX_EXT_ARFS - 1
+                     : 0;
+  else
+    return 0;
+}
 
 #ifdef __cplusplus
 }  // extern "C"
