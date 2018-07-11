34b17aefc20bb5227474a585e624a30f5d65b51e
Luc Trudeau <luc@trud.ca>
2018-07-11 15:44:33 -0400

    VSX version of vpx_lpf_horizontal_16_dual_vsx
    Low bit depth version only. Passes the Loop8Test6Param  test suite.
    
    Loop8Test6Param Speed Test (POWER8 Model 2.1)
    C: 48.7 ms ( ±001 ms ) VSX: 8.5 ms ( ±0.0 ms ) Speedup 5.7x
    
    Change-Id: I827960beebb482bbcffe360c75b15aa897a16c0a

--- test/bench.cc
+++ test/bench.cc
@@ -15,6 +15,7 @@
 #include "vpx_ports/vpx_timer.h"
 
 void AbstractBench::RunNTimes(int n) {
+  use_ref = 0;
   for (int r = 0; r < VPX_BENCH_ROBUST_ITER; r++) {
     vpx_usec_timer timer;
     vpx_usec_timer_start(&timer);
@@ -26,13 +27,61 @@ void AbstractBench::RunNTimes(int n) {
   }
 }
 
-void AbstractBench::PrintMedian(const char *title) {
-  std::sort(times_, times_ + VPX_BENCH_ROBUST_ITER);
-  const int med = times_[VPX_BENCH_ROBUST_ITER >> 1];
+void AbstractBench::RunNTimesWithRef(int n) {
+  use_ref = 1;
+  for (int r = 0; r < VPX_BENCH_ROBUST_ITER; r++) {
+    vpx_usec_timer timer;
+    vpx_usec_timer_start(&timer);
+    for (int j = 0; j < n; ++j) {
+      RunRef();
+    }
+    vpx_usec_timer_mark(&timer);
+    ref_times_[r] = static_cast<int>(vpx_usec_timer_elapsed(&timer));
+
+    vpx_usec_timer_start(&timer);
+    for (int j = 0; j < n; ++j) {
+      Run();
+    }
+    vpx_usec_timer_mark(&timer);
+    times_[r] = static_cast<int>(vpx_usec_timer_elapsed(&timer));
+  }
+}
+
+static INLINE int median(int *times) {
+  std::sort(times, times + VPX_BENCH_ROBUST_ITER);
+  return times[VPX_BENCH_ROBUST_ITER >> 1];
+}
+
+static INLINE int sumAbsDiff(int *times, int med) {
   int sad = 0;
   for (int t = 0; t < VPX_BENCH_ROBUST_ITER; t++) {
-    sad += abs(times_[t] - med);
+    sad += abs(times[t] - med);
   }
+  return sad;
+}
+
+static INLINE void print(const char *title, int med, int sad) {
   printf("[%10s] %s %.1f ms ( ±%.1f ms )\n", "BENCH ", title, med / 1000.0,
          sad / (VPX_BENCH_ROBUST_ITER * 1000.0));
+}
+
+static INLINE void printWithRef(const char *title, int med, int sad,
+                                int ref_med, int ref_sad) {
+  printf("[%10s] Ref %s %.1f ms ( ±%.1f ms ) %.1f ms ( ±%.1f ms ) %.1fx\n",
+         "BENCH ", title, ref_med / 1000.0,
+         ref_sad / (VPX_BENCH_ROBUST_ITER * 1000.0), med / 1000.0,
+         sad / (VPX_BENCH_ROBUST_ITER * 1000.0), ref_med / (double)med);
+}
+
+void AbstractBench::PrintMedian(const char *title) {
+  const int med = median(times_);
+  const int sad = sumAbsDiff(times_, med);
+
+  if (use_ref) {
+    const int ref_med = median(ref_times_);
+    const int ref_sad = sumAbsDiff(ref_times_, ref_med);
+    printWithRef(title, med, sad, ref_med, ref_sad);
+  } else {
+    print(title, med, sad);
+  }
 }
