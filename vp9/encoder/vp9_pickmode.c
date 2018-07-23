82601867a1e1a082e4ffebbf9002023cd29239b7
Marco Paniconi <marpan@google.com>
2018-07-23 16:24:15 -0700

    vp9: Adjustment to intra-mode check for screen content.
    For nonrd-pickmode, when the short_circuit flat_blocks feauture
    is enabled: make sure DC intra is checked on flat blocks.
    Some early exits was preventing this from happening.
    Reduces some artifacts in real-time screenshare encoding.
    
    Change-Id: I9620e424bedc5a13f87cc4f66af7c0e86043c89c

--- vp9/encoder/vp9_pickmode.c
+++ vp9/encoder/vp9_pickmode.c
@@ -2331,7 +2331,8 @@ void vp9_pick_inter_mode(VP9_COMP *cpi, MACROBLOCK *x, TileDataEnc *tile_data,
   // Perform intra prediction search, if the best SAD is above a certain
   // threshold.
   if (best_rdc.rdcost == INT64_MAX ||
-      (scene_change_detected && perform_intra_pred) ||
+      ((scene_change_detected || (sf->short_circuit_flat_blocks &&
+        x->source_variance == 0)) && perform_intra_pred) ||
       ((!force_skip_low_temp_var || bsize < BLOCK_32X32 ||
         x->content_state_sb == kVeryHighSad) &&
        perform_intra_pred && !x->skip && best_rdc.rdcost > inter_mode_thresh &&
