98e3d846955e1ce374812bc3465f58d9f647bcc6
Venkatarama NG. Avadhani <venkatarama.avadhani@ittiam.com>
2018-08-24 11:39:12 +0530

    Fix DoS in Loopfilter
    While decoding an error clip with fewer threads than number of tiles,
    one or more threads will decode more than one tile. It is possible in
    this case that the first tile being decoded is erroneous. This will
    cause the hook to exit without attempting to decode the second tile. In
    this case, the vp9_set_row will not be called for the second tile.
    Another thread that has started the loopfilter function would then wait
    in an infinite loop for the signal saying all the tiles are decoded.
    
    Change-Id: I7d61a35f518b67955cf9e2205d212c8fe834db82

--- vp9/decoder/vp9_decodeframe.c
+++ vp9/decoder/vp9_decodeframe.c
@@ -1498,7 +1498,25 @@ static int tile_worker_hook(void *arg1, void *arg2) {
   if (setjmp(tile_data->error_info.jmp)) {
     tile_data->error_info.setjmp = 0;
     tile_data->xd.corrupted = 1;
+    pbi->mb.corrupted |= tile_data->xd.corrupted;
     tile_data->data_end = NULL;
+    if (cm->lf.filter_level && !cm->skip_loop_filter) {
+      int num_tiles_left = tile_data->buf_end - n;
+      do {
+        /* Initialize mi_row value here, in case there is a longjmp from
+         * setup_token_decoder(). This initialization is defensive, it could be
+         * incorrect, but ensures that all the rows are marked as decoded.
+         */
+        int mi_row;
+        for (mi_row = 0; mi_row < cm->mi_rows; mi_row += MI_BLOCK_SIZE) {
+          const int aligned_rows = mi_cols_aligned_to_sb(cm->mi_rows);
+          const int sb_rows = (aligned_rows >> MI_BLOCK_SIZE_LOG2);
+          const int is_last_row = (sb_rows - 1 == mi_row >> MI_BLOCK_SIZE_LOG2);
+          vp9_set_row(lf_sync, 1 << cm->log2_tile_cols,
+                      mi_row >> MI_BLOCK_SIZE_LOG2, is_last_row);
+        }
+      } while (num_tiles_left--);
+    }
     return 0;
   }
 
@@ -1520,7 +1538,8 @@ static int tile_worker_hook(void *arg1, void *arg2) {
          mi_row += MI_BLOCK_SIZE) {
       vp9_zero(tile_data->xd.left_context);
       vp9_zero(tile_data->xd.left_seg_context);
-      for (mi_col = tile->mi_col_start; mi_col < tile->mi_col_end;
+      for (mi_col = tile->mi_col_start;
+           mi_col < tile->mi_col_end && !tile_data->xd.corrupted;
            mi_col += MI_BLOCK_SIZE) {
         decode_partition(tile_data, pbi, mi_row, mi_col, BLOCK_64X64, 4);
       }
@@ -1536,7 +1555,7 @@ static int tile_worker_hook(void *arg1, void *arg2) {
     if (buf->col == final_col) {
       bit_reader_end = vpx_reader_find_end(&tile_data->bit_reader);
     }
-  } while (!tile_data->xd.corrupted && ++n <= tile_data->buf_end);
+  } while (++n <= tile_data->buf_end);
 
   if (!tile_data->xd.corrupted && cm->lf.filter_level &&
       !cm->skip_loop_filter) {
