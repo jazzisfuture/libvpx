4928f374a4f8fc12f3c42587339aab0dc055da80
Jingning Han <jingning@google.com>
2018-08-31 15:54:10 -0700

    Adaptive ARF factor decision
    Re-count the factors to decide bit boost factor for the
    intermediate layer ARFs. Make the gfu_boost factor assigned to
    each ARF adapt to its local factors.
    
    This and the recursive change 5bfe9eb together improves the
    multi-layer ARF compression performance:
    
              avg_psnr      ovr_psnr     ssim
    lowres    -0.39%        -0.54%       -1.6%
    midres    -0.98%        -1.26%       -2.3%
    hdres     -0.95%        -1.13%       -2.3%
    
    Change-Id: I5fec3ea75cae58825787dc88dadc7e8697a041ea

--- vp9/encoder/vp9_firstpass.h
+++ vp9/encoder/vp9_firstpass.h
@@ -144,6 +144,7 @@ typedef struct {
   unsigned char brf_src_offset[MAX_STATIC_GF_GROUP_LENGTH + 2];
   unsigned char bidir_pred_enabled[MAX_STATIC_GF_GROUP_LENGTH + 2];
   int bit_allocation[MAX_STATIC_GF_GROUP_LENGTH + 2];
+  int gfu_boost[MAX_STATIC_GF_GROUP_LENGTH + 2];
 
   // TODO(jingning): The array size of arf_stack could be reduced.
   int arf_index_stack[MAX_LAG_BUFFERS * 2];
