fd6a2f5b2ff32eb41bb99c23f4b6d9bcfc14bb3f
Venkatarama NG. Avadhani <venkatarama.avadhani@ittiam.com>
2018-09-05 10:35:04 +0530

    Revert "Revert "Loopfilter MultiThread Optimization""
    This reverts commit 753fd86e86ac727dccac88376260b8f54502f2a3.
    
    This also has the fix for the DoS reported in bug 1558.
    
    BUG=webm:1558
    
    Change-Id: I65ea84e0c11d6bd40d8cb0587dfe934b3ac11dce

--- test/invalid_file_test.cc
+++ test/invalid_file_test.cc
@@ -204,6 +204,7 @@ const DecodeParam kMultiThreadedVP9InvalidFileTests[] = {
   { 2, "invalid-vp90-2-09-aq2.webm.ivf.s3984_r01-05_b6-.v2.ivf" },
   { 4, "invalid-vp90-2-09-subpixel-00.ivf.s19552_r01-05_b6-.v2.ivf" },
   { 2, "invalid-crbug-629481.webm" },
+  { 3, "invalid-vp90-lpf-dos.ivf" },
 };
 
 INSTANTIATE_TEST_CASE_P(
