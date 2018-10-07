8a549364b08df0536860968da2baa115211dbfbb
Wan-Teh Chang <wtc@google.com>
2018-10-07 11:39:25 -0700

    Change a pre-increment to a post-increment.
    This CL demonstrates the two scan-build warnings about dereference
    of null pointers in vp9_encoder.c.
    
    Change-Id: I724810ba774306faa0d5db8b30fdf5c8e6c51eda

--- vp9/common/vp9_onyxc_int.h
+++ vp9/common/vp9_onyxc_int.h
@@ -38,8 +38,6 @@ extern "C" {
 #define REF_FRAMES (1 << REF_FRAMES_LOG2)
 
 // 1 scratch frame for the new frame, 3 for scaled references on the encoder.
-// TODO(jkoleszar): These 3 extra references could probably come from the
-// normal reference pool.
 #define FRAME_BUFFERS (REF_FRAMES + 4)
 
 #define FRAME_CONTEXTS_LOG2 2
@@ -326,7 +324,7 @@ static INLINE void vp9_init_macroblockd(VP9_COMMON *cm, MACROBLOCKD *xd,
                                         tran_low_t *dqcoeff) {
   int i;
 
-  for (i = 0; i < MAX_MB_PLANE; ++i) {
+  for (i = 0; i < MAX_MB_PLANE; i++) {
     xd->plane[i].dqcoeff = dqcoeff;
     xd->above_context[i] =
         cm->above_context +
