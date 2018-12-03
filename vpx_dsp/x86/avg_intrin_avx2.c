1dd12c5469fb0e796eaa20de3f99fdf2560ae52e
sdeng <sdeng@google.com>
2018-12-02 18:36:28 -0800

    EXP two rows in one __m256i for first pass
    Change-Id: I94f7324be20405ff55f8a02ad4651c4ab4c10203

--- vpx_dsp/x86/avg_intrin_avx2.c
+++ vpx_dsp/x86/avg_intrin_avx2.c
@@ -16,7 +16,86 @@
 #include "vpx_ports/mem.h"
 
 #if CONFIG_VP9_HIGHBITDEPTH
-static void highbd_hadamard_col8_avx2(__m256i *in, int iter) {
+static void highbd_hadamard_col8_pass1_avx2(__m128i * const in) {
+  // load the 2 strides of source
+  __m256i c0 = _mm256_castsi128_si256(in[0]);
+  c0 = _mm256_inserti128_si256(c0, in[4], 1);
+  __m256i c1 = _mm256_castsi128_si256(in[1]);
+  c1 = _mm256_inserti128_si256(c1, in[5], 1);
+  __m256i c2 = _mm256_castsi128_si256(in[2]);
+  c2 = _mm256_inserti128_si256(c2, in[6], 1);
+  __m256i c3 = _mm256_castsi128_si256(in[3]);
+  c3 = _mm256_inserti128_si256(c3, in[7], 1);
+
+  __m256i b0 = _mm256_add_epi16(c0, c1);
+  __m256i b1 = _mm256_sub_epi16(c0, c1);
+  __m256i b2 = _mm256_add_epi16(c2, c3);
+  __m256i b3 = _mm256_sub_epi16(c2, c3);
+
+  c0 = _mm256_add_epi16(b0, b2);
+  c1 = _mm256_sub_epi16(b0, b2);
+  c2 = _mm256_add_epi16(b1, b3);
+  c3 = _mm256_sub_epi16(b1, b3);
+
+  // repack the 2 strides of source
+  __m128i a0 = _mm256_castsi256_si128(c0);
+  __m128i a4 = _mm256_extractf128_si256(c0, 1);
+  __m128i a2 = _mm256_castsi256_si128(c1);
+  __m128i a6 = _mm256_extractf128_si256(c1, 1);
+  __m128i a1 = _mm256_castsi256_si128(c2);
+  __m128i a5 = _mm256_extractf128_si256(c2, 1);
+  __m128i a3 = _mm256_castsi256_si128(c3);
+  __m128i a7 = _mm256_extractf128_si256(c3, 1);
+
+  __m128i  d0 = _mm_add_epi16(a0, a4);
+  __m128i  d7 = _mm_add_epi16(a1, a5);
+  __m128i  d3 = _mm_add_epi16(a2, a6);
+  __m128i  d4 = _mm_add_epi16(a3, a7);
+  __m128i  d2 = _mm_sub_epi16(a0, a4);
+  __m128i  d6 = _mm_sub_epi16(a1, a5);
+  __m128i  d1 = _mm_sub_epi16(a2, a6);
+  __m128i  d5 = _mm_sub_epi16(a3, a7);
+
+  a0 = _mm_unpacklo_epi16(d0, d1);
+  a1 = _mm_unpacklo_epi16(d2, d3);
+  a2 = _mm_unpackhi_epi16(d0, d1);
+  a3 = _mm_unpackhi_epi16(d2, d3);
+  a4 = _mm_unpacklo_epi16(d4, d5);
+  a5 = _mm_unpacklo_epi16(d6, d7);
+  a6 = _mm_unpackhi_epi16(d4, d5);
+  a7 = _mm_unpackhi_epi16(d6, d7);
+
+  b0 = _mm256_castsi128_si256(a0);
+  b0 = _mm256_inserti128_si256(b0, a2, 1);
+  b1 = _mm256_castsi128_si256(a1);
+  b1 = _mm256_inserti128_si256(b1, a3, 1);
+  b2 = _mm256_castsi128_si256(a4);
+  b2 = _mm256_inserti128_si256(b2, a6, 1);
+  b3 = _mm256_castsi128_si256(a5);
+  b3 = _mm256_inserti128_si256(b3, a7, 1);
+
+  // 'rotate' the matrix
+  c0 = _mm256_unpacklo_epi32(b0, b1);
+  c1 = _mm256_unpackhi_epi32(b0, b1);
+  c2 = _mm256_unpacklo_epi32(b2, b3);
+  c3 = _mm256_unpackhi_epi32(b2, b3);
+
+  b0 = _mm256_unpacklo_epi64(c0, c2);
+  b1 = _mm256_unpackhi_epi64(c0, c2);
+  b2 = _mm256_unpacklo_epi64(c1, c3);
+  b3 = _mm256_unpackhi_epi64(c1, c3);
+
+  in[0] = _mm256_castsi256_si128(b0);
+  in[4] = _mm256_extractf128_si256(b0, 1);
+  in[1] = _mm256_castsi256_si128(b1);
+  in[5] = _mm256_extractf128_si256(b1, 1);
+  in[2] = _mm256_castsi256_si128(b2);
+  in[6] = _mm256_extractf128_si256(b2, 1);
+  in[3] = _mm256_castsi256_si128(b3);
+  in[7] = _mm256_extractf128_si256(b3, 1);
+}
+
+static void highbd_hadamard_col8_pass2_avx2(__m256i * const in) {
   __m256i a0 = in[0];
   __m256i a1 = in[1];
   __m256i a2 = in[2];
@@ -44,43 +123,6 @@ static void highbd_hadamard_col8_avx2(__m256i *in, int iter) {
   a6 = _mm256_sub_epi32(b4, b6);
   a7 = _mm256_sub_epi32(b5, b7);
 
-  if (iter == 0) {
-    b0 = _mm256_add_epi32(a0, a4);
-    b7 = _mm256_add_epi32(a1, a5);
-    b3 = _mm256_add_epi32(a2, a6);
-    b4 = _mm256_add_epi32(a3, a7);
-    b2 = _mm256_sub_epi32(a0, a4);
-    b6 = _mm256_sub_epi32(a1, a5);
-    b1 = _mm256_sub_epi32(a2, a6);
-    b5 = _mm256_sub_epi32(a3, a7);
-
-    a0 = _mm256_unpacklo_epi32(b0, b1);
-    a1 = _mm256_unpacklo_epi32(b2, b3);
-    a2 = _mm256_unpackhi_epi32(b0, b1);
-    a3 = _mm256_unpackhi_epi32(b2, b3);
-    a4 = _mm256_unpacklo_epi32(b4, b5);
-    a5 = _mm256_unpacklo_epi32(b6, b7);
-    a6 = _mm256_unpackhi_epi32(b4, b5);
-    a7 = _mm256_unpackhi_epi32(b6, b7);
-
-    b0 = _mm256_unpacklo_epi64(a0, a1);
-    b1 = _mm256_unpacklo_epi64(a4, a5);
-    b2 = _mm256_unpackhi_epi64(a0, a1);
-    b3 = _mm256_unpackhi_epi64(a4, a5);
-    b4 = _mm256_unpacklo_epi64(a2, a3);
-    b5 = _mm256_unpacklo_epi64(a6, a7);
-    b6 = _mm256_unpackhi_epi64(a2, a3);
-    b7 = _mm256_unpackhi_epi64(a6, a7);
-
-    in[0] = _mm256_permute2x128_si256(b0, b1, 0x20);
-    in[1] = _mm256_permute2x128_si256(b0, b1, 0x31);
-    in[2] = _mm256_permute2x128_si256(b2, b3, 0x20);
-    in[3] = _mm256_permute2x128_si256(b2, b3, 0x31);
-    in[4] = _mm256_permute2x128_si256(b4, b5, 0x20);
-    in[5] = _mm256_permute2x128_si256(b4, b5, 0x31);
-    in[6] = _mm256_permute2x128_si256(b6, b7, 0x20);
-    in[7] = _mm256_permute2x128_si256(b6, b7, 0x31);
-  } else {
     in[0] = _mm256_add_epi32(a0, a4);
     in[7] = _mm256_add_epi32(a1, a5);
     in[3] = _mm256_add_epi32(a2, a6);
@@ -89,11 +131,10 @@ static void highbd_hadamard_col8_avx2(__m256i *in, int iter) {
     in[6] = _mm256_sub_epi32(a1, a5);
     in[1] = _mm256_sub_epi32(a2, a6);
     in[5] = _mm256_sub_epi32(a3, a7);
-  }
 }
 
-void vpx_highbd_hadamard_8x8_avx2(const int16_t *src_diff, ptrdiff_t src_stride,
-                                  tran_low_t *coeff) {
+void vpx_highbd_hadamard_8x8_avx2(const int16_t *  src_diff, const ptrdiff_t src_stride,
+                                  tran_low_t *  coeff) {
   __m128i src16[8];
   __m256i src32[8];
 
@@ -106,6 +147,8 @@ void vpx_highbd_hadamard_8x8_avx2(const int16_t *src_diff, ptrdiff_t src_stride,
   src16[6] = _mm_loadu_si128((const __m128i *)(src_diff += src_stride));
   src16[7] = _mm_loadu_si128((const __m128i *)(src_diff += src_stride));
 
+  highbd_hadamard_col8_pass1_avx2(src16);
+
   src32[0] = _mm256_cvtepi16_epi32(src16[0]);
   src32[1] = _mm256_cvtepi16_epi32(src16[1]);
   src32[2] = _mm256_cvtepi16_epi32(src16[2]);
@@ -115,8 +158,7 @@ void vpx_highbd_hadamard_8x8_avx2(const int16_t *src_diff, ptrdiff_t src_stride,
   src32[6] = _mm256_cvtepi16_epi32(src16[6]);
   src32[7] = _mm256_cvtepi16_epi32(src16[7]);
 
-  highbd_hadamard_col8_avx2(src32, 0);
-  highbd_hadamard_col8_avx2(src32, 1);
+  highbd_hadamard_col8_pass2_avx2(src32);
 
   _mm256_storeu_si256((__m256i *)coeff, src32[0]);
   coeff += 8;
