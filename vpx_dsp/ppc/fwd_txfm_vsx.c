d231faa4d52798088df01026c4a0bbb829d18bb4
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2018-12-04 00:27:59 -0200

    Fix style issues
    Change-Id: I17125a04baf30f0bd748a9717b924e11d317c3fd

--- vpx_dsp/ppc/fwd_txfm_vsx.c
+++ vpx_dsp/ppc/fwd_txfm_vsx.c
@@ -13,28 +13,48 @@
 #include "vpx_dsp/ppc/types_vsx.h"
 #include "vpx_dsp/ppc/transpose_vsx.h"
 
-static uint8x16_t perm_1_v = {0x00, 0x01, 0x02, 0x03, 0x08, 0x09, 0x0A, 0x0B, 0x10, 0x11, 0x12, 0x13, 0x18, 0x19, 0x1A, 0x1B};
-static uint8x16_t perm_2_v = {0x06, 0x07, 0x04, 0x05, 0x0E, 0x0F, 0x0C, 0x0D, 0x16, 0x17, 0x14, 0x15, 0x1E, 0x1F, 0x1C, 0x1D};
-static uint8x16_t perm_3_v = {0x02, 0x03, 0x10, 0x11, 0x02, 0x03, 0x10, 0x11, 0x06, 0x07, 0x14, 0x15, 0x06, 0x07, 0x14, 0x15};
-static uint8x16_t perm_4_v = {0x0A, 0x0B, 0x18, 0x19, 0x0A, 0x0B, 0x18, 0x19, 0x0E, 0x0F, 0x1C, 0x1D, 0x0E, 0x0F, 0x1C, 0x1D};
-static uint8x16_t perm_5_v = {0x00, 0x01, 0x12, 0x13, 0x00, 0x01, 0x12, 0x13, 0x04, 0x05, 0x16, 0x17, 0x04, 0x05, 0x16, 0x17};
-static uint8x16_t perm_6_v = {0x08, 0x09, 0x1A, 0x1B, 0x08, 0x09, 0x1A, 0x1B, 0x0C, 0x0D, 0x1E, 0x1F, 0x0C, 0x0D, 0x1E, 0x1F};
+static uint8x16_t perm_1_v = { 0x00, 0x01, 0x02, 0x03,
+                               0x08, 0x09, 0x0A, 0x0B,
+                               0x10, 0x11, 0x12, 0x13,
+                               0x18, 0x19, 0x1A, 0x1B };
+static uint8x16_t perm_2_v = { 0x06, 0x07, 0x04, 0x05,
+                               0x0E, 0x0F, 0x0C, 0x0D,
+                               0x16, 0x17, 0x14, 0x15,
+                               0x1E, 0x1F, 0x1C, 0x1D };
+static uint8x16_t perm_3_v = { 0x02, 0x03, 0x10, 0x11,
+                               0x02, 0x03, 0x10, 0x11,
+                               0x06, 0x07, 0x14, 0x15,
+                               0x06, 0x07, 0x14, 0x15 };
+static uint8x16_t perm_4_v = { 0x0A, 0x0B, 0x18, 0x19,
+                               0x0A, 0x0B, 0x18, 0x19,
+                               0x0E, 0x0F, 0x1C, 0x1D,
+                               0x0E, 0x0F, 0x1C, 0x1D };
+static uint8x16_t perm_5_v = { 0x00, 0x01, 0x12, 0x13,
+                               0x00, 0x01, 0x12, 0x13,
+                               0x04, 0x05, 0x16, 0x17,
+                               0x04, 0x05, 0x16, 0x17 };
+static uint8x16_t perm_6_v = { 0x08, 0x09, 0x1A, 0x1B,
+                               0x08, 0x09, 0x1A, 0x1B,
+                               0x0C, 0x0D, 0x1E, 0x1F,
+                               0x0C, 0x0D, 0x1E, 0x1F };
 
-static int16x8_t cospi_1_v = { 11585, 15137, -11585, 6270, 11585, 15137, -11585, 6270 };
-static int16x8_t cospi_2_v = { 11585, 6270, 11585, -15137, 11585, 6270, 11585, -15137 };
+static int16x8_t cospi_1_v = { 11585, 15137, -11585, 6270,
+                               11585, 15137, -11585, 6270 };
+static int16x8_t cospi_2_v = { 11585, 6270, 11585, -15137,
+                               11585, 6270, 11585, -15137 };
 
 static INLINE int32x4_t fdct_vector_round_shift(int32x4_t input) {
   uint32x4_t dct_const_0, dct_const_1;
   int32x4_t tmp, sum;
-  int32x4_t one = vec_splat_s32(1);
   dct_const_0 = vec_splat_u32(DCT_CONST_BITS - 1);
   dct_const_1 = vec_splat_u32(DCT_CONST_BITS);
-  tmp = vec_sl(one, dct_const_0);
+  tmp = vec_sl(vec_ones_s32, dct_const_0);
   sum = vec_add(input, tmp);
   return vec_sra(sum, dct_const_1);
 }
 
-static INLINE void vpx_fdct4x4_one_pass(const int16x8_t input[2], int32x4_t output[4]) {
+static INLINE void vpx_fdct4x4_one_pass(const int16x8_t input[2],
+                                        int32x4_t output[4]) {
   int16x8_t v[2];
   int16x8_t s[2];
   int16x8_t a[2];
@@ -98,19 +118,15 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
   int16x8_t v_out[2];
   int32x4_t v_intermediate[4];
   int32x4_t last[4];
-  int32x4_t x[4];
+  int32x4_t loaded[4];
   int32x4_t transposed[4];
 
-  uint16x8_t c = vec_splat_u16(4);
-  int32x4_t one = vec_splat_s32(1);
-  uint32x4_t two = vec_splat_u32(2);
+  loaded[0] = unpack_u16_to_s32_h(vec_vsx_ld(0, input));
+  loaded[1] = unpack_u16_to_s32_h(vec_vsx_ld(0, input + stride));
+  loaded[2] = unpack_u16_to_s32_h(vec_vsx_ld(0, input + stride * 2));
+  loaded[3] = unpack_u16_to_s32_h(vec_vsx_ld(0, input + stride * 3));
 
-  x[0] = unpack_u16_to_s32_h(vec_vsx_ld(0, input));
-  x[1] = unpack_u16_to_s32_h(vec_vsx_ld(0, input + stride));
-  x[2] = unpack_u16_to_s32_h(vec_vsx_ld(0, input + stride * 2));
-  x[3] = unpack_u16_to_s32_h(vec_vsx_ld(0, input + stride * 3));
-
-  vpx_transpose_s32_4x4(x, transposed);
+  vpx_transpose_s32_4x4(loaded, transposed);
 
 #ifdef WORDS_BIGENDIAN
   v[0] = vec_pack(transposed[1], transposed[0]);
@@ -118,10 +134,10 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
 #else
   v[0] = vec_pack(transposed[0], transposed[1]);
   v[1] = vec_pack(transposed[2], transposed[3]);
-#endif // WORDS_BIGENDIAN
+#endif  // WORDS_BIGENDIAN
 
-  v[0] = vec_sl(v[0], c);
-  v[1] = vec_sl(v[1], c);
+  v[0] = vec_sl(v[0], vec_fours_u16);
+  v[1] = vec_sl(v[1], vec_fours_u16);
 
   if (v[0][0]) {
     ++v[0][0];
@@ -137,19 +153,19 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
 #else
   v_out[0] = vec_pack(transposed[0], transposed[1]);
   v_out[1] = vec_pack(transposed[2], transposed[3]);
-#endif // WORDS_BIGENDIAN
+#endif  // WORDS_BIGENDIAN
 
   vpx_fdct4x4_one_pass(v_out, last);
 
-  last[0] = vec_add(last[0], one);
-  last[1] = vec_add(last[1], one);
-  last[2] = vec_add(last[2], one);
-  last[3] = vec_add(last[3], one);
+  last[0] = vec_add(last[0], vec_ones_s32);
+  last[1] = vec_add(last[1], vec_ones_s32);
+  last[2] = vec_add(last[2], vec_ones_s32);
+  last[3] = vec_add(last[3], vec_ones_s32);
 
-  last[0] = vec_sra(last[0], two);
-  last[1] = vec_sra(last[1], two);
-  last[2] = vec_sra(last[2], two);
-  last[3] = vec_sra(last[3], two);
+  last[0] = vec_sra(last[0], vec_twos_u32);
+  last[1] = vec_sra(last[1], vec_twos_u32);
+  last[2] = vec_sra(last[2], vec_twos_u32);
+  last[3] = vec_sra(last[3], vec_twos_u32);
 
 #ifdef WORDS_BIGENDIAN
   v_out[0] = vec_pack(last[1], last[0]);
@@ -157,7 +173,7 @@ void vpx_fdct4x4_vsx(const int16_t *input, tran_low_t *output, int stride) {
 #else
   v_out[0] = vec_pack(last[0], last[1]);
   v_out[1] = vec_pack(last[2], last[3]);
-#endif // WORDS_BIGENDIAN
+#endif  // WORDS_BIGENDIAN
 
   vec_vsx_st(v_out[0], 0, output);
   vec_vsx_st(v_out[1], 0, output + 8);
