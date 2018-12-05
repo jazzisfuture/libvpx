3746d99ec638c23a790cb4a89de670408f320321
Jerome Jiang <jianj@google.com>
2018-12-04 18:37:02 -0800

    vp9 screen: use FAST_DIAMOND when set fullpel search step to 2.
    Change-Id: I51dc584bbf35e998757da326355dd4b8a4d0093f

--- vp9/encoder/vp9_speed_features.c
+++ vp9/encoder/vp9_speed_features.c
@@ -802,13 +802,8 @@ static void set_rt_speed_feature_framesize_independent(
   if (cpi->oxcf.content == VP9E_CONTENT_SCREEN && cpi->oxcf.speed > 5 &&
       cpi->svc.spatial_layer_id == 0 &&
       (cpi->rc.high_num_blocks_with_motion || cpi->svc.last_layer_dropped[0])) {
-    sf->mv.search_method = NSTEP;
+    sf->mv.search_method = FAST_DIAMOND;
     sf->mv.fullpel_search_step_param = 2;
-    // TODO(marpan/jianj): Investigate issue for lower setting of step_param
-    // for spatial layers (namely on lower layers).
-    if (cpi->use_svc && cm->width != cpi->oxcf.width &&
-        cm->height != cpi->oxcf.height)
-      sf->mv.fullpel_search_step_param = 4;
   }
 }
 
