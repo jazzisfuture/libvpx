76a3a2dc9dc3cf133c4705da5124aa51a715c678
Johann <johannkoenig@google.com>
2019-01-07 10:39:04 -0800

    vp8_copy32xn: resolve missing declaration
    BUG=webm:1584
    
    Change-Id: I8279e099fb9595edad858bf7332bf2b40fecae02

--- vp8/common/rtcd_defs.pl
+++ vp8/common/rtcd_defs.pl
@@ -177,8 +177,9 @@ if (vpx_config("CONFIG_VP8_ENCODER") eq "yes") {
 #
 # Block copy
 #
-if ($opts{arch} =~ /x86/) {
     add_proto qw/void vp8_copy32xn/, "const unsigned char *src_ptr, int src_stride, unsigned char *dst_ptr, int dst_stride, int height";
+# Only specialize copy32xn on 32 bit builds.
+if ($opts{arch} =~ /x86/) {
     specialize qw/vp8_copy32xn sse2 sse3/;
 }
 
