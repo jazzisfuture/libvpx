5b0ab40fc32bd19aa890aa95a3df452061f0ec99
Jerome Jiang <jianj@google.com>
2019-01-08 10:51:54 -0800

    vp8 dec: Add flag to bring up threads.
    Instead of creating a new decoder instant when restarting all threads
    after they were shut down, re-create threads on the new flag.
    
    BUG=webm:1577
    
    Change-Id: I6272ecaa1b586afdaa5ed8d6eab80aff8f5eb673

--- vp8/vp8_dx_iface.c
+++ vp8/vp8_dx_iface.c
@@ -45,6 +45,9 @@ struct vpx_codec_alg_priv {
   vpx_codec_dec_cfg_t cfg;
   vp8_stream_info_t si;
   int decoder_init;
+#if CONFIG_MULTITHREAD
+  int restart_threads;
+#endif
   int postproc_cfg_set;
   vp8_postproc_cfg_t postproc_cfg;
   vpx_decrypt_cb decrypt_cb;
@@ -298,6 +301,22 @@ static vpx_codec_err_t vp8_decode(vpx_codec_alg_priv_t *ctx,
 
   if ((ctx->si.h != h) || (ctx->si.w != w)) resolution_change = 1;
 
+#if CONFIG_MULTITHREAD
+  if (!res && ctx->restart_threads) {
+    struct frame_buffers *fb = &ctx->yv12_frame_buffers;
+    if (setjmp(fb->pbi[0]->common.error.jmp)) {
+      vp8_remove_decoder_instances(fb);
+      memset(fb->pbi, 0, sizeof(fb->pbi));
+      vpx_clear_system_state();
+      return VPX_CODEC_ERROR;
+    }
+
+    fb->pbi[0]->common.error.setjmp = 1;
+    fb->pbi[0]->max_threads = ctx->cfg.threads;
+    vp8_decoder_create_threads(fb->pbi[0]);
+    fb->pbi[0]->common.error.setjmp = 0;
+  }
+#endif
   /* Initialize the decoder instance on the first frame*/
   if (!res && !ctx->decoder_init) {
     VP8D_CONFIG oxcf;
@@ -441,7 +460,9 @@ static vpx_codec_err_t vp8_decode(vpx_codec_alg_priv_t *ctx,
       pc->error.setjmp = 0;
       ctx->si.w = 0;
       ctx->si.h = 0;
-      ctx->decoder_init = 0;
+#if CONFIG_MULTITHREAD
+      ctx->restart_threads = 1;
+#endif
       res = update_error_state(ctx, &pbi->common.error);
       return res;
     }
