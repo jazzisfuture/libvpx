19123b4c29fd4318a21541ff7a61ca286bcd6f1f
chiyotsai <chiyotsai@google.com>
2019-01-16 15:33:06 -0800

    Add SSE4 version of new apply_temporal_filter
    This adds a preliminary version of vp9_apply_temporal_filter in SSE4.1.
    This patch merely adds the function and does not enable it yet.
    
    Speed Up:
             | ss_x=1 | ss_x=0 |
      ss_y=1 | 19.80X | 19.04X |
      ss_y=0 | 21.09X | 20.21X |
    
    BUG=webm:1591
    
    Change-Id: If590f1ccf1d0c6c3b47410541d54f2ce37d8305b

--- test/yuv_temporal_filter_test.cc
+++ test/yuv_temporal_filter_test.cc
@@ -77,7 +77,7 @@ void ApplyReferenceFilter(
   u_dif.Set(0);
   v_dif.Set(0);
 
-  // How many bits to we want to round
+  // How many bits do we want to round
   ASSERT_GE(strength, 0);
   ASSERT_LE(strength, 6);
   int rounding = 0;
@@ -230,7 +230,7 @@ class YUVTemporalFilterTest
   ACMRandom rnd_;
 };
 
-TEST_P(YUVTemporalFilterTest, Use_32X32) {
+TEST_P(YUVTemporalFilterTest, Use32X32) {
   const int width = 32, height = 32;
   Buffer<uint8_t> y_src = Buffer<uint8_t>(width, height, 8);
   Buffer<uint8_t> y_pre = Buffer<uint8_t>(width, height, 0);
@@ -347,7 +347,7 @@ TEST_P(YUVTemporalFilterTest, Use_32X32) {
   }
 }
 
-TEST_P(YUVTemporalFilterTest, Use_16X16) {
+TEST_P(YUVTemporalFilterTest, Use16X16) {
   const int width = 32, height = 32;
   Buffer<uint8_t> y_src = Buffer<uint8_t>(width, height, 8);
   Buffer<uint8_t> y_pre = Buffer<uint8_t>(width, height, 0);
@@ -564,5 +564,10 @@ TEST_P(YUVTemporalFilterTest, DISABLED_Speed) {
 }
 
 INSTANTIATE_TEST_CASE_P(C, YUVTemporalFilterTest,
-                        ::testing::Values(&vp9_apply_temporal_filter));
+                        ::testing::Values(&vp9_apply_temporal_filter_c));
+
+#if HAVE_SSE4_1
+INSTANTIATE_TEST_CASE_P(SSE4, YUVTemporalFilterTest,
+                        ::testing::Values(&vp9_apply_temporal_filter_sse4_1));
+#endif  // HAVE_SSE4_1
 }  // namespace
