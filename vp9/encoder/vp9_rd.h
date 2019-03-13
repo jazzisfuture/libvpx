b0d5487fb074b8bd60abbb035dc4f7f994389376
Jerome Jiang <jianj@google.com>
2019-03-13 16:21:43 -0700

    Enclose macro arguments in parentheses
    BUG=webm:1606
    
    Change-Id: I661485b860243c95b6450035dbac77b0dd4d9ff4

--- vp9/encoder/vp9_rd.h
+++ vp9/encoder/vp9_rd.h
@@ -26,8 +26,9 @@ extern "C" {
 #define RDDIV_BITS 7
 #define RD_EPB_SHIFT 6
 
-#define RDCOST(RM, DM, R, D) \
-  (ROUND_POWER_OF_TWO(((int64_t)R) * (RM), VP9_PROB_COST_SHIFT) + (D << DM))
+#define RDCOST(RM, DM, R, D)                                        \
+  (ROUND_POWER_OF_TWO(((int64_t)(R)) * (RM), VP9_PROB_COST_SHIFT) + \
+   ((D) << (DM)))
 #define QIDX_SKIP_THRESH 115
 
 #define MV_COST_WEIGHT 108
