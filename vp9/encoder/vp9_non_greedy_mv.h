d05872773e23caf52954d3b3039771df0a6eb577
Dan Zhu <zxdan@google.com>
2019-08-14 13:55:08 -0700

    smooth motion field
    Change-Id: I1e8273fa65f7655e49f626863fe457efef23fb54

--- vp9/encoder/vp9_non_greedy_mv.h
+++ vp9/encoder/vp9_non_greedy_mv.h
@@ -11,15 +11,25 @@
 #ifndef VPX_VP9_ENCODER_VP9_NON_GREEDY_MV_H_
 #define VPX_VP9_ENCODER_VP9_NON_GREEDY_MV_H_
 
+#include "vp9/common/vp9_blockd.h"
+#include "vp9/common/vp9_enums.h"
+#include "vp9/common/vp9_blockd.h"
+#include "vpx_scale/yv12config.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 #define NB_MVS_NUM 4
 #define LOG2_PRECISION 20
 
+typedef struct VP9_COMP VP9_COMP;
+
 int64_t vp9_nb_mvs_inconsistency(const MV *mv, const int_mv *nb_full_mvs,
                                  int mv_num);
+
+void vp9_get_smooth_motion_field(const MV *search_mf, const int (*M)[4],
+                                 int rows, int cols, float alpha, int num_iters,
+                                 MV *smooth_mf);
 
 #ifdef __cplusplus
 }  // extern "C"
