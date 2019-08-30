d8f1c5a8d18d96e9f7a6809bd4f3cc72fb492268
Marco Paniconi <marpan@google.com>
2019-08-30 10:58:15 -0700

    vp9-svc: Add new frame drop mode, mainly for screenshare use
    Change-Id: I2d37959caf8c4b453b405904831b550367f716ba

--- examples/vp9_spatial_svc_encoder.c
+++ examples/vp9_spatial_svc_encoder.c
@@ -1050,9 +1050,10 @@ int main(int argc, const char **argv) {
 
   vpx_codec_control(&encoder, VP9E_SET_TUNE_CONTENT, app_input.tune_content);
 
-  svc_drop_frame.framedrop_mode = FULL_SUPERFRAME_DROP;
+  svc_drop_frame.framedrop_mode =
+      REVERSE_CONSTRAINED_DROP;  // FULL_SUPERFRAME_DROP;
   for (sl = 0; sl < (unsigned int)svc_ctx.spatial_layers; ++sl)
-    svc_drop_frame.framedrop_thresh[sl] = enc_cfg.rc_dropframe_thresh;
+    svc_drop_frame.framedrop_thresh[sl] = 30;  // enc_cfg.rc_dropframe_thresh;
   svc_drop_frame.max_consec_drop = INT_MAX;
   vpx_codec_control(&encoder, VP9E_SET_SVC_FRAME_DROP_LAYER, &svc_drop_frame);
 
