bbe4bb3ce3850303195dee8f6267fa27f8a2ee61
Johann <johannkoenig@google.com>
2019-12-03 15:29:35 -0800

    vp8 partitions: process found partitions
    Only process partitions which have been initialized. Do this by guarding
    the initialization loop with |multi_partition|.
    
    BUG=chromium:1026961
    
    Change-Id: Iaa480534b49efe1ecc66484b316f8d654e8a1245

--- vp8/decoder/decodeframe.c
+++ vp8/decoder/decodeframe.c
@@ -733,6 +733,7 @@ static void setup_token_decoder(VP8D_COMP *pbi,
   unsigned int num_token_partitions;
   const unsigned char *first_fragment_end =
       pbi->fragments.ptrs[0] + pbi->fragments.sizes[0];
+  int multi_partition = 0;
 
   TOKEN_PARTITION multi_token_partition =
       (TOKEN_PARTITION)vp8_read_literal(&pbi->mbc[8], 2);
@@ -759,6 +760,7 @@ static void setup_token_decoder(VP8D_COMP *pbi,
                            "Corrupted fragment size %d", fragment_size);
       fragment_size -= (unsigned int)ext_first_part_size;
       if (fragment_size > 0) {
+        multi_partition = 1;
         pbi->fragments.sizes[0] = (unsigned int)ext_first_part_size;
         /* The fragment contains an additional partition. Move to
          * next. */
@@ -791,16 +793,18 @@ static void setup_token_decoder(VP8D_COMP *pbi,
 
   pbi->fragments.count = num_token_partitions + 1;
 
-  for (partition_idx = 1; partition_idx < pbi->fragments.count;
-       ++partition_idx) {
-    if (vp8dx_start_decode(bool_decoder, pbi->fragments.ptrs[partition_idx],
-                           pbi->fragments.sizes[partition_idx], pbi->decrypt_cb,
-                           pbi->decrypt_state)) {
-      vpx_internal_error(&pbi->common.error, VPX_CODEC_MEM_ERROR,
-                         "Failed to allocate bool decoder %d", partition_idx);
-    }
+  if (multi_partition) {
+    for (partition_idx = 1; partition_idx < pbi->fragments.count;
+         ++partition_idx) {
+      if (vp8dx_start_decode(bool_decoder, pbi->fragments.ptrs[partition_idx],
+                             pbi->fragments.sizes[partition_idx],
+                             pbi->decrypt_cb, pbi->decrypt_state)) {
+        vpx_internal_error(&pbi->common.error, VPX_CODEC_MEM_ERROR,
+                           "Failed to allocate bool decoder %d", partition_idx);
+      }
 
-    bool_decoder++;
+      bool_decoder++;
+    }
   }
 
 #if CONFIG_MULTITHREAD
