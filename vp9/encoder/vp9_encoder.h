d196b0e575324c1c73cfa305af8aa3f0e1cbf1b8
angiebird <angiebird@google.com>
2020-01-23 14:56:01 -0800

    Add coded_frame to EncodeFrameResults
    This coded_frame represents the raw coded image.
    
    Change-Id: Iea439da2f9e84c4507b082d77ebaac49bfd74fff

--- vp9/encoder/vp9_encoder.h
+++ vp9/encoder/vp9_encoder.h
@@ -851,6 +851,16 @@ typedef struct VP9_COMP {
 #endif
 } VP9_COMP;
 
+#if CONFIG_RATE_CTRL
+// This is the c-version counter part of ImageBuffer
+typedef struct IMAGE_BUFFER {
+  int allocated;
+  int plane_width[3];
+  int plane_height[3];
+  uint8_t *plane_buffer[3];
+} IMAGE_BUFFER;
+#endif
+
 typedef struct ENCODE_FRAME_RESULT {
   int show_idx;
   FRAME_UPDATE_TYPE update_type;
@@ -858,9 +868,12 @@ typedef struct ENCODE_FRAME_RESULT {
   double psnr;
   uint64_t sse;
   FRAME_COUNTS frame_counts;
+  IMAGE_BUFFER coded_frame;
 #endif  // CONFIG_RATE_CTRL
   int quantize_index;
 } ENCODE_FRAME_RESULT;
+
+void vp9_init_encode_frame_result(ENCODE_FRAME_RESULT *encode_frame_result);
 
 void vp9_initialize_enc(void);
 
