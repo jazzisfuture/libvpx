4ec4b211321cdbdef1b5ca320011b348e00a9727
Paul Wilkins <paulwilkins@google.com>
2021-03-03 16:45:42 +0000

    Add fields into RC for Vizier ML experiments.
    This patch adds fields into the RC data structure for the Vizier.
    
    The added fields allow control of some extra rate control parameters
    and rate distortion.
    
    This patch also adds functions to initialize the various parameters
    though many are not yet used / wired in and for now all are set to
    default values. Ultimately many will be set through new command
    line options.
    
    Change-Id: I41591bb627d3837d2104fb363845adedbddf2e02

--- vp9/encoder/vp9_ratectrl.h
+++ vp9/encoder/vp9_ratectrl.h
@@ -204,6 +204,19 @@ typedef struct {
   int preserve_arf_as_gld;
   int preserve_next_arf_as_gld;
   int show_arf_as_gld;
+
+  // Vizeir project experimental rate control parameters.
+  double active_wq_factor;
+  double base_err_per_mb;
+  double sr_default_decay_limit;
+  double sr_diff_part;
+  double kf_frame_max_boost_first;  // Max for first kf in a chunk.
+  double kf_frame_max_boost_subs;   // Max for subsequent mid chunk kfs.
+  double kf_max_total_boost;
+  double kf_err_per_mb;
+  double gf_frame_max_boost;
+  double gf_max_total_boost;
+  double zm_power_factor;
 } RATE_CONTROL;
 
 struct VP9_COMP;
@@ -211,6 +224,8 @@ struct VP9EncoderConfig;
 
 void vp9_rc_init(const struct VP9EncoderConfig *oxcf, int pass,
                  RATE_CONTROL *rc);
+
+void vp9_init_rd_parameters(struct VP9_COMP *cpi);
 
 int vp9_estimate_bits_at_q(FRAME_TYPE frame_type, int q, int mbs,
                            double correction_factor, vpx_bit_depth_t bit_depth);
