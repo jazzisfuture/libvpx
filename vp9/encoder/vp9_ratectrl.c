167a8e3317ae3b1615bdb1d56efd7a893f50dfbb
Mikko Koivisto <mikko.koivisto@unikie.com>
2021-11-15 18:47:05 +0000

    vp9: Fix multiplication overflow
    Fix UBSan error reported from aosp Cuttlefish device:
    /vp9/encoder/vp9_ratectrl.c:238:33: unsigned integer overflow:
    2500000 * 1800 cannot be represented in type 'unsigned int'
    
    ...by casting the operand and the result of multiplication
    to 64bit integer.
    
    Test: vp9 webrtc streaming with Cuttlefish
    Change-Id: Id5bb3d4071a96179caffae0829d3cc4e48c7614b

--- vp9/encoder/vp9_ratectrl.c
+++ vp9/encoder/vp9_ratectrl.c
@@ -234,8 +234,8 @@ int vp9_rc_clamp_iframe_target_size(const VP9_COMP *const cpi, int target) {
   const RATE_CONTROL *rc = &cpi->rc;
   const VP9EncoderConfig *oxcf = &cpi->oxcf;
   if (oxcf->rc_max_intra_bitrate_pct) {
-    const int max_rate =
-        rc->avg_frame_bandwidth * oxcf->rc_max_intra_bitrate_pct / 100;
+    const int64_t max_rate =
+      (int64_t)rc->avg_frame_bandwidth * oxcf->rc_max_intra_bitrate_pct / 100;
     target = VPXMIN(target, max_rate);
   }
   if (target > rc->max_frame_bandwidth) target = rc->max_frame_bandwidth;
