d109005240da62bb5c86cea90e562451eb13ef71
Wan-Teh Chang <wtc@google.com>
2023-09-29 10:45:32 -0700

    Add vpx_sse and vpx_highbd_sse
    The code is ported from libaom's aom_sse and aom_highbd_sse at
    commit 9a9c141c7bf37b2c55a0480f724316534d9a87ca.
    
    The vpx_sse and vpx_highbd_sse functions will be used by vpx_dsp/psnr.c.
    
    Bug: webm:1819
    Change-Id: I4fbffa9000ab92755de5387b1ddd4370cb7020f7

--- test/test.mk
+++ test/test.mk
@@ -4,6 +4,7 @@ LIBVPX_TEST_SRCS-yes += bench.cc
 LIBVPX_TEST_SRCS-yes += buffer.h
 LIBVPX_TEST_SRCS-yes += clear_system_state.h
 LIBVPX_TEST_SRCS-yes += codec_factory.h
+LIBVPX_TEST_SRCS-yes += function_equivalence_test.h
 LIBVPX_TEST_SRCS-yes += md5_helper.h
 LIBVPX_TEST_SRCS-yes += register_state_check.h
 LIBVPX_TEST_SRCS-yes += test.mk
