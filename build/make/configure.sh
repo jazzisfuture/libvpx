39eaf83e7e976955940e32977ce8efea9ae50164
George Steed <george.steed@arm.com>
2024-02-21 09:46:23 +0000

    Only enable AArch64 extensions if the compiler supports them
    We already have some logic in the configure.sh file to selectively
    disable code dependent on particular architecture extensions, however we
    do not yet have anything to check that the compiler being supplied
    recognises and can compile code using these extensions.
    
    This commit adds compiler "-march=..." flag tests to the existing
    extension-disable loop so that we now correctly disable extensions that
    are not supported by the compiler. For AArch64 this loop also needs to
    move below the existing compiler/OS handling to ensure that prefixes
    like $CROSS are handled correctly before running compiler tests.
    
    Bug: webm:1841
    Change-Id: I936b911c4b0ebf03abc34b7532b2bb4568129f57

--- build/make/configure.sh
+++ build/make/configure.sh
@@ -1004,34 +1004,16 @@ EOF
   case ${toolchain} in
     arm*)
       soft_enable runtime_cpu_detect
-      # Arm ISA extensions are treated as supersets.
-      case ${tgt_isa} in
-        arm64|armv8)
-          for ext in ${ARCH_EXT_LIST_AARCH64}; do
-            # Disable higher order extensions to simplify dependencies.
-            if [ "$disable_exts" = "yes" ]; then
-              if ! disabled $ext; then
-                RTCD_OPTIONS="${RTCD_OPTIONS}--disable-${ext} "
-                disable_feature $ext
-              fi
-            elif disabled $ext; then
-              disable_exts="yes"
-            else
-              soft_enable $ext
-            fi
-          done
-          check_neon_sve_bridge_compiles
-          ;;
-        armv7|armv7s)
-          soft_enable neon
-          # Only enable neon_asm when neon is also enabled.
-          enabled neon && soft_enable neon_asm
-          # If someone tries to force it through, die.
-          if disabled neon && enabled neon_asm; then
-            die "Disabling neon while keeping neon-asm is not supported"
-          fi
-          ;;
-      esac
+
+      if [ ${tgt_isa} = "armv7" ] || [ ${tgt_isa} = "armv7s" ]; then
+        soft_enable neon
+        # Only enable neon_asm when neon is also enabled.
+        enabled neon && soft_enable neon_asm
+        # If someone tries to force it through, die.
+        if disabled neon && enabled neon_asm; then
+          die "Disabling neon while keeping neon-asm is not supported"
+        fi
+      fi
 
       asm_conversion_cmd="cat"
 
@@ -1253,6 +1235,37 @@ EOF
           fi
           ;;
       esac
+
+      # AArch64 ISA extensions are treated as supersets.
+      if [ ${tgt_isa} = "arm64" ] || [ ${tgt_isa} = "armv8" ]; then
+        aarch64_march_flag_neon="-march=armv8-a"
+        aarch64_march_flag_neon_dotprod="-march=armv8.2-a+dotprod"
+        aarch64_march_flag_neon_i8mm="-march=armv8.2-a+dotprod+i8mm"
+        aarch64_march_flag_sve="-march=armv8.2-a+dotprod+i8mm+sve"
+        for ext in ${ARCH_EXT_LIST_AARCH64}; do
+          # Check the compiler supports the -march flag for the extension.
+          # This needs to happen after toolchain/OS inspection so we handle
+          # $CROSS etc correctly when checking for flags, else these will
+          # always fail.
+          flag="$(eval echo \$"aarch64_march_flag_${ext}")"
+          if ! check_cflags "${flag}" || ! check_cxxflags "${flag}"; then
+            disable_exts="yes"
+          fi
+          # Disable higher order extensions to simplify dependencies.
+          if [ "$disable_exts" = "yes" ]; then
+            if ! disabled $ext; then
+              RTCD_OPTIONS="${RTCD_OPTIONS}--disable-${ext} "
+              disable_feature $ext
+            fi
+          elif disabled $ext; then
+            disable_exts="yes"
+          else
+            soft_enable $ext
+          fi
+        done
+        check_neon_sve_bridge_compiles
+      fi
+
       ;;
     mips*)
       link_with_cc=gcc
