d0584e5610799397fa6eb96492766b5c46f819e3
Jerome Jiang <jianj@google.com>
2024-03-28 16:02:37 -0400

    ext rc: override lst/gld/alt_fb_idx with ext RC
    Bug: b/329483680
    Change-Id: I78fb70b0c2801b0fdb4f1a6ab692260a15b2fdb6

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -3297,16 +3297,25 @@ static void update_ref_frames(VP9_COMP *cpi) {
   BufferPool *const pool = cm->buffer_pool;
   GF_GROUP *const gf_group = &cpi->twopass.gf_group;
 
-  if (cpi->rc.show_arf_as_gld) {
-    int tmp = cpi->alt_fb_idx;
-    cpi->alt_fb_idx = cpi->gld_fb_idx;
-    cpi->gld_fb_idx = tmp;
-  } else if (cm->show_existing_frame) {
-    // Pop ARF.
-    cpi->lst_fb_idx = cpi->alt_fb_idx;
-    cpi->alt_fb_idx =
-        stack_pop(gf_group->arf_index_stack, gf_group->stack_size);
-    --gf_group->stack_size;
+  if (cpi->ext_ratectrl.ready &&
+      (cpi->ext_ratectrl.funcs.rc_type & VPX_RC_GOP) != 0 &&
+      cpi->ext_ratectrl.funcs.get_gop_decision != NULL) {
+    const int gf_index = gf_group->index;
+    cpi->lst_fb_idx = gf_group->ext_rc_ref[gf_index].last_index;
+    cpi->gld_fb_idx = gf_group->ext_rc_ref[gf_index].golden_index;
+    cpi->alt_fb_idx = gf_group->ext_rc_ref[gf_index].altref_index;
+  } else {
+    if (cpi->rc.show_arf_as_gld) {
+      int tmp = cpi->alt_fb_idx;
+      cpi->alt_fb_idx = cpi->gld_fb_idx;
+      cpi->gld_fb_idx = tmp;
+    } else if (cm->show_existing_frame) {
+      // Pop ARF.
+      cpi->lst_fb_idx = cpi->alt_fb_idx;
+      cpi->alt_fb_idx =
+          stack_pop(gf_group->arf_index_stack, gf_group->stack_size);
+      --gf_group->stack_size;
+    }
   }
 
   // At this point the new frame has been encoded.
