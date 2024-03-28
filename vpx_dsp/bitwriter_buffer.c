35cb6ee21d83448c7e10bda32ab646ede46a7ab4
Wan-Teh Chang <wtc@google.com>
2024-03-27 17:08:08 -0700

    Perform bounds checks in vpx_write_bit_buffer
    Add the `size` and `error` members to the vpx_write_bit_buffer struct.
    Add the vpx_wb_has_error() function, which returns the `error` member.
    
    Instances of the vpx_write_bit_buffer struct are only allocated in the
    vp9_pack_bitstream() function. So vp9_pack_bitstream() is the only
    function outside vpx_dsp/bitwriter_buffer.* that needs updating.
    
    This CL completes the work of adding output buffer bounds checks to
    vp9/encoder/vp9_bitstream.c.
    
    Bug: webm:1844
    Change-Id: I6b362be572852ee51d96023b35bfb334faada7e1

--- vpx_dsp/bitwriter_buffer.c
+++ vpx_dsp/bitwriter_buffer.c
@@ -8,24 +8,35 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#include <assert.h>
 #include <limits.h>
 #include <stdlib.h>
 
 #include "./vpx_config.h"
 #include "./bitwriter_buffer.h"
 
+int vpx_wb_has_error(const struct vpx_write_bit_buffer *wb) {
+  return wb->error;
+}
+
 size_t vpx_wb_bytes_written(const struct vpx_write_bit_buffer *wb) {
+  assert(!wb->error);
   return wb->bit_offset / CHAR_BIT + (wb->bit_offset % CHAR_BIT > 0);
 }
 
 void vpx_wb_write_bit(struct vpx_write_bit_buffer *wb, int bit) {
+  if (wb->error) return;
   const int off = (int)wb->bit_offset;
   const int p = off / CHAR_BIT;
   const int q = CHAR_BIT - 1 - off % CHAR_BIT;
+  if (off == INT_MAX || (size_t)p >= wb->size) {
+    wb->error = 1;
+    return;
+  }
   if (q == CHAR_BIT - 1) {
     wb->bit_buffer[p] = bit << q;
   } else {
-    wb->bit_buffer[p] &= ~(1 << q);
+    assert((wb->bit_buffer[p] & (1 << q)) == 0);
     wb->bit_buffer[p] |= bit << q;
   }
   wb->bit_offset = off + 1;
