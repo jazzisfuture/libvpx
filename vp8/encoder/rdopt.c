26cc145f07acadc179e0f56e2822f6e8215ae160
James Zern <jzern@google.com>
2024-07-23 15:52:44 -0700

    vp8: fix OOB access in x->MVcount
    best_ref_mv is now clamped in vp8_rd_pick_inter_mode() and
    vp8_pick_inter_mode() to avoid issues in rd_update_mvcount() and
    update_mvcount(). More clamping may be necessary so those functions
    additionally check the row and column indices before modifying
    MVcount[].
    
    Note the test source and settings (speed 1 and GOOD quality mode) come
    from the issue report; additional coverage is added for realtime. The
    realtime path does not trigger the error without the fix, but as it's
    similar to the rd path, the same clamp is done to be safe.
    
    Fixes:
    vp8/encoder/rdopt.c:1579:5: runtime error: index 17467 out of bounds for
      type 'unsigned int[2047]'
    
    Bug: oss-fuzz:69906
    Change-Id: Ia8bd087cfe4475ab09ba711ed806fbcbaa72e552

--- vp8/encoder/rdopt.c
+++ vp8/encoder/rdopt.c
@@ -1564,21 +1564,31 @@ static void rd_update_mvcount(MACROBLOCK *x, int_mv *best_ref_mv) {
 
     for (i = 0; i < x->partition_info->count; ++i) {
       if (x->partition_info->bmi[i].mode == NEW4X4) {
-        x->MVcount[0][mv_max + ((x->partition_info->bmi[i].mv.as_mv.row -
-                                 best_ref_mv->as_mv.row) >>
-                                1)]++;
-        x->MVcount[1][mv_max + ((x->partition_info->bmi[i].mv.as_mv.col -
-                                 best_ref_mv->as_mv.col) >>
-                                1)]++;
+        const int row_val = ((x->partition_info->bmi[i].mv.as_mv.row -
+                              best_ref_mv->as_mv.row) >>
+                             1);
+        const int col_val = ((x->partition_info->bmi[i].mv.as_mv.col -
+                              best_ref_mv->as_mv.col) >>
+                             1);
+        if (row_val >= -mv_max && row_val <= mv_max &&
+            col_val >= -mv_max && col_val <= mv_max) {
+          x->MVcount[0][mv_max + row_val]++;
+          x->MVcount[1][mv_max + col_val]++;
+        }
       }
     }
   } else if (x->e_mbd.mode_info_context->mbmi.mode == NEWMV) {
-    x->MVcount[0][mv_max + ((x->e_mbd.mode_info_context->mbmi.mv.as_mv.row -
-                             best_ref_mv->as_mv.row) >>
-                            1)]++;
-    x->MVcount[1][mv_max + ((x->e_mbd.mode_info_context->mbmi.mv.as_mv.col -
-                             best_ref_mv->as_mv.col) >>
-                            1)]++;
+    const int row_val = ((x->e_mbd.mode_info_context->mbmi.mv.as_mv.row -
+                          best_ref_mv->as_mv.row) >>
+                         1);
+    const int col_val = ((x->e_mbd.mode_info_context->mbmi.mv.as_mv.col -
+                          best_ref_mv->as_mv.col) >>
+                         1);
+    if (row_val >= -mv_max && row_val <= mv_max &&
+        col_val >= -mv_max && col_val <= mv_max) {
+      x->MVcount[0][mv_max + row_val]++;
+      x->MVcount[1][mv_max + col_val]++;
+    }
   }
 }
 
@@ -1750,7 +1760,6 @@ void vp8_rd_pick_inter_mode(VP8_COMP *cpi, MACROBLOCK *x, int recon_yoffset,
   int num00;
   int best_mode_index = 0;
   BEST_MODE best_mode;
-
   int i;
   int mode_index;
   int mdcounts[4];
@@ -2121,6 +2130,8 @@ void vp8_rd_pick_inter_mode(VP8_COMP *cpi, MACROBLOCK *x, int recon_yoffset,
           }
         }
 
+        vp8_clamp_mv(&best_ref_mv, x->mv_col_min, x->mv_col_max, x->mv_row_min,
+                     x->mv_row_max);
         x->mv_col_min = tmp_col_min;
         x->mv_col_max = tmp_col_max;
         x->mv_row_min = tmp_row_min;
