874ff9025f18e80ddf6050cfb0a7de08e75c36f5
Jerome Jiang <jianj@google.com>
2024-07-30 16:25:07 -0400

    Remove unused parameters from ext rc callback
    Bug: b/356424505
    Change-Id: I1c684e7f4cc9bb7b916354d391abd1ae168af39f

--- vp9/encoder/vp9_ext_ratectrl.h
+++ vp9/encoder/vp9_ext_ratectrl.h
@@ -45,9 +45,7 @@ vpx_codec_err_t vp9_extrc_get_encodeframe_decision(
 
 vpx_codec_err_t vp9_extrc_update_encodeframe_result(
     EXT_RATECTRL *ext_ratectrl, int64_t bit_count,
-    const YV12_BUFFER_CONFIG *source_frame,
-    const YV12_BUFFER_CONFIG *coded_frame, uint32_t bit_depth,
-    uint32_t input_bit_depth, const int actual_encoding_qindex);
+    const int actual_encoding_qindex);
 
 vpx_codec_err_t vp9_extrc_get_key_frame_decision(
     EXT_RATECTRL *ext_ratectrl,
