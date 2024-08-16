5c34aff779b771141d9b2abdaaefa2a89b6a7447
Yunqing Wang <yunqingwang@google.com>
2024-08-15 18:12:56 -0700

    Key frame temporal filtering
    Change-Id: If6abea3e21efcb96f1978cd9dfaa742c40dc2a56

--- vp9/encoder/vp9_encoder.c
+++ vp9/encoder/vp9_encoder.c
@@ -1043,6 +1043,7 @@ static void dealloc_compressor_data(VP9_COMP *cpi) {
   vpx_free_frame_buffer(&cpi->scaled_source);
   vpx_free_frame_buffer(&cpi->scaled_last_source);
   vpx_free_frame_buffer(&cpi->alt_ref_buffer);
+  vpx_free_frame_buffer(&cpi->key_buffer);
 #ifdef ENABLE_KF_DENOISE
   vpx_free_frame_buffer(&cpi->raw_unscaled_source);
   vpx_free_frame_buffer(&cpi->raw_scaled_source);
@@ -1308,6 +1309,16 @@ static void alloc_raw_frame_buffers(VP9_COMP *cpi) {
                                NULL, NULL, NULL))
     vpx_internal_error(&cm->error, VPX_CODEC_MEM_ERROR,
                        "Failed to allocate altref buffer");
+
+  if (vpx_realloc_frame_buffer(&cpi->key_buffer, oxcf->width, oxcf->height,
+                               cm->subsampling_x, cm->subsampling_y,
+#if CONFIG_VP9_HIGHBITDEPTH
+                               cm->use_highbitdepth,
+#endif
+                               VP9_ENC_BORDER_IN_PIXELS, cm->byte_alignment,
+                               NULL, NULL, NULL))
+    vpx_internal_error(&cm->error, VPX_CODEC_MEM_ERROR,
+                       "Failed to allocate key buffer");
 }
 
 static void alloc_util_frame_buffers(VP9_COMP *cpi) {
@@ -6465,7 +6476,7 @@ int vp9_get_compressed_data(VP9_COMP *cpi, unsigned int *frame_flags,
         start_timing(cpi, vp9_temporal_filter_time);
 #endif
         // Produce the filtered ARF frame.
-        vp9_temporal_filter(cpi, arf_src_index);
+        vp9_temporal_filter(cpi, arf_src_index, &cpi->alt_ref_buffer);
         vpx_extend_frame_borders(&cpi->alt_ref_buffer);
 #if CONFIG_COLLECT_COMPONENT_TIMING
         end_timing(cpi, vp9_temporal_filter_time);
@@ -6592,6 +6603,24 @@ int vp9_get_compressed_data(VP9_COMP *cpi, unsigned int *frame_flags,
 #endif
   } else if (oxcf->pass == 1) {
     set_frame_size(cpi);
+  }
+
+  // Key frame temporal filtering
+  const int is_key_temporal_filter_enabled =
+      cpi->oxcf.mode != REALTIME && (oxcf->pass != 1) && !cpi->use_svc &&
+      !is_lossless_requested(&cpi->oxcf) && cm->frame_type == KEY_FRAME &&
+      (oxcf->arnr_max_frames > 0) && (oxcf->arnr_strength > 0) &&
+      cpi->oxcf.speed < 2;
+  // Save the pointer to the original source image.
+  YV12_BUFFER_CONFIG *source_buffer = cpi->un_scaled_source;
+
+  if (is_key_temporal_filter_enabled && source != NULL) {
+    // Produce the filtered Key frame.
+    vp9_temporal_filter(cpi, 0, &cpi->key_buffer);
+    vpx_extend_frame_borders(&cpi->key_buffer);
+    force_src_buffer = &cpi->key_buffer;
+    cpi->un_scaled_source = cpi->Source =
+        force_src_buffer ? force_src_buffer : &source->img;
   }
 #endif  // !CONFIG_REALTIME_ONLY
 
@@ -6722,6 +6751,11 @@ int vp9_get_compressed_data(VP9_COMP *cpi, unsigned int *frame_flags,
 
   if (cpi->keep_level_stats && oxcf->pass != 1)
     update_level_info(cpi, size, arf_src_index);
+
+  if (is_key_temporal_filter_enabled && cpi->b_calculate_psnr) {
+    cpi->raw_source_frame = vp9_scale_if_required(
+        cm, source_buffer, &cpi->scaled_source, (oxcf->pass == 0), EIGHTTAP, 0);
+  }
 
 #if CONFIG_INTERNAL_STATS
 
